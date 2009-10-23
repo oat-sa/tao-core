7c96ea207b318b501ce6c97b87e61fc86002fe9d
bchevrier <bchevrier@62dfeb86-a93f-4914-9d13-15c0a160985d>
2009-10-23 13:07:01 +0000

    enable to edit class properties with the dynamic helper form
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/tao/trunk@1578 62dfeb86-a93f-4914-9d13-15c0a160985d

--- helpers/form/class.GenerisFormFactory.php
+++ helpers/form/class.GenerisFormFactory.php
@@ -174,10 +174,11 @@ class tao_helpers_form_GenerisFormFactory
      * @access public
      * @author Bertrand Chevrier, <chevrier.bertrand@gmail.com>
      * @param  Class clazz
+     * @param  Class topClazz
      * @param  string renderMode
      * @return tao_helpers_form_Form
      */
-    public static function classEditor( core_kernel_classes_Class $clazz, $renderMode = 'xhtml')
+    public static function classEditor( core_kernel_classes_Class $clazz,  core_kernel_classes_Class $topClazz = null, $renderMode = 'xhtml')
     {
         $returnValue = null;
 
@@ -198,9 +199,11 @@ class tao_helpers_form_GenerisFormFactory
 					return null;
 			}
 			
+			
+			
+			
 			$defaultProperties = self::getDefaultProperties();
-			$otherProperties = self::getClassProperties(new core_kernel_classes_Class( 'http://www.w3.org/1999/02/22-rdf-syntax-ns#Property'));
-			foreach(array_merge($defaultProperties, $otherProperties) as $property){
+			foreach($defaultProperties  as $property){
 				
 				//map properties widgets to form elments 
 				$element = self::elementMap($property, $renderMode);
@@ -227,6 +230,41 @@ class tao_helpers_form_GenerisFormFactory
 					$myForm->addElement($element);
 				}
 			}
+			
+			
+			if(is_null($topClazz)){
+				$topClazz = new core_kernel_classes_Class(self::DEFAULT_TOP_LEVEL_CLASS);
+			}
+			$level  = 2;
+			foreach(self::getClassProperties($clazz, $topClazz) as $classProperty){
+				foreach(array_merge(self::getDefaultProperties(),self::getPropertyProperties()) as $property){
+					
+					//map properties widgets to form elments 
+					$element = self::elementMap($property, $renderMode);
+					if(!is_null($element)){
+						
+						//take property values to populate the form
+						$values = $classProperty->getPropertyValuesCollection($property);
+						foreach($values->getIterator() as $value){
+							if(!is_null($value)){
+								if($value instanceof core_kernel_classes_Resource){
+									$element->setValue($value->uriResource);
+								}
+								if($value instanceof core_kernel_classes_Literal){
+									$element->setValue((string)$value);
+								}
+							}
+						}
+						$element->setLevel($level);
+						$myForm->addElement($element);
+						$level++;
+					}
+					$level++;
+				}
+			}
+			
+			
+			
 			
 			//add an hidden elt for the class uri
 			$classUriElt = new $hiddenEltClass('classUri');
@@ -326,16 +364,29 @@ class tao_helpers_form_GenerisFormFactory
 		}
 		else{
 			$returnValue = $clazz->getProperties(false);
+			if($clazz->uriResource == $topLevelClazz->uriResource){
+				return (array) $returnValue;
+			}
 			$top = false;
+			$parent = null;
 			do{
-				$parents = $clazz->getParentClasses(false);
+				if(is_null($parent)){
+					$parents = $clazz->getParentClasses(false);
+				}
+				else{
+					$parents = $parent->getParentClasses(false);
+				}
 				if(count($parents) == 0){
 					break;
 				}
+				
 				foreach($parents as $parent){
 					if( !($parent instanceof core_kernel_classes_Class) || is_null($parent)){
 						$top = true; 
 						break;
+					}
+					if($parent->uriResource == 'http://www.w3.org/2000/01/rdf-schema#Class'){
+						continue;
 					}
 					
 					$returnValue = array_merge($returnValue, $parent->getProperties(false));
@@ -343,8 +394,9 @@ class tao_helpers_form_GenerisFormFactory
 						$top = true; 
 						break;
 					}
+					
 				}
-			} while(!$top);
+			}while($top === false);
 		}
 		
 		
@@ -378,6 +430,39 @@ class tao_helpers_form_GenerisFormFactory
 		}
 		
         // section 127-0-1-1--5ce810e0:1244ce713f8:-8000:0000000000001A43 end
+
+        return (array) $returnValue;
+    }
+
+    /**
+     * Short description of method getPropertyProperties
+     *
+     * @access public
+     * @author Bertrand Chevrier, <chevrier.bertrand@gmail.com>
+     * @return array
+     */
+    public static function getPropertyProperties()
+    {
+        $returnValue = array();
+
+        // section 127-0-1-1-696660da:12480a2774f:-8000:0000000000001AB5 begin
+		
+		$defaultUris = array(
+			'http://www.w3.org/2000/01/rdf-schema#label',
+			'http://www.tao.lu/datatypes/WidgetDefinitions.rdf#widget',
+			'http://www.w3.org/2000/01/rdf-schema#domain',
+			'http://www.w3.org/2000/01/rdf-schema#range',
+			'http://www.tao.lu/Ontologies/generis.rdf#is_language_dependent'
+		);
+		
+		$resourceClass = new core_kernel_classes_Class('http://www.w3.org/1999/02/22-rdf-syntax-ns#Property');
+		foreach($resourceClass->getProperties() as $property){
+			if(in_array($property->uriResource, $defaultUris)){
+				array_push($returnValue, $property);
+			}
+		}
+		
+        // section 127-0-1-1-696660da:12480a2774f:-8000:0000000000001AB5 end
 
         return (array) $returnValue;
     }
