26f185d6ae813fc840e168ad75f20a92ce0db38f
llecaque <llecaque@62dfeb86-a93f-4914-9d13-15c0a160985d>
2009-12-10 17:11:42 +0000

    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/tao/trunk@1774 62dfeb86-a93f-4914-9d13-15c0a160985d

--- includes/common.php
+++ includes/common.php
@@ -1,34 +1,13 @@
 <?php
 /**
  * Bootstraping
- * @todo create a proper class to bootstrap by context 
+ * @todo create a proper class to bootstrap by context
  */
 
 // 01 SESSION
 
 session_start();
 
-
-// 02 CONF INIT
-
-/**
- * this code is executed before all other script
- */
-
-# constants definition
-define('HTTP_GET', 		'GET');
-define('HTTP_POST', 	'POST');
-define('HTTP_PUT', 		'PUT');
-define('HTTP_DELETE', 	'DELETE');
-define('HTTP_HEAD', 	'HEAD');
-
-# all error
-error_reporting(E_ALL);
-
-# xdebug custom error reporting
-if (function_exists("xdebug_enable"))  {
-	xdebug_enable();
-}
 
 
 // 03 INCLUDES
@@ -44,43 +23,23 @@ require_once 	dirname(__FILE__). "/constants.php";
  * @param 	string		$pClassName		Name of the class
  */
 function tao_autoload($pClassName) {
-	
+
 	global $__autoload;
-	
-	if ( isset($__autoload[$pClassName])) {
-		require_once $__autoload[$pClassName];
+
+
+	$split = explode("_",$pClassName);
+	$path = GENERIS_BASE_PATH.'/../';
+	for ( $i = 0 ; $i<sizeof($split)-1 ; $i++){
+		$path .= $split[$i].'/';
 	}
-	else {
-		$split = explode("_",$pClassName);
-		$path = GENERIS_BASE_PATH.'/../';
-		for ( $i = 0 ; $i<sizeof($split)-1 ; $i++){
-			$path .= $split[$i].'/';
-		}
-		
-	    $filePath = $path . 'class.'.$split[sizeof($split)-1] . '.php';
-		if (file_exists($filePath)){
-			require_once $filePath;
-		}
+
+	$filePath = $path . 'class.'.$split[sizeof($split)-1] . '.php';
+	if (file_exists($filePath)){
+		require_once $filePath;
 	}
+
 }
 
-/**
- * @function fw_autoload
- * permits to include classes automatically
- * @param 	string		$pClassName		Name of the class
- */
-function fw_autoload($pClassName) {
-	if (isset($GLOBALS['classpath']) && is_array($GLOBALS['classpath'])) {
-		foreach($GLOBALS['classpath'] as $path) {
-			if (file_exists($path. $pClassName . '.class.php')) {
-				require_once $path . $pClassName . '.class.php';
-    			break;
-			}
-		}
-	}
-}
-
-spl_autoload_register("fw_autoload");
 spl_autoload_register("tao_autoload");
 
 
@@ -90,26 +49,26 @@ set_include_path(get_include_path() . PATH_SEPARATOR . GENERIS_BASE_PATH.'/..');
 // 05 SCRIPTS
 tao_helpers_Scriptloader::addCssFiles(array(
 
-	BASE_WWW . 'css/overcast/jquery-ui-1.7.2.custom.css',
-	BASE_WWW . 'js/jwysiwyg/jquery.wysiwyg.css',
-	BASE_WWW . 'css/layout.css',
-	BASE_WWW . 'css/form.css'
+BASE_WWW . 'css/overcast/jquery-ui-1.7.2.custom.css',
+BASE_WWW . 'js/jwysiwyg/jquery.wysiwyg.css',
+BASE_WWW . 'css/layout.css',
+BASE_WWW . 'css/form.css'
 
 ));
 
 tao_helpers_Scriptloader::addJsFiles(array(
-	
-	BASE_WWW . 'js/jquery-1.3.2.min.js',
-	BASE_WWW . 'js/jquery-ui-1.7.2.custom.min.js',
-	BASE_WWW . 'js/jsTree/jquery.tree.min.js',
-	BASE_WWW . 'js/jsTree/plugins/jquery.tree.contextmenu.js',
-	BASE_WWW . 'js/jsTree/plugins/jquery.tree.checkbox.js',
-	BASE_WWW . 'js/jwysiwyg/jquery.wysiwyg.js',
-	BASE_WWW . 'js/ajaxupload.js',
-	BASE_WWW . 'js/form.js',
-	BASE_WWW . 'js/control.js',
-	BASE_WWW . 'js/generis.tree.js',
-	BASE_WWW . 'js/generis.treeform.js'
+
+BASE_WWW . 'js/jquery-1.3.2.min.js',
+BASE_WWW . 'js/jquery-ui-1.7.2.custom.min.js',
+BASE_WWW . 'js/jsTree/jquery.tree.min.js',
+BASE_WWW . 'js/jsTree/plugins/jquery.tree.contextmenu.js',
+BASE_WWW . 'js/jsTree/plugins/jquery.tree.checkbox.js',
+BASE_WWW . 'js/jwysiwyg/jquery.wysiwyg.js',
+BASE_WWW . 'js/ajaxupload.js',
+BASE_WWW . 'js/form.js',
+BASE_WWW . 'js/control.js',
+BASE_WWW . 'js/generis.tree.js',
+BASE_WWW . 'js/generis.treeform.js'
 
 ));
 require_once(BASE_PATH.'/helpers/class.Uri.php');
--- manifest.php
+++ manifest.php
@@ -10,8 +10,12 @@
 				'sql' => dirname(__FILE__). '/install/db/tao',
 				'php' => dirname(__FILE__). '/install/install.php'
 			),
-			'configFile' => dirname(__FILE__). '/includes/common.php'
-
+			'configFile' => dirname(__FILE__). '/includes/common.php',
+			'classLoaderPackages' => array( 
+				dirname(__FILE__).'/actions/',
+				dirname(__FILE__).'/helpers/',
+				dirname(__FILE__).'/helpers/form'
+			 )
 			
 
 				
