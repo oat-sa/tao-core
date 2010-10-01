ef206f74aec79a1cd1846ed44a26ad25af58259d
bchevrier <bchevrier@62dfeb86-a93f-4914-9d13-15c0a160985d>
2010-10-01 11:20:18 +0000

    * QTI begin to implement XHTML export  * add taoApi js package  * add runtime environment for XHTML items
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/tao/trunk@3380 62dfeb86-a93f-4914-9d13-15c0a160985d

---
+++ actions/Api.class.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @author CRP Henri Tudor - TAO Team - {@link http://www.tao.lu}
+ * @license GPLv2  http://www.opensource.org/licenses/gpl-2.0.php
+ * @package tao
+ * @subpackage action
+ *
+ */
+class Api extends CommonModule {
+
+
+	/**
+	 * @return void
+	 */
+	public function index(){
+		
+	}
+	
+	/**
+	 * @return void
+	 */
+	public function getContext(){
+		
+	}
+	
+	/**
+	 * @return void
+	 */
+	public function save(){
+		
+	}
+
+}
+?>
