8cbcc20eb9ada127e5288a318dc87b0e477c7cbb
ssipasseuth <ssipasseuth@62dfeb86-a93f-4914-9d13-15c0a160985d>
2010-11-10 09:51:47 +0000

    test model updated with authoring mode management
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/tao/trunk@3614 62dfeb86-a93f-4914-9d13-15c0a160985d

---
+++ install/update/1.3/11_test_process.sql
@@ -0,0 +1,19 @@
+INSERT INTO `statements` (`modelID`, `subject`, `predicate`, `object`, `l_language`, `author`, `stread`, `stedit`, `stdelete`) VALUES
+(9, 'http://www.tao.lu/Ontologies/TAOTest.rdf#AuthoringMode', 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type', 'http://www.w3.org/1999/02/22-rdf-syntax-ns#Property', '', 'generis', 'yyy[]', 'yy-[]', 'y--[Administrators]'),
+(9, 'http://www.tao.lu/Ontologies/TAOTest.rdf#AuthoringMode', 'http://www.w3.org/2000/01/rdf-schema#label', 'Authoring Mode', 'EN', 'generis', 'yyy[]', 'yy-[]', 'y--[Administrators]'),
+(9, 'http://www.tao.lu/Ontologies/TAOTest.rdf#AuthoringMode', 'http://www.w3.org/2000/01/rdf-schema#comment', 'AuthoringMode', 'EN', 'generis', 'yyy[]', 'yy-[]', 'y--[Administrators]'),
+(9, 'http://www.tao.lu/Ontologies/TAOTest.rdf#AuthoringMode', 'http://www.w3.org/2000/01/rdf-schema#domain', 'http://www.tao.lu/Ontologies/TAOTest.rdf#Test', '', 'generis', 'yyy[]', 'yy-[]', 'y--[Administrators]'),
+(9, 'http://www.tao.lu/Ontologies/TAOTest.rdf#AuthoringMode', 'http://www.w3.org/2000/01/rdf-schema#range', 'http://www.tao.lu/Ontologies/TAOTest.rdf#TestAuthoringModes', '', 'generis', 'yyy[]', 'yy-[]', 'y--[Administrators]'),
+(9, 'http://www.tao.lu/Ontologies/TAOTest.rdf#AuthoringMode', 'http://www.tao.lu/datatypes/WidgetDefinitions.rdf#widget', 'http://www.tao.lu/datatypes/WidgetDefinitions.rdf#RadioBox', '', 'generis', 'yyy[]', 'yy-[]', 'y--[Administrators]'),
+
+(9, 'http://www.tao.lu/Ontologies/TAOTest.rdf#TestAuthoringModes', 'http://www.w3.org/2000/01/rdf-schema#label', 'Test Authoring Modes', 'EN', 'generis', 'yyy[]', 'yy-[]', 'y--[Administrators]'),
+(9, 'http://www.tao.lu/Ontologies/TAOTest.rdf#TestAuthoringModes', 'http://www.w3.org/2000/01/rdf-schema#comment', 'TestAuthoringModes', 'EN', 'generis', 'yyy[]', 'yy-[]', 'y--[Administrators]'),
+(9, 'http://www.tao.lu/Ontologies/TAOTest.rdf#TestAuthoringModes', 'http://www.w3.org/2000/01/rdf-schema#subClassOf', 'http://www.tao.lu/Ontologies/TAO.rdf#TAOObject', '', 'generis', 'yyy[]', 'yy-[]', 'y--[Administrators]'),
+
+(9, 'http://www.tao.lu/Ontologies/TAOTest.rdf#i1268049036038811802', 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type', 'http://www.tao.lu/Ontologies/TAOTest.rdf#TestAuthoringModes', 'EN', 'generis', 'yyy[admin,administrators,authors]', 'yyy[admin,administrators,authors]', 'yyy[admin,administrators,authors]'),
+(9, 'http://www.tao.lu/Ontologies/TAOTest.rdf#i1268049036038811802', 'http://www.w3.org/2000/01/rdf-schema#label', 'simple', '', 'generis', 'yyy[admin,administrators,authors]', 'yyy[admin,administrators,authors]', 'yyy[admin,administrators,authors]'),
+(9, 'http://www.tao.lu/Ontologies/TAOTest.rdf#i1268049036038811802', 'http://www.w3.org/2000/01/rdf-schema#comment', 'simple test authoring mode', '', 'generis', 'yyy[admin,administrators,authors]', 'yyy[admin,administrators,authors]', 'yyy[admin,administrators,authors]'),
+
+(9, 'http://www.tao.lu/Ontologies/TAOTest.rdf#i1268049036038811803', 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type', 'http://www.tao.lu/Ontologies/TAOTest.rdf#TestAuthoringModes', 'EN', 'generis', 'yyy[admin,administrators,authors]', 'yyy[admin,administrators,authors]', 'yyy[admin,administrators,authors]'),
+(9, 'http://www.tao.lu/Ontologies/TAOTest.rdf#i1268049036038811803', 'http://www.w3.org/2000/01/rdf-schema#label', 'advanced', '', 'generis', 'yyy[admin,administrators,authors]', 'yyy[admin,administrators,authors]', 'yyy[admin,administrators,authors]'),
+(9, 'http://www.tao.lu/Ontologies/TAOTest.rdf#i1268049036038811803', 'http://www.w3.org/2000/01/rdf-schema#comment', 'advanced test authoring mode', '', 'generis', 'yyy[admin,administrators,authors]', 'yyy[admin,administrators,authors]', 'yyy[admin,administrators,authors]');
