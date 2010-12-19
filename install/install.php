b504ca9a9a20e94b4102de7d93fa3eea82b6b750
bchevrier <bchevrier@62dfeb86-a93f-4914-9d13-15c0a160985d>
2010-12-19 21:50:58 +0000

    doc, install, fixs
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/tao/trunk@3998 62dfeb86-a93f-4914-9d13-15c0a160985d

--- install/install.php
+++ install/install.php
@@ -130,9 +130,9 @@ function install($param){
 		loadSqlReplaceNS('db/sampleData.sql',$con,$nameSpace);
 		loadSqlReplaceNS('../../wfEngine/install/db/services.sql',$con,$nameSpace);
 		
-		$fileContent = file_get_contents('db/qcmContent.sql');
-		$fileContent = str_replace("##NAMESPACE",$nameSpace,$fileContent);
-		$con->Execute($fileContent); 
+//		$fileContent = file_get_contents('db/qcmContent.sql');
+//		$fileContent = str_replace("##NAMESPACE",$nameSpace,$fileContent);
+//		$con->Execute($fileContent); 
 		$con->Close();
 
 	} catch (exception $e) {
