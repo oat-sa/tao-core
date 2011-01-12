dba3ed7ad3d10c14f6a75bc884af65839e696acf
ssipasseuth <ssipasseuth@62dfeb86-a93f-4914-9d13-15c0a160985d>
2011-01-12 15:18:59 +0000

    clean the delivery model
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/tao/trunk@4146 62dfeb86-a93f-4914-9d13-15c0a160985d

---
+++ install/update/2.0/01_taoDelivery_cleanModel.sql
@@ -0,0 +1,2 @@
+DELETE FROM `statements` WHERE `subject` LIKE 'http://www.tao.lu/Ontologies/TAODelivery.rdf#SubjectCache' LIMIT 5;
+DELETE FROM `statements` WHERE `subject` LIKE 'http://www.tao.lu/Ontologies/TAODelivery.rdf#ExcludedGroups' LIMIT 2;
