1ed0c099a7628acbfb2a43fa6bc44c3a99f2f3e0
ssipasseuth <ssipasseuth@62dfeb86-a93f-4914-9d13-15c0a160985d>
2011-01-20 09:05:10 +0000

    fix bug #429: corrected the old delivery process var uri.
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/tao/trunk@4222 62dfeb86-a93f-4914-9d13-15c0a160985d

--- install/update/2.0/02_taoDelivery_processResources.sql
+++ install/update/2.0/02_taoDelivery_processResources.sql
@@ -1,6 +1,6 @@
-UPDATE `statements` set modelID=14, subject="http://www.tao.lu/Ontologies/TAODelivery.rdf#i1278922897063796600" WHERE  subject like "%#i1278922897063796600";
-UPDATE `statements` set predicate="http://www.tao.lu/Ontologies/TAODelivery.rdf#i1278922897063796600" WHERE  predicate like "%#i1278922897063796600";
-UPDATE `statements` set object="http://www.tao.lu/Ontologies/TAODelivery.rdf#i1278922897063796600" WHERE  object="%#i1278922897063796600";
+UPDATE `statements` set `modelID`=14, subject="http://www.tao.lu/Ontologies/TAODelivery.rdf#i1278922897063796600" WHERE  `subject` like "%#i1267544223024059902";
+UPDATE `statements` set `predicate`="http://www.tao.lu/Ontologies/TAODelivery.rdf#i1278922897063796600" WHERE  `predicate` like "%#i1267544223024059902";
+UPDATE `statements` set `object`="http://www.tao.lu/Ontologies/TAODelivery.rdf#i1278922897063796600" WHERE  `object`="%#i1267544223024059902";
 
 INSERT INTO `statements` (`modelID`, `subject`, `predicate`, `object`, `l_language`, `author`, `stread`, `stedit`, `stdelete`) VALUES
 (14, 'http://www.tao.lu/Ontologies/TAODelivery.rdf#ServiceTestContainer', 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type', 'http://www.tao.lu/middleware/taoqual.rdf#i118588779325312', '', '', 'yyy[admin,administrators,authors]', 'yyy[admin,administrators,authors]', 'yyy[admin,administrators,authors]'),
