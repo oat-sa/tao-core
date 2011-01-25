d877dbfde7cbccf72399c29a19d29064b2befa57
bchevrier <bchevrier@62dfeb86-a93f-4914-9d13-15c0a160985d>
2011-01-25 15:13:39 +0000

    * Add Namespaces in Generis  * Move scripts folder in tao  * Enable generis unti tests to run
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/tao/trunk@4322 62dfeb86-a93f-4914-9d13-15c0a160985d

---
+++ scripts/exportRDFModels.php
@@ -0,0 +1,7 @@
+<?php
+require_once dirname(__FILE__) . '/../includes/raw_start.php';
+
+$api = core_kernel_impl_ApiModelOO::singleton();
+$api->
+$api->exportXmlRdf(array($localModel, $sourceModel));
+?>
