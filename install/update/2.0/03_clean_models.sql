7a0d0f78c7b92b102b37abf2adbc04208c73324f
bchevrier <bchevrier@62dfeb86-a93f-4914-9d13-15c0a160985d>
2011-01-25 16:40:17 +0000

    implement the export model script update on the model table
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/tao/trunk@4331 62dfeb86-a93f-4914-9d13-15c0a160985d

---
+++ install/update/2.0/03_clean_models.sql
@@ -0,0 +1,2 @@
+DELETE FROM models WHERE modelID=16 LIMIT 1;
+UPDATE models SET modelURI="http://www.tao.lu/middleware/wfEngine.rdf#", baseURI="http://www.tao.lu/middleware/wfEngine.rdf#" WHERE modelID=15;
--- scripts/exportRDFModels.php
+++ scripts/exportRDFModels.php
@@ -1,7 +1,56 @@
 <?php
 require_once dirname(__FILE__) . '/../includes/raw_start.php';
 
+//output regarding the context
+function out($msg = ''){
+	print $msg;
+	print (PHP_SAPI == 'cli') ? "\n" : "<br />";
+}
+out();
+out("Running ".basename(__FILE__));
+
+$exportDir = sys_get_temp_dir();
+
+if(PHP_SAPI == 'cli'){	//from command line
+	
+	if(isset($_SERVER['argv'][1])){
+		$exportDir = $_SERVER['argv'][1]; 
+	}
+}
+else{					//from a browser
+	
+	if(isset($_GET['exportDir'])){
+		$exportDir = $_GET['exportDir']; 
+	}
+}
+if(!is_dir($exportDir)){
+	out("$exportDir is not a directory");
+	exit;
+}
+
 $api = core_kernel_impl_ApiModelOO::singleton();
-$api->
-$api->exportXmlRdf(array($localModel, $sourceModel));
+
+$nsManager = common_ext_NamespaceManager::singleton();
+$namespaces = $nsManager->getAllNamespaces();
+
+foreach($namespaces as $namespace){
+	out("Exporting $namespace");
+	$rdfData = $api->exportXmlRdf(array($namespace));
+	if(empty($rdfData)){
+		out("Nothing exported!");
+		continue;
+	}
+	$filename = str_replace('/', '_', str_replace('#', '', $namespace));
+	if(!preg_match("/\.rdf$/", $filename)){
+		$filename .= '.rdf';
+	}
+	$path = tao_helpers_File::concat(array($exportDir, $filename));
+	if(file_put_contents($path, $rdfData) != false){
+		out("Namespace exported at $path");
+	}
+	else{
+		out("Error during the file creation : $path");
+	}
+	out();
+}
 ?>
