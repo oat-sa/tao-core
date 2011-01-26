908d307748299583e298b330ba166dc1ae96aa26
bchevrier <bchevrier@62dfeb86-a93f-4914-9d13-15c0a160985d>
2011-01-26 14:37:45 +0000

    * Update extensions manifests to include model dependancy
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/tao/trunk@4352 62dfeb86-a93f-4914-9d13-15c0a160985d

--- manifest.php
+++ manifest.php
@@ -4,37 +4,23 @@
  * @author CRP Henri Tudor - TAO Team - {@link http://www.tao.lu}
  * @license GPLv2  http://www.opensource.org/licenses/gpl-2.0.php
  */
-
-
-	return array(
-		'name' => 'TAO',
-		'description' => 'TAO http://www.tao.lu',
-		'additional' => array(
-			'version' => '1.2',
-			'author' => 'CRP Henri Tudor',
-			'dependances' => array(),
-			'install' => array( 
-				'sql' => dirname(__FILE__). '/install/db/tao.sql',
-				'php' => dirname(__FILE__). '/install/install.php'
-			),
-			
-			'model' => array(
-				'http://www.w3.org/2000/01/rdf-schema',
-				'http://www.w3.org/1999/02/22-rdf-syntax-ns',
-				'http://www.tao.lu/datatypes/WidgetDefinitions.rdf',
-				'http://www.tao.lu/Ontologies/generis.rdf',
-				'http://www.tao.lu/Ontologies/TAO.rdf',
-				'http://www.tao.lu/middleware/hyperclass.rdf'
-			),
-			'classLoaderPackages' => array( 
-				dirname(__FILE__).'/actions/',
-				dirname(__FILE__).'/helpers/',
-				dirname(__FILE__).'/helpers/form'
-			 )
-			
-
-				
-			
-		)
-	);
+return array(
+	'name' => 'tao',
+	'description' => 'TAO is the meta-extension, a container for the TAOs sub extensions',
+	'additional' => array(
+		'version' => '2.0',
+		'author' => 'CRP Henri Tudor',
+		'dependances' => array('generis'),
+		'models' => 'http://www.tao.lu/Ontologies/TAO.rdf',
+		'install' => array( 
+			'php' => dirname(__FILE__). '/install/install.php',
+			'rdf' => dirname(__FILE__). '/models/ontology/tao.rdf'
+		),
+		'classLoaderPackages' => array( 
+			dirname(__FILE__).'/actions/',
+			dirname(__FILE__).'/helpers/',
+			dirname(__FILE__).'/helpers/form'
+		 )
+	)
+);
 ?>
