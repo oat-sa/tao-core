8b63c69c7280c13e9db7cbff230fc4b6e8164d25
bchevrier <bchevrier@62dfeb86-a93f-4914-9d13-15c0a160985d>
2011-03-21 09:41:07 +0000

    update the xml reporter for unit tests
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/tao/trunk@5014 62dfeb86-a93f-4914-9d13-15c0a160985d

--- test/XmlTimeReporter.php
+++ test/XmlTimeReporter.php
@@ -18,8 +18,8 @@ class XmlTimeReporter extends XmlReporter {
 	      $duration = $post - $this->pre;
 	      // how can post time be less than pre?  assuming zero if this happens..
 	      if ($post < $this->pre) $duration = 0;
-	      print $this->_getIndent(1);
-	      print "<time>$duration</time>\n";
+	      print $this->getIndent(1);
+	     print "<time>$duration</time>\n";
 	    }
 	    parent::paintMethodEnd($test_name);
 	    $this->pre = null;
