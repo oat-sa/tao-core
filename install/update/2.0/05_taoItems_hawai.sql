f1c1a1aa3500bdbec2eb7e1a0884a192004827dd
bchevrier <bchevrier@62dfeb86-a93f-4914-9d13-15c0a160985d>
2011-03-23 22:46:21 +0000

    Fix bug #553 on QTI scoring Remove the crapy "warning" which was added to the item predicated Add an update scritp for the change of the HAWAI runtime file
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/tao/trunk@5096 62dfeb86-a93f-4914-9d13-15c0a160985d

---
+++ install/update/2.0/05_taoItems_hawai.sql
@@ -0,0 +1,2 @@
+
+UPDATE statements SET object = 'index.xhtml' WHERE predicate = 'http://www.tao.lu/Ontologies/TAOItem.rdf#DataFileName' AND subject='http://www.tao.lu/Ontologies/TAOItem.rdf#Hawai';
