9c77ab494b18c05a1cdc72808f1658d361c64781
jbogaerts <jbogaerts@62dfeb86-a93f-4914-9d13-15c0a160985d>
2011-04-19 13:00:26 +0000

    Now includes HTML Report when going to /tao/selenium. It uses the xsl files from the Phing project to transform the PHPUnit XML report to XHTML.
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/tao/trunk@5482 62dfeb86-a93f-4914-9d13-15c0a160985d

--- test/selenium/index.php
+++ test/selenium/index.php
@@ -1,5 +1,5 @@
 <?php
-
+error_reporting(E_ALL);
 # include TestRunner
 require 'PHPUnit/Autoload.php';
 
@@ -9,5 +9,31 @@ $testSuite = new PHPUnit_Framework_TestSuite('TAO Functional Test Suite (tao ext
 
 $testSuite->addTestFile(dirname(__FILE__) . '/SeleniumBackendMainTestCase.php');
 
-PHPUnit_TextUI_TestRunner::run($testSuite);
+if (PHP_SAPI == 'cli') {
+	PHPUnit_TextUI_TestRunner::run($testSuite);
+}
+else {
+	// Do not output anything before the report in Server mode.
+	// We only want the HTML.
+	ob_start();
+	
+	$reportFileName = tempnam(sys_get_temp_dir(), 'tao');
+	PHPUnit_TextUI_TestRunner::run($testSuite, array('junitLogfile' => $reportFileName,
+													 'verbose', false));
+	
+	// Get the report and transform the XML to an HTML readable report.
+	$xmlDoc = new DOMDocument();
+	$xslDoc = new DOMDocument();
+	
+	$xmlDoc->load($reportFileName);
+	$xslDoc->load(dirname(__FILE__) . '/includes/phpunit-noframes.xsl');
+	
+	$xsl = new XSLTProcessor();
+	$xsl->importStyleSheet($xslDoc);
+	
+	ob_clean();
+	
+	echo $xsl->transformToXML($xmlDoc);
+	unlink($reportFileName);
+}
 ?>
