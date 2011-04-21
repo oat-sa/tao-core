eb364e26bb0cc266959c439af0cb9e8198b353ee
bchevrier <bchevrier@62dfeb86-a93f-4914-9d13-15c0a160985d>
2011-04-21 16:01:40 +0000

    update ontologies by addding #Multiple Property on porperties that could have more than one value
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/tao/trunk@5526 62dfeb86-a93f-4914-9d13-15c0a160985d

--- install/db/tao.sql
+++ install/db/tao.sql
@@ -3,17 +3,6 @@ CREATE DATABASE {DATABASE_NAME};
 USE {DATABASE_NAME};
 SET NAMES UTF8;
 
-DROP TABLE IF EXISTS `_mask`;
-CREATE TABLE `_mask` (
-  `user` varchar(255) default NULL,
-  `Scope` varchar(255) default NULL,
-  `Method` varchar(255) default NULL,
-  `onAssertPrivileges` longtext,
-  `_comment` varchar(255) default NULL,
-  `id` int(11) NOT NULL auto_increment,
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
 DROP TABLE IF EXISTS `extensions`;
 CREATE TABLE `extensions` (
   `id` varchar(25) NOT NULL default '',
@@ -23,7 +12,7 @@ CREATE TABLE `extensions` (
   `loadAtStartUp` tinyint(1) NOT NULL,
   `ghost` tinyint(1) NOT NULL default '0',
   PRIMARY KEY  (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) DEFAULT CHARSET=utf8;
 
 INSERT INTO `extensions` VALUES 
 ('generis','generis','2.0',1,1,0),
@@ -43,7 +32,7 @@ CREATE TABLE `models` (
   `baseURI` varchar(255) default NULL,
   PRIMARY KEY  (`modelID`),
   KEY `idx_models_modelURI` (`modelURI`)
-) ENGINE=MyISAM AUTO_INCREMENT=19 DEFAULT CHARSET=utf8;
+)DEFAULT CHARSET=utf8;
 
 INSERT INTO `models` VALUES 
 (3,'http://www.tao.lu/datatypes/WidgetDefinitions.rdf#','http://www.tao.lu/datatypes/WidgetDefinitions.rdf#'),
@@ -78,26 +67,29 @@ CREATE TABLE `statements` (
   KEY `idx_statements_subject` (`subject`),
   KEY `idx_statements_predicate` (`predicate`),
   KEY `idx_statements_object` (`object` (128) )
-) ENGINE=MyISAM AUTO_INCREMENT=4337 DEFAULT CHARSET=utf8;
+)DEFAULT CHARSET=utf8;
 
-DROP TABLE IF EXISTS `user`;
-CREATE TABLE `user` (
-  `login` varchar(32) NOT NULL default '',
-  `password` varchar(32) default NULL,
-  `admin` tinyint(1) NOT NULL default '0',
-  `usergroup` varchar(32) default NULL,
-  `LastName` varchar(64) default NULL,
-  `FirstName` varchar(64) default NULL,
-  `E_Mail` varchar(128) default NULL,
-  `Company` varchar(128) default NULL,
-  `Deflg` char(2) default NULL,
-  `Uilg` varchar(2) default NULL,
-  `enabled` tinyint(1) NOT NULL default '0',
-  PRIMARY KEY  (`login`),
-  KEY `login` (`login`),
-  KEY `usergroup` (`usergroup`),
-  KEY `login_2` (`login`),
-  KEY `usergroup_2` (`usergroup`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+DROP TABLE IF EXISTS `class_to_table`;
+CREATE TABLE `class_to_table` (
+	`id` int NOT NULL auto_increment,
+	`uri` VARCHAR(255) NOT NULL,
+	`table` VARCHAR(64) NOT NULL,
+	PRIMARY KEY (`id`),
+	KEY `idx_class_to_table_uri` (`uri`) 
+)DEFAULT CHARSET=utf8;
 
-INSERT INTO `user` VALUES ('generis','b01a52f727b0810639526fe2c8188331',1,'admin','','','','','','EN',0);
+DROP TABLE IF EXISTS `resource_to_table`;
+CREATE TABLE `resource_to_table` (
+	`id` int NOT NULL auto_increment,
+	`uri` VARCHAR(255) NOT NULL,
+	`table` VARCHAR(64) NOT NULL,
+	PRIMARY KEY (`id`) ,
+	KEY `idx_resource_to_table_uri` (`uri`)
+)DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `resource_has_class`;
+CREATE TABLE `resource_has_class` (
+	`resource_id` int NOT NULL,
+	`class_id` int NOT NULL,
+	PRIMARY KEY (`resource_id`, `class_id`) 
+)DEFAULT CHARSET=utf8;
---
+++ install/update/2.1/01_harddb.sql
@@ -0,0 +1,21 @@
+CREATE TABLE `class_to_table`(
+	`id` int NOT NULL auto_increment,
+	`uri` VARCHAR(255) NOT NULL,
+	`table` VARCHAR(64) NOT NULL,
+	PRIMARY KEY (`id`),
+	KEY `idx_class_to_table_uri` (`uri`)
+)DEFAULT CHARSET=utf8;
+
+CREATE TABLE `resource_to_table`(
+	`id` int NOT NULL auto_increment,
+	`uri` VARCHAR(255) NOT NULL,
+	`table` VARCHAR(64) NOT NULL,
+	PRIMARY KEY (`id`),
+	KEY `idx_resource_to_table_uri` (`uri`)
+)DEFAULT CHARSET=utf8;
+
+CREATE TABLE `resource_has_class`(
+	`resource_id` int NOT NULL,
+	`class_id` int NOT NULL,
+	PRIMARY KEY (`resource_id`, `class_id`)
+)DEFAULT CHARSET=utf8;
