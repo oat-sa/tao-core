44ae74ed048d4cb39ad875ef4dad0a298c565955
calfonsi <calfonsi@62dfeb86-a93f-4914-9d13-15c0a160985d>
2011-05-04 08:30:49 +0000

    ADD scripts update for the 2.1 revision * 04 adds the multiple property to the type property * 05 adds the right (and correct one) range to some properties of the wfengine
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/tao/trunk@5576 62dfeb86-a93f-4914-9d13-15c0a160985d

---
+++ install/update/2.1/05_property_range_missing_or_correct.sql
@@ -0,0 +1,18 @@
+UPDATE statements SET object = "http://www.w3.org/1999/02/22-rdf-syntax-ns#ClassProcessVariables" WHERE 
+    subject="http://www.tao.lu/middleware/wfEngine.rdf#PropertyActualParametersProcessVariable" 
+    AND predicate = "http://www.w3.org/2000/01/rdf-schema#range";
+
+INSERT INTO `statements` (`modelID`, `subject`, `predicate`, `object`, `l_language`, `author`, `stread`, `stedit`, `stdelete`) VALUES
+
+(15, 'http://www.tao.lu/middleware/wfEngine.rdf#PropertyActualParametersConstantValue', 'http://www.w3.org/2000/01/rdf-schema#range', 'http://www.w3.org/2000/01/rdf-schema#Literal', '',  'generis', 'yyy[admin,administrators,authors]', 'yyy[admin,administrators,authors]', 'yyy[admin,administrators,authors]'),
+(15, 'http://www.tao.lu/middleware/wfEngine.rdf#PropertyFormalParameterDataType', 'http://www.w3.org/2000/01/rdf-schema#range', 'http://www.w3.org/2000/01/rdf-schema#Literal', '',  'generis', 'yyy[admin,administrators,authors]', 'yyy[admin,administrators,authors]', 'yyy[admin,administrators,authors]'),
+
+(15, 'http://www.tao.lu/middleware/wfEngine.rdf#PropertyFormalParametersName', 'http://www.w3.org/2000/01/rdf-schema#range', 'http://www.w3.org/2000/01/rdf-schema#Literal', '',  'generis', 'yyy[admin,administrators,authors]', 'yyy[admin,administrators,authors]', 'yyy[admin,administrators,authors]'),
+(15, 'http://www.tao.lu/middleware/wfEngine.rdf#PropertyFormalParameterName', 'http://www.w3.org/2000/01/rdf-schema#range', 'http://www.w3.org/2000/01/rdf-schema#Literal', '',  'generis', 'yyy[admin,administrators,authors]', 'yyy[admin,administrators,authors]', 'yyy[admin,administrators,authors]'),
+(15, 'http://www.tao.lu/middleware/wfEngine.rdf#PropertyCode', 'http://www.w3.org/2000/01/rdf-schema#range', 'http://www.w3.org/2000/01/rdf-schema#Literal', '',  'generis', 'yyy[admin,administrators,authors]', 'yyy[admin,administrators,authors]', 'yyy[admin,administrators,authors]'),
+
+(15, 'http://www.tao.lu/middleware/wfEngine.rdf#PropertyCallOfServicesTop', 'http://www.w3.org/2000/01/rdf-schema#range', 'http://www.w3.org/2000/01/rdf-schema#Literal', '',  'generis', 'yyy[admin,administrators,authors]', 'yyy[admin,administrators,authors]', 'yyy[admin,administrators,authors]'),
+(15, 'http://www.tao.lu/middleware/wfEngine.rdf#PropertyCallOfServicesLeft', 'http://www.w3.org/2000/01/rdf-schema#range', 'http://www.w3.org/2000/01/rdf-schema#Literal', '',  'generis', 'yyy[admin,administrators,authors]', 'yyy[admin,administrators,authors]', 'yyy[admin,administrators,authors]'),
+(15, 'http://www.tao.lu/middleware/wfEngine.rdf#PropertyCallOfServicesWidth', 'http://www.w3.org/2000/01/rdf-schema#range', 'http://www.w3.org/2000/01/rdf-schema#Literal', '',  'generis', 'yyy[admin,administrators,authors]', 'yyy[admin,administrators,authors]', 'yyy[admin,administrators,authors]'),
+(15, 'http://www.tao.lu/middleware/wfEngine.rdf#PropertyCallOfServicesHeight', 'http://www.w3.org/2000/01/rdf-schema#range', 'http://www.w3.org/2000/01/rdf-schema#Literal', '',  'generis', 'yyy[admin,administrators,authors]', 'yyy[admin,administrators,authors]', 'yyy[admin,administrators,authors]');
+
