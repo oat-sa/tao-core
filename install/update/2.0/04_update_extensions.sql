4b28f431c4cb245cd064fce1c589f3a1cb799727
pplichart <pplichart@62dfeb86-a93f-4914-9d13-15c0a160985d>
2011-05-26 09:36:10 +0000

    fix the insert extension statements
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/tao/trunk@5865 62dfeb86-a93f-4914-9d13-15c0a160985d

--- install/update/2.0/04_update_extensions.sql
+++ install/update/2.0/04_update_extensions.sql
@@ -1,5 +1,5 @@
 TRUNCATE TABLE `extensions`;
-INSERT INTO `extensions` VALUES 
+INSERT INTO `extensions` (`id`, `name`, `version`, `loaded`, `loadAtStartUp`, `ghost`) VALUES 
 ('generis','generis','2.0',1,1,0),
 ('tao','tao','2.0',1,1,0),
 ('taoGroups','Tao Groups','2.0',1,1,0),
@@ -7,5 +7,6 @@ INSERT INTO `extensions` VALUES
 ('taoResults','Tao Results','2.0',1,1,0),
 ('taoSubjects','Tao Subjects','2.0',1,1,0),
 ('taoTests','Tao Test','2.0',1,1,0),
-('taoDelivery','taoDelivery','2.0',1,1,0),
+('taoDelivery','Tao Delivery','2.0',1,1,0),
 ('wfEngine','Workflow Engine Extension','2.0',1,1,0);
+
