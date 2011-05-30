6d2ee306911cda17c50dcc710f499372f0c94b4d
pplichart <pplichart@62dfeb86-a93f-4914-9d13-15c0a160985d>
2011-05-30 16:17:38 +0000

    update the tao version in the install db
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/tao/trunk@5921 62dfeb86-a93f-4914-9d13-15c0a160985d

--- install/db/tao.sql
+++ install/db/tao.sql
@@ -15,15 +15,15 @@ CREATE TABLE `extensions` (
 ) DEFAULT CHARSET=utf8;
 
 INSERT INTO `extensions` VALUES 
-('generis','generis','2.0',1,1,0),
-('tao','tao','2.0',1,1,0),
-('taoGroups','Tao Groups','2.0',1,1,0),
-('taoItems','Tao Items','2.0',1,1,0),
-('taoResults','Tao Results','2.0',1,1,0),
-('taoSubjects','Tao Subjects','2.0',1,1,0),
-('taoTests','Tao Test','2.0',1,1,0),
-('taoDelivery','taoDelivery','2.0',1,1,0),
-('wfEngine','Workflow Engine Extension','2.0',1,1,0);
+('generis','generis','2.1',1,1,0),
+('tao','tao','2.1',1,1,0),
+('taoGroups','Tao Groups','2.1',1,1,0),
+('taoItems','Tao Items','2.1',1,1,0),
+('taoResults','Tao Results','2.1',1,1,0),
+('taoSubjects','Tao Subjects','2.1',1,1,0),
+('taoTests','Tao Test','2.1',1,1,0),
+('taoDelivery','taoDelivery','2.1',1,1,0),
+('wfEngine','Workflow Engine Extension','2.1',1,1,0);
 
 DROP TABLE IF EXISTS `models`;
 CREATE TABLE `models` (
