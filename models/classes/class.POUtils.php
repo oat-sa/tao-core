d941f5879f8c046fe348adb718ae54798a1fae9d
jbogaerts <jbogaerts@62dfeb86-a93f-4914-9d13-15c0a160985d>
2011-10-20 12:28:39 +0000

    Missing class from previous commit.
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/tao/trunk@7042 62dfeb86-a93f-4914-9d13-15c0a160985d

---
+++ models/classes/class.POUtils.php
@@ -0,0 +1,79 @@
+<?php
+
+error_reporting(E_ALL);
+
+/**
+ * TAO - tao\models\classes\class.POUtils.php
+ *
+ * $Id$
+ *
+ * This file is part of TAO.
+ *
+ * Automatically generated on 20.10.2011, 12:05:47 with ArgoUML PHP module 
+ * (last revised $Date: 2008-04-19 08:22:08 +0200 (Sat, 19 Apr 2008) $)
+ *
+ * @author firstname and lastname of author, <author@example.org>
+ * @package tao
+ * @subpackage models_classes
+ */
+
+if (0 > version_compare(PHP_VERSION, '5')) {
+    die('This file was generated for PHP 5');
+}
+
+/* user defined includes */
+// section 10-13-1-85--20e08ece:13320c7798f:-8000:0000000000003545-includes begin
+// section 10-13-1-85--20e08ece:13320c7798f:-8000:0000000000003545-includes end
+
+/* user defined constants */
+// section 10-13-1-85--20e08ece:13320c7798f:-8000:0000000000003545-constants begin
+// section 10-13-1-85--20e08ece:13320c7798f:-8000:0000000000003545-constants end
+
+/**
+ * Short description of class tao_models_classes_POUtils
+ *
+ * @access public
+ * @author firstname and lastname of author, <author@example.org>
+ * @package tao
+ * @subpackage models_classes
+ */
+class tao_models_classes_POUtils
+{
+    // --- ASSOCIATIONS ---
+
+
+    // --- ATTRIBUTES ---
+
+    // --- OPERATIONS ---
+
+    /**
+     * Short description of method sanitize
+     *
+     * @access public
+     * @author firstname and lastname of author, <author@example.org>
+     * @param  string string
+     * @param  boolean reverse
+     * @return string
+     */
+    public static function sanitize($string, $reverse = false)
+    {
+        $returnValue = (string) '';
+
+        // section 10-13-1-85--20e08ece:13320c7798f:-8000:0000000000003546 begin
+    	if ($reverse) {
+			$smap = array('"', "\n", "\t", "\r");
+			$rmap = array('\\"', '\\n"' . "\n" . '"', '\\t', '\\r');
+			$returnValue = trim((string) str_replace($smap, $rmap, $string));
+		} else {
+			$smap = array('/"\s+"/', '/\\\\n/', '/\\\\r/', '/\\\\t/', '/\\\"/');
+			$rmap = array('', "\n", "\r", "\t", '"');
+			$returnValue = trim((string) preg_replace($smap, $rmap, $string));
+		}
+        // section 10-13-1-85--20e08ece:13320c7798f:-8000:0000000000003546 end
+
+        return (string) $returnValue;
+    }
+
+} /* end of class tao_models_classes_POUtils */
+
+?>
