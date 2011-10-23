7feb5712c3a95457d8e92add2459de3559068fb8
jbogaerts <jbogaerts@62dfeb86-a93f-4914-9d13-15c0a160985d>
2011-10-23 21:54:51 +0000

    TranslationException class missing.
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/tao/trunk@7079 62dfeb86-a93f-4914-9d13-15c0a160985d

---
+++ helpers/translation/class.TranslationException.php
@@ -0,0 +1,58 @@
+<?php
+
+error_reporting(E_ALL);
+
+/**
+ * TAO - tao\helpers\translation\class.TranslationException.php
+ *
+ * $Id$
+ *
+ * This file is part of TAO.
+ *
+ * Automatically generated on 23.10.2011, 23:52:40 with ArgoUML PHP module 
+ * (last revised $Date: 2010-01-12 20:14:42 +0100 (Tue, 12 Jan 2010) $)
+ *
+ * @author firstname and lastname of author, <author@example.org>
+ * @package tao
+ * @subpackage helpers_translation
+ */
+
+if (0 > version_compare(PHP_VERSION, '5')) {
+    die('This file was generated for PHP 5');
+}
+
+/**
+ * include Exception
+ *
+ * @author firstname and lastname of author, <author@example.org>
+ */
+
+/* user defined includes */
+// section 10-13-1-85-72d0ca97:1331b62f595:-8000:00000000000034E7-includes begin
+// section 10-13-1-85-72d0ca97:1331b62f595:-8000:00000000000034E7-includes end
+
+/* user defined constants */
+// section 10-13-1-85-72d0ca97:1331b62f595:-8000:00000000000034E7-constants begin
+// section 10-13-1-85-72d0ca97:1331b62f595:-8000:00000000000034E7-constants end
+
+/**
+ * Short description of class tao_helpers_translation_TranslationException
+ *
+ * @access public
+ * @author firstname and lastname of author, <author@example.org>
+ * @package tao
+ * @subpackage helpers_translation
+ */
+class tao_helpers_translation_TranslationException
+    extends Exception
+{
+    // --- ASSOCIATIONS ---
+
+
+    // --- ATTRIBUTES ---
+
+    // --- OPERATIONS ---
+
+} /* end of class tao_helpers_translation_TranslationException */
+
+?>
