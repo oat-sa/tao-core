06c1fa08e7d52474a74490d2b7134afe01c86d2a
jbout <jbout@62dfeb86-a93f-4914-9d13-15c0a160985d>
2012-01-04 14:03:02 +0000

    fix for calls to common_Logger during install
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/tao/trunk@7946 62dfeb86-a93f-4914-9d13-15c0a160985d

--- install/init.php
+++ install/init.php
@@ -9,6 +9,9 @@ function __autoload($class_name) {
 	$file =  'class.' . basename($path). '.php';
     require_once  dirname($path) . '/' . $file;
 }
+
+require_once ('tao/install/stub/common_Logger.php');
+
 require_once ('tao/helpers/class.Display.php');
 require_once ('tao/helpers/class.Uri.php');
 ?>
