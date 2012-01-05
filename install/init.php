7cab7a43d40b8f3ecaa399b65846b79a4c45d3da
jbout <jbout@62dfeb86-a93f-4914-9d13-15c0a160985d>
2012-01-05 09:25:08 +0000

    bugfix to install init script calls from login screens
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/tao/trunk@7961 62dfeb86-a93f-4914-9d13-15c0a160985d

--- install/init.php
+++ install/init.php
@@ -5,12 +5,19 @@ $root = realpath($rootDir->path).'/';
 set_include_path(get_include_path() . PATH_SEPARATOR . $root);
 
 function __autoload($class_name) {
-	$path = str_replace('_', '/', $class_name);
-	$file =  'class.' . basename($path). '.php';
-    require_once  dirname($path) . '/' . $file;
+	//check if we have a pseudo implementation in /stub
+	if (file_exists(dirname(__FILE__).'/stub/'.$class_name.'.php')) {
+		
+		// use the stub instead of the real class
+		require_once dirname(__FILE__).'/stub/'.$class_name.'.php';
+	} else {
+		
+		// include normaly
+		$path = str_replace('_', '/', $class_name);
+		$file =  'class.' . basename($path). '.php';
+	    require_once  dirname($path) . '/' . $file;
+	}
 }
-
-require_once ('tao/install/stub/common_Logger.php');
 
 require_once ('tao/helpers/class.Display.php');
 require_once ('tao/helpers/class.Uri.php');
