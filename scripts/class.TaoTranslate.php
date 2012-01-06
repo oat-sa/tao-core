87795b97aab5fa7abad0fc028748fb1277392072
jbogaerts <jbogaerts@62dfeb86-a93f-4914-9d13-15c0a160985d>
2012-01-06 10:22:47 +0000

    New translation scripts in progress.
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/tao/trunk@8007 62dfeb86-a93f-4914-9d13-15c0a160985d

--- scripts/class.TaoTranslate.php
+++ scripts/class.TaoTranslate.php
@@ -9,7 +9,7 @@ error_reporting(E_ALL);
  *
  * This file is part of TAO.
  *
- * Automatically generated on 05.01.2012, 15:50:10 with ArgoUML PHP module 
+ * Automatically generated on 06.01.2012, 11:17:41 with ArgoUML PHP module 
  * (last revised $Date: 2008-04-19 08:22:08 +0200 (Sat, 19 Apr 2008) $)
  *
  * @author firstname and lastname of author, <author@example.org>
@@ -52,6 +52,22 @@ class tao_scripts_TaoTranslate
 
     // --- ATTRIBUTES ---
 
+    /**
+     * Short description of attribute DEF_INPUT_DIR
+     *
+     * @access public
+     * @var string
+     */
+    const DEF_INPUT_DIR = 'locales';
+
+    /**
+     * Short description of attribute DEF_OUTPUT_DIR
+     *
+     * @access public
+     * @var string
+     */
+    const DEF_OUTPUT_DIR = 'locales';
+
     // --- OPERATIONS ---
 
     /**
@@ -64,7 +80,7 @@ class tao_scripts_TaoTranslate
     public function preRun()
     {
         // section -64--88-1-7-6b37e1cc:1336002dd1f:-8000:0000000000003287 begin
-    $this->options = array('verbose' => false,
+    	$this->options = array('verbose' => false,
         					   'action' => null);
         
         $this->options = array_merge($this->options, $this->parameters);
@@ -79,7 +95,7 @@ class tao_scripts_TaoTranslate
         if ($this->options['action'] == null) {
         	self::err("Please enter the 'action' parameter.", true);
         } else {
-        	$this->action = strtolower($action);
+        	$this->options['action'] = strtolower($this->options['action']);
         	$allowedActions = array('create',
         							'update',
         							'delete',
@@ -89,7 +105,7 @@ class tao_scripts_TaoTranslate
         	if (!in_array($this->options['action'], $allowedActions)) {
         		self::err("Please enter a valid 'action' parameter.", true);
         	} else {
-        		
+        		$this->checkInput();
         	}
         }
         // section -64--88-1-7-6b37e1cc:1336002dd1f:-8000:0000000000003287 end
@@ -105,7 +121,7 @@ class tao_scripts_TaoTranslate
     public function run()
     {
         // section -64--88-1-7-6b37e1cc:1336002dd1f:-8000:0000000000003289 begin
-        
+        self::out('GG');
         // section -64--88-1-7-6b37e1cc:1336002dd1f:-8000:0000000000003289 end
     }
 
@@ -132,6 +148,32 @@ class tao_scripts_TaoTranslate
     private function checkInput()
     {
         // section 10-13-1-85--7b8e6d0a:134ae555568:-8000:0000000000003840 begin
+        switch ($this->options['action']) {
+        	case 'create':
+        		$this->checkCreateInput();
+        	break;
+        	
+        	case 'update':
+        		$this->checkUpdateInput();
+        	break;
+        	
+        	case 'updateAll':
+        		$this->checkUpdateAllInput();
+        	break;
+        	
+        	case 'delete':
+        		$this->checkDeleteInput();
+        	break;
+        	
+        	case 'deleteAll':
+        		$this->checkDeleteAllInput();
+        	break;
+        	
+        	default:
+        		// Should not happen.
+        		self::err("Fatal error while checking input parameters. Unknown 'action'.", true);
+        	break;
+        }
         // section 10-13-1-85--7b8e6d0a:134ae555568:-8000:0000000000003840 end
     }
 
@@ -145,6 +187,25 @@ class tao_scripts_TaoTranslate
     private function checkCreateInput()
     {
         // section 10-13-1-85--7b8e6d0a:134ae555568:-8000:0000000000003842 begin
+        $defaults = array('language' => null,
+        				  'output' => 'defaultoutputdir',
+        				  'build' => true,
+        				  'force' => true);
+        
+        $this->options = array_merge($defaults, $this->options);
+    	
+    	if (is_null($this->options['language'])) {
+        	self::err("Please provide a 'language' identifier such as en-US, fr-CA, IT, ...", true);
+        } else {
+        	// The 'output' parameter is optional.
+        	if (!is_null($this->options['output'])) {
+        		if (!is_dir($this->options['output'])) {
+        			self::err("The 'output' parameter you provided is not a directory.", true);
+        		} else if (!is_writable($this->options['output'])) {
+        			self::err("The 'output' directory is not writable.", true);
+        		}
+        	}
+        }
         // section 10-13-1-85--7b8e6d0a:134ae555568:-8000:0000000000003842 end
     }
 
@@ -158,6 +219,33 @@ class tao_scripts_TaoTranslate
     private function checkUpdateInput()
     {
         // section 10-13-1-85--7b8e6d0a:134ae555568:-8000:0000000000003844 begin
+        $defaults = array('language' => null,
+        				  'input' => 'defaultinputdir',
+        				  'output' => 'defaultoutputdir');
+        
+        $this->options = array_merge($defaults, $this->options);
+        
+        if (is_null($this->options['language'])) {
+        	self::err("Please provide a 'language' identifier such as en-US, fr-CA, IT, ...", true);
+        } else {
+        	// The input 'parameter' is optional.
+        	if (!is_null($this->options['input'])) {
+        		if (!is_dir($this->options['input'])) {
+        			self::err("The 'input' parameter you provided is not a directory.", true);
+        		} else if (!is_readable($this->options['input'])) {
+        			self::err("The 'input' directory is not readable.", true);
+        		}
+        	}
+        	
+        	// The output 'parameter' is optional.
+        	if (!is_null($this->options['output'])) {
+        		if (!is_dir($this->options['output'])) {
+        			self::err("The 'output' parameter you provided is not a directory.", true);
+        		} else if (!is_writable($this->options['output'])) {
+        			self::err("The 'output' directory is not writable.", true);
+        		}
+        	}
+        }
         // section 10-13-1-85--7b8e6d0a:134ae555568:-8000:0000000000003844 end
     }
 
@@ -171,6 +259,28 @@ class tao_scripts_TaoTranslate
     private function checkUpdateAllInput()
     {
         // section 10-13-1-85--7b8e6d0a:134ae555568:-8000:0000000000003846 begin
+        $defaults = array('input' => 'defaultinputdir',
+        				  'output' => 'defaultoutputdir');
+        
+        $this->options = array_merge($defaults, $this->options);
+        
+    	// The input 'parameter' is optional.
+        if (!is_null($this->options['input'])) {
+        	if (!is_dir($this->options['input'])) {
+        		self::err("The 'input' parameter you provided is not a directory.", true);
+        	} else if (!is_readable($this->options['input'])) {
+        		self::err("The 'input' directory is not readable.", true);
+        	}
+        }
+        
+        // The output 'parameter' is optional.
+        if (!is_null($this->options['output'])) {
+        	if (!is_dir($this->options['output'])) {
+        		self::err("The 'output' parameter you provided is not a directory.", true);
+        	} else if (!is_writable($this->options['output'])) {
+        		self::err("The 'output' directory is not writable.", true);
+        	}
+        }
         // section 10-13-1-85--7b8e6d0a:134ae555568:-8000:0000000000003846 end
     }
 
@@ -184,6 +294,23 @@ class tao_scripts_TaoTranslate
     private function checkDeleteInput()
     {
         // section 10-13-1-85--7b8e6d0a:134ae555568:-8000:0000000000003848 begin
+        $defaults = array('language' => null,
+        				  'input' => 'defaultinputdir');
+        
+        $this->options = array_merge($defaults, $this->options);
+        
+    	if (is_null($this->options['language'])) {
+        	self::err("Please provide a 'language' identifier such as en-US, fr-CA, IT, ...", true);
+        } else {
+        	// The input 'parameter' is optional.
+        	if (!is_null($this->options['input'])) {
+        		if (!is_dir($this->options['input'])) {
+        			self::err("The 'input' parameter you provided is not a directory.", true);
+        		} else if (!is_readable($this->options['input'])) {
+        			self::err("The 'input' directory is not readable.", true);
+        		}
+        	}
+        }
         // section 10-13-1-85--7b8e6d0a:134ae555568:-8000:0000000000003848 end
     }
 
@@ -197,6 +324,18 @@ class tao_scripts_TaoTranslate
     private function checkDeleteAllInput()
     {
         // section 10-13-1-85--7b8e6d0a:134ae555568:-8000:000000000000384A begin
+     	$defaults = array('input' => 'defaultinputdir');
+        
+        $this->options = array_merge($defaults, $this->options);
+        
+    	// The input 'parameter' is optional.
+        if (!is_null($this->options['input'])) {
+        	if (!is_dir($this->options['input'])) {
+        		self::err("The 'input' parameter you provided is not a directory.", true);
+        	} else if (!is_readable($this->options['input'])) {
+        		self::err("The 'input' directory is not readable.", true);
+        	}
+        }
         // section 10-13-1-85--7b8e6d0a:134ae555568:-8000:000000000000384A end
     }
 
--- scripts/taoTranslate.php
+++ scripts/taoTranslate.php
@@ -2,7 +2,7 @@
 require_once dirname(__FILE__) .'/../includes/raw_start.php';
 
 new tao_scripts_TaoTranslate(array(
-	'min' => 0,
+	'min' => 1,
 	'parameters' => array(
 		array(
 			'name' => 'verbose',
@@ -14,7 +14,37 @@ new tao_scripts_TaoTranslate(array(
 			'name' => 'action',
 			'type' => 'string',
 			'shortcut' => 'a',
-			'description' => 'Action to undertake like Create, Update, Delete, ...'
+			'description' => 'Action to undertake. Available actions are create, update, updateAll, delete, deleteAll'
+		),
+		array(
+			'name' => 'language',
+			'type' => 'string',
+			'shortcut' => 'l',
+			'description' => 'A language identifier like en-US, be-NL, FR, ...'
+		),
+		array(
+			'name' => 'output',
+			'type' => 'string',
+			'shortcut' => 'o',
+			'description' => 'An output directory'
+		),
+		array(
+			'name' => 'input',
+			'type' => 'string',
+			'shortcut' => 'i',
+			'description' => 'An input directory'
+		),
+		array(
+			'name' => 'build',
+			'type' => 'boolean',
+			'shortcut' => 'b',
+			'description' => 'Sets if the language has to built when created or not'
+		),
+		array(
+			'name' => 'force',
+			'type' => 'boolean',
+			'shortcut' => 'f',
+			'description' => 'Force to erase an existing language if you use the create action'
 		)
 	)
 ));
