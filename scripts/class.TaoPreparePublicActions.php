e937aa14e6237bdd85f28d9817f5f2b64809f428
jbihin <jbihin@62dfeb86-a93f-4914-9d13-15c0a160985d>
2012-03-16 14:31:00 +0000

    Correct the script with name and base
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/tao/trunk@10147 62dfeb86-a93f-4914-9d13-15c0a160985d

--- scripts/class.TaoPreparePublicActions.php
+++ scripts/class.TaoPreparePublicActions.php
@@ -96,7 +96,7 @@ class tao_scripts_TaoPreparePublicActions
 						//Add the module
 						//http://www.tao.lu/Ontologies/taoFuncACL.rdf#m_{name}
 						$module = substr($action, 6, -4);
-						$rdf .= "\n".str_replace("{name}", $dir.'_'.$module, str_replace("{extension}", $dir, $rdf_modtpl))."\n";
+						$rdf .= "\n".str_replace("{base}", $dir, str_replace("{name}", $module, str_replace("{extension}", $dir, $rdf_modtpl)))."\n";
 						$ext = "http://www.tao.lu/Ontologies/taoFuncACL.rdf#m_".$dir.'_'.$module;
 
 						$class = $dir.'_actions_'.$module;
@@ -107,7 +107,7 @@ class tao_scripts_TaoPreparePublicActions
 							$methods = $reflector->getMethods(ReflectionMethod::IS_PUBLIC);
 							foreach ($methods as $m) {
 								if (!$m->isConstructor() && !$m->isDestructor() && $m->class == $class) {
-									$rdf .= "\n".str_replace("{name}", $dir.'_'.$module.' '.$m->name, str_replace("{member}", $ext, $rdf_acttpl));
+									$rdf .= "\n".str_replace("{base}", $dir.'_'.$module, str_replace("{name}", $m->name, str_replace("{member}", $ext, $rdf_acttpl)));
 								}
 							}
 						}
