bbef146a3621a940fb388566f211cd7a18ddd792
jbogaerts <jbogaerts@62dfeb86-a93f-4914-9d13-15c0a160985d>
2012-03-22 08:45:08 +0000

    tao_install_utils_DbCreator now extends tao_install_utils_DbConnector.
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/tao/trunk@10239 62dfeb86-a93f-4914-9d13-15c0a160985d

--- install/utils/class.DbCreator.php
+++ install/utils/class.DbCreator.php
@@ -3,94 +3,28 @@ require_once 'generis/includes/adodb5/adodb-exceptions.inc.php';
 require_once 'generis/includes/adodb5/adodb.inc.php';
 
 /**
- * Dedicated database wrapper used for database installation
+ * Dedicated database wrapper used for database creation.
+ * Its purpose is to establish a connection with a database
+ * and load a given SQL file containing simple statements.
+ * 
+ * Please note that this Database Creator does not support
+ * complex SQL statements such as Stored Procedure or Functions
+ * creations. If you need to load complex statements such as
+ * Stored Procedure or Function declarations, use tao_install_utils_SpCreator.
+ * 
  * @see ADOConnection
+ * @see tao_install_utils_SpConnector
  * @author Bertrand CHEVRIER <bertrand.chevrier@tudor.lu>
+ * @author Jerome BOGAERTS <jerome.bogaerts@tudor.lu>
  *
  */
-class tao_install_utils_DbCreator{
-	
+class tao_install_utils_DbCreator extends tao_install_utils_DbConnector{
+
 	/**
-	 * @var ADOConnection
-	 */
-	protected $adoConnection = null;
-	
-	/**
-	 * The constructor initialize the connection.
-	 * It's a good way to test the connection by catching the exception
-	 * @param string $host
-	 * @param string $user
-	 * @param string $pass
-	 * @param string $driver
-	 * @throws tao_install_utils_Exception
-	 */
-	public function __construct( $host = 'localhost', $user = 'root', $pass = '', $driver = 'mysql', $dbName = "")
-	{
-		try{
-			$this->adoConnection = NewADOConnection($driver);
-			$this->adoConnection->Connect($host, $user, $pass);
-			
-			//if the database exists already, connect to it
-			if ($this->dbExists($dbName)){
-				$this->adoConnection->Close();
-				$this->adoConnection->Connect($host, $user, $pass, $dbName);
-			}
-		}
-		catch(ADODB_Exception $ae){
-			$this->adoConnection = null;
-			throw new tao_install_utils_Exception("Unable to connect to the database with the provided credentials.");
-		}
-		if($driver == 'mysql'){
-			$this->adoConnection->Execute('SET NAMES utf8');
-			// If the target Sgbd is mysql, force the engine to work with the standard identifier escape
-			$this->adoConnection->Execute('SET SESSION SQL_MODE="ANSI_QUOTES"');
-		}
-	}
-	
-	/**
-	 * Check if the database exists already
-	 * @param string $name
-	 */
-	public function dbExists($dbName)
-	{
-		$databases = $this->adoConnection->MetaDatabases();
-		if (in_array($dbName, $databases)){
-			return true;
-		}
-		return false;
-	}
-	
-	/**
-	 * Clean database by droping all tables
-	 * @param string $name
-	 */
-	public function cleanDb()
-	{
-		foreach ($this->adoConnection->MetaTables() as $table){
-			$this->execute('DROP TABLE "'.$table.'";');
-		}
-	}
-	
-	/**
-	 * Set up the database name
-	 * @param string $name
-	 * @throws tao_install_utils_Exception
-	 */
-	public function setDatabase($name)
-	{
-		if(!is_null($this->adoConnection)){
-			try{
-				$this->adoConnection->SelectDB($name);
-			}
-			catch(ADODB_Exception $ae){
-				throw new tao_install_utils_Exception("Unable to connect to the database $name");
-			}
-		}
-	}
-	
-	/**
-	 * Load an SQL file into the current database
-	 * Use it to load the database schema
+	 * Load a given SQL file containing simple statements.
+	 * SQL files containing Stored Procedure or Function declarations
+	 * are not supported. Use tao_install_utils_SpConnector instead.
+	 * 
 	 * @param string $file path to the SQL file
 	 * @param array repalce variable to replace into the file: array keys are search with {} around
 	 * @throws tao_install_utils_Exception
