ecd17094380ccd9cca0736e2366b4c6b66c2e0b0
jbogaerts <jbogaerts@62dfeb86-a93f-4914-9d13-15c0a160985d>
2012-04-12 08:08:16 +0000

    Created RDFUtils stub & model update.
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/tao/trunk@10594 62dfeb86-a93f-4914-9d13-15c0a160985d

---
+++ helpers/translation/class.RDFUtils.php
@@ -0,0 +1,69 @@
+<?php
+
+error_reporting(E_ALL);
+
+/**
+ * This class aims at providing utility methods for handling RDF files for
+ * and internationalization.
+ *
+ * @author Jerome Bogaerts
+ * @package tao
+ * @since 2.2
+ * @subpackage helpers_translation
+ * @version 1.0
+ */
+
+if (0 > version_compare(PHP_VERSION, '5')) {
+    die('This file was generated for PHP 5');
+}
+
+/* user defined includes */
+// section -64--88-56-1-52e480ce:136a58c45e6:-8000:00000000000038F7-includes begin
+// section -64--88-56-1-52e480ce:136a58c45e6:-8000:00000000000038F7-includes end
+
+/* user defined constants */
+// section -64--88-56-1-52e480ce:136a58c45e6:-8000:00000000000038F7-constants begin
+// section -64--88-56-1-52e480ce:136a58c45e6:-8000:00000000000038F7-constants end
+
+/**
+ * This class aims at providing utility methods for handling RDF files for
+ * and internationalization.
+ *
+ * @access public
+ * @author Jerome Bogaerts
+ * @package tao
+ * @since 2.2
+ * @subpackage helpers_translation
+ * @version 1.0
+ */
+class tao_helpers_translation_RDFUtils
+{
+    // --- ASSOCIATIONS ---
+
+
+    // --- ATTRIBUTES ---
+
+    // --- OPERATIONS ---
+
+    /**
+     * Creates a language description file for TAO using the RDF-XML language.
+     *
+     * @access public
+     * @author firstname and lastname of author, <author@example.org>
+     * @param  string code The language code e.g. fr-FR.
+     * @param  string label The language label e.g. French in english.
+     * @return DomDocument
+     */
+    public function createLanguageDescription($code, $label)
+    {
+        $returnValue = null;
+
+        // section -64--88-56-1-52e480ce:136a58c45e6:-8000:00000000000038FC begin
+        // section -64--88-56-1-52e480ce:136a58c45e6:-8000:00000000000038FC end
+
+        return $returnValue;
+    }
+
+} /* end of class tao_helpers_translation_RDFUtils */
+
+?>
