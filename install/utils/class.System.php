a39f4c4989c68b9f790e2bebf1c466c422708ae6
jbogaerts <jbogaerts@62dfeb86-a93f-4914-9d13-15c0a160985d>
2012-04-18 09:16:47 +0000

    Creation of a method to get available languages from the FS at install time + unit testing + test sample files.
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/tao/trunk@10759 62dfeb86-a93f-4914-9d13-15c0a160985d

--- install/utils/class.System.php
+++ install/utils/class.System.php
@@ -1,9 +1,17 @@
 <?php
+/**
+ * This class aims at providing utilities about the current installation from
+ * the host system and its filesystem, including the tao platform directory.
+ * 
+ * @author Somsack Sipasseuth <somsack.sipasseuth@tudor.lu>
+ * @author Jerome Bogaerts <jerome.bogaerts@tudor.lu>
+ */
 class tao_install_utils_System{
 	
 	/**
-	 * Get informations on the system
-	 * @return array
+	 * Get informations on the host system.
+     * 
+	 * @return array where key/values are 'folder' as string, 'host' as string, 'https' as boolean.
 	 */
 	public static function getInfos(){
                 
@@ -21,12 +29,61 @@ class tao_install_utils_System{
 	}
 	
 	/**
-	 * Check if TAO is already installed
+	 * Check if TAO is already installed.
+     * 
 	 * @return boolean
 	 */
 	public static function isTAOInstalled(){
 		$config = realpath(dirname(__FILE__).'/../../../generis/common/config.php');
 		return (file_exists($config));
 	}
+    
+    /**
+     * Returns the availables locales (languages or cultures) of the tao platform
+     * on the basis of a particular locale folder e.g.  the /locales folder of the tao
+     * meta-extension.
+     * 
+     * A locale will be included in the resulting array only if a valid 'lang.rdf'
+     * file is found.
+     * 
+     * @param string $path The location of the /locales folder to inspect.
+     * @return array An array of strings containing the folder names contained in the /locales folder of an extension.
+     * @throws UnexpectedValueException
+     */
+    public static function getAvailableLocales($path){
+        $locales = @scandir($path);
+        $returnValue = array();
+        
+        if ($locales !== false){
+            foreach ($locales as $l){
+                if ($l[0] !== '.'){
+                    // We found a locale folder. Does it contain a valid lang.rdf file?
+                    $langFilePath = $path . '/' . $l . '/lang.rdf';
+                    if (is_file($langFilePath) && is_readable($langFilePath)){
+                        try{
+                            $doc = new DOMDocument('1.0', 'UTF-8');
+                            $doc->load($langFilePath);
+                            $xpath = new DOMXPath($doc);
+                            $xpath->registerNamespace('rdf', 'http://www.w3.org/1999/02/22-rdf-syntax-ns#');
+                            
+                            // Look for an rdf:value equals to the folder name.
+                            $rdfValues = $xpath->query("//rdf:value");
+                            if ($rdfValues->length == 1 && $rdfValues->item(0)->nodeValue == $l){
+                                $returnValue[] = $l;
+                            }
+                        }
+                        catch (DOMException $e){
+                            // Invalid lang.rdf file, we continue to look for other ones.
+                            continue;
+                        }    
+                    }
+                }
+            }
+
+            return $returnValue;
+        }else{
+            throw new UnexpectedValueException("Unable to list locales in '${path}'.");
+        }
+    }
 }
 ?>
--- test/InstallTestCase.php
+++ test/InstallTestCase.php
@@ -9,6 +9,8 @@ include_once dirname(__FILE__) . '/../includes/raw_start.php';
  * @subpackage test
  */
 class InstallTestCase extends UnitTestCase {
+    
+    const SAMPLE_LOCALES = '/samples/locales';
 	
 	/**
 	 * Test the Installation Model Creator.
@@ -38,5 +40,19 @@ class InstallTestCase extends UnitTestCase {
         
         $this->assertTrue($enFound, "English language description not found for model '${taoNs}'.");
 	}
+
+    /**
+     * This test aims at testing the tao_install_utils_System class methods.
+     */
+    public function testSystemUtils(){
+        // - Check if tao platform locales can be correctly retrieved.
+        $locales = tao_install_utils_System::getAvailableLocales(dirname(__FILE__) . self::SAMPLE_LOCALES);
+        $this->assertTrue(is_array($locales), 'Locales should be returned as an array of strings.');
+        $this->assertTrue(in_array('EN', $locales), "Locale 'EN' should be found.");
+        $this->assertTrue(in_array('DE', $locales), "Locale 'DE' should be found.");
+        $this->assertTrue(in_array('FR', $locales), "Locale 'FR' should be found.");
+        $this->assertTrue(in_array('LU', $locales), "Locale 'LU' should be found.");
+        $this->assertTrue(in_array('SE', $locales), "Locale 'SE' should be found.");
+    }
 }
 ?>
