2f8cbdaa50f579d88218f2f3949e1f5f6c3ee111
jbogaerts <jbogaerts@62dfeb86-a93f-4914-9d13-15c0a160985d>
2012-04-19 13:00:09 +0000

    URIs are now generated as MODEL_URI . 'i' . [0-9]* with DataBaseSerialUriProvider.
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/tao/trunk@10804 62dfeb86-a93f-4914-9d13-15c0a160985d

--- install/db/tao_stored_procedures_mysql.sql
+++ install/db/tao_stored_procedures_mysql.sql
@@ -6,7 +6,7 @@ READS SQL DATA
 BEGIN
 	DECLARE uri VARCHAR(255);
 	INSERT INTO sequence_uri_provider (uri_sequence) VALUES ('');
-	SELECT CONCAT(modelUri, UNIX_TIMESTAMP(), FLOOR(RAND() * 10000), LAST_INSERT_ID()) INTO uri;
+	SELECT CONCAT(modelUri, 'i' , UNIX_TIMESTAMP(), FLOOR(RAND() * 10000), LAST_INSERT_ID()) INTO uri;
 	DELETE FROM sequence_uri_provider;
 	RETURN uri;
 END;
--- install/db/tao_stored_procedures_postgres.sql
+++ install/db/tao_stored_procedures_postgres.sql
@@ -10,7 +10,7 @@ DECLARE stamp int;
 BEGIN
 	INSERT INTO sequence_uri_provider DEFAULT VALUES;
 	SELECT INTO stamp extract(epoch FROM now())::int;
-	uri := modelUri || stamp || FLOOR(RANDOM() * 10000) || currval('sequence_uri_provider_uri_sequence_seq');
+	uri := modelUri || 'i' || stamp || FLOOR(RANDOM() * 10000) || currval('sequence_uri_provider_uri_sequence_seq');
 	RETURN uri;
 END
 $$;
