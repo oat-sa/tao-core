dd2d81fc9a4f968e49e147c75f87bc83500e03d2
jbogaerts <jbogaerts@62dfeb86-a93f-4914-9d13-15c0a160985d>
2012-04-20 12:12:03 +0000

    tao::helpers_translation_Annotable interface + implementations (stubs)
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/tao/trunk@10830 62dfeb86-a93f-4914-9d13-15c0a160985d

--- helpers/translation/class.TranslationFile.php
+++ helpers/translation/class.TranslationFile.php
@@ -21,6 +21,13 @@ if (0 > version_compare(PHP_VERSION, '5')) {
     die('This file was generated for PHP 5');
 }
 
+/**
+ * Any Object that claims to be annotable should implement this interface.
+ *
+ * @author Jerome Bogaerts, <jerome.bogaerts@tudor.lu>
+ */
+require_once('tao/helpers/translation/interface.Annotable.php');
+
 /* user defined includes */
 // section 10-13-1-85-72d0ca97:1331b62f595:-8000:000000000000348D-includes begin
 // section 10-13-1-85-72d0ca97:1331b62f595:-8000:000000000000348D-includes end
@@ -45,6 +52,7 @@ tao_model_classes_TranslationFileWriter
  * @version 1.0
  */
 class tao_helpers_translation_TranslationFile
+        implements tao_helpers_translation_Annotable
 {
     // --- ASSOCIATIONS ---
 
@@ -107,7 +115,76 @@ class tao_helpers_translation_TranslationFile
      */
     const SORT_DESC_I = 4;
 
+    /**
+     * The annotations bound to this translation file.
+     *
+     * @access private
+     * @var array
+     */
+    private $annotations = array();
+
     // --- OPERATIONS ---
+
+    /**
+     * Sets the collection of annotations bound to this Translation Object.
+     *
+     * @access public
+     * @author Jerome Bogaerts, <jerome.bogaerts@tudor.lu>
+     * @param  array annotations An associative array of annotations where keys are the annotation names and values are annotation values.
+     * @return void
+     */
+    public function setAnnotations($annotations)
+    {
+        // section -64--88-56-1--5deb8f54:136cf746d4c:-8000:0000000000003948 begin
+        // section -64--88-56-1--5deb8f54:136cf746d4c:-8000:0000000000003948 end
+    }
+
+    /**
+     * Returns an associative array that represents a collection of annotations
+     * keys are annotation names and values annotation values.
+     *
+     * @access public
+     * @author Jerome Bogaerts, <jerome.bogaerts@tudor.lu>
+     * @return array
+     */
+    public function getAnnotations()
+    {
+        $returnValue = array();
+
+        // section -64--88-56-1--5deb8f54:136cf746d4c:-8000:0000000000003952 begin
+        // section -64--88-56-1--5deb8f54:136cf746d4c:-8000:0000000000003952 end
+
+        return (array) $returnValue;
+    }
+
+    /**
+     * Adds an annotation with a given name and value.
+     *
+     * @access public
+     * @author Jerome Bogaerts, <jerome.bogaerts@tudor.lu>
+     * @param  string name The name of the annotation to add.
+     * @param  string value The value of the annotation to add.
+     * @return void
+     */
+    public function addAnnotation($name, $value)
+    {
+        // section -64--88-56-1--5deb8f54:136cf746d4c:-8000:0000000000003955 begin
+        // section -64--88-56-1--5deb8f54:136cf746d4c:-8000:0000000000003955 end
+    }
+
+    /**
+     * Removes an annotation for a given annotation name.
+     *
+     * @access public
+     * @author Jerome Bogaerts, <jerome.bogaerts@tudor.lu>
+     * @param  string name The name of the annotation to remove.
+     * @return void
+     */
+    public function removeAnnotation($name)
+    {
+        // section -64--88-56-1--5deb8f54:136cf746d4c:-8000:000000000000395C begin
+        // section -64--88-56-1--5deb8f54:136cf746d4c:-8000:000000000000395C end
+    }
 
     /**
      * Creates a new instance of TranslationFile for a specific source and
---
+++ helpers/translation/interface.Annotable.php
@@ -0,0 +1,82 @@
+<?php
+
+error_reporting(E_ALL);
+
+/**
+ * Any Object that claims to be annotable should implement this interface.
+ *
+ * @author Jerome Bogaerts, <jerome.bogaerts@tudor.lu>
+ * @package tao
+ * @subpackage helpers_translation
+ */
+
+if (0 > version_compare(PHP_VERSION, '5')) {
+    die('This file was generated for PHP 5');
+}
+
+/* user defined includes */
+// section -64--88-56-1--5deb8f54:136cf746d4c:-8000:0000000000003947-includes begin
+// section -64--88-56-1--5deb8f54:136cf746d4c:-8000:0000000000003947-includes end
+
+/* user defined constants */
+// section -64--88-56-1--5deb8f54:136cf746d4c:-8000:0000000000003947-constants begin
+// section -64--88-56-1--5deb8f54:136cf746d4c:-8000:0000000000003947-constants end
+
+/**
+ * Any Object that claims to be annotable should implement this interface.
+ *
+ * @access public
+ * @author Jerome Bogaerts, <jerome.bogaerts@tudor.lu>
+ * @package tao
+ * @subpackage helpers_translation
+ */
+interface tao_helpers_translation_Annotable
+{
+
+
+    // --- OPERATIONS ---
+
+    /**
+     * Sets the collection of annotations bound to this Translation Object.
+     *
+     * @access public
+     * @author Jerome Bogaerts, <jerome.bogaerts@tudor.lu>
+     * @param  array annotations An associative array of annotations where keys are the annotation names and values are annotation values.
+     * @return void
+     */
+    public function setAnnotations($annotations);
+
+    /**
+     * Returns an associative array that represents a collection of annotations
+     * keys are annotation names and values annotation values.
+     *
+     * @access public
+     * @author Jerome Bogaerts, <jerome.bogaerts@tudor.lu>
+     * @return array
+     */
+    public function getAnnotations();
+
+    /**
+     * Adds an annotation with a given name and value.
+     *
+     * @access public
+     * @author Jerome Bogaerts, <jerome.bogaerts@tudor.lu>
+     * @param  string name The name of the annotation to add.
+     * @param  string value The value of the annotation to add.
+     * @return void
+     */
+    public function addAnnotation($name, $value);
+
+    /**
+     * Removes an annotation for a given annotation name.
+     *
+     * @access public
+     * @author Jerome Bogaerts, <jerome.bogaerts@tudor.lu>
+     * @param  string name The name of the annotation to remove.
+     * @return void
+     */
+    public function removeAnnotation($name);
+
+} /* end of interface tao_helpers_translation_Annotable */
+
+?>
