64a6075d9c762fb6bef424736329fe1e59a0d59b
jbogaerts <jbogaerts@62dfeb86-a93f-4914-9d13-15c0a160985d>
2012-05-30 14:09:45 +0000

    Script to update languages instances for 2.1 -> 2.2
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/tao/trunk@11306 62dfeb86-a93f-4914-9d13-15c0a160985d

---
+++ update/scripts/2.2/08_tao_languages.php
@@ -0,0 +1,26 @@
+<?php
+core_control_FrontController::connect(SYS_USER_LOGIN, SYS_USER_PASS, DATABASE_NAME);
+$session = core_kernel_classes_Session::singleton();
+$tmpSessionLg = $session->lg;
+$session->setLg($session->defaultLg);
+
+$langClass = new core_kernel_classes_Class(CLASS_LANGUAGES);
+$langInstances = $langClass->getInstances();
+$valueProperty = new core_kernel_classes_Property(RDF_VALUE);
+
+foreach ($langInstances as $lang){
+    $tmpCode = trim($lang->getLabel());
+    $tmpName = trim($lang->getComment());
+    $lang->delete();
+    
+    $newLang = core_kernel_classes_ClassFactory::createInstance($langClass,
+                                                                $tmpName,
+                                                                $tmpName,
+                                                                'http://www.tao.lu/Ontologies/TAO.rdf#Lang' . $tmpCode);
+    $newLang->setPropertyValue($valueProperty, $tmpCode);
+}
+
+
+// Rollback session language.
+$session->setLg($tmpSessionLg);
+?>
