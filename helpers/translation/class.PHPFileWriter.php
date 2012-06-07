d6c5ada87524b4c644c10dc3790b58a95d855846
jbogaerts <jbogaerts@62dfeb86-a93f-4914-9d13-15c0a160985d>
2012-06-07 11:04:21 +0000

    added tao::helpers::translation::PHPFileWriter to compile TranslationFiles as PHP compiled files.
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/tao/trunk@11393 62dfeb86-a93f-4914-9d13-15c0a160985d

---
+++ helpers/translation/class.PHPFileWriter.php
@@ -0,0 +1,69 @@
+<?php
+
+error_reporting(E_ALL);
+
+/**
+ * A Translation File Writer that produces compiled PHP files.
+ *
+ * @author Jerome Bogaerts, <jerome.bogaerts@tudor.lu>
+ * @package tao
+ * @subpackage helpers_translation
+ */
+
+if (0 > version_compare(PHP_VERSION, '5')) {
+    die('This file was generated for PHP 5');
+}
+
+/**
+ * A Writing class for TranslationFiles. Must be implemented by a concrete class
+ * a given Translation Format such as XLIFF, PO, ... The write method must be
+ * by subclasses.
+ *
+ * @author Jerome Bogaerts
+ * @since 2.2
+ * @version 1.0
+ */
+require_once('tao/helpers/translation/class.TranslationFileWriter.php');
+
+/* user defined includes */
+// section -64--88-56-1--3f1036:137c6806719:-8000:0000000000003B11-includes begin
+// section -64--88-56-1--3f1036:137c6806719:-8000:0000000000003B11-includes end
+
+/* user defined constants */
+// section -64--88-56-1--3f1036:137c6806719:-8000:0000000000003B11-constants begin
+// section -64--88-56-1--3f1036:137c6806719:-8000:0000000000003B11-constants end
+
+/**
+ * A Translation File Writer that produces compiled PHP files.
+ *
+ * @access public
+ * @author Jerome Bogaerts, <jerome.bogaerts@tudor.lu>
+ * @package tao
+ * @subpackage helpers_translation
+ */
+class tao_helpers_translation_PHPFileWriter
+    extends tao_helpers_translation_TranslationFileWriter
+{
+    // --- ASSOCIATIONS ---
+
+
+    // --- ATTRIBUTES ---
+
+    // --- OPERATIONS ---
+
+    /**
+     * Writes the TranslationFile as a PHP compiled file.
+     *
+     * @access public
+     * @author Jerome Bogaerts, <jerome.bogaerts@tudor.lu>
+     * @return mixed
+     */
+    public function write()
+    {
+        // section -64--88-56-1--3f1036:137c6806719:-8000:0000000000003B13 begin
+        // section -64--88-56-1--3f1036:137c6806719:-8000:0000000000003B13 end
+    }
+
+} /* end of class tao_helpers_translation_PHPFileWriter */
+
+?>
