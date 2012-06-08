544a6ba738db6c209506c6e75f9989147e948927
llecaque <llecaque@62dfeb86-a93f-4914-9d13-15c0a160985d>
2012-06-08 14:49:16 +0000

    add update script
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/tao/trunk@11427 62dfeb86-a93f-4914-9d13-15c0a160985d

---
+++ update/scripts/2.2/99_update_version.sql
@@ -0,0 +1,10 @@
+UPDATE extensions SET version = '2.2' WHERE id = 'tao';
+UPDATE extensions SET version = '2.2' WHERE id = 'taoItems';
+UPDATE extensions SET version = '2.2' WHERE id = 'taoGroups';
+UPDATE extensions SET version = '2.2' WHERE id = 'taoResults';
+UPDATE extensions SET version = '2.2' WHERE id = 'taoSubjects';
+UPDATE extensions SET version = '2.2' WHERE id = 'taoTests';
+UPDATE extensions SET version = '2.2' WHERE id = 'taoCoding';
+UPDATE extensions SET version = '2.2' WHERE id = 'wfEngine';
+UPDATE extensions SET version = '2.2' WHERE id = 'taoDelivery';
+UPDATE extensions SET version = '2.2' WHERE id = 'generis';
