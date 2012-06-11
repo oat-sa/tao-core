ca44c9963105de22be8c1811189dcea2a0acf1db
jbout <jbout@62dfeb86-a93f-4914-9d13-15c0a160985d>
2012-06-11 14:09:08 +0000

    tao_version to 2.2-rc01 release state to 'beta'
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/tao/trunk@11446 62dfeb86-a93f-4914-9d13-15c0a160985d

--- includes/constants.php
+++ includes/constants.php
@@ -6,16 +6,16 @@
  *
  */
 #TAO version number
-define('TAO_VERSION', '2.2-beta2');
+define('TAO_VERSION', '2.2-rc01');
 
 #TAO version label
-define('TAO_VERSION_NAME', 'v2.2-beta2');
+define('TAO_VERSION_NAME', 'v2.2-rc01');
 
 #the name to display
 define('PRODUCT_NAME', 'TAO');
 
 #TAO release status, use to add specific footer to TAO, available alpha, beta, demo, stable
-define('TAO_RELEASE_STATUS', 'demoB');
+define('TAO_RELEASE_STATUS', 'beta');
 
 #the temporary suffix of property URIs at CSV import.
 define('TEMP_SUFFIX_CSV', '-taocsvdef');
