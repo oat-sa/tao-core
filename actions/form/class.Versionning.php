0ef2f9192292bc25f11c02f707dc0d3f7876bc04
jbihin <jbihin@62dfeb86-a93f-4914-9d13-15c0a160985d>
2012-07-03 09:43:54 +0000

    improve the form
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/tao/trunk@11667 62dfeb86-a93f-4914-9d13-15c0a160985d

--- actions/form/class.Versionning.php
+++ actions/form/class.Versionning.php
@@ -66,6 +66,16 @@ class tao_actions_form_Versionning
     {
         // section 127-0-1-1-35689176:13832da9f32:-8000:0000000000003B3C begin
 		$this->form = tao_helpers_form_FormFactory::getForm('settings');
+
+		$this->form->setDecorators(array(
+			'element'			=> new tao_helpers_form_xhtml_TagWrapper(array('tag' => 'div')),
+			'group'				=> new tao_helpers_form_xhtml_TagWrapper(array('tag' => 'div', 'cssClass' => 'form-group')),
+			'error'				=> new tao_helpers_form_xhtml_TagWrapper(array('tag' => 'div', 'cssClass' => 'form-error ui-state-error ui-corner-all')),
+			'help'				=> new tao_helpers_form_xhtml_TagWrapper(array('tag' => 'span','cssClass' => 'form-help')),
+			'actions-bottom'	=> new tao_helpers_form_xhtml_TagWrapper(array('tag' => 'div', 'cssClass' => 'form-toolbar')),
+			'actions-top'		=> new tao_helpers_form_xhtml_TagWrapper(array('tag' => 'div', 'cssClass' => 'form-toolbar'))
+		));
+
 		$actions = tao_helpers_form_FormFactory::getCommonActions('top');
 		$this->form->setActions($actions, 'top');
         // section 127-0-1-1-35689176:13832da9f32:-8000:0000000000003B3C end
@@ -83,11 +93,13 @@ class tao_actions_form_Versionning
         // section 127-0-1-1-35689176:13832da9f32:-8000:0000000000003B3E begin
 		$loginElt = tao_helpers_form_FormFactory::getElement('login', 'Textbox');
 		$loginElt->setDescription(__('Login'));
+		$loginElt->setValue(GENERIS_VERSIONED_REPOSITORY_LOGIN);
 		$loginElt->addValidator(tao_helpers_form_FormFactory::getValidator('NotEmpty'));
 		$this->form->addElement($loginElt);
 
 		$passElt = tao_helpers_form_FormFactory::getElement('password', 'Hiddenbox');
 		$passElt->setDescription(__('Password'));
+		$passElt->setValue(GENERIS_VERSIONED_REPOSITORY_PASSWORD);
 		$passElt->addValidator(tao_helpers_form_FormFactory::getValidator('NotEmpty'));
 		$this->form->addElement($passElt);
 
@@ -100,6 +112,7 @@ class tao_actions_form_Versionning
 
 		$elt = tao_helpers_form_FormFactory::getElement('url', 'Textbox');
 		$elt->setDescription(__('URL'));
+		$elt->setValue(GENERIS_VERSIONED_REPOSITORY_URL);
 		$elt->setHelp('http://mydomain/svn');
 		$elt->addValidator(tao_helpers_form_FormFactory::getValidator('NotEmpty'));
 		$this->form->addElement($elt);
@@ -107,6 +120,7 @@ class tao_actions_form_Versionning
 
 		$elt = tao_helpers_form_FormFactory::getElement('path', 'Textbox');
 		$elt->setDescription(__('Path'));
+		$elt->setValue(GENERIS_VERSIONED_REPOSITORY_PATH);
 		$elt->setHelp(GENERIS_FILES_PATH.'versionning/DEFAULT');
 		$elt->addValidator(tao_helpers_form_FormFactory::getValidator('NotEmpty'));
 		$this->form->addElement($elt);
