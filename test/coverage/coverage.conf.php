0ef05cf462b9ec1072c93d883320c7876e388170
llecaque <llecaque@62dfeb86-a93f-4914-9d13-15c0a160985d>
2012-07-17 15:39:08 +0000

    add code coverage
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/tao/trunk@11728 62dfeb86-a93f-4914-9d13-15c0a160985d

---
+++ test/coverage/coverage.conf.php
@@ -0,0 +1,3 @@
+<?php
+define("PHPCOVERAGE_HOME", INCLUDES_PATH.'/spikephpcoverage/src/');
+define('PHPCOVERAGE_REPORTS',PHPCOVERAGE_HOME.'/../reports/');
---
+++ test/coverage/generateCoverage.php
@@ -0,0 +1,35 @@
+<?php
+require_once dirname(__FILE__) . '/../TestRunner.php';
+require_once  PHPCOVERAGE_HOME. "/CoverageRecorder.php";
+require_once PHPCOVERAGE_HOME . "/reporter/HtmlCoverageReporter.php";
+
+
+//get the test into each extensions
+$tests = TestRunner::getTests(array('tao'));
+
+//create the test sutie
+$testSuite = new TestSuite('TAO extensions tests');
+foreach($tests as $testCase){
+	$testSuite->addFile($testCase);
+}
+
+//add the reporter regarding the context
+if(PHP_SAPI == 'cli'){
+	$reporter = new XmlTimeReporter();
+}
+else{
+	$reporter = new HtmlReporter();
+}
+
+$includePaths = array(ROOT_PATH.'tao/actions', ROOT_PATH.'tao/models',ROOT_PATH.'tao/helpers');
+$excludePaths = array();
+$covReporter = new HtmlCoverageReporter("Code Coverage Report TAO", "", PHPCOVERAGE_REPORTS."tao/");
+$cov = new CoverageRecorder($includePaths, $excludePaths, $covReporter);
+//run the unit test suite
+$cov->startInstrumentation();
+$testSuite->run($reporter);
+$cov->stopInstrumentation();
+
+$cov->generateReport();
+$covReporter->printTextSummary(PHPCOVERAGE_REPORTS.'tao_coverage.txt');
+?>
