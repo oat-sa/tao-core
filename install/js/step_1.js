ee94a4ccb496b617775fca1c401149ad57cc48ea
jbogaerts <jbogaerts@62dfeb86-a93f-4914-9d13-15c0a160985d>
2012-08-29 12:19:54 +0000

    Sync is done each configuration check.
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/tao/trunk@11977 62dfeb86-a93f-4914-9d13-15c0a160985d

--- install/js/step_1.js
+++ install/js/step_1.js
@@ -37,6 +37,10 @@ function onLoad(){
     // Feed install API help store.
     initHelp();
 
+	checkConfig();
+}
+
+function checkConfig(){
 	// Launch the configuration check procedure only if we can talk JSON
 	// with the server side.
 	install.sync(function(status, data){
@@ -44,7 +48,96 @@ function onLoad(){
 			// Save useful information.
 			install.addData('root_url', data.value.rootURL);
 			install.addData('available_drivers', data.value.availableDrivers);
-			checkConfig();	
+			
+			// Remove captions.
+			$('#explOk, #explMandatory, #explOptional').css('display', 'none');
+			
+			// Empty existing reports.
+			var $list = $('#forms_check_content ul');
+			$list.find('tao-input').each(function(){
+				install.unregister(this);
+			});
+			$list.empty();
+			
+			// set a spinner up.
+			
+			var $target = $('<li id="loadingCheck"><label>Checking configuration. Please wait...</label></li>');
+			$('#forms_check_content ul').prepend($target);
+			var spinner = new Spinner(getSpinnerOptions('small')).spin($target[0]);
+			
+			setTimeout(function(){ // Fake a small processing time... -> 500ms
+				var data = [{type: "CheckPHPRuntime", value: {min: "5.3", max: "5.3.13", optional:false}},
+							{type: "CheckPHPExtension", value: {name: "curl", optional: false}},
+			                {type: "CheckPHPExtension", value: {name: "zip", optional: false}},
+			                {type: "CheckPHPExtension", value: {name: "json", optional: false}},
+			                {type: "CheckPHPExtension", value: {name: "spl", optional: false}},
+			                {type: "CheckPHPExtension", value: {name: "dom", optional: false}},
+			                {type: "CheckPHPExtension", value: {name: "mbstring", optional: false}},
+			                {type: "CheckPHPExtension", value: {name: "svn", optional: true}},
+			                {type: "CheckPHPExtension", value: {name: "suhosin", optional: true}},
+			                {type: "CheckPHPINIValue", value: {name: "magic_quotes_gpc", value: "0", optional: false}},
+			                {type: "CheckPHPINIValue", value: {name: "short_open_tag", value: "1", optional: false}},
+			                {type: "CheckPHPINIValue", value: {name: "register_globals", value: "0", optional: false}},
+			                {type: "CheckFileSystemComponent", value: {location: ".", rights: "rw", name: "fs_root", optional: false}},
+			                {type: "CheckFileSystemComponent", value: {location: "generis/data/cache", rights: "rw", name: "fs_generis_data_cache", optional: false}},
+			                {type: "CheckFileSystemComponent", value: {location: "generis/data/versioning", rights: "rw", name: "fs_generis_data_versionning", optional: false}},
+			                {type: "CheckFileSystemComponent", value: {location: "generis/common", rights: "rw", name: "fs_generis_common", optional: false}},
+			                {type: "CheckFileSystemComponent", value: {location: "generis/common/conf", rights: "rw", name: "fs_generis_common_conf", optional: false}},
+			                {type: "CheckFileSystemComponent", value: {location: "generis/common/conf/default", rights: "r", name: "fs_generis_common_conf_default", optional: false}},
+			                {type: "CheckFileSystemComponent", value: {location: "generis/common/conf/sample", rights: "r", name: "fs_generis_common_conf_sample", optional: false}},
+			                {type: "CheckFileSystemComponent", value: {location: "filemanager/views/data", rights: "rw", name: "fs_filemanager_views_data", optional: false}},
+			                {type: "CheckFileSystemComponent", value: {location: "filemanager/includes", rights: "rw", name: "fs_filemanager_includes", optional: false}},
+			                {type: "CheckFileSystemComponent", value: {location: "tao/views/export", rights: "rw", name: "fs_tao_views_export", optional: false}},
+			                {type: "CheckFileSystemComponent", value: {location: "tao/includes", rights: "rw", name: "fs_tao_includes", optional: false}},
+			                {type: "CheckFileSystemComponent", value: {location: "tao/data/cache", rights: "rw", name: "fs_tao_data_cache", optional: false}},
+			                {type: "CheckFileSystemComponent", value: {location: "tao/update/patches", rights: "rw", name: "fs_tao_update_patches", optional: false}},
+			                {type: "CheckFileSystemComponent", value: {location: "tao/update/bash", rights: "rw", name: "fs_tao_update_bash", optional: false}},
+			                {type: "CheckFileSystemComponent", value: {location: "taoItems/data", rights: "rw", name: "fs_taoItems_data", optional: false}},
+			                {type: "CheckFileSystemComponent", value: {location: "taoItems/views/export", rights: "rw", name: "fs_taoItems_views_export", optional: false}},
+			                {type: "CheckFileSystemComponent", value: {location: "taoItems/includes", rights: "rw", name: "fs_taoItems_includes", optional: false}},
+			                {type: "CheckFileSystemComponent", value: {location: "taoDelivery/compiled", rights: "rw", name: "fs_taoDelivery_compiled", optional: false}},
+			                {type: "CheckFileSystemComponent", value: {location: "taoDelivery/views/export", rights: "rw", name: "fs_taoDelivery_views_export", optional: false}},
+			                {type: "CheckFileSystemComponent", value: {location: "taoDelivery/includes", rights: "rw", name: "fs_taoDelivery_includes", optional: false}},
+			                {type: "CheckFileSystemComponent", value: {location: "taoGroups/views/export", rights: "rw", name: "fs_taoGroups_views_export", optional: false}},
+			                {type: "CheckFileSystemComponent", value: {location: "taoGroups/includes", rights: "rw", name: "fs_taoGroups_includes", optional: false}},
+			                {type: "CheckFileSystemComponent", value: {location: "taoSubjects/views/export", rights: "rw", name: "fs_taoSubjects_views_export", optional: false}},
+			                {type: "CheckFileSystemComponent", value: {location: "taoSubjects/includes", rights: "rw", name: "fs_taoSubjects_includes", optional: false}},
+			                {type: "CheckFileSystemComponent", value: {location: "taoTests/views/export", rights: "rw", name: "fs_taoTests_views_export", optional: false}},
+			                {type: "CheckFileSystemComponent", value: {location: "taoTests/includes", rights: "rw", name: "fs_taoTests_includes", optional: false}},
+			                {type: "CheckFileSystemComponent", value: {location: "taoResults/views/export", rights: "w", name: "fs_taoResults_views_export", optional: false}},
+			                {type: "CheckFileSystemComponent", value: {location: "wfEngine/includes", rights: "w", name: "fs_wfEngine_includes", optional: false}},
+			                {type: "CheckCustom", value: {name: "mod_rewrite", extension: "tao", optional: false}},
+			                {type: "CheckCustom", value: {name: "database_drivers", extension: "tao", optional: false}}];
+			    
+				install.checkConfiguration(data, function(status, data){
+					if (status == 200){
+						var $list = $('#forms_check_content ul');
+						
+						// Stop spinner.
+						spinner.stop();
+						$list.empty();
+		
+						// Append new reports.
+						var mandatoryCount = 0;
+				    	for (report in data.value){
+				    		var r = data.value[report];
+				    		if (r.value.status != 'valid'){
+				    			var kind = (r.value.optional == true) ? 'optional' : 'mandatory';
+				    			mandatoryCount += (r.value.optional == true) ? 0 : 1;
+				    			addReport(r.value.name, r.value.message, kind);
+				    		}
+				    	}
+				    	
+				    	if (mandatoryCount == 0){
+				    		// Add a fake report that states the config is OK.
+				    		addReport('tao_config_ok', 'Your system is ready to deploy TAO.', 'ok', true, true);
+				    	}
+				    	
+				    	install.stateChange();
+					}
+			    });
+			}, 500);
+			
 		}
 		else {
 			// We cannot exchange data with the server side.
@@ -52,97 +145,6 @@ function onLoad(){
 			addReport('json', msg, false);
 		}
 	});
-}
-
-function checkConfig(){
-	// Remove captions.
-	$('#explOk, #explMandatory, #explOptional').css('display', 'none');
-	
-	// Empty existing reports.
-	var $list = $('#forms_check_content ul');
-	$list.find('tao-input').each(function(){
-		install.unregister(this);
-	});
-	$list.empty();
-	
-	// set a spinner up.
-	
-	var $target = $('<li id="loadingCheck"><label>Checking configuration. Please wait...</label></li>');
-	$('#forms_check_content ul').prepend($target);
-	var spinner = new Spinner(getSpinnerOptions('small')).spin($target[0]);
-	
-	setTimeout(function(){ // Fake a small processing time... -> 500ms
-		var data = [{type: "CheckPHPRuntime", value: {min: "5.3", max: "5.3.13", optional:false}},
-					{type: "CheckPHPExtension", value: {name: "curl", optional: false}},
-	                {type: "CheckPHPExtension", value: {name: "zip", optional: false}},
-	                {type: "CheckPHPExtension", value: {name: "json", optional: false}},
-	                {type: "CheckPHPExtension", value: {name: "spl", optional: false}},
-	                {type: "CheckPHPExtension", value: {name: "dom", optional: false}},
-	                {type: "CheckPHPExtension", value: {name: "mbstring", optional: false}},
-	                {type: "CheckPHPExtension", value: {name: "svn", optional: true}},
-	                {type: "CheckPHPExtension", value: {name: "suhosin", optional: true}},
-	                {type: "CheckPHPINIValue", value: {name: "magic_quotes_gpc", value: "0", optional: false}},
-	                {type: "CheckPHPINIValue", value: {name: "short_open_tag", value: "1", optional: false}},
-	                {type: "CheckPHPINIValue", value: {name: "register_globals", value: "0", optional: false}},
-	                {type: "CheckFileSystemComponent", value: {location: ".", rights: "rw", name: "fs_root", optional: false}},
-	                {type: "CheckFileSystemComponent", value: {location: "generis/data/cache", rights: "rw", name: "fs_generis_data_cache", optional: false}},
-	                {type: "CheckFileSystemComponent", value: {location: "generis/data/versioning", rights: "rw", name: "fs_generis_data_versionning", optional: false}},
-	                {type: "CheckFileSystemComponent", value: {location: "generis/common", rights: "rw", name: "fs_generis_common", optional: false}},
-	                {type: "CheckFileSystemComponent", value: {location: "generis/common/conf", rights: "rw", name: "fs_generis_common_conf", optional: false}},
-	                {type: "CheckFileSystemComponent", value: {location: "generis/common/conf/default", rights: "r", name: "fs_generis_common_conf_default", optional: false}},
-	                {type: "CheckFileSystemComponent", value: {location: "generis/common/conf/sample", rights: "r", name: "fs_generis_common_conf_sample", optional: false}},
-	                {type: "CheckFileSystemComponent", value: {location: "filemanager/views/data", rights: "rw", name: "fs_filemanager_views_data", optional: false}},
-	                {type: "CheckFileSystemComponent", value: {location: "filemanager/includes", rights: "rw", name: "fs_filemanager_includes", optional: false}},
-	                {type: "CheckFileSystemComponent", value: {location: "tao/views/export", rights: "rw", name: "fs_tao_views_export", optional: false}},
-	                {type: "CheckFileSystemComponent", value: {location: "tao/includes", rights: "rw", name: "fs_tao_includes", optional: false}},
-	                {type: "CheckFileSystemComponent", value: {location: "tao/data/cache", rights: "rw", name: "fs_tao_data_cache", optional: false}},
-	                {type: "CheckFileSystemComponent", value: {location: "tao/update/patches", rights: "rw", name: "fs_tao_update_patches", optional: false}},
-	                {type: "CheckFileSystemComponent", value: {location: "tao/update/bash", rights: "rw", name: "fs_tao_update_bash", optional: false}},
-	                {type: "CheckFileSystemComponent", value: {location: "taoItems/data", rights: "rw", name: "fs_taoItems_data", optional: false}},
-	                {type: "CheckFileSystemComponent", value: {location: "taoItems/views/export", rights: "rw", name: "fs_taoItems_views_export", optional: false}},
-	                {type: "CheckFileSystemComponent", value: {location: "taoItems/includes", rights: "rw", name: "fs_taoItems_includes", optional: false}},
-	                {type: "CheckFileSystemComponent", value: {location: "taoDelivery/compiled", rights: "rw", name: "fs_taoDelivery_compiled", optional: false}},
-	                {type: "CheckFileSystemComponent", value: {location: "taoDelivery/views/export", rights: "rw", name: "fs_taoDelivery_views_export", optional: false}},
-	                {type: "CheckFileSystemComponent", value: {location: "taoDelivery/includes", rights: "rw", name: "fs_taoDelivery_includes", optional: false}},
-	                {type: "CheckFileSystemComponent", value: {location: "taoGroups/views/export", rights: "rw", name: "fs_taoGroups_views_export", optional: false}},
-	                {type: "CheckFileSystemComponent", value: {location: "taoGroups/includes", rights: "rw", name: "fs_taoGroups_includes", optional: false}},
-	                {type: "CheckFileSystemComponent", value: {location: "taoSubjects/views/export", rights: "rw", name: "fs_taoSubjects_views_export", optional: false}},
-	                {type: "CheckFileSystemComponent", value: {location: "taoSubjects/includes", rights: "rw", name: "fs_taoSubjects_includes", optional: false}},
-	                {type: "CheckFileSystemComponent", value: {location: "taoTests/views/export", rights: "rw", name: "fs_taoTests_views_export", optional: false}},
-	                {type: "CheckFileSystemComponent", value: {location: "taoTests/includes", rights: "rw", name: "fs_taoTests_includes", optional: false}},
-	                {type: "CheckFileSystemComponent", value: {location: "taoResults/views/export", rights: "w", name: "fs_taoResults_views_export", optional: false}},
-	                {type: "CheckFileSystemComponent", value: {location: "wfEngine/includes", rights: "w", name: "fs_wfEngine_includes", optional: false}},
-	                {type: "CheckCustom", value: {name: "mod_rewrite", extension: "tao", optional: false}},
-	                {type: "CheckCustom", value: {name: "database_drivers", extension: "tao", optional: false}}];
-	    
-		install.checkConfiguration(data, function(status, data){
-			if (status == 200){
-				var $list = $('#forms_check_content ul');
-				
-				// Stop spinner.
-				spinner.stop();
-				$list.empty();
-
-				// Append new reports.
-				var mandatoryCount = 0;
-		    	for (report in data.value){
-		    		var r = data.value[report];
-		    		if (r.value.status != 'valid'){
-		    			var kind = (r.value.optional == true) ? 'optional' : 'mandatory';
-		    			mandatoryCount += (r.value.optional == true) ? 0 : 1;
-		    			addReport(r.value.name, r.value.message, kind);
-		    		}
-		    	}
-		    	
-		    	if (mandatoryCount == 0){
-		    		// Add a fake report that states the config is OK.
-		    		addReport('tao_config_ok', 'Your system is ready to deploy TAO.', 'ok', true, true);
-		    	}
-		    	
-		    	install.stateChange();
-			}
-	    });
-	}, 500);
 }
 
 function addReport(name, message, kind, prepend, noHelp){
