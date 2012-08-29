2174e0d87338ad58cc53ced9981da55a3496c6cf
jbogaerts <jbogaerts@62dfeb86-a93f-4914-9d13-15c0a160985d>
2012-08-29 10:09:49 +0000

    styling update.
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/tao/trunk@11970 62dfeb86-a93f-4914-9d13-15c0a160985d

--- install/js/help.js
+++ install/js/help.js
@@ -3,10 +3,35 @@ $(document).ready(function(){
 	$(".ui-icon-help").bind("click", displayTaoHelp);
 	// display help inside inout fields
 	$("input:text, textarea").labelify({labelledClass: "helpTaoInputLabel"});
+
+	// feedback popup show/hide
+	$("#suppportTab").bind("click",openSupportTab);
+
+	$("#supportPopupClose").bind("click",function(){
+		$("#mainSupportPopup").hide();
+		$("#screenShield").hide();
+		$("#mainSupportPopup").find("#supportFrameId").attr("src","supportFrameIndex.html");
+	});
+
+	var popupDocConcext=parent.document
+
+	$("#genericPopupClose").bind("click",function(){
+		$("#mainGenericPopup").hide();
+		$("#screenShield").hide();
+	});
+
+	function openSupportTab(){
+		$("#mainSupportPopup").show();
+		$("#screenShield").show();
+		$("#mainSupportPopup").find("#supportFrameId").attr("src","supportFrameIndex.html");
+	}
 });
 
 
 function displayTaoHelp(event){
+
+	console.log("ok");
+
 	var inputId = $(event.currentTarget).attr('id');
 	var msg = 'No help for input <strong>' + inputId + '</strong>.';
 
@@ -14,15 +39,12 @@ function displayTaoHelp(event){
 		msg = storeMsg;
 	}
 
-	$("#helpPopUp, .ui-overlay").remove();
+	var popupDocConcext=parent.document
 
-	$("body").append('<div class="ui-overlay"><div class="ui-widget-overlay  ui-corner-all"></div></div>');
-	$("body").append('<div id="helpPopUp" class="ui-widget ui-widget-content ui-corner-all"><div id="helpPopUpClose" title="Close the Help Topic">X</div><h4>Help</h4><p id="helpPopupContent">' + msg + '</p></div>');
-	
-	$("#helpPopUpClose").bind("click",function(){
-		$(".ui-overlay").remove();
-		$("#helpPopUp").remove();
-	});
+	$(popupDocConcext).find("#mainGenericPopup").show();
+	$(popupDocConcext).find("#screenShield").show();
+	$(popupDocConcext).find("#genericPopupContent h4").html("");
+	$(popupDocConcext).find("#genericPopupContent").html(msg);
 }
 
 function displayTaoError(msg, title){
@@ -31,15 +53,10 @@ function displayTaoError(msg, title){
 		var title = 'Error';
 	}
 
-	$("#errorPopUp, #helpPopUp, .ui-overlay").remove();
-
-	$("body").append('<div class="ui-overlay"><div class="ui-widget-overlay  ui-corner-all"></div></div>');
-	$("body").append('<div id="errorPopUp" class="ui-widget ui-widget-content ui-corner-all"><div id="errorPopUpClose" title="Close">X</div><h4>' + title + '</h4><p id="errorPopupContent">' + msg + '</p></div>');
-	
-	$("#errorPopUpClose").bind("click",function(){
-		$(".ui-overlay").remove();
-		$("#errorPopUp").remove();
-	});
+	$("#mainGenericPopup").show();
+	$("#screenShield").show();
+	$("#genericPopupContent h4").html(title);
+	$("#genericPopupContent").html(msg);
 }
 
 /*function displaySupport(){
