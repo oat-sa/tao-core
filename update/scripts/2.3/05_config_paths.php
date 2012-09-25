7cf1a51a36f2f988ac7f68aa3ee5b453a6598eff
jbogaerts <jbogaerts@62dfeb86-a93f-4914-9d13-15c0a160985d>
2012-09-25 16:21:24 +0000

    Add trailing slashes to ROOT_URL & ROOT_PATH at update time (2.2->2.3).
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/tao/trunk@12302 62dfeb86-a93f-4914-9d13-15c0a160985d

---
+++ update/scripts/2.3/05_config_paths.php
@@ -0,0 +1,66 @@
+<?php
+function tao2223_change_roots($inputFile, $outputFile, $forceLinux = false){
+	if (!is_readable($inputFile)){
+		die("'${inputFile}' is not readable by the current process.");
+	}
+	else if (!is_writable($outputFile)){
+		die("'${outputFile}' is not writable by the current process.");
+	}
+	else{
+		$input = file_get_contents($inputFile);
+		$matches = array();
+		
+		// -- ROOT_PATH -- Add a trailing slash/backslash depending on the operating system.
+		if (preg_match("/define\s*\(\s*'ROOT_PATH'\s*,\s*'(.+)'\s*\)\s*;/", $input, $matches) !== false){
+			$path = $matches[1];
+			$match = $matches[0];
+			if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN' && !$forceLinux){
+				// The string was not addslashed and might not contain a valid trailing slash.
+				// Replace slashes by backslashes.
+				$path = str_replace('/', '\\', $path);
+				$last = substr($path, -1);
+				if ($last != '\\'){
+					$path = $path . '\\';
+				}
+				
+				$path = addslashes($path);
+				$path = str_replace('\\\\\\\\', '\\\\', $path);
+			}
+			else{
+				// Linux system.
+				$last = substr($path, -1);
+				if ($last != '/'){
+					$path = $path . '/';
+				}
+			}
+
+			// We replace in the input.
+			$input = str_replace($match, "define('ROOT_PATH', '${path}');\n", $input);
+			
+			// -- ROOT_URL -- Add a traling slash.
+			if (preg_match("/define\s*\(\s*'ROOT_URL'\s*,\s*'(.+)'\s*\)\s*;/", $input, $matches) !== false){
+				$path = $matches[1];
+				$match = $matches[0];
+				$last = substr($path, -1);
+				if ($last != '/'){
+					$path = $path . '/';
+				}
+				
+				// We replace in the input.
+				$input = str_replace($match, "define('ROOT_URL', '${path}');\n", $input);
+				
+				file_put_contents($outputFile, $input);
+			}
+			else{
+				die("'ROOT_URL' constant not found in '${inputFile}'.");
+			}
+		}
+		else{
+			die("'ROOT_PATH' constant not found in '${inputFile}'.");
+		}
+	}
+}
+
+tao2223_change_roots(dirname(__FILE__) . '/../../../../generis/common/conf/generis.conf.php',
+					 dirname(__FILE__) . '/../../../../generis/common/conf/generis.conf.php');
+?>
