dd3365d62e039f76fa0f0f40ed45fe1a465e0fa4
llecaque <llecaque@62dfeb86-a93f-4914-9d13-15c0a160985d>
2012-10-24 16:00:56 +0000

    fix issue about true and false expression
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/tao/trunk@12544 62dfeb86-a93f-4914-9d13-15c0a160985d

---
+++ update/scripts/2.4/01_expressionBoolean.sql
@@ -0,0 +1,3 @@
+UPDATE statements SET subject = 'http://www.tao.lu/middleware/Rules.rdf#FalseExpression' WHERE subject = 'http://www.tao.lu/middleware/Rules.rdf#i12339287061376' AND object like '%True';
+UPDATE statements SET subject = 'http://www.tao.lu/middleware/Rules.rdf#TrueExpression' WHERE subject = 'http://www.tao.lu/middleware/Rules.rdf#i12339287061376' AND object like '%False';
+
