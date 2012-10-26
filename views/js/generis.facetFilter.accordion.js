657628dca177fd5f94c4c01c5199239d375a9413
jbihin <jbihin@62dfeb86-a93f-4914-9d13-15c0a160985d>
2012-10-26 13:01:13 +0000

    fix accordion facetfilter behavior
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/tao/trunk@12575 62dfeb86-a93f-4914-9d13-15c0a160985d

--- views/js/generis.facetFilter.accordion.js
+++ views/js/generis.facetFilter.accordion.js
@@ -32,48 +32,38 @@ define(['require', 'jquery'], function(req, $) {
 		 * Render the content
 		 */
 		content: function(id, label) {
-			var html = '<h3><a href="#">' + label + '</a></h3> \
-				<div style="padding:5px 0 5px 12px;"> \
-					<div id="list-' + id + '"></div> \
-				</div>';
+			var html = '<h3><a href="#">' + label + '</a></h3><div style="padding:5px 0 5px 12px;"><div id="list-' + id + '"></div></div>';
 			return html;
 		},
 		/**
 		 * Render
 		 */
 		render: function(selector, filterNodes) {
-			var output = '';
-			output += this.header();
+			var $sel = $('<div class="accordion"></div>').appendTo($(selector)).addClass("ui-accordion ui-accordion-icons ui-widget ui-helper-reset");
+
 			for (var i in filterNodes) {
-				output += this.content(filterNodes[i].id, filterNodes[i].label);
-			}
-			output += this.footer();
-
-			$(selector).append(output);
-			//$(selector).find('.accordion').accordion({
-			//	autoHeight: false,collapsible: true
-			//});
-
-			$(selector).find('.accordion')
-					.addClass("ui-accordion ui-accordion-icons ui-widget ui-helper-reset")
-					.find("h3")
-					.addClass("ui-accordion-header ui-helper-reset ui-state-default ui-corner-top ui-corner-bottom")
+				var $bloc = $(this.content(filterNodes[i].id, filterNodes[i].label)).appendTo($sel);
+				$($bloc[0]).addClass("ui-accordion-header ui-helper-reset ui-state-default ui-corner-top ui-corner-bottom")
 					.hover(function() {
 						$(this).toggleClass("ui-state-hover");
 					})
 					.prepend('<span class="ui-icon ui-icon-triangle-1-e"></span>')
-					.click(
-						function() {
-							$(this).toggleClass("ui-accordion-header-active ui-state-active ui-state-default ui-corner-bottom")
-								.find("> .ui-icon")
-								.toggleClass("ui-icon-triangle-1-e ui-icon-triangle-1-s")
-								.end().next().toggleClass("ui-accordion-content-active")
-								.slideToggle();
-							return false;
-						})
+					.click(function(e) {
+						e.preventDefault();
+						$(this).toggleClass("ui-accordion-header-active ui-state-active ui-state-default ui-corner-bottom");
+						$("> .ui-icon", $(this)).toggleClass("ui-icon-triangle-1-e ui-icon-triangle-1-s")
+							.end()
+							.next()
+							.toggleClass("ui-accordion-content-active")
+							.slideToggle();
+					})
 					.next()
-					.addClass("ui-accordion-content  ui-helper-reset ui-widget-content ui-corner-bottom")
-					.hide();
+					//addClass and CSS is a system hack to have the correct behavior
+					//Don't hesitate to remake not using UI css class
+					.addClass("ui-accordion-content ui-helper-reset ui-widget-content ui-corner-bottom")
+					.css({padding: '5px 0px 5px 12px', display: 'block'})
+					.slideUp();
+			}
 		}
 	});
 
