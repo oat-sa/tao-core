1b8fd28f0fe5536a21d8f48bd31eb7c2f13d32f4
jbogaerts <jbogaerts@62dfeb86-a93f-4914-9d13-15c0a160985d>
2013-02-08 14:59:41 +0000

    slight changes.
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/tao/trunk@13684 62dfeb86-a93f-4914-9d13-15c0a160985d

--- actions/class.Main.php
+++ actions/class.Main.php
@@ -329,11 +329,7 @@ class tao_actions_Main extends tao_actions_CommonModule {
 			$this->setData('openUri', $openUri);
 
 			//differentiate the instanceName of Deliveries and Process definition from the others
-			if($extname=="taoDelivery" || $extname=="wfEngine"){
-				$this->setData('instanceName', $this->getSessionAttribute('currentSection'));
-			}else{
-				$this->setData('instanceName', strtolower(str_replace('tao', '', substr($extname, 0, strlen($extname) - 1))));
-			}
+			$this->setData('instanceName', strtolower(str_replace('tao', '', substr($extname, 0, strlen($extname) - 1))));
 		}
 
 		$this->setView('main/trees.tpl', true);
