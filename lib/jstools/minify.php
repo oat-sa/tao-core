0ffc2c76b837fe13f40a6b26756687e34cf3f83b
ssipasseuth <ssipasseuth@62dfeb86-a93f-4914-9d13-15c0a160985d>
2013-02-27 10:23:46 +0000

    updating htmlpurifier 4.4.0 to 4.5.0
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/tao/trunk@13847 62dfeb86-a93f-4914-9d13-15c0a160985d

--- lib/jstools/minify.php
+++ lib/jstools/minify.php
@@ -5,6 +5,7 @@ require_once(dirname (__FILE__).'/cssmin.php');
 function minifyJSFiles($files, $outputFileName) {
     $i = 0;
     foreach ($files as $file){
+		var_dump($file);
         $jsMin = new JSMin($file, $outputFileName, null, !$i);
         $jsMin -> minify();
         $i++;   
