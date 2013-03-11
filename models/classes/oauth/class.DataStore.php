4f1ac5cf70e2085aa1809f216d20236460ffec87
jbout <jbout@62dfeb86-a93f-4914-9d13-15c0a160985d>
2013-03-11 09:57:49 +0000

    fixed dependencies
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/tao/trunk@13990 62dfeb86-a93f-4914-9d13-15c0a160985d

--- models/classes/oauth/class.DataStore.php
+++ models/classes/oauth/class.DataStore.php
@@ -15,6 +15,13 @@ if (0 > version_compare(PHP_VERSION, '5')) {
     die('This file was generated for PHP 5');
 }
 
+/**
+ * include OAuthDataStore
+ *
+ * @author Joel Bout, <joel@taotesting.com>
+ */
+//require_once('class.OAuthDataStore.php');
+
 /* user defined includes */
 // section 10-30-1--78-7fe2a05b:13d4a3616e9:-8000:0000000000003CA0-includes begin
 require_once dirname(__FILE__).'/../../../includes/oauth/OAuth.php';
@@ -44,6 +51,33 @@ class tao_models_classes_oauth_DataStore
     // --- OPERATIONS ---
 
     /**
+     * Short description of method findOauthConsumerResource
+     *
+     * @access public
+     * @author Joel Bout, <joel@taotesting.com>
+     * @param  string consumer_key
+     * @return core_kernel_classes_Resource
+     */
+    public function findOauthConsumerResource($consumer_key)
+    {
+        $returnValue = null;
+
+        // section 10-30-1--78--71850a20:13d58c9a548:-8000:0000000000003CC4 begin
+		$class = new core_kernel_classes_Class(CLASS_OAUTH_CONSUMER);
+		$instances = $class->searchInstances(array(PROPERTY_OAUTH_KEY => $consumer_key), array('like' => false, 'recursive' => true));
+		if (count($instances) == 0) {
+			throw new tao_models_classes_oauth_Excpetion('No Credentials for consumer key '.$consumer_key);
+		}
+		if (count($instances) > 1) {
+			throw new tao_models_classes_oauth_Excpetion('Multiple Credentials for consumer key '.$consumer_key);
+		}
+		$returnValue	= current($instances);
+        // section 10-30-1--78--71850a20:13d58c9a548:-8000:0000000000003CC4 end
+
+        return $returnValue;
+    }
+
+    /**
      * returns the OauthConsumer for the specified key
      *
      * @access public
@@ -56,17 +90,7 @@ class tao_models_classes_oauth_DataStore
         $returnValue = null;
 
         // section 10-30-1--78-7fe2a05b:13d4a3616e9:-8000:0000000000003CA5 begin
-        common_Logger::d(__CLASS__.'::'.__FUNCTION__.' called');
-		
-		$class = new core_kernel_classes_Class(CLASS_LTI_CONSUMER);
-		$instances = $class->searchInstances(array(PROPERTY_OAUTH_KEY => $consumer_key), array('like' => false));
-		if (count($instances) == 0) {
-			throw new ltiProvider_models_classes_LTIException('No Credentials for consumer key '.$consumer_key);
-		}
-		if (count($instances) > 1) {
-			throw new ltiProvider_models_classes_LTIException('Multiple Credentials for consumer key '.$consumer_key);
-		}
-		$consumer		= current($instances);
+		$consumer = $this->findOauthConsumerResource($consumer_key);
 		$secret			= (string)$consumer->getUniquePropertyValue(new core_kernel_classes_Property(PROPERTY_OAUTH_SECRET));
 		$callbackUrl	= null;
 		
---
+++ models/classes/oauth/class.Excpetion.php
@@ -0,0 +1,59 @@
+<?php
+
+error_reporting(E_ALL);
+
+/**
+ * TAO - tao/models/classes/oauth/class.Excpetion.php
+ *
+ * $Id$
+ *
+ * This file is part of TAO.
+ *
+ * Automatically generated on 11.03.2013, 10:38:32 with ArgoUML PHP module 
+ * (last revised $Date: 2010-01-12 20:14:42 +0100 (Tue, 12 Jan 2010) $)
+ *
+ * @author Joel Bout, <joel@taotesting.com>
+ * @package tao
+ * @subpackage models_classes_oauth
+ */
+
+if (0 > version_compare(PHP_VERSION, '5')) {
+    die('This file was generated for PHP 5');
+}
+
+/**
+ * include common_Exception
+ *
+ * @author Joel Bout, <joel@taotesting.com>
+ */
+require_once('common/class.Exception.php');
+
+/* user defined includes */
+// section 10-30-1--78--71850a20:13d58c9a548:-8000:0000000000003CC7-includes begin
+// section 10-30-1--78--71850a20:13d58c9a548:-8000:0000000000003CC7-includes end
+
+/* user defined constants */
+// section 10-30-1--78--71850a20:13d58c9a548:-8000:0000000000003CC7-constants begin
+// section 10-30-1--78--71850a20:13d58c9a548:-8000:0000000000003CC7-constants end
+
+/**
+ * Short description of class tao_models_classes_oauth_Excpetion
+ *
+ * @access public
+ * @author Joel Bout, <joel@taotesting.com>
+ * @package tao
+ * @subpackage models_classes_oauth
+ */
+class tao_models_classes_oauth_Excpetion
+    extends common_Exception
+{
+    // --- ASSOCIATIONS ---
+
+
+    // --- ATTRIBUTES ---
+
+    // --- OPERATIONS ---
+
+} /* end of class tao_models_classes_oauth_Excpetion */
+
+?>
