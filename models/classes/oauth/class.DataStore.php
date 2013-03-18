9057f618f6e55b07941ff89614f128a68e8d33bb
jbout <jbout@62dfeb86-a93f-4914-9d13-15c0a160985d>
2013-03-18 14:43:47 +0000

    added comments
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/tao/trunk@14092 62dfeb86-a93f-4914-9d13-15c0a160985d

--- models/classes/oauth/class.DataStore.php
+++ models/classes/oauth/class.DataStore.php
@@ -15,7 +15,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  * 
  * Copyright (c) 2002-2008 (original work) Public Research Centre Henri Tudor & University of Luxembourg (under the project TAO & TAO2);
- *               2008-2010 (update and modification) Deutsche Institut für Internationale Pädagogische Forschung (under the project TAO-TRANSFER);\n *               2009-2012 (update and modification) Public Research Centre Henri Tudor (under the project TAO-SUSTAIN & TAO-DEV);
+ *			   2008-2010 (update and modification) Deutsche Institut für Internationale Pädagogische Forschung (under the project TAO-TRANSFER);\n *			   2009-2012 (update and modification) Public Research Centre Henri Tudor (under the project TAO-SUSTAIN & TAO-DEV);
  * 
  */
 ?>
@@ -33,24 +33,15 @@ error_reporting(E_ALL);
  */
 
 if (0 > version_compare(PHP_VERSION, '5')) {
-    die('This file was generated for PHP 5');
+	die('This file was generated for PHP 5');
 }
 
 /**
- * include OAuthDataStore
+ * include OAuth Classes
  *
  * @author Joel Bout, <joel@taotesting.com>
  */
-//require_once('class.OAuthDataStore.php');
-
-/* user defined includes */
-// section 10-30-1--78-7fe2a05b:13d4a3616e9:-8000:0000000000003CA0-includes begin
 require_once dirname(__FILE__).'/../../../lib/oauth/OAuth.php';
-// section 10-30-1--78-7fe2a05b:13d4a3616e9:-8000:0000000000003CA0-includes end
-
-/* user defined constants */
-// section 10-30-1--78-7fe2a05b:13d4a3616e9:-8000:0000000000003CA0-constants begin
-// section 10-30-1--78-7fe2a05b:13d4a3616e9:-8000:0000000000003CA0-constants end
 
 /**
  * Tao Implementation of an OAuthDatastore
@@ -62,28 +53,27 @@ require_once dirname(__FILE__).'/../../../lib/oauth/OAuth.php';
  * @subpackage models_classes_oauth
  */
 class tao_models_classes_oauth_DataStore
-    extends OAuthDataStore
+	extends OAuthDataStore
 {
-    // --- ASSOCIATIONS ---
+	// --- ASSOCIATIONS ---
 
 
-    // --- ATTRIBUTES ---
+	// --- ATTRIBUTES ---
 
-    // --- OPERATIONS ---
+	// --- OPERATIONS ---
 
-    /**
-     * Short description of method findOauthConsumerResource
-     *
-     * @access public
-     * @author Joel Bout, <joel@taotesting.com>
-     * @param  string consumer_key
-     * @return core_kernel_classes_Resource
-     */
-    public function findOauthConsumerResource($consumer_key)
-    {
-        $returnValue = null;
+	/**
+	 * Helper function to find the OauthConsumer RDF Resource
+	 *
+	 * @access public
+	 * @author Joel Bout, <joel@taotesting.com>
+	 * @param  string consumer_key
+	 * @return core_kernel_classes_Resource
+	 */
+	public function findOauthConsumerResource($consumer_key)
+	{
+		$returnValue = null;
 
-        // section 10-30-1--78--71850a20:13d58c9a548:-8000:0000000000003CC4 begin
 		$class = new core_kernel_classes_Class(CLASS_OAUTH_CONSUMER);
 		$instances = $class->searchInstances(array(PROPERTY_OAUTH_KEY => $consumer_key), array('like' => false, 'recursive' => true));
 		if (count($instances) == 0) {
@@ -93,104 +83,97 @@ class tao_models_classes_oauth_DataStore
 			throw new tao_models_classes_oauth_Exception('Multiple Credentials for consumer key '.$consumer_key);
 		}
 		$returnValue	= current($instances);
-        // section 10-30-1--78--71850a20:13d58c9a548:-8000:0000000000003CC4 end
 
-        return $returnValue;
-    }
+		return $returnValue;
+	}
 
-    /**
-     * returns the OauthConsumer for the specified key
-     *
-     * @access public
-     * @author Joel Bout, <joel@taotesting.com>
-     * @param  consumer_key
-     * @return OAuthConsumer
-     */
-    public function lookup_consumer($consumer_key)
-    {
-        $returnValue = null;
+	/**
+	 * returns the OauthConsumer for the specified key
+	 *
+	 * @access public
+	 * @author Joel Bout, <joel@taotesting.com>
+	 * @param  consumer_key
+	 * @return OAuthConsumer
+	 */
+	public function lookup_consumer($consumer_key)
+	{
+		$returnValue = null;
 
-        // section 10-30-1--78-7fe2a05b:13d4a3616e9:-8000:0000000000003CA5 begin
 		$consumer = $this->findOauthConsumerResource($consumer_key);
 		$secret			= (string)$consumer->getUniquePropertyValue(new core_kernel_classes_Property(PROPERTY_OAUTH_SECRET));
 		$callbackUrl	= null;
 		
 		$returnValue = new OAuthConsumer($consumer_key, $secret, $callbackUrl);
-        // section 10-30-1--78-7fe2a05b:13d4a3616e9:-8000:0000000000003CA5 end
 
-        return $returnValue;
-    }
+		return $returnValue;
+	}
 
-    /**
-     * Short description of method lookup_token
-     *
-     * @access public
-     * @author Joel Bout, <joel@taotesting.com>
-     * @param  consumer
-     * @param  token_type
-     * @param  token
-     * @return mixed
-     */
-    public function lookup_token($consumer, $token_type, $token)
-    {
-        // section 10-30-1--78-7fe2a05b:13d4a3616e9:-8000:0000000000003CA7 begin
-		common_Logger::d(__CLASS__.'::'.__FUNCTION__.' called');
+	/**
+	 * Should verify if the token exists and return it
+	 * Always returns an token with an empty secret for now
+	 *
+	 * @access public
+	 * @author Joel Bout, <joel@taotesting.com>
+	 * @param  consumer
+	 * @param  token_type
+	 * @param  token
+	 * @return mixed
+	 */
+	public function lookup_token($consumer, $token_type, $token)
+	{
+  		common_Logger::d(__CLASS__.'::'.__FUNCTION__.' called for token '.$token.' of type '.$token_type);
 		return new OAuthToken($consumer, "");
-        // section 10-30-1--78-7fe2a05b:13d4a3616e9:-8000:0000000000003CA7 end
-    }
+	}
 
-    /**
-     * Short description of method lookup_nonce
-     *
-     * @access public
-     * @author Joel Bout, <joel@taotesting.com>
-     * @param  consumer
-     * @param  token
-     * @param  nonce
-     * @param  timestamp
-     * @return mixed
-     */
-    public function lookup_nonce($consumer, $token, $nonce, $timestamp)
-    {
-        // section 10-30-1--78-7fe2a05b:13d4a3616e9:-8000:0000000000003CA9 begin
-        common_Logger::d(__CLASS__.'::'.__FUNCTION__.' called');
+	/**
+	 * Should verify if a nonce has already been used
+	 * always return NULL, meaning that nonces can be reused
+	 *
+	 * @access public
+	 * @author Joel Bout, <joel@taotesting.com>
+	 * @param  consumer
+	 * @param  token
+	 * @param  nonce
+	 * @param  timestamp
+	 * @return mixed
+	 */
+	public function lookup_nonce($consumer, $token, $nonce, $timestamp)
+	{
+		common_Logger::d(__CLASS__.'::'.__FUNCTION__.' called');
 		return NULL;
-        // section 10-30-1--78-7fe2a05b:13d4a3616e9:-8000:0000000000003CA9 end
-    }
+	}
 
-    /**
-     * Short description of method new_request_token
-     *
-     * @access public
-     * @author Joel Bout, <joel@taotesting.com>
-     * @param  consumer
-     * @param  callback
-     * @return mixed
-     */
+	/**
+	 * Should create a new request token
+	 * not implemented
+	 *
+	 * @access public
+	 * @author Joel Bout, <joel@taotesting.com>
+	 * @param  consumer
+	 * @param  callback
+	 * @return mixed
+	 */
 	function new_request_token($consumer, $callback = null)
-    {
-        // section 10-30-1--78-7fe2a05b:13d4a3616e9:-8000:0000000000003CAB begin
-        common_Logger::d(__CLASS__.'::'.__FUNCTION__.' called');
+	{
+		common_Logger::d(__CLASS__.'::'.__FUNCTION__.' called');
 		return NULL;
-        // section 10-30-1--78-7fe2a05b:13d4a3616e9:-8000:0000000000003CAB end
-    }
+	}
 
-    /**
-     * Short description of method new_access_token
-     *
-     * @access public
-     * @author Joel Bout, <joel@taotesting.com>
-     * @param  token
-     * @param  consumer
-     * @return mixed
-     */
-    public function new_access_token($token, $consumer, $verifier = null)
-    {
-        // section 10-30-1--78-7fe2a05b:13d4a3616e9:-8000:0000000000003CAD begin
-        common_Logger::d(__CLASS__.'::'.__FUNCTION__.' called');
+	/**
+	 * Should create a new access token
+	 * not implemented
+	 * 
+	 * @access public
+	 * @author Joel Bout, <joel@taotesting.com>
+	 * @param  token
+	 * @param  consumer
+	 * @return mixed
+	 */
+	public function new_access_token($token, $consumer, $verifier = null)
+	{
+		common_Logger::d(__CLASS__.'::'.__FUNCTION__.' called');
 		return NULL;
-        // section 10-30-1--78-7fe2a05b:13d4a3616e9:-8000:0000000000003CAD end
-    }
+	}
 
 } /* end of class tao_models_classes_oauth_DataStore */
 
