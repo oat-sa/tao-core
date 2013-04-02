10d4a07f4622007304ea572deb8b270ce4b3e1ad
jbogaerts <jbogaerts@62dfeb86-a93f-4914-9d13-15c0a160985d>
2013-04-02 08:44:50 +0000

    selected=true is not XHTML compliant. Changed with selected=selected
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/tao/trunk@14335 62dfeb86-a93f-4914-9d13-15c0a160985d

--- install/js/step_2.js
+++ install/js/step_2.js
@@ -26,7 +26,7 @@ function onLoad(){
 		var $defaultLanguageElement = $('#default_language').empty();
 		
 		for (var i in availableLanguages){
-			var selected  = (i == 'EN') ? 'selected="true"' : '';
+			var selected  = (i == 'EN') ? 'selected="selected"' : '';
 			$defaultLanguageElement.append('<option value="' + i + '" ' + selected + '>' + availableLanguages[i] + '</option>');
 		}
 	}
