7f5da316935f4af2ce55cdfe4abdf6003a4c79db
jbout <jbout@62dfeb86-a93f-4914-9d13-15c0a160985d>
2013-04-10 09:52:28 +0000

    fix fileUpload
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/tao/trunk@14465 62dfeb86-a93f-4914-9d13-15c0a160985d

--- models/classes/dataBinding/class.GenerisFormDataBindingException.php
+++ models/classes/dataBinding/class.GenerisFormDataBindingException.php
@@ -19,39 +19,6 @@
  *               2009-2012 (update and modification) Public Research Centre Henri Tudor (under the project TAO-SUSTAIN & TAO-DEV);
  * 
  */
-?>
-<?php
-
-error_reporting(E_ALL);
-
-/**
- * Exception to be thrown when an error occurs when binding data coming from a
- * Instance Form to the Generis persistent memory.
- *
- * @author Jerome Bogaerts, <jerome@taotesting.com>
- * @package tao
- * @subpackage models_classes_dataBinding
- */
-
-if (0 > version_compare(PHP_VERSION, '5')) {
-    die('This file was generated for PHP 5');
-}
-
-/**
- * Exception to be thrown when an error occurs at Binding time between the data
- * be bound and the Generis Persistent memory.
- *
- * @author Jerome Bogaerts, <jerome@taotesting.com>
- */
-require_once('tao/models/classes/dataBinding/class.GenerisClassDataBindingException.php');
-
-/* user defined includes */
-// section 127-0-1-1-2d2ef7de:13d10c8a117:-8000:0000000000003CC0-includes begin
-// section 127-0-1-1-2d2ef7de:13d10c8a117:-8000:0000000000003CC0-includes end
-
-/* user defined constants */
-// section 127-0-1-1-2d2ef7de:13d10c8a117:-8000:0000000000003CC0-constants begin
-// section 127-0-1-1-2d2ef7de:13d10c8a117:-8000:0000000000003CC0-constants end
 
 /**
  * Exception to be thrown when an error occurs when binding data coming from a
@@ -63,7 +30,7 @@ require_once('tao/models/classes/dataBinding/class.GenerisClassDataBindingExcept
  * @subpackage models_classes_dataBinding
  */
 class tao_models_classes_dataBinding_GenerisFormDataBindingException
-    extends tao_models_classes_dataBinding_GenerisClassDataBindingException
+    extends tao_models_classes_dataBinding_DataBindingException
 {
     // --- ASSOCIATIONS ---
 
