fc09ea909622c42c241c79350b9173d1f0611d71
jbogaerts <jbogaerts@62dfeb86-a93f-4914-9d13-15c0a160985d>
2013-04-15 07:25:57 +0000

    argo cleaning + doc.
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/tao/trunk@14535 62dfeb86-a93f-4914-9d13-15c0a160985d

--- helpers/class.Environment.php
+++ helpers/class.Environment.php
@@ -18,101 +18,79 @@
  *               2009-2012 (update and modification) Public Research Centre Henri Tudor (under the project TAO-SUSTAIN & TAO-DEV);
  * 
  */
-?>
-<?php
-
-error_reporting(E_ALL);
 
 /**
- * Utility class for server environment retrieval
- *
- * @author Joel Bout, <joel.bout@tudor.lu>
- * @package tao
- * @subpackage helpers
- */
-
-if (0 > version_compare(PHP_VERSION, '5')) {
-    die('This file was generated for PHP 5');
-}
-
-/* user defined includes */
-// section 127-0-1-1--19cc545a:133f9dd1f55:-8000:0000000000003442-includes begin
-// section 127-0-1-1--19cc545a:133f9dd1f55:-8000:0000000000003442-includes end
-
-/* user defined constants */
-// section 127-0-1-1--19cc545a:133f9dd1f55:-8000:0000000000003442-constants begin
-// section 127-0-1-1--19cc545a:133f9dd1f55:-8000:0000000000003442-constants end
-
-/**
- * Utility class for server environment retrieval
+ * Utility class focusing  on the server environment.
  *
  * @access public
- * @author Joel Bout, <joel.bout@tudor.lu>
+ * @author Joel Bout, <joel@taotesting.com>
  * @package tao
  * @subpackage helpers
  */
 class tao_helpers_Environment
 {
-    // --- ASSOCIATIONS ---
-
-
-    // --- ATTRIBUTES ---
-
-    // --- OPERATIONS ---
 
     /**
-     * Returns the maximum size for fileuploads in bytes
+     * Returns the maximum size for fileuploads in bytes.
      *
-     * @access public
-     * @author Joel Bout, <joel.bout@tudor.lu>
-     * @return int
+     * @author Joel Bout, <joel@taotesting.com>
+     * @return int The upload file limit.
      */
     public static function getFileUploadLimit()
     {
         $returnValue = (int) 0;
 
-        // section 127-0-1-1--19cc545a:133f9dd1f55:-8000:0000000000003443 begin
         $max_upload		= self::toBytes(ini_get('upload_max_filesize'));
         $max_post		= self::toBytes(ini_get('post_max_size'));
         $memory_limit	= self::toBytes(ini_get('memory_limit'));
         
         $returnValue = min($max_upload, $max_post, $memory_limit);        
-        // section 127-0-1-1--19cc545a:133f9dd1f55:-8000:0000000000003443 end
 
         return (int) $returnValue;
     }
 
     /**
      * Returns the Operating System running TAO as a String.
+     * 
+     * The returned value can be AFAIK:
+     * 
+     * * 'WINNT' (Win XP, Windows NT, ...)
+     * * 'Linux'
+     * * 'FreeBSD'
+     * * 'Darwin' (Mac OS X)
+     * * 'CYGWIN_NT-5.1'
+     * * 'HP-UX'
+     * * 'IRIX64'
+     * * 'NetBSD'
+     * * 'OpenBSD'
+     * * 'SunOS'
+     * * 'Unix'
+     * * 'WIN32'
+     * * 'Windows'
      *
-     * @access public
-     * @author Joel Bout, <joel.bout@tudor.lu>
-     * @return string
+     * @author Joel Bout, <joel@taotesting.com>
+     * @return string The operating system that runs the script.
      */
     public function getOperatingSystem()
     {
         $returnValue = (string) '';
 
-        // section 10-13-1-85--245f9798:135a41f7e17:-8000:0000000000003832 begin
         $returnValue = PHP_OS;
-        // section 10-13-1-85--245f9798:135a41f7e17:-8000:0000000000003832 end
 
         return (string) $returnValue;
     }
 
     /**
-     * Short description of method toBytes
+     * Get the size in bytes of a PHP variable given as a string.
      *
-     * @access private
-     * @author Joel Bout, <joel.bout@tudor.lu>
-     * @param  string phpSyntax
-     * @return int
+     * @author Joel Bout, <joel@taotesting.com>
+     * @param  string phpSyntax The PHP syntax to describe the variable.
+     * @return int The size in bytes.
      */
     private static function toBytes($phpSyntax)
     {
         $returnValue = (int) 0;
 
-        // section 127-0-1-1-209b93e2:1374a5ddd41:-8000:0000000000003A47 begin
         $val = trim($phpSyntax);
         	$last = strtolower($val[strlen($val)-1]);
         	switch($last) {
@@ -125,11 +103,10 @@ class tao_helpers_Environment
         	}
         
         return $val;
-        // section 127-0-1-1-209b93e2:1374a5ddd41:-8000:0000000000003A47 end
 
         return (int) $returnValue;
     }
 
-} /* end of class tao_helpers_Environment */
+}
 
 ?>
