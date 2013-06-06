822b831c606fb8e56540d3654817c682732286ff
pplichart <pplichart@62dfeb86-a93f-4914-9d13-15c0a160985d>
2013-06-06 13:47:35 +0000

    work in progress (rest ) , exception handler
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/tao/trunk@14770 62dfeb86-a93f-4914-9d13-15c0a160985d

--- actions/class.RESTExceptionHandler.php
+++ actions/class.RESTExceptionHandler.php
@@ -19,13 +19,20 @@ class tao_actions_RESTExceptionHandler extends common_exception_ExceptionHandler
 	
 	case "common_exception_BadRequest":{header("HTTP/1.0 400 Bad Request" );break;}
 	case "common_exception_MissingParameter":{header("HTTP/1.0 400 Bad Request" );break;}
-	case "common_exception_NotAcceptable":{header("HTTP/1.0 406 Bad Request" );break;}
-	case "common_exception_BadRequest":{header("HTTP/1.0 401 Bad Request" );break;}
-	
-	
+	case "common_exception_NotAcceptable":{header("HTTP/1.0 406 Not Acceptable" );break;}
+	case "common_exception_Unauthorized":{header("HTTP/1.0 401 Unauthorized" );break;}
+	case "common_exception_NotFound":{header("HTTP/1.0 404 Not Found" );break;}
+	case "common_exception_MethodNotAllowed":{header("HTTP/1.0 405 Not Found" );break;}
+	case "common_exception_NotAcceptable":{header("HTTP/1.0 406 Not Acceptable" );break;}
+	case "common_exception_TimeOut":{header("HTTP/1.0 408 Request Timeout" );break;}
+	case "common_exception_Conflict":{header("HTTP/1.0 409 Conflict" );break;}
+	case "common_exception_UnsupportedMediaType":{header("HTTP/1.0 415 Unsupported Media Type" );break;}
+	case "common_exception_NotImplemented":{header("HTTP/1.0 501 Not Implemented" );break;}
+	case "common_exception_PreConditionFailure":{header("HTTP/1.0 412 Precondition Failed" );break;}
 
-
-	default: {header("HTTP/1.0 500 Bad Request" );}
+	//throw this one
+	case "common_exception_teapotAprilFirst":{header("HTTP/1.0 418 I'm a teapot (RFC 2324)" );break;}
+	default: {header("HTTP/1.0 500 Internal Server Error" );}
 	}
     }
 }
