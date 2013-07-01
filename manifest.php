28b46571c198c037c634f92e15df3a8dbb95c486
jbout <jbout@62dfeb86-a93f-4914-9d13-15c0a160985d>
2013-07-01 09:16:53 +0000

    removed export folder check
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/tao/trunk@14981 62dfeb86-a93f-4914-9d13-15c0a160985d

--- manifest.php
+++ manifest.php
@@ -72,7 +72,6 @@ return array(
 				array('type' => 'CheckFileSystemComponent', 'value' => array('id' => 'fs_generis_common_conf', 'location' => 'generis/common/conf', 'rights' => 'rw')),
 				array('type' => 'CheckFileSystemComponent', 'value' => array('id' => 'fs_generis_common_conf_default', 'location' => 'generis/common/conf/default', 'rights' => 'r')),
 				array('type' => 'CheckFileSystemComponent', 'value' => array('id' => 'fs_generis_common_conf_sample', 'location' => 'generis/common/conf/sample', 'rights' => 'r')),
-				array('type' => 'CheckFileSystemComponent', 'value' => array('id' => 'fs_tao_views_export', 'location' => 'tao/views/export', 'rights' => 'rw')),
 				array('type' => 'CheckFileSystemComponent', 'value' => array('id' => 'fs_tao_includes', 'location' => 'tao/includes', 'rights' => 'rw')),
 				array('type' => 'CheckFileSystemComponent', 'value' => array('id' => 'fs_tao_update_patches', 'location' => 'tao/update/patches', 'rights' => 'rw')),
 				array('type' => 'CheckFileSystemComponent', 'value' => array('id' => 'fs_tao_locales', 'location' => 'tao/locales', 'rights' => 'r')),
