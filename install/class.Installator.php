b67072f78c30d5d374f7a0df491c3b6901e9a6b5
jbout <jbout@62dfeb86-a93f-4914-9d13-15c0a160985d>
2013-08-14 13:56:27 +0000

    added taoWfTest to default install
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/tao/trunk@15385 62dfeb86-a93f-4914-9d13-15c0a160985d

--- install/class.Installator.php
+++ install/class.Installator.php
@@ -33,7 +33,7 @@
 
 class tao_install_Installator{
 
-	static $defaultExtensions = array('tao','filemanager','taoItems','wfEngine','taoResults','taoTests','taoDelivery','taoGroups','taoSubjects', 'taoQTI', 'taoOpenWebItem');
+	static $defaultExtensions = array('tao','filemanager','taoItems','wfEngine','taoResults','taoTests','taoDelivery','taoGroups','taoSubjects', 'taoQTI', 'taoOpenWebItem', 'taoWfTest');
 
     protected $options = array();
 	
