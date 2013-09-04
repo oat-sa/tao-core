1328ef2f111ede44f411bf02ed253542867a851f
jbout <jbout@62dfeb86-a93f-4914-9d13-15c0a160985d>
2013-09-04 13:21:50 +0000

    added check for service variables
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/tao/trunk@15767 62dfeb86-a93f-4914-9d13-15c0a160985d

--- manifest.php
+++ manifest.php
@@ -78,7 +78,8 @@ return array(
 				array('type' => 'CheckFileSystemComponent', 'value' => array('id' => 'fs_tao_data_cache', 'location' => 'tao/data/cache/htmlpurifier', 'rights' => 'rw')),
 				array('type' => 'CheckFileSystemComponent', 'value' => array('id' => 'fs_tao_data_cache_htmlpurifier', 'location' => 'tao/data/cache/htmlpurifier', 'rights' => 'rw')),
 				array('type' => 'CheckFileSystemComponent', 'value' => array('id' => 'fs_tao_data_upload', 'location' => 'tao/data/upload', 'rights' => 'rw')),
-				array('type' => 'CheckCustom', 'value' => array('id' => 'tao_custom_not_nginx', 'name' => 'not_nginx', 'extension' => 'tao', "optional" => true)),
+				array('type' => 'CheckFileSystemComponent', 'value' => array('id' => 'fs_tao_data_service', 'location' => 'tao/data/serviceStorage', 'rights' => 'rw')),
+		        array('type' => 'CheckCustom', 'value' => array('id' => 'tao_custom_not_nginx', 'name' => 'not_nginx', 'extension' => 'tao', "optional" => true)),
 				array('type' => 'CheckCustom', 'value' => array('id' => 'tao_custom_mod_rewrite', 'name' => 'mod_rewrite', 'extension' => 'tao', 'dependsOn' => array('tao_custom_not_nginx'))),
 				array('type' => 'CheckCustom', 'value' => array('id' => 'tao_custom_database_drivers', 'name' => 'database_drivers', 'extension' => 'tao'))
 		),
