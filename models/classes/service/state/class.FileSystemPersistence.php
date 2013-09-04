9723141931e6eed205fed56d26949fb373a3accb
jbout <jbout@62dfeb86-a93f-4914-9d13-15c0a160985d>
2013-09-04 12:45:15 +0000

    fixed license
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/tao/trunk@15758 62dfeb86-a93f-4914-9d13-15c0a160985d

--- models/classes/service/state/class.FileSystemPersistence.php
+++ models/classes/service/state/class.FileSystemPersistence.php
@@ -1,5 +1,5 @@
 <?php
-/*  
+/**  
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; under version 2
@@ -14,12 +14,9 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  * 
- * Copyright (c) 2008-2010 (original work) Deutsche Institut für Internationale Pädagogische Forschung (under the project TAO-TRANSFER);
- *               2009-2012 (update and modification) Public Research Centre Henri Tudor (under the project TAO-SUSTAIN & TAO-DEV);
+ * Copyright (c) 2013 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
  * 
  */
-
-
 /**
  * Persistence for the item delivery service
  *
