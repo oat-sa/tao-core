fea6e2dd5d2830030ec8b588c11bed8b33f5a66e
jbogaerts <jbogaerts@62dfeb86-a93f-4914-9d13-15c0a160985d>
2013-09-05 14:15:44 +0000

    included TaoQtiTest.
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/tao/trunk@15810 62dfeb86-a93f-4914-9d13-15c0a160985d

--- install/class.Installator.php
+++ install/class.Installator.php
@@ -33,7 +33,7 @@
 
 class tao_install_Installator{
 
-	static $defaultExtensions = array('tao','filemanager','taoItems','wfEngine','taoResults','taoTests','taoDelivery','taoGroups','taoSubjects', 'taoQTI', 'taoOpenWebItem', 'taoWfTest', 'taoSimpleDelivery');
+	static $defaultExtensions = array('tao','filemanager','taoItems','wfEngine','taoResults','taoTests','taoDelivery','taoGroups','taoSubjects', 'taoQTI', 'taoQtiTest', 'taoOpenWebItem', 'taoWfTest', 'taoSimpleDelivery');
 
     protected $options = array();
 	
