62e97b144a5c8eb820b36575f9b6b46f258ecc27
ssipasseuth <ssipasseuth@62dfeb86-a93f-4914-9d13-15c0a160985d>
2013-09-09 14:30:03 +0000

    more options added to autogrow
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/tao/trunk@15870 62dfeb86-a93f-4914-9d13-15c0a160985d

--- views/js/jquery.autogrow.js
+++ views/js/jquery.autogrow.js
@@ -2,12 +2,27 @@
  * Jehan Bihin
  * Autogrow for textarea
  */
-$.fn.autogrow = function() {
-	return this.each(function() {
-	$(this).before('<div style="width: '+$(this).width()+'px; display: none;" class="helper"></div>');
-	$(this).keyup(function(event) {
-		$(this).prev('.helper').html($(this).val().replace(/\r\n|\r|\n/g, '<br>'));
-		$(this).height($(this).prev().height() + 18 + "px");
-		}).keyup();
-	});
+$.fn.autogrow = function(options){
+    return this.each(function(){
+        var offset = 20;
+        var minHeight = $(this).height();
+        var maxHeight = 0;
+        if(options && options.minHeight){
+            minHeight = options.minHeight ? options.minHeight : minHeight;
+            maxHeight = options.maxHeight ? options.maxHeight : maxHeight;
+        }
+        $(this).before('<div style="width: ' + $(this).width() + 'px; display: none;" class="helper"></div>');
+        $(this).keyup(function(e){
+            CL('keyuped');
+            $(this).prev('.helper').html($(this).val().replace(/\r\n|\r|\n/g, '<br>'));
+            var height = $(this).prev().height() + offset;
+            if(height < minHeight){
+                height = minHeight;
+            }
+            if(maxHeight && height > maxHeight){
+                height = maxHeight;
+            }
+            $(this).height(height + "px");
+        }).keyup();
+    });
 };
