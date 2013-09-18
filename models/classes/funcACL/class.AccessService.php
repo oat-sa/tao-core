e5eca9ca5006785e04d04bc8f10efe4cc962c51b
llecaque <llecaque@62dfeb86-a93f-4914-9d13-15c0a160985d>
2013-09-18 09:39:36 +0000

    reformat code
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/tao/trunk@16287 62dfeb86-a93f-4914-9d13-15c0a160985d

--- models/classes/funcACL/class.AccessService.php
+++ models/classes/funcACL/class.AccessService.php
@@ -1,5 +1,5 @@
 <?php
-/*  
+/**  
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; under version 2
@@ -18,10 +18,7 @@
  *               
  * 
  */
-?>
-<?php
 
-error_reporting(E_ALL);
 
 /**
  * mother class for access operations
@@ -32,43 +29,11 @@ error_reporting(E_ALL);
  * @subpackage models_classes_funcACL
  */
 
-if (0 > version_compare(PHP_VERSION, '5')) {
-    die('This file was generated for PHP 5');
-}
 
-/**
- * This class provide the services for the Tao extension
- *
- * @author Jehan Bihin, <jehan.bihin@tudor.lu>
- */
-require_once('tao/models/classes/class.TaoService.php');
-
-/* user defined includes */
-// section 127-0-1-1--43b2a85f:1372be1e0be:-8000:0000000000003A09-includes begin
-// section 127-0-1-1--43b2a85f:1372be1e0be:-8000:0000000000003A09-includes end
-
-/* user defined constants */
-// section 127-0-1-1--43b2a85f:1372be1e0be:-8000:0000000000003A09-constants begin
-// section 127-0-1-1--43b2a85f:1372be1e0be:-8000:0000000000003A09-constants end
-
-/**
- * mother class for access operations
- *
- * @access public
- * @author Jehan Bihin
- * @package tao
- * @since 2.2
- * @subpackage models_classes_funcACL
- */
 class tao_models_classes_funcACL_AccessService
     extends tao_models_classes_GenerisService
 {
-    // --- ASSOCIATIONS ---
 
-
-    // --- ATTRIBUTES ---
-
-    // --- OPERATIONS ---
 
     /**
      * Short description of method makeEMAUri
@@ -84,19 +49,29 @@ class tao_models_classes_funcACL_AccessService
     {
         $returnValue = (string) '';
 
-        // section 127-0-1-1--43b2a85f:1372be1e0be:-8000:0000000000003A0B begin
+       
 		$returnValue = FUNCACL_NS.'#';
-		if (!is_null($act)) $type = 'a';
-		else if (!is_null($mod)) $type = 'm';
-		else $type = 'e';
+		if (!is_null($act)) {
+            $type = 'a';
+        }
+		else if (!is_null($mod)) {
+            $type = 'm';
+        }
+		else {
+            $type = 'e';
+        }
 		$returnValue .= $type.'_'.$ext;
-		if (!is_null($mod)) $returnValue .= '_'.$mod;
-		if (!is_null($act)) $returnValue .= '_'.$act;
-        // section 127-0-1-1--43b2a85f:1372be1e0be:-8000:0000000000003A0B end
+		if (!is_null($mod)) {
+            $returnValue .= '_'.$mod;
+        }
+		if (!is_null($act)) {
+            $returnValue .= '_'.$act;
+        }
+        
 
         return (string) $returnValue;
     }
 
-} /* end of class tao_models_classes_funcACL_AccessService */
+} 
 
 ?>
