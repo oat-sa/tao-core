1a37f46a55c2193f7483567ebb70b6c3f6b24aab
llecaque <llecaque@62dfeb86-a93f-4914-9d13-15c0a160985d>
2013-09-18 09:32:22 +0000

    reformat code
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/tao/trunk@16285 62dfeb86-a93f-4914-9d13-15c0a160985d

--- helpers/form/validators/class.Numeric.php
+++ helpers/form/validators/class.Numeric.php
@@ -17,10 +17,7 @@
  * Copyright (c) 2008-2010 (original work) Deutsche Institut für Internationale Pädagogische Forschung (under the project TAO-TRANSFER);
  *               2009-2012 (update and modification) Public Research Centre Henri Tudor (under the project TAO-SUSTAIN & TAO-DEV);
  * 
- */
-
-
-/**
+ * 
  * The validators enable you to perform a validation callback on a form element.
  * It's provide a model of validation and must be overriden.
  *
@@ -28,31 +25,20 @@
  * @package tao
  * @subpackage helpers_form_validators
  */
-class tao_helpers_form_validators_Numeric
-    extends tao_helpers_form_Validator
+class tao_helpers_form_validators_Numeric extends tao_helpers_form_Validator
 {
-    // --- ASSOCIATIONS ---
-
-
-    // --- ATTRIBUTES ---
-
-    // --- OPERATIONS ---
 
     /**
      * Short description of method __construct
      *
      * @access public
      * @author Jehan Bihin, <jehan.bihin@tudor.lu>
-     * @param  array options
+     * @param array options
      * @return mixed
      */
     public function __construct($options = array())
     {
-        // section 127-0-1-1-d42bee:127af842275:-8000:0000000000002382 begin
-
-   		parent::__construct($options);
-
-        // section 127-0-1-1-d42bee:127af842275:-8000:0000000000002382 end
+        parent::__construct($options);
     }
 
     /**
@@ -60,93 +46,87 @@ class tao_helpers_form_validators_Numeric
      *
      * @access public
      * @author Jehan Bihin, <jehan.bihin@tudor.lu>
-     * @param  values
+     * @param values
      * @return boolean
      */
     public function evaluate($values)
     {
         $returnValue = (bool) false;
-
-        // section 127-0-1-1-d42bee:127af842275:-8000:0000000000002386 begin
-
-		$rowValue = $values;
-		$value = tao_helpers_Numeric::parseFloat($rowValue);
-		if (empty($rowValue)) {
-			$returnValue = true; //no need to go further. To check if not empty, use the NotEmpty validator
-			return $returnValue;
-		}
-		if (!is_numeric($rowValue) || $value != $rowValue) {
-			$this->message = __('The value of this field must be numeric');
-			$returnValue = false;
-		} else {
-			if (isset($this->options['min']) || isset($this->options['max'])) {
-				$this->message = __('Invalid field range');
-
-				if (isset($this->options['min']) && isset($this->options['max'])){
-					$this->message .= ' (' . __('minimum value: ').$this->options['min'] . ', ' .  __('maximum value: ').$this->options['max'].')';
-
-					if ($this->options['min'] <=  $value && $value <= $this->options['max']) {
-						$returnValue = true;
-					}
-				} else if (isset($this->options['min']) && !isset($this->options['max'])){
-					$this->message .= ' (' . __('minimum value: ').$this->options['min'] .')';
-
-					if ($this->options['min'] <=  $value) {
-						$returnValue = true;
-					}
-				} else if (!isset($this->options['min']) && isset($this->options['max'])){
-					$this->message .= ' (' . __('maximum value: ').$this->options['max'].')';
-
-					if ($value <= $this->options['max']) {
-						$returnValue = true;
-					}
-				}
-			} else {
-				$returnValue = true;
-			}
-		}
-
-		//Test less, greater, equal to another
-		if ($returnValue && isset($this->options['integer2_ref']) && $this->options['integer2_ref'] instanceof tao_helpers_form_FormElement) {
-			$secondElement = $this->options['integer2_ref'];
-			switch ($this->options['comparator']) {
-				case '>':
-				case 'sup':
-					if ($value > $secondElement->getRawValue()) {
-					    $returnValue = true;
-					}
-					else {
+        
+        $rowValue = $values;
+        $value = tao_helpers_Numeric::parseFloat($rowValue);
+        if (empty($rowValue)) {
+            $returnValue = true; // no need to go further. To check if not empty, use the NotEmpty validator
+            return $returnValue;
+        }
+        if (! is_numeric($rowValue) || $value != $rowValue) {
+            $this->message = __('The value of this field must be numeric');
+            $returnValue = false;
+        } else {
+            if (isset($this->options['min']) || isset($this->options['max'])) {
+                $this->message = __('Invalid field range');
+                
+                if (isset($this->options['min']) && isset($this->options['max'])) {
+                    $this->message .= ' (' . __('minimum value: ') . $this->options['min'] . ', ' . __('maximum value: ') . $this->options['max'] . ')';
+                    
+                    if ($this->options['min'] <= $value && $value <= $this->options['max']) {
+                        $returnValue = true;
+                    }
+                } else 
+                    if (isset($this->options['min']) && ! isset($this->options['max'])) {
+                        $this->message .= ' (' . __('minimum value: ') . $this->options['min'] . ')';
+                        
+                        if ($this->options['min'] <= $value) {
+                            $returnValue = true;
+                        }
+                    } else 
+                        if (! isset($this->options['min']) && isset($this->options['max'])) {
+                            $this->message .= ' (' . __('maximum value: ') . $this->options['max'] . ')';
+                            
+                            if ($value <= $this->options['max']) {
+                                $returnValue = true;
+                            }
+                        }
+            } else {
+                $returnValue = true;
+            }
+        }
+        
+        // Test less, greater, equal to another
+        if ($returnValue && isset($this->options['integer2_ref']) && $this->options['integer2_ref'] instanceof tao_helpers_form_FormElement) {
+            $secondElement = $this->options['integer2_ref'];
+            switch ($this->options['comparator']) {
+                case '>':
+                case 'sup':
+                    if ($value > $secondElement->getRawValue()) {
+                        $returnValue = true;
+                    } else {
                         $returnValue = false;
                     }
-					break;
-
-				case '<':
-				case 'inf':
-					if ($value < $secondElement->getRawValue()) {
-					    $returnValue = true;
-					}
-					else {
+                    break;
+                
+                case '<':
+                case 'inf':
+                    if ($value < $secondElement->getRawValue()) {
+                        $returnValue = true;
+                    } else {
                         $returnValue = false;
                     }
-					break;
-
-				case '=':
-				case 'equal':
-					if ($value == $secondElement->getRawValue()) {
-					    $returnValue = true;
-					}
-					else {
+                    break;
+                
+                case '=':
+                case 'equal':
+                    if ($value == $secondElement->getRawValue()) {
+                        $returnValue = true;
+                    } else {
                         $returnValue = false;
                     }
-					break;
-			}
-		}
-
-        // section 127-0-1-1-d42bee:127af842275:-8000:0000000000002386 end
-
+                    break;
+            }
+        }
+        
         return (bool) $returnValue;
     }
-
-} /* end of class tao_helpers_form_validators_Numeric */
+}
 
 ?>
