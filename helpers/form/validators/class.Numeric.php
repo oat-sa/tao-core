fe6139bc7103ff4d6bd6c31a7210b139e380bbfe
llecaque <llecaque@62dfeb86-a93f-4914-9d13-15c0a160985d>
2013-09-18 09:31:15 +0000

    inline control are forbidden
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/tao/trunk@16284 62dfeb86-a93f-4914-9d13-15c0a160985d

--- helpers/form/validators/class.Numeric.php
+++ helpers/form/validators/class.Numeric.php
@@ -1,5 +1,5 @@
 <?php
-/*  
+/**  
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; under version 2
@@ -18,50 +18,12 @@
  *               2009-2012 (update and modification) Public Research Centre Henri Tudor (under the project TAO-SUSTAIN & TAO-DEV);
  * 
  */
-?>
-<?php
 
-error_reporting(E_ALL);
-
-/**
- * TAO - tao/helpers/form/validators/class.Numeric.php
- *
- * $Id$
- *
- * This file is part of TAO.
- *
- * Automatically generated on 30.05.2012, 11:09:47 with ArgoUML PHP module
- * (last revised $Date: 2010-01-12 20:14:42 +0100 (Tue, 12 Jan 2010) $)
- *
- * @author Jehan Bihin, <jehan.bihin@tudor.lu>
- * @package tao
- * @subpackage helpers_form_validators
- */
-
-if (0 > version_compare(PHP_VERSION, '5')) {
-    die('This file was generated for PHP 5');
-}
 
 /**
  * The validators enable you to perform a validation callback on a form element.
  * It's provide a model of validation and must be overriden.
  *
- * @author Jehan Bihin, <jehan.bihin@tudor.lu>
- */
-require_once('tao/helpers/form/class.Validator.php');
-
-/* user defined includes */
-// section 127-0-1-1-d42bee:127af842275:-8000:0000000000002380-includes begin
-// section 127-0-1-1-d42bee:127af842275:-8000:0000000000002380-includes end
-
-/* user defined constants */
-// section 127-0-1-1-d42bee:127af842275:-8000:0000000000002380-constants begin
-// section 127-0-1-1-d42bee:127af842275:-8000:0000000000002380-constants end
-
-/**
- * Short description of class tao_helpers_form_validators_Numeric
- *
- * @access public
  * @author Jehan Bihin, <jehan.bihin@tudor.lu>
  * @package tao
  * @subpackage helpers_form_validators
@@ -150,20 +112,32 @@ class tao_helpers_form_validators_Numeric
 			switch ($this->options['comparator']) {
 				case '>':
 				case 'sup':
-					if ($value > $secondElement->getRawValue()) $returnValue = true;
-					else $returnValue = false;
+					if ($value > $secondElement->getRawValue()) {
+					    $returnValue = true;
+					}
+					else {
+                        $returnValue = false;
+                    }
 					break;
 
 				case '<':
 				case 'inf':
-					if ($value < $secondElement->getRawValue()) $returnValue = true;
-					else $returnValue = false;
+					if ($value < $secondElement->getRawValue()) {
+					    $returnValue = true;
+					}
+					else {
+                        $returnValue = false;
+                    }
 					break;
 
 				case '=':
 				case 'equal':
-					if ($value == $secondElement->getRawValue()) $returnValue = true;
-					else $returnValue = false;
+					if ($value == $secondElement->getRawValue()) {
+					    $returnValue = true;
+					}
+					else {
+                        $returnValue = false;
+                    }
 					break;
 			}
 		}
