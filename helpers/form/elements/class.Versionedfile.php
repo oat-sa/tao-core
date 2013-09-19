406aa5232ad71fea892ad265e36a17bf4e2586e7
jbout <jbout@62dfeb86-a93f-4914-9d13-15c0a160985d>
2013-09-19 14:46:28 +0000

    added missing abstract
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/tao/trunk@16359 62dfeb86-a93f-4914-9d13-15c0a160985d

--- helpers/form/elements/class.Versionedfile.php
+++ helpers/form/elements/class.Versionedfile.php
@@ -18,44 +18,6 @@
  *               2009-2012 (update and modification) Public Research Centre Henri Tudor (under the project TAO-SUSTAIN & TAO-DEV);
  * 
  */
-?>
-<?php
-
-error_reporting(E_ALL);
-
-/**
- * TAO - tao/helpers/form/elements/class.Versionedfile.php
- *
- * $Id$
- *
- * This file is part of TAO.
- *
- * Automatically generated on 12.04.2012, 12:31:20 with ArgoUML PHP module 
- * (last revised $Date: 2010-01-12 20:14:42 +0100 (Tue, 12 Jan 2010) $)
- *
- * @author Somsack Sipasseuth, <somsack.sipasseuth@tudor.lu>
- * @package tao
- * @subpackage helpers_form_elements
- */
-
-if (0 > version_compare(PHP_VERSION, '5')) {
-    die('This file was generated for PHP 5');
-}
-
-/**
- * Represents a FormElement entity
- *
- * @author Somsack Sipasseuth, <somsack.sipasseuth@tudor.lu>
- */
-require_once('tao/helpers/form/class.FormElement.php');
-
-/* user defined includes */
-// section 127-0-1-1-1ae05cc0:132f22f86c1:-8000:0000000000003F4D-includes begin
-// section 127-0-1-1-1ae05cc0:132f22f86c1:-8000:0000000000003F4D-includes end
-
-/* user defined constants */
-// section 127-0-1-1-1ae05cc0:132f22f86c1:-8000:0000000000003F4D-constants begin
-// section 127-0-1-1-1ae05cc0:132f22f86c1:-8000:0000000000003F4D-constants end
 
 /**
  * Short description of class tao_helpers_form_elements_Versionedfile
@@ -65,7 +27,7 @@ require_once('tao/helpers/form/class.FormElement.php');
  * @package tao
  * @subpackage helpers_form_elements
  */
-class tao_helpers_form_elements_Versionedfile
+abstract class tao_helpers_form_elements_Versionedfile
     extends tao_helpers_form_FormElement
 {
     // --- ASSOCIATIONS ---
