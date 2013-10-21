b223daa25811f0b2efebd7c1860d5d21698aa231
pplichart <pplichart@62dfeb86-a93f-4914-9d13-15c0a160985d>
2013-10-21 14:05:17 +0000

    header fixed for OAT copyright
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/tao/trunk@16841 62dfeb86-a93f-4914-9d13-15c0a160985d

--- test/OntoLockTestCase.php
+++ test/OntoLockTestCase.php
@@ -14,9 +14,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  * 
- * Copyright (c) 2008-2010 (original work) Deutsche Institut für Internationale Pädagogische Forschung (under the project TAO-TRANSFER);
- *               2009-2012 (update and modification) Public Research Centre Henri Tudor (under the project TAO-SUSTAIN & TAO-DEV);
- * 
+ * Copyright (c) 2013 Open Assessment Technologies S.A.
  */
 
 require_once dirname(__FILE__) . '/TaoTestRunner.php';
