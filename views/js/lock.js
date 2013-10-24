2f4c469ab493259970adf8f20387031fce56f26f
pplichart <pplichart@62dfeb86-a93f-4914-9d13-15c0a160985d>
2013-10-24 13:18:48 +0000

    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/tao/trunk@16883 62dfeb86-a93f-4914-9d13-15c0a160985d

--- views/js/lock.js
+++ views/js/lock.js
@@ -36,11 +36,17 @@ Lock.prototype.release = function (){
 		   type: 'POST',
 		   dataType: 'json'};
     $.ajax(this.url, options).done(function(retData, textStatus, jqxhr){
+	
 	helpers._load(
 		helpers.getMainContainerSelector(),
-		helpers._url(ctx_form_action,ctx_form_module,ctx_form_extension),//how to retrieve current action, controller, ?
+		helpers._url(
+			ctx_form_action,
+			ctx_form_module,
+			ctx_form_extension
+		    ),//how to retrieve current action, controller, ?
 		data 
-	    );
+		);
+	
     }).fail(function(jqxhr){
 	alert(__('A problem occured when releasing the lock'));
     });
