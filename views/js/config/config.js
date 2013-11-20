d0b889a43ecc737d3fc8276fc91898d9e5d730e8
ssipasseuth <ssipasseuth@62dfeb86-a93f-4914-9d13-15c0a160985d>
2013-11-20 15:10:23 +0000

    added taoQtiItemCreator in paths
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/tao/trunk@17195 62dfeb86-a93f-4914-9d13-15c0a160985d

--- views/js/config/config.js
+++ views/js/config/config.js
@@ -1,26 +1,27 @@
 require.config({
-	baseUrl: taobase_www + 'js',
-	paths: {
-		'jqueryUI': ['jquery-ui-1.8.23.custom.min'],
-                'lodash': ['lodash.min'],
-                'moment': ['moment.min'],
-                'taoQtiTest': '../../../taoQtiTest/views/js',
-                'cards': '../../../taoQtiTest/views/js/cards',
-                'ckeditor-jquery' : ['ckeditor/adapters/jquery']
-	},
-	shim: {
-		'jqueryUI': ['jquery'],
-		'jsTree/plugins/jquery.tree.contextmenu': ['jsTree/jquery.tree'],
-		'jsTree/plugins/jquery.tree.checkbox': ['jsTree/jquery.tree'],
-		'generis.tree.select': ['generis.tree', 'jsTree/plugins/jquery.tree.checkbox'],
-		'generis.tree.browser': ['generis.tree', 'jsTree/plugins/jquery.tree.contextmenu'],
-		'grid/tao.grid': ['jquery.jqGrid-4.4.0/js/jquery.jqGrid.min', 'jquery.jqGrid-4.4.0/js/i18n/grid.locale-' + base_lang],
-		'grid/tao.grid.downloadFileResource': ['grid/tao.grid'],
-		'grid/tao.grid.rowId': ['grid/tao.grid'],
-		'AsyncFileUpload': ['jquery.uploadify/swfobject', 'jquery.uploadify/jquery.uploadify.v2.1.4.min'],
-                'handlebars': { exports : 'Handlebars' },
-                'moment': { exports : 'moment' },
-                'ckeditor/ckeditor' : {  exports : 'CKEDITOR' },
-                'ckeditor-jquery' : ['ckeditor/ckeditor']
-	}
+    baseUrl : taobase_www + 'js',
+    paths : {
+        'jqueryUI' : ['jquery-ui-1.8.23.custom.min'],
+        'lodash' : ['lodash.min'],
+        'moment' : ['moment.min'],
+        'taoQtiTest' : '../../../taoQtiTest/views/js',
+        'cards' : '../../../taoQtiTest/views/js/cards',
+        'ckeditor-jquery' : ['ckeditor/adapters/jquery'],
+        'taoQtiItemCreator' : '../../../taoQTI/views/js/qtiCreator'
+    },
+    shim : {
+        'jqueryUI' : ['jquery'],
+        'jsTree/plugins/jquery.tree.contextmenu' : ['jsTree/jquery.tree'],
+        'jsTree/plugins/jquery.tree.checkbox' : ['jsTree/jquery.tree'],
+        'generis.tree.select' : ['generis.tree', 'jsTree/plugins/jquery.tree.checkbox'],
+        'generis.tree.browser' : ['generis.tree', 'jsTree/plugins/jquery.tree.contextmenu'],
+        'grid/tao.grid' : ['jquery.jqGrid-4.4.0/js/jquery.jqGrid.min', 'jquery.jqGrid-4.4.0/js/i18n/grid.locale-' + base_lang],
+        'grid/tao.grid.downloadFileResource' : ['grid/tao.grid'],
+        'grid/tao.grid.rowId' : ['grid/tao.grid'],
+        'AsyncFileUpload' : ['jquery.uploadify/swfobject', 'jquery.uploadify/jquery.uploadify.v2.1.4.min'],
+        'handlebars' : {exports : 'Handlebars'},
+        'moment' : {exports : 'moment'},
+        'ckeditor/ckeditor' : {exports : 'CKEDITOR'},
+        'ckeditor-jquery' : ['ckeditor/ckeditor']
+    }
 });
