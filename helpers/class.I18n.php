7da30d6c43cbe747dbcfaf6e7e36d17a8ab80a3f
jbout <jbout@62dfeb86-a93f-4914-9d13-15c0a160985d>
2013-12-05 13:37:30 +0000

    added _isRtl()
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/tao/trunk@17397 62dfeb86-a93f-4914-9d13-15c0a160985d

--- helpers/class.I18n.php
+++ helpers/class.I18n.php
@@ -1,5 +1,5 @@
 <?php
-/*  
+/**  
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; under version 2
@@ -16,32 +16,9 @@
  * 
  * Copyright (c) 2008-2010 (original work) Deutsche Institut für Internationale Pädagogische Forschung (under the project TAO-TRANSFER);
  *               2009-2012 (update and modification) Public Research Centre Henri Tudor (under the project TAO-SUSTAIN & TAO-DEV);
+ *               2013 (update and modification) Open Assessment Technologies SA (under the project TAO-PRODUCT);
  * 
  */
-?>
-<?php
-
-error_reporting(E_ALL);
-
-/**
- * Internationalization helper.
- *
- * @author Jerome Bogaerts, <jerome.bogaerts@tudor.lu>
- * @package tao
- * @subpackage helpers
- */
-
-if (0 > version_compare(PHP_VERSION, '5')) {
-    die('This file was generated for PHP 5');
-}
-
-/* user defined includes */
-// section 127-0-1-1--7d879eb4:12693e522d7:-8000:0000000000001E7B-includes begin
-// section 127-0-1-1--7d879eb4:12693e522d7:-8000:0000000000001E7B-includes end
-
-/* user defined constants */
-// section 127-0-1-1--7d879eb4:12693e522d7:-8000:0000000000001E7B-constants begin
-// section 127-0-1-1--7d879eb4:12693e522d7:-8000:0000000000001E7B-constants end
 
 /**
  * Internationalization helper.
@@ -65,14 +42,6 @@ class tao_helpers_I18n
      * @var string
      */
     const AVAILABLE_LANGS_CACHEKEY = 'i18n_available_langs';
-
-    /**
-     * Short description of attribute langCode
-     *
-     * @access private
-     * @var string
-     */
-    private static $langCode = '';
 
     /**
      * Short description of attribute availableLangs
@@ -101,13 +70,6 @@ class tao_helpers_I18n
     		throw new Exception("Language is not defined");
     	}
     	
-		self::$langCode = $langCode;
-		
-		
-		//only for backward compatibility
-		$_SESSION['lang'] = self::$langCode;
-		$_SESSION['ui_lang'] = self::$langCode;
-
 		//init the ClearFw l10n tools
 		l10n::init();
 		
@@ -126,11 +88,11 @@ class tao_helpers_I18n
 			}
 		}
 		
-		l10n::set($basePath . 'locales' . DIRECTORY_SEPARATOR . self::$langCode. DIRECTORY_SEPARATOR . 'messages');
+		l10n::set($basePath . 'locales' . DIRECTORY_SEPARATOR . $langCode. DIRECTORY_SEPARATOR . 'messages');
 		
         if (PHP_SAPI != 'cli') {    
     		tao_helpers_Scriptloader::addJsFiles(array(
-    			$baseURL . 'locales/' .self::$langCode. '/messages_po.js',
+    			$baseURL . 'locales/' .$langCode. '/messages_po.js',
     			TAOBASE_WWW . 'js/i18n.js',
     		));
         }
@@ -138,27 +100,18 @@ class tao_helpers_I18n
     }
 
     /**
-     * Short description of method getLangCode
+     * Returns the current interface language for backwards compatibility
      *
      * @access public
-     * @author Jerome Bogaerts, <jerome.bogaerts@tudor.lu>
      * @return string
      */
     public static function getLangCode()
     {
-        $returnValue = (string) '';
-
-        // section 127-0-1-1-12d76932:128aaed4c91:-8000:0000000000001FAA begin
-        
-        $returnValue = self::$langCode;
-        
-        // section 127-0-1-1-12d76932:128aaed4c91:-8000:0000000000001FAA end
-
-        return (string) $returnValue;
+        return common_session_SessionManager::getSession()->getInterfaceLanguage();
     }
 
     /**
-     * Short description of method getLangResourceByCode
+     * Returns the code of a resource
      *
      * @access public
      * @author Jerome Bogaerts, <jerome.bogaerts@tudor.lu>
@@ -167,25 +120,19 @@ class tao_helpers_I18n
      */
     public static function getLangResourceByCode($code)
     {
-        $returnValue = null;
-
-        // section 127-0-1-1-3cbd0a97:12a8039803a:-8000:0000000000002491 begin
-        
-        if(!empty($code)){
-	        $langClass = new core_kernel_classes_Class(CLASS_LANGUAGES);
-	        $valueProperty = new core_kernel_classes_Property(RDF_VALUE);
-                foreach($langClass->getInstances() as $lang){
-                        $lgPropertyValue = $lang->getUniquePropertyValue($valueProperty);
-                        if(trim($lgPropertyValue) == trim($code)){
-                                $returnValue = $lang;
-                                break;
-                        }
-                }
-        }
-        
-        // section 127-0-1-1-3cbd0a97:12a8039803a:-8000:0000000000002491 end
-
-        return $returnValue;
+        return isset($langs[$code]) ? new core_kernel_classes_Resource($langs[$code]['uri']) : null; 
+    }
+    
+    /**
+     * @param unknown $code
+     * @return string
+     */
+    public static function isLanguageRightToLeft($code)
+    {
+        $orientation = null;
+        $langs = self::getAvailableLangs();
+        $orientation = isset($langs[$code]) ? $langs[$code][PROPERTY_LANGUAGE_ORIENTATION] : null; 
+        return $orientation == INSTANCE_ORIENTATION_RTL;
     }
 
     /**
@@ -196,12 +143,8 @@ class tao_helpers_I18n
      * @param  boolean langName If set to true, an associative array where keys are language codes and values are language labels. If set to false (default), a simple array of language codes is returned.
      * @return array
      */
-    public static function getAvailableLangs($langName = false)
+    private static function getAvailableLangs()
     {
-        $returnValue = array();
-
-        // section 127-0-1-1-3cbd0a97:12a8039803a:-8000:0000000000002494 begin
-        
         //get it into the api only once 
         if(count(self::$availableLangs) == 0){
         	try {
@@ -210,26 +153,34 @@ class tao_helpers_I18n
 	        	$langClass = new core_kernel_classes_Class(CLASS_LANGUAGES);
 	        	$valueProperty = new core_kernel_classes_Property(RDF_VALUE);
 	        	foreach($langClass->getInstances() as $lang){
-	               	self::$availableLangs[] = $lang->getUniquePropertyValue($valueProperty)->literal;
+	        	    $values = $lang->getPropertiesValues(array(
+	        	    	RDF_VALUE,
+	        	        PROPERTY_LANGUAGE_USAGES,
+	        	        PROPERTY_LANGUAGE_ORIENTATION
+	        	    ));
+	        	    if (count($values[RDF_VALUE]) != 1) {
+	        	        throw new common_exception_InconsistentData('Error with value of language '.$lang->getUri());
+	        	    }
+	        	    $value = current($values[RDF_VALUE])->__toString();
+	        	    $usages = array();
+	        	    foreach ($values[PROPERTY_LANGUAGE_USAGES] as $usage) {
+	        	        $usages[] = $usage->getUri();
+	        	    }
+	        	    if (count($values[PROPERTY_LANGUAGE_ORIENTATION]) != 1) {
+	        	        throw new common_exception_InconsistentData('Error with orrientation of language '.$lang->getUri());
+	        	    }
+	        	    $orientation = current($values[PROPERTY_LANGUAGE_ORIENTATION])->getUri();
+	        	    self::$availableLangs[$value] = array(
+	        	        'uri'                         => $lang->getUri(), 
+	        	        PROPERTY_LANGUAGE_USAGES      => $usages,
+	        	        PROPERTY_LANGUAGE_ORIENTATION => $orientation
+	        	    );
 	        	}
-	        	
 	        	common_cache_FileCache::singleton()->put(self::$availableLangs, self::AVAILABLE_LANGS_CACHEKEY);
         	}
         }
-	
-        if($langName) {
-        	foreach(self::$availableLangs as $code){
-        		$lang = self::getLangResourceByCode($code);
-       			$returnValue[$code] = $lang->getLabel(); 
-        	}
-        }
-        else{
-       		$returnValue = self::$availableLangs;
-        }
         
-        // section 127-0-1-1-3cbd0a97:12a8039803a:-8000:0000000000002494 end
-
-        return (array) $returnValue;
+        return self::$availableLangs;
     }
 
     /**
@@ -244,28 +195,22 @@ class tao_helpers_I18n
     public static function getAvailableLangsByUsage( core_kernel_classes_Resource $usage)
     {
         $returnValue = array();
+        
+        foreach (self::getAvailableLangs() as $code => $langData) {
+            if (in_array($usage->getUri(), $langData[PROPERTY_LANGUAGE_USAGES])) {
+                $lang = new core_kernel_classes_Resource($langData['uri']);
+                $returnValue[$code] = $lang->getLabel();
+            }
+        }
+        return $returnValue;
 
-        // section 10-13-1-85-4d042f2c:13ada6173fa:-8000:0000000000003C1B begin
-    	$langClass = new core_kernel_classes_Class(CLASS_LANGUAGES);
-	    $valueProperty = new core_kernel_classes_Property(RDF_VALUE);
-	    $usageProperty = new core_kernel_classes_Property(PROPERTY_LANGUAGE_USAGES);
-	    foreach($langClass->getInstances() as $lang){
-	    	$code = $lang->getUniquePropertyValue($valueProperty)->literal;
-	    	$lang = self::getLangResourceByCode($code);
-	    	$usages = $lang->getPropertyValues($usageProperty);
-	    	
-	    	foreach ($usages as $u){
-	    		if ($u == $usage->getUri()){
-	    			$returnValue[$code] = $lang->getLabel();
-	    			break;
-	    		}
-	    	}
-	    }
         // section 10-13-1-85-4d042f2c:13ada6173fa:-8000:0000000000003C1B end
 
         return (array) $returnValue;
     }
 
-} /* end of class tao_helpers_I18n */
+}
 
-?>
+function _isRtl() {
+    return tao_helpers_I18n::isLanguageRightToLeft(common_session_SessionManager::getSession()->getInterfaceLanguage());
+}
