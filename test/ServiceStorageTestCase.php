2cf5368c008459c8415a9992957ef23bcea7ff69
jbout <jbout@62dfeb86-a93f-4914-9d13-15c0a160985d>
2013-12-09 10:50:41 +0000

    corrected testcase name
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/tao/trunk@17430 62dfeb86-a93f-4914-9d13-15c0a160985d

--- test/ServiceStorageTestCase.php
+++ test/ServiceStorageTestCase.php
@@ -21,7 +21,7 @@
 require_once dirname(__FILE__) . '/../../tao/test/TaoTestRunner.php';
 include_once dirname(__FILE__) . '/../includes/raw_start.php';
 
-class ServiceServiceTestCase extends UnitTestCase {
+class ServiceStorageTestCase extends UnitTestCase {
 
 	/**
 	 * tests initialization
