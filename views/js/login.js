213c6fc2f008a019e362f85bf211ae8d390f3cef
bertrand <bertrand@62dfeb86-a93f-4914-9d13-15c0a160985d>
2014-01-28 16:05:23 +0000

    Remove jquery from the login page
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/tao/trunk@18165 62dfeb86-a93f-4914-9d13-15c0a160985d

--- views/js/login.js
+++ views/js/login.js
@@ -1,16 +1,8 @@
-//loading the main config isn't usefull since we just need jquery
-require(['lib/jquery-1.8.0.min'], function() {
+define(function() {
     'use strict';
 
     /**
      * Set focus on the login field.
      */
-    function focusFirstField() {
-        $('input[name="login"]').focus();
-    }
-    
-    //dom ready
-    $(function() {
-        focusFirstField();
-    });
+     document.getElementById('login').focus();
 });
