e8972e57653cb20d5b337669ce5946860ca7a298
draber <draber@62dfeb86-a93f-4914-9d13-15c0a160985d>
2014-02-21 09:48:22 +0000

    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/tao/trunk@18696 62dfeb86-a93f-4914-9d13-15c0a160985d

--- views/js/lib/ckeditor/config.js
+++ views/js/lib/ckeditor/config.js
@@ -4,8 +4,27 @@
  */
 
 CKEDITOR.editorConfig = function( config ) {
-	// Define changes to default configuration here. For example:
-	// config.language = 'fr';
-	// config.uiColor = '#AADC6E';
-  config.skin= 'tao';
+  // Define changes to default configuration here. For example:
+  // config.language = 'fr';
+  // config.uiColor = '#AADC6E';
+
+  config.skin = 'tao';
+  config.toolbar = 'Block';
+  //config.toolbar = 'Inline';
+  config.toolbar_Inline = [
+    { name: 'clipboard', groups: [ 'undo' ], items: [ 'Undo', 'Redo' ] },
+    { name: 'insert', items: [ 'SpecialChar' ] },
+    { name: 'basicstyles', groups: [ 'basicstyles' ], items: [ 'Bold', 'Italic', 'Subscript', 'Superscript' ] },
+    { name: 'links', items: [ 'Link', 'Unlink', 'Anchor' ] }
+  ];
+  config.toolbar_Block = [
+    { name: 'clipboard', groups: [ 'clipboard', 'undo' ], items: [ 'Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo' ] },
+    { name: 'insert', items: [ 'Image', 'Table', 'SpecialChar' ] },
+    '/',
+    { name: 'basicstyles', groups: [ 'basicstyles' ], items: [ 'Bold', 'Italic', 'Subscript', 'Superscript' ] },
+    { name: 'links', items: [ 'Link', 'Unlink', 'Anchor' ] },
+    { name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align' ], items: [ 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock'] }
+
+    //  { name: 'styles', items: [ 'Styles' ] }, // not for now, might be nice at some point to enter custom feedback and such
+  ];
 };
