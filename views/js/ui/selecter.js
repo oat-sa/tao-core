34f8e85149b368821b966c47ffc9391c67f6dbfe
draber <draber@62dfeb86-a93f-4914-9d13-15c0a160985d>
2014-03-06 12:55:02 +0000

    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/tao/trunk@18912 62dfeb86-a93f-4914-9d13-15c0a160985d

--- views/js/ui/selecter.js
+++ views/js/ui/selecter.js
@@ -1,25 +1,42 @@
 define(['jquery', 'select2'], function($){
     'use strict';
-    
-    /**
-    * Look up for element in the container that will be used as select2 widgets
-    * 
-    * @public
-    * @example define(['ui/selecter'], function(selecter){ selecter($('rootContainer')); });
-    * @param {jQueryElement} $container - the root context to lookup inside
-    */
-    return function lookupSelecter($container){
-        $('.select2', $container).each(function() {
-            var $elt = $(this);
-            var hasSearch = false === $elt.data('has-search'); 
-            var options = {
-                width: '100%'
-            };
-            if(hasSearch) {
-                options.minimumResultsForSearch = -1;
-            }
 
-            $elt.select2(options);
-      });
+    /**
+     * Look up for element in the container that will be used as select2 widgets
+     *
+     * @public
+     * @example define(['ui/selecter'], function(selecter){ selecter($('rootContainer')); });
+     * @param {jQueryElement} $container - the root context to lookup inside
+     */
+    return function lookupSelecter($container) {
+        $('select.select2', $container).each(function () {
+            var $elt = $(this),
+                hasSearch = false === $elt.data('has-search'),
+                hasPlaceholder = !!($elt.attr('placeholder') || $elt.data('placeholder')),
+                hasSelectedIndex = (function(options) {
+                    var i = options.length,
+                        selected = false;
+                    while(i--) {
+                        if(typeof (options[i].getAttribute('selected')) === 'string'){
+                            selected = true;
+                            break;
+                        }
+                    }
+                    return selected;
+                }(this.options)),
+                settings = {
+                    width: '100%'
+                };
+            if(hasPlaceholder && this.options[0].text) {
+                $elt.prepend('<option>');
+                if(!hasSelectedIndex) {
+                    this.selectedIndex = this.options[0];
+                }
+            }
+            if (hasSearch) {
+                settings.minimumResultsForSearch = -1;
+            }
+            $elt.select2(settings);
+        });
     };
 });
