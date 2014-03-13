dccc85472106d118aa3457b6789cbc8607112ce4
jbout <jbout@62dfeb86-a93f-4914-9d13-15c0a160985d>
2014-03-13 10:01:07 +0000

    moved fileupload to data folder
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/tao/trunk@19016 62dfeb86-a93f-4914-9d13-15c0a160985d

--- scripts/install/addFileUploadSource.php
+++ scripts/install/addFileUploadSource.php
@@ -24,8 +24,11 @@
  * This post-installation script creates a new local file source for file uploaded
  * by end-users through the TAO GUI.
  */
-$extension = common_ext_ExtensionsManager::singleton()->getExtensionById('tao');
-$dataPath = $extension ->getConstant('BASE_PATH'). 'data' . DIRECTORY_SEPARATOR . 'upload' . DIRECTORY_SEPARATOR;
+
+$dataPath = FILES_PATH.'tao'.DIRECTORY_SEPARATOR.'upload'.DIRECTORY_SEPARATOR;
+if (file_exists($dataPath)) {
+    helpers_File::emptyDirectory($dataPath);
+}
 
 $source = tao_models_classes_FileSourceService::singleton()->addLocalSource('fileUploadDirectory', $dataPath);
 tao_models_classes_TaoService::singleton()->setUploadFileSource($source);
