df78c227e73d680f501fca5d29a161e5a03adbc3
ssipasseuth <ssipasseuth@62dfeb86-a93f-4914-9d13-15c0a160985d>
2014-03-14 16:27:49 +0000

    decimal option added to incrementer
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/tao/trunk@19057 62dfeb86-a93f-4914-9d13-15c0a160985d

--- views/js/test/ui/incrementer/test.js
+++ views/js/test/ui/incrementer/test.js
@@ -4,31 +4,52 @@ define(['jquery', 'ui',  'ui/incrementer'], function($, ui, incrementer){
     module('Incrementer Stand Alone Test');
    
     test('plugin', function(){
-       expect(1);
-       ok(typeof $.fn.incrementer === 'function', 'The Durationer plugin is registered');
+        expect(1);
+        ok(typeof $.fn.incrementer === 'function', 'The Durationer plugin is registered');
     });
-   
-    asyncTest('initialization', function(){
+
+    test('initialization', function(){
         expect(4);
-        
+
         var $container = $('#container-1');
         ok($container.length === 1, 'Test the fixture is available');
-        
+
         var $elt = $(':text', $container);
         ok($elt.length === 1, 'Test input is available');
-        
+
         $elt.on('create.incrementer', function(){
             ok(typeof $elt.data('ui.incrementer') === 'object');
-            
+
             var $control = $container.find('.ctrl > a');
             equal($control.length, 2, 'The plugins has created controls');
-            
-            start();
         });
         $elt.incrementer();
     });
-      
-     asyncTest('update seconds', function(){
+
+    test('update seconds', function(){
+        expect(3);
+
+        var $container = $('#container-1');
+        ok($container.length === 1, 'Test the fixture is available');
+
+        var $elt = $(':text', $container);
+        ok($elt.length === 1, 'Test input is available');
+
+        $elt.on('create.incrementer', function(){
+            $container.find('.ctrl > a.inc').click();
+        });
+        $elt.on('increment.incrementer', function(){
+            equal($elt.val(), 2, "The value has been incremented");
+        });
+
+        $elt.incrementer({
+            min : 0,
+            max : 10,
+            step : 2
+        });
+    });
+    
+    test('increment decimal 0.5 + 1.00 = 1', function(){
         expect(3);
         
         var $container = $('#container-1');
@@ -36,26 +57,90 @@ define(['jquery', 'ui',  'ui/incrementer'], function($, ui, incrementer){
         
         var $elt = $(':text', $container);
         ok($elt.length === 1, 'Test input is available');
+        $elt.val(0.5);
+        
+        $elt.on('create.incrementer', function(){
+            $container.find('.ctrl > a.inc').click();
+        });
+         $elt.on('increment.incrementer', function(){
+            equal($elt.val(), 1, "The value has been incremented");
+        });
+        
+        $elt.incrementer({
+            step: 1.00
+        });
+        
+    });
+    
+    test('increment decimal 1.01 + 1.00 = 2', function(){
+        expect(3);
+        
+        var $container = $('#container-1');
+        ok($container.length === 1, 'Test the fixture is available');
+        
+        var $elt = $(':text', $container);
+        ok($elt.length === 1, 'Test input is available');
+        $elt.val(1.01);
         
         $elt.on('create.incrementer', function(){
             $container.find('.ctrl > a.inc').click();
         });
          $elt.on('increment.incrementer', function(){
             equal($elt.val(), 2, "The value has been incremented");
-            
-            start();
         });
         
         $elt.incrementer({
-            min : 0,
-            max : 10,
-            step: 2
+            step: 1.00
+        });
+    });
+    
+    test('decrement decimal 0.5 - 1.00 = 0', function(){
+        expect(3);
+        
+        var $container = $('#container-1');
+        ok($container.length === 1, 'Test the fixture is available');
+        
+        var $elt = $(':text', $container);
+        ok($elt.length === 1, 'Test input is available');
+        $elt.val(0.5);
+        
+        $elt.on('create.incrementer', function(){
+            $container.find('.ctrl > a.dec').click();
+        });
+         $elt.on('decrement.incrementer', function(){
+            equal($elt.val(), 0, "The value has been decremented");
+        });
+        
+        $elt.incrementer({
+            step: 1.00
+        });
+    });
+    
+    test('decrement decimal 0 - 1.00 = -1', function(){
+        expect(3);
+        
+        var $container = $('#container-1');
+        ok($container.length === 1, 'Test the fixture is available');
+        
+        var $elt = $(':text', $container);
+        ok($elt.length === 1, 'Test input is available');
+        $elt.val(0);
+        
+        $elt.on('create.incrementer', function(){
+            $container.find('.ctrl > a.dec').click();
+        });
+         $elt.on('decrement.incrementer', function(){
+            equal($elt.val(), -1, "The value has been decremented");
+        });
+        
+        $elt.incrementer({
+            step: 1.00
         });
     });
     
      module('Incrementer Data Attr Test');
      
-     asyncTest('initialization', function(){
+     test('initialization', function(){
         expect(3);
         
         var $container = $('#container-2');
@@ -69,11 +154,24 @@ define(['jquery', 'ui',  'ui/incrementer'], function($, ui, incrementer){
         });
          $elt.on('increment.incrementer', function(){
             equal($elt.val(), 5, "The value has been incremented");
-            
-            start();
         });
        
         incrementer($container);
+    });
+    
+     test('decimal', function(){
+        
+        var $container = $('#container-3');
+        ok($container.length === 1, 'Test the fixture is available');
+        
+        var $elt = $(':text', $container);
+        ok($elt.length === 1, 'Test input is available');
+        
+        incrementer($container);
+        var options = $elt.data('ui.incrementer');
+        
+        equal(options.decimal, 2, 'option decimal ok');
+        equal(options.step, 1, 'option step ok');
     });
 });
 
--- views/js/ui/incrementer.js
+++ views/js/ui/incrementer.js
@@ -4,26 +4,26 @@
  * @requires core/pluginifier
  */
 define(['jquery', 'lodash', 'core/pluginifier'], function($, _, Pluginifier){
-   'use strict';
-   
-   var ns = 'incrementer';
-   var dataNs = 'ui.' + ns;
-   
-   var defaults = {
-       step : 1,
-       min : null,
-       max : null,
-       incrementerClass : 'incrementer',
-       incrementerCtrlClass : 'incrementer-ctrl'
-   };
-   
-   /** 
-    * The Incrementer component, it transforms a text input in an number input, the data-attr way 
-    * (has the HTML5 number input type is not yet very well supported, we don't use polyfill to have a consistent UI) 
-    * @exports ui/incrementer
-    */
-   var Incrementer = {
-       
+    'use strict';
+
+    var ns = 'incrementer';
+    var dataNs = 'ui.' + ns;
+
+    var defaults = {
+        step : 1,
+        min : null,
+        max : null,
+        incrementerClass : 'incrementer',
+        incrementerCtrlClass : 'incrementer-ctrl',
+        decimal : 0
+    };
+
+    /** 
+     * The Incrementer component, it transforms a text input in an number input, the data-attr way 
+     * (has the HTML5 number input type is not yet very well supported, we don't use polyfill to have a consistent UI) 
+     * @exports ui/incrementer
+     */
+    var Incrementer = {
         /**
          * Initialize the plugin.
          * 
@@ -40,58 +40,66 @@ define(['jquery', 'lodash', 'core/pluginifier'], function($, _, Pluginifier){
          */
         init : function(options){
             var self = Incrementer;
-            
+
             //get options using default
             options = _.defaults(options || {}, defaults);
-           
-            return this.each(function() {
+
+            return this.each(function(){
                 var $elt = $(this);
                 var $ctrl, currentValue;
-                
+
                 if(!$elt.data(dataNs)){
-                
+
                     //basic type checking
                     if(!$elt.is('input[type="text"]')){
                         $.error('The incrementer plugin applies only on input element of type text');
-                    } else {
-                        currentValue = parseInt($elt.val(), 10);
+                    }else{
+                        currentValue = parseFloat($elt.val()).toFixed(options.decimal);
                         $elt.data(dataNs, options)                      //add data to the element
                             .addClass(options.incrementerClass)         //add the css class
-                            .after(                                     //set up controls
-                             "<div class='ctrl "+options.incrementerCtrlClass+"'>\
+                            .after(//set up controls
+                            "<div class='ctrl " + options.incrementerCtrlClass + "'>\
                                 <a href='#' class='inc' title='+' tabindex='-1'></a>\
                                 <a href='#' class='dec' title='-' tabindex='-1'></a>\
                               </div>")
-                           .on('keydown', function(e){  
-                                if(e.which === 38){                      //up
-                                    self._inc($elt);
-                                    this.select();
-                                } else if(e.which === 40){               //down
-                                    self._dec($elt);
-                                    this.select();
-                                } 
-                            })
-                            .on('keyup', function(){                   
-                               $elt.val($elt.val().replace(/[\D]/g, ''));       //allow only digits
-                            })
-                            .on('focus', function(){
+                            .on('keydown', function(e){
+                            if(e.which === 38){                      //up
+                                self._inc($elt);
                                 this.select();
-                            })
+                            }else if(e.which === 40){               //down
+                                self._dec($elt);
+                                this.select();
+                            }
+                        })
+                            .on('keyup', function(){
+//                            var val = $elt.val();
+//                            if(val.slice(-1) !== '.'){
+//                                var currentFloat = parseFloat(val);
+//                                if(self._decimalPlaces(currentFloat) > options.decimal){
+//                                    val = self._toFixedDown(currentFloat, options.decimal);
+//                                }
+//                            }
+//                            $elt.val(val);
+                            $elt.val($elt.val().replace(/[\D]/g, ''));
+                        })
+                            .on('focus', function(){
+                            this.select();
+                        })
                             .on('disable', function(){
-                                $ctrl.find('.inc,.dec').prop('disabled', true)
-                                                        .addClass('disabled');
-                            })
+                            $ctrl.find('.inc,.dec').prop('disabled', true)
+                                .addClass('disabled');
+                        })
                             .on('enable', function(){
-                                $ctrl.find('.inc,.dec').prop('disabled', false)
-                                                        .removeClass('disabled');
-                            });
+                            $ctrl.find('.inc,.dec').prop('disabled', false)
+                                .removeClass('disabled');
+                        });
 
                         //set up the default value if needed
-                        if(_.isNaN(currentValue) 
-                                || (options.min !== null && currentValue < options.min) 
-                                || (options.max !== null && currentValue > options.max) ) {
+                        if(_.isNaN(currentValue)
+                            || (options.min !== null && currentValue < options.min)
+                            || (options.max !== null && currentValue > options.max)){
                             $elt.val(options.min || 0);
-                        } 
+                        }
 
                         $ctrl = $elt.next('.' + options.incrementerCtrlClass);
 
@@ -101,7 +109,7 @@ define(['jquery', 'lodash', 'core/pluginifier'], function($, _, Pluginifier){
                                 self._inc($elt);
                             }
                         });
-                         $ctrl.find('.dec').click(function(e){
+                        $ctrl.find('.dec').click(function(e){
                             e.preventDefault();
                             if(!$(this).prop('disabled')){
                                 self._dec($elt);
@@ -116,70 +124,95 @@ define(['jquery', 'lodash', 'core/pluginifier'], function($, _, Pluginifier){
                     }
                 }
             });
-       },
-       
-       /**
-        * Increment value
-        * 
-        * @private
-        * @param {jQueryElement} $elt - plugin's element 
-        * @fires Incrementer#plus.incrementer
-        */
-       _inc: function($elt){
-           var options = $elt.data(dataNs);
-           var current = parseInt($elt.val(), 10);
-           var value = current + options.step;
-           
-           if(options.max === null || (_.isNumber(options.max) && value <= options.max)){
+        },
+        _toFixedDown : function(number, precision){
+            var m = Math.pow(10, precision||0);
+            return Math.floor(number * m) / m;
+        },
+        _decimalPlaces : function(number){
+            var match = ('' + number).match(/(?:\.(\d+))?(?:[eE]([+-]?\d+))?$/);
+            if(!match){
+                return 0;
+            }
+            return Math.max(0, (match[1] ? match[1].length : 0) - (match[2] ? +match[2] : 0));
+        },
+        /**
+         * Increment value
+         * 
+         * @private
+         * @param {jQueryElement} $elt - plugin's element 
+         * @fires Incrementer#plus.incrementer
+         */
+        _inc : function($elt){
+
+            var options = $elt.data(dataNs),
+                currentFloat = parseFloat($elt.val()),
+                stepDecimal = Incrementer._decimalPlaces(options.step),
+                current,
+                value;
+
+            if(Incrementer._decimalPlaces(currentFloat) > stepDecimal){
+                current = Incrementer._toFixedDown(currentFloat, stepDecimal);
+            }else{
+                current = parseFloat(currentFloat.toFixed(options.decimal));
+            }
+            var value = current + options.step;
+            if(options.max === null || (_.isNumber(options.max) && value <= options.max)){
                 $elt.val(value);
-            
-               /**
-                * The target has been toggled. 
-                * @event Incrementer#increment.incrementer
-                */
-                $elt.trigger('increment.' + ns, [value])
-                        .trigger('change');
-           } 
-       },
-       
-       /**
-        * Decrement value
-        * 
-        * @private
-        * @param {jQueryElement} $elt - plugin's element 
-        * @fires Incrementer#minus.incrementer
-        */
-       _dec: function($elt){
-           var options = $elt.data(dataNs);
-           var current = parseInt($elt.val(), 10);
-           var value = current - options.step;
-           
-           if(options.min === null ||  (_.isNumber(options.min) && value >= options.min)){
+
+                /**
+                 * The target has been toggled. 
+                 * @event Incrementer#increment.incrementer
+                 */
+                $elt.trigger('increment.' + ns, [value]).trigger('change');
+            }
+        },
+        /**
+         * Decrement value
+         * 
+         * @private
+         * @param {jQueryElement} $elt - plugin's element 
+         * @fires Incrementer#minus.incrementer
+         */
+        _dec : function($elt){
+
+            var options = $elt.data(dataNs),
+                currentFloat = parseFloat($elt.val()),
+                stepDecimal = Incrementer._decimalPlaces(options.step),
+                current,
+                value;
+
+            if(Incrementer._decimalPlaces(currentFloat) > stepDecimal){
+                value = Math.floor(currentFloat * Math.pow(10, stepDecimal)) / Math.pow(10, stepDecimal);
+            }else{
+                current = parseFloat(currentFloat.toFixed(options.decimal));
+                value = current - options.step;
+            }
+
+            if(options.min === null || (_.isNumber(options.min) && value >= options.min)){
                 $elt.val(value);
-            
-               /**
-                * The target has been toggled. 
-                * @event Incrementer#decrement.incrementer
-                */
-                $elt.trigger('decrement.' + ns, [value])
-                        .trigger('change');
-           } 
-       },
-               
-       /**
-        * Destroy completely the plugin.
-        * 
-        * Called the jQuery way once registered by the Pluginifier.
-        * @example $('selector').incrementer('destroy');
-        * @public
-        */
-       destroy : function(){
-            this.each(function() {
+
+                /**
+                 * The target has been toggled. 
+                 * @event Incrementer#decrement.incrementer
+                 */
+                $elt.trigger('decrement.' + ns, [value]).trigger('change');
+            }
+        },
+        /**
+         * Destroy completely the plugin.
+         * 
+         * Called the jQuery way once registered by the Pluginifier.
+         * @example $('selector').incrementer('destroy');
+         * @public
+         */
+        destroy : function(){
+            this.each(function(){
                 var $elt = $(this);
                 var options = $elt.data(dataNs);
                 $elt.off('keyup keydown')
                     .siblings('.' + options.incrementerCtrlClass).remove();
-                
+
                 /**
                  * The plugin have been destroyed.
                  * @event Incrementer#destroy.incrementer
@@ -187,43 +220,47 @@ define(['jquery', 'lodash', 'core/pluginifier'], function($, _, Pluginifier){
                 $elt.trigger('destroy.' + ns);
             });
         }
-   };
-   
-   //Register the incrementer to behave as a jQuery plugin.
-   Pluginifier.register(ns, Incrementer);
-   
-   /**
-    * The only exposed function is used to start listening on data-attr
-    * 
-    * @public
-    * @example define(['ui/incrementer'], function(incrementer){ incrementer($('rootContainer')); });
-    * @param {jQueryElement} $container - the root context to listen in
-    */
-   return function listenDataAttr($container){
-       
-       $container.find('[data-increment]').each(function(){
-           var $elt = $(this);
-           var step = _.parseInt($elt.attr('data-increment'));
-           var min, max;
-           
-           var options = {};
-           if(!_.isNaN(step)){
-               options.step = step;
-           }
-           if($elt.attr('data-min')){
-               min = _.parseInt($elt.attr('data-min'));
-               if(!_.isNaN(min)){
+    };
+
+    //Register the incrementer to behave as a jQuery plugin.
+    Pluginifier.register(ns, Incrementer);
+
+    /**
+     * The only exposed function is used to start listening on data-attr
+     * 
+     * @public
+     * @example define(['ui/incrementer'], function(incrementer){ incrementer($('rootContainer')); });
+     * @param {jQueryElement} $container - the root context to listen in
+     */
+    return function listenDataAttr($container){
+
+        $container.find('[data-increment]').each(function(){
+            var $elt = $(this);
+            var decimal = Incrementer._decimalPlaces($elt.attr('data-increment'));
+            var step = parseFloat($elt.attr('data-increment'));
+            var min, max;
+
+            var options = {};
+            if(!_.isNaN(step)){
+                options.step = step;
+            }
+            if(!_.isNaN(decimal)){
+                options.decimal = decimal;
+            }
+            if($elt.attr('data-min')){
+                min = parseFloat($elt.attr('data-min'));
+                if(!_.isNaN(min)){
                     options.min = min;
                 }
-           }
-           if($elt.attr('data-max')){
-               max = _.parseInt($elt.attr('data-max'));
-               if(!_.isNaN(max)){
+            }
+            if($elt.attr('data-max')){
+                max = parseFloat($elt.attr('data-max'));
+                if(!_.isNaN(max)){
                     options.max = max;
                 }
-           }
-           $elt.incrementer(options);
-       });
+            }
+            $elt.incrementer(options);
+        });
     };
 });
 
