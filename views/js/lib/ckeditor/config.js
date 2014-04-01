dd76d348beb5ea38eb39a9895b971387b6752fa5
draber <draber@62dfeb86-a93f-4914-9d13-15c0a160985d>
2014-04-01 08:18:28 +0000

    added implementation-example.html for CKEDITOR and some lang files that are required
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/tao/trunk@19370 62dfeb86-a93f-4914-9d13-15c0a160985d

--- views/js/lib/ckeditor/config.js
+++ views/js/lib/ckeditor/config.js
@@ -5,7 +5,9 @@
 
 CKEDITOR.editorConfig = function(config) {
     // Define changes to default configuration here. For example:
-    // config.language = 'fr';
     // config.uiColor = '#AADC6E';
+
+
 };
 
+CKEDITOR.disableAutoInline = true;
