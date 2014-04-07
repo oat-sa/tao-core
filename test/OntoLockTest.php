6ef61672e3cf59e4ce81a0362a55d0d50e9add84
llecaque <llecaque@62dfeb86-a93f-4914-9d13-15c0a160985d>
2014-04-07 13:53:02 +0000

    rename testcase properly
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/tao/trunk@19490 62dfeb86-a93f-4914-9d13-15c0a160985d

--- test/OntoLockTest.php
+++ test/OntoLockTest.php
@@ -27,7 +27,7 @@ include_once dirname(__FILE__) . '/../includes/raw_start.php';
  * @package tao
  
  */
-class OntoLockTestCase extends TaoPhpUnitTestRunner {
+class OntoLockTest extends TaoPhpUnitTestRunner {
 	
     protected $tempResource = null;
     protected $lockService = null;
