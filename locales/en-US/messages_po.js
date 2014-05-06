56a3a801613c7a0cb21187e6e27794ba91af07f1
chazotte <chazotte@62dfeb86-a93f-4914-9d13-15c0a160985d>
2014-05-06 08:08:52 +0000

    globally change test taker wording to test-taker; localization should not be broken
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/tao/trunk@20079 62dfeb86-a93f-4914-9d13-15c0a160985d

--- locales/en-US/messages_po.js
+++ locales/en-US/messages_po.js
@@ -3,6 +3,6 @@
 define(function(){
  return {
    langCode : 'en-US',
-   i18n_tr : []
+   i18n_tr : {"Group test takers according to global features and classifications.":"Group test-takers according to global features and classifications.","Manage test takers":"Manage test-takers","Record and manage test takers.":"Record and manage test-takers.","Test takers":"Test-takers"}
  };
 });
