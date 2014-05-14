34ad19413f23cfa2ac8e53e83479f104bf731b48
draber <draber@62dfeb86-a93f-4914-9d13-15c0a160985d>
2014-05-14 09:53:12 +0000

    added resource manager to tao-media-manager plugin
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/tao/trunk@20243 62dfeb86-a93f-4914-9d13-15c0a160985d

--- views/js/lib/ckeditor/plugins/taomediamanager/plugin.js
+++ views/js/lib/ckeditor/plugins/taomediamanager/plugin.js
@@ -1,25 +1,39 @@
 // Register the plugin with the editor.
 // http://docs.cksource.com/ckeditor_api/symbols/CKEDITOR.plugins.html
+
+
 CKEDITOR.plugins.add('taomediamanager', {
     // The plugin initialization logic goes inside this method.
     // http://docs.cksource.com/ckeditor_api/symbols/CKEDITOR.pluginDefinition.html#init
-    init: function(editor) {
-        // Define an editor command that inserts a taomediamanager. 
+    init: function (editor) {
+        // Define an editor command that inserts a taomediamanager.
         // http://docs.cksource.com/ckeditor_api/symbols/CKEDITOR.editor.html#addCommand
-        var imgPath = this.path + 'images/';
+        var resourceMgrDefaults = {
+            params: {
+                lang: 'en-US',
+                filters: 'image/jpeg,image/png,image/gif'
+            },
+            pathParam: 'path',
+            select: function (e, uris) {
+                var i, l = uris.length;
+                for (i = 0; i < l; i++) {
+                    editor.insertHtml('<img src="' + uris[i] + '"/>')
+                }
+            }
+        };
+
 
         editor.addCommand('insertMedia', {
             // Define a function that will be fired when the command is executed.
             // http://docs.cksource.com/ckeditor_api/symbols/CKEDITOR.commandDefinition.html#exec
-            exec: function(editor) {
-
-                var img = confirm('Do you like Eric Cartman?') ? 'happy.png' : 'upset.png';
+            exec: function (editor) {
                 // Insert the taomediamanager into the document.
                 // http://docs.cksource.com/ckeditor_api/symbols/CKEDITOR.editor.html#insertHtml
-                editor.insertHtml('<img style="border: 2px red solid" src="' + imgPath + img + '"/>');
+                $('<div>').resourcemgr(_.defaults(editor.config.resourcemgr, resourceMgrDefaults));
+
             }
         });
-        // Create a toolbar button that executes the plugin command. 
+        // Create a toolbar button that executes the plugin command.
         // http://docs.cksource.com/ckeditor_api/symbols/CKEDITOR.ui.html#addButton
         editor.ui.addButton('TaoMediaManager', {
             // Toolbar button tooltip.
