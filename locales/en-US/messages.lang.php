b59307653440e998c5530b9372714fed2050cbc9
jbout <jbout@62dfeb86-a93f-4914-9d13-15c0a160985d>
2014-05-30 15:49:55 +0000

    updated and compiled translations
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/tao/trunk@20638 62dfeb86-a93f-4914-9d13-15c0a160985d

--- locales/en-US/messages.lang.php
+++ locales/en-US/messages.lang.php
@@ -2,6 +2,5 @@
 $GLOBALS['__l10n']["Group test takers according to global features and classifications."]="Group test-takers according to global features and classifications.";
 $GLOBALS['__l10n']["Manage test takers"]="Manage test-takers";
 $GLOBALS['__l10n']["Record and manage test takers."]="Record and manage test-takers.";
-$GLOBALS['__l10n']["Test takers"]="Test-takers";
 
 ?>
