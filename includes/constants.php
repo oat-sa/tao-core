660b2bc4bf2c0c494f5d1455a13ad05bb7c3b0df
llecaque <llecaque@62dfeb86-a93f-4914-9d13-15c0a160985d>
2014-06-17 09:43:33 +0000

    update constant
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/tao/trunk@20889 62dfeb86-a93f-4914-9d13-15c0a160985d

--- includes/constants.php
+++ includes/constants.php
@@ -30,10 +30,10 @@
  *
  */
 #TAO version number
-define('TAO_VERSION', '2.6-beta3');
+define('TAO_VERSION', '2.6-RC03');
 
 #TAO version label
-define('TAO_VERSION_NAME', 'v2.6-beta3');
+define('TAO_VERSION_NAME', 'v2.6-RC03');
 
 #the name to display
 define('PRODUCT_NAME', 'TAO');
