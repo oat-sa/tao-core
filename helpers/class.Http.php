f7211ea8ecf0977413e566f822319a25d09ae027
mgeorgiev <mgeorgiev@62dfeb86-a93f-4914-9d13-15c0a160985d>
2014-06-19 08:02:00 +0000

    Fixes serving of big files, video, audio and other kind of files. Partial contents are supported, and PHP session is closed, before file content starts being sent, so other requests in that session could be served without waiting for file sending to be done.
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/tao/trunk@20934 62dfeb86-a93f-4914-9d13-15c0a160985d

--- helpers/class.Http.php
+++ helpers/class.Http.php
@@ -45,7 +45,7 @@ class tao_helpers_Http
         } else {
             return false;
         }
-        
+
         return $digest;
     }
 
@@ -68,9 +68,9 @@ class tao_helpers_Http
         );
         $data = array();
         $keys = implode('|', array_keys($needed_parts));
-        
+
         preg_match_all('@(' . $keys . ')=(?:([\'"])([^\2]+?)\2|([^\s,]+))@', $digest, $matches, PREG_SET_ORDER);
-        
+
         foreach ($matches as $m) {
             $data[$m[1]] = $m[3] ? $m[3] : $m[4];
             unset($needed_parts[$m[1]]);
@@ -91,7 +91,7 @@ class tao_helpers_Http
     {
         return $_FILES;
     }
-   
+
     /**
      * Get the files data from an HTTP file upload (ie. from the $_FILES)
      * @author "Bertrand Chevrier <bertrand@taotesting.com>
@@ -102,12 +102,12 @@ class tao_helpers_Http
     public static function getUploadedFile($name)
     {
 
-        $files = self::getFiles(); 
+        $files = self::getFiles();
         $fileData = $files[$name];
-        if(isset($files[$name])){
+        if (isset($files[$name])) {
 
             //check for upload errors
-            if(isset($fileData['error']) && $fileData['error'] != UPLOAD_ERR_OK){
+            if (isset($fileData['error']) && $fileData['error'] != UPLOAD_ERR_OK) {
                 switch ($fileData['error']) {
                     case UPLOAD_ERR_NO_FILE:
                         throw new common_exception_Error('No file sent.');
@@ -118,11 +118,10 @@ class tao_helpers_Http
                         throw new common_exception_Error('Upload just fails.');
                 }
             }
-
         }
         return $fileData;
     }
- 
+
     /**
      * @author "Patrick Plichart, <patrick@taotesting.com>"
      * @param string $supportedMimeTypes
@@ -148,7 +147,7 @@ class tao_helpers_Http
             $acceptTypes[$a] = $q;
         }
         arsort($acceptTypes);
-        if (! $supportedMimeTypes) {
+        if (!$supportedMimeTypes) {
             return $AcceptTypes;
         }
         $supportedMimeTypes = array_map('strtolower', (array) $supportedMimeTypes);
@@ -161,8 +160,17 @@ class tao_helpers_Http
         throw new common_exception_NotAcceptable();
         return null;
     }
-    
-    public static function returnFile($filename) {
+
+    /**
+     * Sends file content to the client(browser or video/audio player in the browser), it serves images, video/audio files and any other type of file.<br />
+     * If the client asks for partial contents, then partial contents are served, if not, the whole file is send.<br />
+     * Works well with big files, without eating up memory.
+     * @author "Martin for OAT <code@taotesting.com>"
+     * @param string the file name
+     * @return mixed the file data
+     */
+    public static function returnFile($filename)
+    {
         if (tao_helpers_File::securityCheck($filename, true)) {
             if (file_exists($filename)) {
                 $mimeType = tao_helpers_File::getMimeType($filename, true);
@@ -171,14 +179,92 @@ class tao_helpers_Http
                 if ($fp === false) {
                     header("HTTP/1.0 404 Not Found");
                 } else {
-                    fpassthru($fp);
+
+                    // session must be closed because, for example, video files might take a while to be sent to the client
+                    //  and we need the client to be able to make other calls to the server during that time
+                    session_write_close();
+                    
+                    $http416RequestRangeNotSatisfiable = 'HTTP/1.1 416 Requested Range Not Satisfiable';
+                    $http206PartialContent = 'HTTP/1.1 206 Partial Content';
+                    $http200OK = 'HTTP/1.1 200 OK';
+                    $filesize = filesize($filename);
+                    $offset = 0;
+                    $length = $filesize;
+                    $useFpassthru = false;
+                    $partialContent = false;
+                    header('Accept-Ranges: bytes');
+                    
+                    if (isset($_SERVER['HTTP_RANGE'])) {
+                        $partialContent = true;
+                        preg_match('/bytes=(\d+)-(\d+)?/', $_SERVER['HTTP_RANGE'], $matches);
+                        $offset = intval($matches[1]);
+                        if (!isset($matches[2])) {
+                            // no end position is given, so we serve the file from the start position to the end
+                            $useFpassthru = true;
+                        } else {
+                            $length = intval($matches[2]) - $offset;
+                        }
+                    }
+                    
+                    fseek($fp, $offset);
+                    
+                    if ($partialContent) {
+                        if (($offset < 0) || ($offset > $filesize)) {
+                            header($http416RequestRangeNotSatisfiable);
+                        } else {
+                            if ($useFpassthru) {
+                                // only a starting position is given
+                                header($http206PartialContent);
+                                header("Content-Length: " . ($filesize - $offset));
+                                header('Content-Range: bytes ' . $offset . '-' . ($filesize - 1) . '/' . $filesize);
+                                if (ob_get_level() > 0) ob_end_flush();
+                                fpassthru($fp);
+                            } else {
+                                // we are given a starting position and how many bytes the client asks for
+                                $endPosition = $offset + $length;
+                                if ($endPosition > $filesize) {
+                                    header($http416RequestRangeNotSatisfiable);
+                                } else {
+                                    header($http206PartialContent);
+                                    header("Content-Length: " . ($length));
+                                    header('Content-Range: bytes ' . $offset . '-' . ($offset + $length - 1) . '/' . $filesize);
+                                    // send 500KB per cycle
+                                    $bytesPerCycle = (1024 * 1024) * 0.5;
+                                    $currentPosition = $offset;
+                                    if (ob_get_level() > 0) ob_end_flush();
+                                    // because the client might ask for the whole file, we split the serving into little pieces
+                                    // this is also good in case someone with bad intentions tries to get the whole file many times
+                                    //  and eat up the server memory, we are not loading the whole file into the memory.
+                                    while (!feof($fp)) {
+                                        if (($currentPosition + $bytesPerCycle) <= $endPosition) {
+                                            $data = fread($fp, $bytesPerCycle);
+                                            $currentPosition = $currentPosition + $bytesPerCycle;
+                                            echo $data;
+                                        } else {
+                                            $data = fread($fp, ($endPosition - $currentPosition));
+                                            echo $data;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    } else {
+                        // client does not want partial contents so we just serve the whole file
+                        header($http200OK);
+                        header("Content-Length: " . $filesize);
+                        if (ob_get_level() > 0) ob_end_flush();
+                        fpassthru($fp);
+                    }
+                    fclose($fp);
                 }
             } else {
                 header("HTTP/1.0 404 Not Found");
             }
         } else {
-            throw new common_exception_Error('Security exception for path '.$path);
+            throw new common_exception_Error('Security exception for path ' . $path);
         }
     }
+
 }
+
 ?>
