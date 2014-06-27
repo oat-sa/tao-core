0bd45fe60a6cbe2023676de3d0460c98ed288cb4
ssipasseuth <ssipasseuth@62dfeb86-a93f-4914-9d13-15c0a160985d>
2014-06-27 10:26:40 +0000

    added new option "validateOnInit" to trigger validation on init
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/tao/trunk@21088 62dfeb86-a93f-4914-9d13-15c0a160985d

--- views/js/ui/groupvalidator.js
+++ views/js/ui/groupvalidator.js
@@ -1,4 +1,4 @@
-define(['jquery', 'lodash', 'ui/validator'], function($, _) {
+define(['jquery', 'lodash', 'ui/validator'], function($, _){
 
     /**
      * Register a plugin to validate a group of elements
@@ -9,62 +9,71 @@ define(['jquery', 'lodash', 'ui/validator'], function($, _) {
      * @param {Object} options - the plugin options
      * @param {string} [options.errorClass = 'error'] - the class added to the element itself if the validation fails
      * @param {string} [options.errorMessageClass = 'validate-error'] - the class added to the inserted node that contains the failure message itself
+     * @param {string} [options.validateOnInit = false] - trigger validation upon initialization
      * @param {string|Array} [options.defaultEvent = ['change', 'blur']] - the default event that triggers the validation
      * @fires validated.group
      * @returns {jQueryElement} for chaining
      */
-    $.fn.groupValidator = function(options) {
+    $.fn.groupValidator = function(options){
 
         options = _.defaults(options || {}, $.fn.groupValidator.defaults);
-        
-        return this.each(function() {
+
+        return this.each(function(){
+
             var $container = $(this);
             var states = [];
+            var callback = function(valid, results){
 
-            $('[data-validate]', $container).validator({
-                event: options.events,
-                validated: function(valid, results) {
+                var $elt = $(this);
 
-                    var $elt = $(this);
+                //update global state
+                states[$elt.attr('name')] = valid;
 
-                    //update global state
-                    states[$elt.attr('name')] = valid;
+                //call custom callback
+                options.callback.call(this, valid, results, options);
 
-                    //call custom callback
-                    options.callback.call(this, valid, results, options);
+                //trigger event on single validation
+                $elt.trigger('validated.single', [valid]);
 
-                    //trigger event on single validation
-                    $elt.trigger('validated.single', [valid]);
+                /**
+                 * Gives the validation state of the entire group. 
+                 * Fired at each validation
+                 * @event validated.group
+                 * @param {boolean} isValid - wheter the group is valid 
+                 */
+                $container.trigger('validated.group', [_(states).values().contains(false) === false, this]);
+            };
 
-                    /**
-                     * Gives the validation state of the entire group. 
-                     * Fired at each validation
-                     * @event validated.group
-                     * @param {boolean} isValid - wheter the group is valid 
-                     */
-                    $container.trigger('validated.group', [_(states).values().contains(false) === false, this]);
-                }
+            var $toValidate = $('[data-validate]', $container).validator({
+                event : options.events,
+                validated : callback
             });
+
+            if(options.validateOnInit){
+                $toValidate.validator('validate', {}, callback);
+            }
+
         });
     };
 
     $.fn.groupValidator.defaults = {
-        errorClass: 'error',
-        errorMessageClass: 'validate-error',
-        events: ['change', 'blur'],
-        callback: function(valid, results, options) {
+        validateOnInit : false,
+        errorClass : 'error',
+        errorMessageClass : 'validate-error',
+        events : ['change', 'blur'],
+        callback : function(valid, results, options){
 
             var $elt = $(this), rule;
             //removes previous error messages
             $elt.siblings('.' + options.errorMessageClass).remove();
 
-            if (valid === false) {
-                rule = _.where(results, {type: 'failure'})[0];
+            if(valid === false){
+                rule = _.where(results, {type : 'failure'})[0];
                 $elt.addClass(options.errorClass);
-                if (rule && rule.data.message) {
+                if(rule && rule.data.message){
                     $elt.after("<span class='" + options.errorMessageClass + "'>" + rule.data.message + "</span>");
                 }
-            } else {
+            }else{
                 $elt.removeClass(options.errorClass);
             }
         }
