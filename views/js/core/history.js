980775317a6abcb916d22d360de8687e4e029e5e
bertrand <bertrand@62dfeb86-a93f-4914-9d13-15c0a160985d>
2014-07-07 13:53:28 +0000

    update selectors for the backspace button fix

--- views/js/core/history.js
+++ views/js/core/history.js
@@ -19,20 +19,7 @@ define(['jquery'], function($){
     
             //to be completed if needed
             var enabledSelector = [
-                'input[type=color]', 
-                'input[type=date]', 
-                'input[type=datetime]', 
-                'input[type="datetime-local"]', 
-                'input[type=email]', 
-                'input[type=month]', 
-                'input[type=number]', 
-                'input[type=password]', 
-                'input[type=search]', 
-                'input[type=tel]', 
-                'input[type=text]', 
-                'input[type=time]', 
-                'input[type=url]', 
-                'input[type=week]', 
+                'input',
                 'textarea', 
                 '[contenteditable=true]'
             ].join(',');
