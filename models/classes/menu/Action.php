1d9c897443c57b479ed6c823c0f82f6740cafecf
Bertrand Chevrier <bertrand@taotesting.com>
2014-08-18 14:27:36 +0200

    Update the Action model: group, icon and binding

--- models/classes/menu/Action.php
+++ models/classes/menu/Action.php
@@ -28,21 +28,27 @@ class Action implements PhpSerializable
 {
     const SERIAL_VERSION = 1392821334;
     
-    protected $data;
-    
     public static function fromSimpleXMLElement(\SimpleXMLElement $node) {
         $data = array(
-            'name' => (string) $node['name'],
-            'url' => isset($node['url']) ? (string) $node['url'] : null,
-            'js' => (isset($node['js'])) ? (string) $node['js'] : false,
-            'context' => (string) $node['context'],
-            'reload' => (isset($node['reload'])) ? true : false,
+            'name'      => (string) $node['name'],
+            'url'       => isset($node['url']) ? (string) $node['url'] : '#',
+            'binding'   => isset($node['binding']) ? (string) $node['binding'] : isset($node['js']) ? (string) $node['js'] : 'load',
+            'context'   => (string) $node['context'],
+            'reload'    => isset($node['reload']) ? true : false,
+            'group'     => isset($node['group']) ? (string) $node['group'] : 'tree'
         );
+        if(isset($node->icon)){
+            $data['icon'] = Icon::fromSimpleXMLElement($node->icon);
+        }
+        
         return new static($data);
     }
     
     public function __construct($data, $version = self::SERIAL_VERSION) {
         $this->data = $data;
+        if(!isset($this->data['icon'])){
+            $this->data['icon'] = $this->inferLegacyIcon($data);
+        }
     }
     
     public function getName() {
@@ -53,8 +59,8 @@ class Action implements PhpSerializable
         return $this->data['url'];
     }
     
-    public function getJs() {
-        return $this->data['js'];
+    public function getBinding() {
+        return $this->data['binding'];
     }
     
     public function getContext() {
@@ -65,16 +71,55 @@ class Action implements PhpSerializable
         return $this->data['reload'];
     }
     
-    public function getExtensionId() {
-        $url = explode('/', trim($this->data['url'], '/'));
-        if (count($url) == 3) {
-            $ext = (isset($url[0])) ? $url[0] : null;
+    /**
+     * @return Icon
+     */
+    public function getIcon() {
+        return $this->data['icon'];
+    }
+
+    /**
+     * Get the extension id from the action's URL.
+     *
+     * @param string $url (optional) the url to extract the extension id, use $this->data['url'] if not set.
+     * @return string the extension id
+     */
+    public function getExtensionId($url = null) {
+        if(is_null($url)){
+            $url = $this->data['url'];
+        }
+        $urlParts = explode('/', trim($url, '/'));
+        if (count($urlParts) == 3) {
+            $ext = (isset($urlParts[0])) ? $urlParts[0] : null;
             return $ext;
         } else {
             return null;
         }
     }
-    
+
+    /**
+     * Try to get the action's icon the old way. 
+     * I/O impact (file_exists) is limited as the results can be serialized.
+     *
+     * @return Icon the icon with the src property set to the icon URL.
+     */
+    private function inferLegacyIcon(){
+        $ext = $this->getExtensionId();
+        $name = \tao_helpers_Display::textCleaner($this->data['name']);
+        $file = $ext .  '/views/img/actions/' . $name . '.png';
+        if(file_exists(ROOT_PATH . $file)) {
+            $src = ROOT_URL . $file;
+        } else {
+            $src = TAOBASE_WWW . 'img/actions/' . $name . '.png';
+        }
+        return Icon::createLegacyItem(null, $src);
+    }
+   
+    /**
+     *  Check whether the current is allowed to see this action (against ACL).
+     *
+     *  @return bool true if access is granted
+     */ 
     public function hasAccess() {
         $access = true;
         if (!empty($this->data['url'])) {
