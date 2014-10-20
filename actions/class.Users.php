834b7fd0c92e52923664c92e99717bac7d35a78b
Thibault Milan <thibault.milan@vesperiagroup.com>
2014-10-20 11:19:28 +0200

    Use the constructed array for call options

--- actions/class.Users.php
+++ actions/class.Users.php
@@ -1,23 +1,23 @@
 <?php
-/*  
+/*
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; under version 2
  * of the License (non-upgradable).
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- * 
+ *
  * Copyright (c) 2002-2008 (original work) Public Research Centre Henri Tudor & University of Luxembourg (under the project TAO & TAO2);
  *               2008-2010 (update and modification) Deutsche Institut für Internationale Pädagogische Forschung (under the project TAO-TRANSFER);
  *               2009-2012 (update and modification) Public Research Centre Henri Tudor (under the project TAO-SUSTAIN & TAO-DEV);
- * 
+ *
  */
 ?>
 <?php
@@ -27,7 +27,7 @@
  * @author CRP Henri Tudor - TAO Team - {@link http://www.tao.lu}
  * @license GPLv2  http://www.opensource.org/licenses/gpl-2.0.php
  * @package tao
- 
+
  *
  */
 class tao_actions_Users extends tao_actions_CommonModule {
@@ -51,7 +51,7 @@ class tao_actions_Users extends tao_actions_CommonModule {
 		parent::__construct();
 		$this->userService = tao_models_classes_UserService::singleton();
 		$this->defaultData();
-		
+
 		$extManager = common_ext_ExtensionsManager::singleton();
 	}
 
@@ -108,7 +108,7 @@ class tao_actions_Users extends tao_actions_CommonModule {
 		$counti =  count($users);
 
 		// get the users using requested paging...
-		$users = $this->userService->getAllUsers(array('offset' => $start, 'limit' => $limit));
+		$users = $this->userService->getAllUsers($gau);
 		$rolesProperty		= new core_kernel_classes_Property(PROPERTY_USER_ROLES);
 
 		$response = new stdClass();
