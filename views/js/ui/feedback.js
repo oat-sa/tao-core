899cb0b8356a581ee734bdda9ba16c1632bb20e2
Michael <Komarouski@1pt.com>
2014-11-03 13:45:05 +0300

    Separate timeouts for different type of notification at flash-messenger

--- views/js/ui/feedback.js
+++ views/js/ui/feedback.js
@@ -23,7 +23,7 @@ define([
         //persistent feedback stay until their are closed.
         //Other persistent feedback are merged to keep all the info.
         //To prevent UI pollution, they may be collapsed  in a notification area
-        'persistent'    : ['warning', 'error']  
+        'persistent'    : ['warning', 'error']
     };
 
     //extract the available levels from the categories
@@ -40,7 +40,12 @@ define([
 
     //the default options
     var defaultOptions = {
-        timeout : 8000
+        timeout: {
+            info: 2000,
+            success: 2000,
+            warning: 4000,
+            error: 8000
+        }
     };
 
     /**
@@ -156,7 +161,7 @@ define([
                 
                 this._trigger();
 
-                if(this.options.timeout >= 0){
+                if(this.options.timeout[this.level] >= 0){
                     setTimeout(function(){
                     
                         //volatiles messages auto close and peristent collaspe
@@ -166,7 +171,7 @@ define([
                             //self.collapse();
                         //}
 
-                    }, this.options.timeout);
+                    }, this.options.timeout[this.level]);
                 }
             }
             return this;
