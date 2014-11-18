3219e13c4ab17d8ef412773304de6650edb66ce1
Joel Bout <joel@taotesting.com>
2014-11-18 17:45:18 +0100

    Corrected license block and comments

--- actions/class.Search.php
+++ actions/class.Search.php
@@ -14,29 +14,17 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  * 
- * Copyright (c) 2002-2008 (original work) Public Research Centre Henri Tudor & University of Luxembourg (under the project TAO & TAO2);
- *               2008-2010 (update and modification) Deutsche Institut für Internationale Pädagogische Forschung (under the project TAO-TRANSFER);
- *               2009-2012 (update and modification) Public Research Centre Henri Tudor (under the project TAO-SUSTAIN & TAO-DEV);
- *               2013-2014 (update and modification) Open Assessment Technologies SA;
- * 
- */
+ * Copyright (c) 2014 (original work) Open Assessment Technologies SA;
+ *               
+ */               
 
-use oat\tao\model\accessControl\AclProxy;
-use oat\tao\model\accessControl\ActionResolver;
-use oat\tao\model\menu\MenuService;
-use oat\generis\model\data\permission\PermissionManager;
-use oat\tao\model\accessControl\data\DataAccessControl;
 use oat\tao\model\search\SearchService;
 
 /**
- * The TaoModule is an abstract controller, 
- * the tao children extensions Modules should extends the TaoModule to beneficiate the shared methods.
- * It regroups the methods that can be applied on any extension (the rdf:Class managment for example)
- *
- * @author CRP Henri Tudor - TAO Team - {@link http://www.tao.lu}
+ * Controller for indexed searches
+ * 
  * @license GPLv2  http://www.opensource.org/licenses/gpl-2.0.php
  * @package tao
- 
  */
 class tao_actions_Search extends tao_actions_CommonModule {
 	
