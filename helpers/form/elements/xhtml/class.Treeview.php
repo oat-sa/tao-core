f96f12a7bf8ae32c996786a4ad59b79720c24d59
antoinerobin <antoine.robin@vesperiagroup.com>
2015-02-19 16:33:38 +0100

    scope checkbox selection to avoid selecting checkbox in an other tree checkboxes in each tree have the same keys

--- helpers/form/elements/xhtml/class.Treeview.php
+++ helpers/form/elements/xhtml/class.Treeview.php
@@ -151,8 +151,10 @@ class tao_helpers_form_elements_xhtml_Treeview
     				callback:{
 	    				onload: function(TREE_OBJ) {
 	    					checkedElements = '.json_encode($this->values).';
+                            var tree = $("#'. $widgetTreeName.'");
 	    					$.each(checkedElements, function(i, elt){
-								NODE = $("li[id=\'"+elt+"\']");
+								NODE = $("li[id=\'"+elt+"\']", tree);
+								console.log(NODE);
 								if(NODE.length > 0){
 									parent = TREE_OBJ.parent(NODE);
 									TREE_OBJ.open_branch(parent);
