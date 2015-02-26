f961a3d68dc0fdc4d5220275dc3eda5076c3a6e8
Joel Bout <joel@taotesting.com>
2015-02-26 09:41:49 +0100

    Moved interval rendering to date helper

--- helpers/class.Date.php
+++ helpers/class.Date.php
@@ -28,9 +28,12 @@
  */
 class tao_helpers_Date
 {
-	const FORMAT_LONG		= 0;
-	const FORMAT_VERBOSE	= 1;
-	const FORMAT_DATEPICKER	= 2;
+	const FORMAT_LONG          = 0;
+	const FORMAT_VERBOSE       = 1;
+	const FORMAT_DATEPICKER	   = 2;
+	
+	const FORMAT_INTERVAL_LONG  = 100;
+	const FORMAT_INTERVAL_SHORT = 101;
 	
     /**
      * Dispalys a date/time
@@ -40,7 +43,7 @@ class tao_helpers_Date
      * @param  int format The date format. See tao_helpers_Date's constants.
      * @return string The formatted date.
      */
-    public static function displayeDate($timestamp, $format = self::FORMAT_LONG)
+    static public function displayeDate($timestamp, $format = self::FORMAT_LONG)
     {
     	$returnValue = '';
         
@@ -64,10 +67,79 @@ class tao_helpers_Date
     		case self::FORMAT_VERBOSE :
     			$returnValue = $dateTime->format('F j, Y, g:i:s a');
     			break;
-    		default:
+			default:
     			common_Logger::w('Unkown date format '.$format.' for '.__FUNCTION__, 'TAO');
     	}
     	return $returnValue;
+    }
+    
+    static public function displayInterval($interval, $format = self::FORMAT_LONG) {
+        if (is_object($interval)) {
+            $intervalObj = $interval;
+        } else {
+            $intervalObj = new DateTime();
+            $intervalObj->setTimestamp($interval);
+        }
+            
+        $intervalObj = $intervalObj instanceof DateTime ? (new \DateTime())->diff($intervalObj, true) : $intervalObj;
+        if (!$intervalObj instanceof DateInterval) {
+            common_Logger::w('Unkown interval format '.get_class($interval).' for '.__FUNCTION__, 'TAO');
+            return '';
+        }
+        
+        $formatStrings = self::getNonNullIntervalFormats($intervalObj);
+        if (empty($formatStrings)) {
+            $returnValue = __("less than a minute");
+        } else {
+            $returnValue = '';
+            switch ($format) {
+        		case self::FORMAT_INTERVAL_SHORT :
+        			$returnValue = $intervalObj->format(array_shift($formatStrings));
+        			break;
+    			case self::FORMAT_INTERVAL_LONG :
+    			    $returnValue = self::formatElapsed($intervalObj, $formatStrings);
+        			break;
+    			default:
+        			common_Logger::w('Unkown date format '.$format.' for '.__FUNCTION__, 'TAO');
+        	}
+        }
+    	return $returnValue;
+    }
+    
+    static protected function formatElapsed(DateInterval $interval, $formatStrings)
+    {
+        $string = '';
+        while (!empty($formatStrings)) {
+            $string .= $interval->format(array_shift($formatStrings))
+                .(count($formatStrings) == 0
+                    ? ''
+                    : (count($formatStrings) == 1
+                        ? __(' and ')
+                        : ' '
+            ));
+        }
+        return $string;
+    }
+                    
+    static private function getNonNullIntervalFormats(DateInterval $interval)
+    {
+        $formats = array();
+        if ($interval->y > 0) {
+            $formats[] = $interval->y == 1 ? __("%y year") : __("%y years");
+        }
+        if ($interval->m > 0) {
+            $formats[] = $interval->m == 1 ? __("%m month") : __("%m months");
+        }
+        if ($interval->d > 0) {
+            $formats[] = $interval->d == 1 ? __("%d day") : __("%d days");
+        }
+        if ($interval->h > 0) {
+            $formats[] = $interval->h == 1 ? __("%h hour") : __("%h hours");
+        }
+        if ($interval->i > 0) {
+            $formats[] = $interval->i == 1 ? __("%i minute") : __("%i minutes");
+        }
+        return $formats;
     }
 
    static function getTimeStamp($microtime)
