8e95f9acb7e047d4f0b402bc4a8fc7233d06169d
Lionel Lecaque <lionel@taotesting.com>
2015-03-10 19:05:55 +0100

    add test on websourceManager

--- test/FsAccessTest.php
+++ test/FsAccessTest.php
@@ -1,5 +1,5 @@
 <?php
-/*  
+/**  
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; under version 2
@@ -16,8 +16,10 @@
  * 
  * Copyright (c) 2008-2010 (original work) Deutsche Institut für Internationale Pädagogische Forschung (under the project TAO-TRANSFER);
  *               2009-2012 (update and modification) Public Research Centre Henri Tudor (under the project TAO-SUSTAIN & TAO-DEV);
- * 
+ *               2013-2015 (update and modification) Open Assessment Technologies SA;
  */
+
+
 use oat\tao\test\TaoPhpUnitTestRunner;
 use oat\tao\model\websource\WebsourceManager;
 use oat\tao\model\websource\ActionWebSource;
@@ -80,6 +82,69 @@ class tao_test_FsAccessTest extends TaoPhpUnitTestRunner {
             array(TokenWebSource::spawnWebsource(self::$fileSystem)),
             array(ActionWebSource::spawnWebsource(self::$fileSystem))
         );
+    }
+    
+
+    
+    /**
+     * @expectedException common_Exception
+     * @expectedExceptionMessage Missing identifier for websource
+     * @author Lionel Lecaque, lionel@taotesting.com
+     */
+    public function testAddWebSourceException()
+    {
+        $websource = $this->prophesize('oat\tao\model\websource\BaseWebsource');
+        WebsourceManager::singleton()->addWebsource($websource->reveal());
+    }
+    
+    /**
+     * 
+     * @author Lionel Lecaque, lionel@taotesting.com
+     */
+    public function testAddWebSource()
+    {
+        $ext = common_ext_ExtensionsManager::singleton()->getExtensionById('tao');
+        $this->assertFalse($ext->hasConfig(WebsourceManager::CONFIG_PREFIX.'fake'));
+                
+        $websource = $this->prophesize('oat\tao\model\websource\BaseWebsource');
+        $websource->getId()->willReturn('fake');
+        $websource->getOptions()->willReturn('options');
+        $ws = $websource->reveal();
+        WebsourceManager::singleton()->addWebsource($ws);
+        
+        $config = $ext->getConfig(WebsourceManager::CONFIG_PREFIX.'fake');
+       
+
+        $expected = array( 'className' => get_class($ws) , 'options' => 'options');
+        $this->assertEquals($expected, $config);
+        return $ws;
+    }
+    
+    
+    
+    /**
+     * @depends testAddWebSource
+     * @author Lionel Lecaque, lionel@taotesting.com
+     */
+    public function testRemoveWebSource($websource)
+    {
+        $ext = common_ext_ExtensionsManager::singleton()->getExtensionById('tao');
+        $this->assertTrue($ext->hasConfig(WebsourceManager::CONFIG_PREFIX.'fake'));
+        
+        WebsourceManager::singleton()->removeWebsource($websource);
+        $this->assertFalse($ext->hasConfig(WebsourceManager::CONFIG_PREFIX.'fake'));
+    }
+    
+    /**
+     * @expectedException common_Exception
+     * @expectedExceptionMessage Attempting to remove inexistent fake
+     * @author Lionel Lecaque, lionel@taotesting.com
+     */
+    public function testRemoveWebSourceException()
+    {
+        $websource = $this->prophesize('oat\tao\model\websource\BaseWebsource');
+        $websource->getId()->willReturn('fake');
+        WebsourceManager::singleton()->removeWebsource($websource->reveal());
     }
     
     /**
