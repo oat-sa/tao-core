56d31abaad4069c2ab6de7501d838d1c888ccca6
Joel Bout <joel@taotesting.com>
2015-03-30 16:01:40 +0200

    Added new interface and service  to messaging

---
+++ models/classes/messaging/MessagingService.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * 
+ * Copyright (c) 2013 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
+ * 
+ */
+namespace oat\tao\model\messaging;
+
+use oat\tao\model\messaging\transportStrategy\MailAdapter;
+class MessagingService extends tao_models_classes_Service
+{
+    const CONFIG_KEY = 'messaging';
+    
+    /**
+     * 
+     * @param Message $message
+     * @return boolean
+     */
+    public function send(Message $message)
+    {
+        $adapter = new MailAdapter();
+        $adapter->addMessage($message);
+        $count = $adapter->send();
+        return $count === 1;
+    }
+    
+    /**
+     * 
+     * @return boolean
+     */
+    public function isAvailable()
+    {
+        $tao = \common_ext_ExtensionsManager::singleton()->getExtensionById('tao');
+        return $tao->hasConfig(self::CONFIG_KEY);
+    }
+}
