fdeb258a625f80ed3a31f2feb6b70002666c3a51
jsconan <jsconan@gmail.com>
2015-05-21 15:42:19 +0200

    Fix wrong headers

--- views/js/ui/ckeditor/ckConfigurator.js
+++ views/js/ui/ckeditor/ckConfigurator.js
@@ -1,3 +1,20 @@
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2015 (original work) Open Assessment Technologies SA ;
+ */
 define([
     'jquery',
     'lodash',
@@ -214,22 +231,21 @@ define([
 
         };
 
-
         /**
          * Generate a configuration object for CKEDITOR
          *
-         * Options not covered in http://docs.ckeditor.com/#!/api/CKEDITOR.config:
-         * options.dtdOverrides         -> @see dtdOverrides which pre-defines them
-         * options.positionedPlugins    -> @see ckConfig.positionedPlugins
-         * options.qtiImage             -> enables the qtiImage plugin
-         * options.qtiInclude           -> enables the qtiInclude plugin
-         * options.underline            -> enables the underline plugin
-         * options.mathJax              -> enables the mathJax plugin
-         *
          * @param editor instance of ckeditor
          * @param toolbarType block | inline | flow | qtiBlock | qtiInline | qtiFlow | reset to get back to normal
-         * @param options is based on the CKEDITOR config object with some additional sugar
-         *        Note that it's here you need to add parameters for the resource manager
+         * @param {Object} [options] - is based on the CKEDITOR config object with some additional sugar
+         *        Note that it's here you need to add parameters for the resource manager.
+         *        Some options are not covered in http://docs.ckeditor.com/#!/api/CKEDITOR.config
+         * @param [options.dtdOverrides] - @see dtdOverrides which pre-defines them
+         * @param {Object} [options.positionedPlugins] - @see ckConfig.positionedPlugins
+         * @param {Boolean} [options.qtiImage] - enables the qtiImage plugin
+         * @param {Boolean} [options.qtiInclude] - enables the qtiInclude plugin
+         * @param {Boolean} [options.underline] - enables the underline plugin
+         * @param {Boolean} [options.mathJax] - enables the mathJax plugin
+         *
          * @see http://docs.ckeditor.com/#!/api/CKEDITOR.config
          */
         var getConfig = function(editor, toolbarType, options){
