577516362aef290f21fc5b7ba386db63a65ce12f
jsconan <jsconan@gmail.com>
2015-06-17 14:22:30 +0200

    Add method for HTML attribute value encoding

--- views/js/util/encode.js
+++ views/js/util/encode.js
@@ -26,11 +26,38 @@ define([
 ], function () {
     'use strict';
 
+    var _reQuotes = /(['"])/g;
+    var _quotesEntities = {
+        "'" : '&apos;',
+        '"' : '&quot;'
+    };
+
+    /**
+     * Encodes an HTML string to be safely displayed without code interpretation
+     *
+     * @param {String} html
+     * @returns {String}
+     */
+    var encodeHTML = function encodeHTML(html) {
+        // @see http://tinyurl.com/ko75kph
+        return document.createElement('a').appendChild(
+            document.createTextNode(html)).parentNode.innerHTML;
+    };
+
+    /**
+     * Encodes an HTML string to be safely use inside an attribute
+     *
+     * @param {String} html
+     * @returns {String}
+     */
+    var encodeAttribute = function encodeAttribute(html) {
+        return encodeHTML(html).replace(_reQuotes, function(substr, $1) {
+            return _quotesEntities[$1] || $1;
+        });
+    };
+
     return {
-        html: function (html) {
-            // @see http://tinyurl.com/ko75kph
-            return document.createElement('a').appendChild(
-                document.createTextNode(html)).parentNode.innerHTML;
-        }
+        html: encodeHTML,
+        attribute: encodeAttribute
     };
 });
