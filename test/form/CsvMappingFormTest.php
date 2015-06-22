6eb4f4c9052f823fea2d807971387669f9cd38a3
Joel Bout <joel@taotesting.com>
2015-06-22 11:33:27 +0200

    Small adaptions to test-case

--- test/form/CsvMappingFormTest.php
+++ test/form/CsvMappingFormTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use oat\tao\test\TaoPhpUnitTestRunner;
+use oat\tao\model\passwordRecovery\PasswordRecoveryService;
 
 include_once dirname(__FILE__) . '/../includes/raw_start.php';
 
@@ -9,26 +10,25 @@ class CSVMappingFormTestCase extends TaoPhpUnitTestRunner
     public function testMapping()
     {
 
-        $csv_column = ['login', 'password', 'title', 'lastname', 'firstname', 'gender', 'email', 'picture', 'address'];
+        $csv_column = array('login', 'password', 'title', 'last-name', 'firstname', 'gender', 'email', 'token', 'abelabel');
 
-        $properties = [
-            'http_2_www_0_w3_0_org_1_2000_1_01_1_rdf-schema_3_label' => 'Label',
-            'http_2_www_0_tao_0_lu_1_Ontologies_1_generis_0_rdf_3_userFirstName' => 'First Name',
-            'http_2_www_0_tao_0_lu_1_Ontologies_1_generis_0_rdf_3_userLastName' => 'Last Name',
-            'http_2_www_0_tao_0_lu_1_Ontologies_1_generis_0_rdf_3_login' => 'Login',
-            'http_2_www_0_tao_0_lu_1_Ontologies_1_generis_0_rdf_3_password' => 'Password',
-            'http_2_www_0_tao_0_lu_1_Ontologies_1_generis_0_rdf_3_userMail' => 'Mail',
-            'http_2_www_0_tao_0_lu_1_Ontologies_1_generis_0_rdf_3_userUILg' => 'Interface Language',
-            'http_2_www_0_tao_0_lu_1_Ontologies_1_generis_0_rdf_3_passwordRecoveryToken' => 'Password recovery token',
-            'http_2_www_0_tao_0_lu_1_Ontologies_1_TAOGroup_0_rdf_3_member' => 'Member'
-        ];
-
+        $properties = tao_helpers_Uri::encodeArray(array(
+            RDFS_LABEL => 'Label',
+            PROPERTY_USER_FIRSTNAME => 'First Name',
+            PROPERTY_USER_LASTNAME => 'Last Name',
+            PROPERTY_USER_LOGIN => 'Login',
+            PROPERTY_USER_PASSWORD => 'Password',
+            PROPERTY_USER_MAIL => 'Mail',
+            PROPERTY_USER_UILG => 'Interface Language',
+            PasswordRecoveryService::PROPERTY_PASSWORD_RECOVERY_TOKEN => 'Password recovery token',
+        ), tao_helpers_Uri::ENCODE_ARRAY_KEYS);
+        
         $data = array();
         $options = array(
             'class_properties' => $properties,
             'ranged_properties' => array(),
             'csv_column' => $csv_column,
-            tao_helpers_data_CsvFile::FIRST_ROW_COLUMN_NAMES => 'first_row_column_names',
+            tao_helpers_data_CsvFile::FIRST_ROW_COLUMN_NAMES => true,
         );
 
         $formContainer = new tao_models_classes_import_CSVMappingForm($data, $options);
@@ -42,7 +42,6 @@ class CSVMappingFormTestCase extends TaoPhpUnitTestRunner
         $this->assertEquals('6_O', $form->getElement('http_2_www_0_tao_0_lu_1_Ontologies_1_generis_0_rdf_3_userMail')->getEvaluatedValue());
         $this->assertEquals('csv_select', $form->getElement('http_2_www_0_tao_0_lu_1_Ontologies_1_generis_0_rdf_3_userUILg')->getEvaluatedValue());
         $this->assertEquals('csv_select', $form->getElement('http_2_www_0_tao_0_lu_1_Ontologies_1_generis_0_rdf_3_passwordRecoveryToken')->getEvaluatedValue());
-        $this->assertEquals('csv_select', $form->getElement('http_2_www_0_tao_0_lu_1_Ontologies_1_TAOGroup_0_rdf_3_member')->getEvaluatedValue());
 
     }
 }
