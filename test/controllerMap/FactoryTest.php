0545193a31bab7e7b8db0565f0157420f5bfb46f
Kostya Sasim <ksasim@ksasim-VirtualBox.(none)>
2015-07-01 16:47:34 +0300

    fixed phpDoc

--- test/controllerMap/FactoryTest.php
+++ test/controllerMap/FactoryTest.php
@@ -1,9 +1,25 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: ksasim
- * Date: 01.07.15
- * Time: 12:13
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2015 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
+ *
+ * @author Konstantin Sasim <sasim@taotesting.com>
+ * @license GPLv2
+ * @package tao
+ *
  */
 
 use oat\tao\model\controllerMap\Factory;
@@ -133,6 +149,7 @@ class FactoryTest extends TaoPhpUnitTestRunner {
     }
 
     /**
+     * Test {@link Factory::getActionDescription}
      *
      * @dataProvider controllerActionNameProvider
      * @param $controllerClassName
