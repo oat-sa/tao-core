e67a6e281c9baea1bd1f80cdc79f555d22c5f950
Dieter Raber <dieter@taotesting.com>
2015-07-13 15:35:34 +0200

    corrected missing reference

--- views/js/ui/form.js
+++ views/js/ui/form.js
@@ -1,4 +1,24 @@
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2013 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
+ *
+ *
+ */
 define(['jquery'], function($){
+    
     'use strict';
 
     /**
@@ -7,8 +27,8 @@ define(['jquery'], function($){
      */
     var pseudoLabel = function pseudoLabel(selector){
 
-        $(document).on('click', selector + ' .pseudo-label-box', function (e) {
-            $box.find('input').trigger('click');
+        $(document).on('click', selector + ' .pseudo-label-box', function () {
+            $(this).find('input').trigger('click');
         });
     };
 
