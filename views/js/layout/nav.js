942469005f6dd81f1beec49ad904dbaafc33b58b
Dieter Raber <dieter@taotesting.com>
2015-08-13 15:26:05 +0200

    fixed oversized navi

--- views/js/layout/nav.js
+++ views/js/layout/nav.js
@@ -1,22 +1,70 @@
+/*
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2015 (original work) Open Assessment Technologies SA;
+ *
+ */
 
-define(['jquery',], function($) {
+/**
+ * This component manage the navigation bar of TAO.
+ *
+ * @author Bertrand Chevrier <bertrand@taotesting.com>
+ * @author Dieter Raber <dieter@taotesting.com>
+ */
+define(['jquery', 'lodash'], function($, _) {
+
     'use strict';
 
+    var $body = $('body'),
+        $navContainer = $('header.dark-bar'),
+        $nav = $navContainer.find($('nav')),
+        navHeight = $nav.find('.main-menu').height() || 0,
+        navIsOversized = $body.hasClass('oversized-nav');
+
     /**
-     * This component manage the navigation bar of TAO.
-     * 
+     * The regular height of the header is ~64px. If it's higher than that
+     * this means that the right menu has slipped under the left one due
+     * to a lack of space. This can happen when the logo is very long and/or
+     * many extensions are installed.
+     *
+     * @returns {{init: init}}
+     */
+    var checkHeight = function checkHeight() {
+        if($navContainer.height() > navHeight) {
+            $body.addClass('oversized-nav');
+            navIsOversized = true;
+        }
+        else if (navIsOversized) {
+            $body.removeClass('oversized-nav');
+            navIsOversized = false;
+        }
+    };
+
+    /**
      * @exports layout/nav
      */
     return {
 
         /**
          * Initialize the navigation bar
+         *
+         * @author Bertrand Chevrier <bertrand@taotesting.com>
          */
         init : function(){
-            var $container = $('header.dark-bar > nav');
-
             //here the bindings are controllers or even the name of any AMD file to load
-            $('[data-action]', $container).off('click').on('click', function(e){
+            $('[data-action]', $nav).off('click').on('click', function(e){
                 e.preventDefault();
                 var binding = $(this).data('action');
                 if(binding){
@@ -27,6 +75,12 @@ define(['jquery',], function($) {
                     });
                 }
             });
+
+            // check the height of the header on load and on resize
+            checkHeight();
+            $(window).off('resize.navheight').on('resize.navheight', _.debounce(function () {
+                checkHeight();
+            }, 150));
         }
     };
 });
