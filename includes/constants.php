0bb83d28d703455f4826c35ca0e99b671ea1ae7a
Michael <Komarouski@1pt.com>
2015-08-14 09:20:55 +0300

    version update

--- includes/constants.php
+++ includes/constants.php
@@ -76,6 +76,7 @@ $todefine = array(
 	'TAO_INSTALLATOR'  					=> 'http://www.tao.lu/Ontologies/TAO.rdf#installator',
 	'PROPERTY_WIDGET_CALENDAR'			=> 'http://www.tao.lu/datatypes/WidgetDefinitions.rdf#Calendar',
 	'PROPERTY_WIDGET_TEXTBOX'			=> 'http://www.tao.lu/datatypes/WidgetDefinitions.rdf#TextBox',
+	'PROPERTY_WIDGET_TREEBOX'			=> 'http://www.tao.lu/datatypes/WidgetDefinitions.rdf#TreeBox',
 	'PROPERTY_WIDGET_TEXTAREA'			=> 'http://www.tao.lu/datatypes/WidgetDefinitions.rdf#TextArea',
 	'PROPERTY_WIDGET_HTMLAREA'			=> 'http://www.tao.lu/datatypes/WidgetDefinitions.rdf#HTMLArea',
 	'PROPERTY_WIDGET_PASSWORD'			=> 'http://www.tao.lu/datatypes/WidgetDefinitions.rdf#Password',
