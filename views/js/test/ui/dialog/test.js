308c9f78e3ac39a979a159dc9d71f68092e0fdc4
jsconan <jsconan@gmail.com>
2015-08-17 16:07:36 +0200

    Add full destroy to dialog box Display immediately the dialog box by default (animate option)

--- views/js/test/ui/dialog/test.js
+++ views/js/test/ui/dialog/test.js
@@ -85,7 +85,7 @@ define([
         Promise.all(promises).then(function() {
             modal.destroy();
             assert.ok(null === modal.getDom(), "The dialog instance does not have a DOM element anymore");
-            assert.equal($(renderTo).find('.modal').length, 0, "The container does not contains the dialog box anymore");
+            assert.equal($(renderTo).children().length, 0, "The container does not contains the dialog box anymore");
 
             QUnit.start();
         });
