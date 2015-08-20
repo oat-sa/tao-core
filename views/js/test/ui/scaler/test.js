1e742e2966f8685ca969685afacdcee78a9a7d8e
Dieter Raber <dieter@taotesting.com>
2015-08-20 10:24:45 +0200

    added class

--- views/js/test/ui/scaler/test.js
+++ views/js/test/ui/scaler/test.js
@@ -15,22 +15,29 @@ define([
         assert.ok(typeof scaler === 'object', 'Scaler returns an object');
         assert.ok(typeof scaler.scale === 'function', 'The loader exposes a method scale()');
         assert.ok(typeof scaler.reset === 'function', 'The loader exposes a method reset()');
+        assert.ok(typeof scaler.toggle === 'function', 'The loader exposes a method toggle()');
 
     });
 
-    QUnit.test('Scaling and resetting', function(assert){
+    QUnit.test('Scaling', function(assert){
         var $container = $('#scale-container');
         assert.equal($container.length, 1, 'The container exists');
 
-        scaler.scale($container, 2);
-        assert.equal($container[0].getBoundingClientRect().width, 200, 'x-scaling works correctly');
-        assert.equal($container[0].getBoundingClientRect().height, 200, 'y-scaling works correctly');
+        scaler.scale($container, 3);
+        assert.equal($container[0].getBoundingClientRect().width, 200, 'X-scaling works correctly');
+        assert.equal($container[0].getBoundingClientRect().height, 200, 'Y-scaling works correctly');
 
         scaler.scale($container, -5);
-        assert.equal($container[0].getBoundingClientRect().width, 0, 'Factors below 0 default to factor 0');
+        assert.equal($container[0].getBoundingClientRect().width, 100, 'Negative factors default to 1');
 
         scaler.reset($container);
         assert.equal($container[0].getBoundingClientRect().width, 100, 'Container resets to its original size');
+
+        scaler.toggle($container, 2);
+        assert.equal($container[0].getBoundingClientRect().width, 200, 'Toggle transforms when container has original size');
+
+        scaler.toggle($container);
+        assert.equal($container[0].getBoundingClientRect().width, 100, 'Toggle resets when the container had been transformed before');
     });
 
 });
--- views/js/ui/scaler.js
+++ views/js/ui/scaler.js
@@ -21,12 +21,70 @@
  *
  * @author dieter <dieter@taotesting.com>
  */
+
+/*
+ Input:  a, b, c, d ; 2x2 matrix (rotate, scale, shear) components
+ tx, ty     ; translation components
+ Output: translate  ; a 2 component vector
+ rotate     ; an angle
+ scale      ; a 2 component vector
+ skew       ; skew factor
+ Returns false if the matrix cannot be decomposed, true if it can
+
+ Supporting functions (point is a 2 component vector):
+ float  length(point)                returns the length of the passed vector
+ point  normalize(point)             normalizes the length of the passed point to 1
+ float  dot(point, point)            returns the dot product of the passed points
+ float  atan2(float y, float x)      returns the principal value of the arc tangent of
+ y/x, using the signs of both arguments to determine
+ the quadrant of the return value
+
+ Decomposition also makes use of the following function:
+ point combine(point a, point b, float ascl, float bscl)
+ result[0] = (ascl * a[0]) + (bscl * b[0])
+ result[1] = (ascl * a[1]) + (bscl * b[1])
+ return result
+
+ // Make sure the matrix is invertible
+ if ((a * d - b * c) == 0)
+ return false
+
+ // Take care of translation
+ translate[0] = tx
+ translate[1] = matrix[3][1]
+
+ // Put the components into a 2x2 matrix 'mat'
+ mat[0][0] = a
+ mat[0][1] = b
+ mat[1][0] = c
+ mat[1][1] = d
+
+ // Compute X scale factor and normalize first row.
+ scale[0] = length(row[0])
+ row[0] = normalize(row[0])
+
+ // Compute shear factor and make 2nd row orthogonal to 1st.
+ skew = dot(row[0], row[1])
+ row[1] = combine(row[1], row[0], 1.0, -skew)
+
+ // Now, compute Y scale and normalize 2nd row.
+ scale[1] = length(row[1])
+ row[1] = normalize(row[1])
+ skew /= scale[1];
+
+ // Now, get the rotation out
+ rotate = atan2(mat[0][1], mat[0][0])
+
+ return true;
+ */
 define([
 ], function () {
     'use strict';
 
 
     var ns = 'scaler';
+
+    var className = 'scaled';
 
     /**
      * Figure out the vendor prefix, if any
@@ -53,19 +111,38 @@ define([
      * @param $container
      * @param {number} factor
      */
-    function scale($container, factor) {
+    function scale($container, factor, r) {
 
         var cssObj = {};
 
         // defaults to 1
-        factor = factor || 1;
+        if(isNaN(factor)) {
+            factor = 1;
+        }
 
         // avoid negative scale factors
-        factor = Math.max(0, factor);
+        if(factor < 0){
+            factor = 1;
+        }
 
-        cssObj[prefix + 'transform'] = 'scale(' + factor + ',' + factor + ')';
+        // memorize old transformation
+        if(!$container.data('original-transform'))
+
+        if(r === 'reset'){
+            console.log(factor)
+        }
+
+        cssObj[prefix + 'transition'] = '1s ease-in-out';
+        cssObj[prefix + 'transform']  = 'scale(' + factor + ',' + factor + ')';
 
         $container.css(cssObj);
+        if(factor !== 1){
+            $container.addClass(className);
+        }
+        else{
+            $container.removeClass(className);
+        }
+
         $container.trigger('scale.' + ns, { factor: factor });
     }
 
@@ -76,8 +153,16 @@ define([
     return {
         scale: scale,
         reset: function($container) {
-            scale($container, 1);
+            this.scale($container, 1, 'reset');
             $container.trigger('reset.' + ns);
+        },
+        toggle: function($container, factor) {
+            if($container.hasClass(className)) {
+               this.reset($container);
+            }
+            else {
+                this.scale($container, factor);
+            }
         }
     };
 });
