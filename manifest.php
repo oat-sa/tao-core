e58b40bba6d3bb9062723508135dd7e25a82c30a
Michael <Komarouski@1pt.com>
2015-08-27 18:10:36 +0300

    Widget registration

--- manifest.php
+++ manifest.php
@@ -27,7 +27,7 @@ return array(
     'label' => 'Tao base',
     'description' => 'TAO meta-extension',
     'license' => 'GPL-2.0',
-    'version' => '2.10.0',
+    'version' => '2.10.1',
     'author' => 'Open Assessment Technologies, CRP Henri Tudor',
     'requires' => array(
         'generis' => '>=2.7.1'
@@ -44,7 +44,8 @@ return array(
             dirname(__FILE__). '/models/ontology/webservice.rdf',
             dirname(__FILE__). '/models/ontology/services.rdf',
             dirname(__FILE__). '/models/ontology/indexation.rdf',
-            dirname(__FILE__). '/models/ontology/model.rdf'
+            dirname(__FILE__). '/models/ontology/model.rdf',
+            dirname(__FILE__). '/models/ontology/widegetdefinitions.rdf'
         ),
         'checks' => array(
                 array('type' => 'CheckPHPRuntime', 'value' => array('id' => 'tao_php_runtime', 'min' => '5.3.10')),
