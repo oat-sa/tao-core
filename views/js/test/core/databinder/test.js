be25902300bdf23f94485a68275c369b9eb585e4
Aleh Hutnikau <hutnikau@1pt.com>
2015-11-25 09:59:37 +0300

    create test

--- views/js/test/core/databinder/test.js
+++ views/js/test/core/databinder/test.js
@@ -182,6 +182,26 @@ define(['jquery', 'core/databinder'], function($, DataBinder){
         assert.equal(model.testParts[0].assessmentSections[0].sectionParts[4].href, 'http://new.rdf#test', 'model element has been added');
     });
 
+    QUnit.test('Add value which is not represented in the model', function(assert){
+        QUnit.expect(3);
+
+        var $container = $('#container-4');
+        var $sectionParts = $('ul', $container);
+        assert.ok($container.length === 1, 'Test the fixture is available');
+
+        var $newSection = $("<li>" +
+            "<input checked='checked' type='checkbox' value='true' data-bind-encoder='boolean' data-bind='visible'>" +
+            "</li>");
+
+        $sectionParts.append($newSection);
+
+        databinder = new DataBinder($container, model);
+        databinder.bind();
+
+        strictEqual(model.testParts[0].assessmentSections[0].sectionParts.length, 4, 'model length has been updated');
+        assert.equal(model.testParts[0].assessmentSections[0].sectionParts[3].visible, true, 'new element has been added');
+    });
+
      QUnit.test('Array value filter', function(assert){
         QUnit.expect(2);
 
