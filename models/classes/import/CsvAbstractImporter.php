575ec818e8fb058802a70413fbac50f72120dd98
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2015-11-27 17:01:34 +0100

    enable to add a staticMap in addition

--- models/classes/import/CsvAbstractImporter.php
+++ models/classes/import/CsvAbstractImporter.php
@@ -134,6 +134,18 @@ abstract class CsvAbstractImporter
 	}
 
 
+        /**
+         * Additional mapping values but that comes from another source than the CSV file.
+         * It enables you to define a mapping that will to work along with the CSV mapping.
+         *
+         * @return the mapping in the same form than the staticData (uri -> val/uri)
+         */
+        public function getStaticMap()
+        {
+            return array();
+        }
+
+
 	/**
 	 * @param \core_kernel_classes_Class $class where data will be imported
 	 * @param array $options contains parameters under key => value format
@@ -150,8 +162,11 @@ abstract class CsvAbstractImporter
 	 */
 	public function importFile($class, $options) {
 
-
-		$options['staticMap'] = $this->getStaticData();
+                if(!isset($options['staticMap']) && !is_array($options['staticMap'])){
+                    $options['staticMap'] = $this->getStaticData();
+                } else {
+                    $options['staticMap'] = array_merge($options['staticMap'], $this->getStaticData());
+                }
 		$options = array_merge($options, $this->getAdditionAdapterOptions());
 
 		// Check if we have a proper UTF-8 file.
@@ -164,9 +179,12 @@ abstract class CsvAbstractImporter
 		$adapter->setValidators($this->getValidators());
 
 		//import it!
-		$report = $adapter->import($options['file'], $class);
+                $report = $adapter->import($options['file'], $class);
+
+
 		if ($report->getType() == \common_report_Report::TYPE_SUCCESS) {
-			@unlink($options['file']);
+                    @unlink($options['file']);
+                    $report->setData($adapter->getOptions());
 		}
 		return $report;
 	}
