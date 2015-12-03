2f950dfb138604ea79c355f9cf60da4207371f61
jsconan <jsconan@gmail.com>
2015-12-03 16:11:32 +0100

    Move aynchronous MIME type getter to existing module

--- views/js/test/core/mimetype/test.js
+++ views/js/test/core/mimetype/test.js
@@ -4,6 +4,7 @@ define(['core/mimetype'], function(mimeType){
     QUnit.test('protoype', function(assert){
        assert.ok(typeof mimeType === 'object', 'The module mimeType expose an object');
        assert.ok(typeof mimeType.getFileType === 'function', 'The module mimeType has a getFileType method');
+       assert.ok(typeof mimeType.getResourceType === 'function', 'The module mimeType has a getResourceType method');
     });
 
     QUnit.test('getFileType', function(assert){
@@ -13,9 +14,43 @@ define(['core/mimetype'], function(mimeType){
         assert.equal(mimeType.getFileType({name : 'style.css'}), 'css', 'Css extension');
         assert.equal(mimeType.getFileType({mime : 'text/plain'}), 'text', 'Text mime type');
     });
-    
+
     QUnit.test('getCategory', function(assert){
         assert.equal(mimeType.getCategory('video'), 'media', 'video files are media');
         assert.equal(mimeType.getCategory('css'), 'sources', 'css files are sources');
     });
+
+    var resources = [
+        { url: '/tao/views/js/test/core/mimetype/samples/audio.mp3', type: 'audio/mpeg', error: false, title : 'MP3' },
+        { url: '/tao/views/js/test/core/mimetype/samples/video.mp4', type: 'video/mp4', error: false, title : 'MP4' },
+        { url: '/tao/views/js/test/core/mimetype/samples/unknown', type: null, error: true, title : 'Unknown resource' }
+    ];
+
+    QUnit
+        .cases(resources)
+        .asyncTest('getResourceType ', function(data, assert) {
+            QUnit.stop();
+
+            var p = mimeType.getResourceType(data.url, function(err, type) {
+                assert.equal(!!err, data.error, 'The callback accept an error');
+                if (!data.error) {
+                    assert.equal(type, data.type, 'The callback received the correct MIME type');
+                }
+                QUnit.start();
+            });
+
+            assert.equal(p instanceof Promise, true, 'The mime function return a promise.');
+
+            p.then(function(type) {
+                if (data.error) {
+                    assert.ok(false, 'The promise must throw an error!');
+                } else {
+                    assert.equal(type, data.type, 'The promise resolved with the correct MIME type');
+                }
+                QUnit.start();
+            }).catch(function(err) {
+                assert.equal(!!err, data.error, 'The promise thrown an error!');
+                QUnit.start();
+            });
+        });
 });
