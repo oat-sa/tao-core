4094209763f69bd610dd537a95721352af3eab75
Lionel Lecaque <lionel@taotesting.com>
2015-12-23 14:07:30 +0100

    Automatic complitation of Js files and Language

--- locales/uk-UA/messages.lang.php
+++ locales/uk-UA/messages.lang.php
@@ -1,6 +1,3 @@
 <?php
-$GLOBALS['__l10n']["Manage test takers"]="Manage test-takers";
-$GLOBALS['__l10n']["Record and manage test takers."]="Record and manage test-takers.";
-$GLOBALS['__l10n']["Test takers library"]="Test-takers library";
 
 ?>
