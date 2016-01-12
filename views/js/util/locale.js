e87e78ee1ceb1dd857eebeac5e6a7e87d8c583c0
Ivan Klimchuk <klimchuk@1pt.com>
2016-01-12 10:39:50 +0300

    Added locale helper for parse numbers with defined decimal and thousands separators

---
+++ views/js/util/locale.js
@@ -0,0 +1,86 @@
+/*
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2016 (original work) Open Assessment Technologies SA;
+ *
+ */
+
+/**
+ * @author Ivan Klimchuk <klimchuk@1pt.com>
+ */
+define(['module'], function (module) {
+    'use strict';
+
+    var config = module.config();
+
+    /**
+     * Util object for manipulate locale dependent data
+     * @exports util/locale
+     */
+    return {
+
+        /**
+         * Returns current system decimal separator
+         * @returns {string}
+         */
+        getDecimalSeparator: function getDecimalSeparator() {
+            return config && config.decimalSeparator ? config.decimalSeparator : '.';
+        },
+
+        /**
+         * Returns current system thousands separator
+         * @returns {string}
+         */
+        getThousandsSeparator: function getThousandsSeparator() {
+            return config && config.thousandsSeparator ? config.thousandsSeparator : '';
+        },
+
+        /**
+         * Parse float values with process locale features
+         * @param number
+         * @returns {Number}
+         */
+        parseFloat: function (number) {
+            if (!number) {
+                return parseFloat(number);
+            }
+
+            var parts = number.split(this.getDecimalSeparator(), 2);
+            var ones = parts[0];
+
+            if (this.getThousandsSeparator().length) {
+                ones = parts[0].replace(new RegExp('\\' + this.getThousandsSeparator(), 'g'), '');
+            }
+
+            return parseFloat(ones) + parseFloat('0.' + parts[1]);
+        },
+
+        /**
+         * Parse integer values with process locale features
+         * @param number
+         * @param numericBase
+         * @returns {Number}
+         */
+        parseInt: function (number, numericBase) {
+
+            if (this.getThousandsSeparator().length) {
+                number = number.replace(new RegExp('\\' + this.getThousandsSeparator(), 'g'), '');
+            }
+
+            return parseInt(number, numericBase);
+        }
+    };
+
+});
