765e63f7a9610051d8a2478369104b74fed2f0cf
Joel Bout <joel@taotesting.com>
2016-04-01 15:32:22 +0200

    Prevent error on files without extensions

--- models/classes/websource/BaseWebsource.php
+++ models/classes/websource/BaseWebsource.php
@@ -123,7 +123,7 @@ implements Websource
         $mimeType = $this->getFileSystem()->getMimetype($filePath);
         //for css files mimetype can be 'text/plain' due to bug in finfo (see more: https://bugs.php.net/bug.php?id=53035)
         $pathParts = pathinfo($filePath);
-        if ($mimeType === 'text/plain' && $pathParts['extension'] === 'css') {
+        if ($mimeType === 'text/plain' && isset($pathParts['extension']) && $pathParts['extension'] === 'css') {
             $mimeType = 'text/css';
         }
         return $mimeType;
