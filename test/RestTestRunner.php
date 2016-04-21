c4e43f681c4b06d1a683b5ead240d1b1f7d2e566
Joel Bout <joel@taotesting.com>
2016-04-21 11:47:15 +0200

    Allow override of userdata and curloptions

--- test/RestTestRunner.php
+++ test/RestTestRunner.php
@@ -18,14 +18,9 @@ abstract class RestTestRunner extends TaoPhpUnitTestRunner
 
     protected $password = "";
     
-    public function setUp()
+    protected function getUserData()
     {
-        TaoPhpUnitTestRunner::initTest();
-        $this->disableCache();
-        
-        // creates a user using remote script from joel
-        
-        $testUserData = array(
+        return array(
             PROPERTY_USER_LOGIN => 'tjdoe',
             PROPERTY_USER_PASSWORD => 'test123',
             PROPERTY_USER_LASTNAME => 'Doe',
@@ -33,39 +28,29 @@ abstract class RestTestRunner extends TaoPhpUnitTestRunner
             PROPERTY_USER_MAIL => 'jdoe@tao.lu',
             PROPERTY_USER_DEFLG => \tao_models_classes_LanguageService::singleton()->getLanguageByCode(DEFAULT_LANG)->getUri(),
             PROPERTY_USER_UILG => \tao_models_classes_LanguageService::singleton()->getLanguageByCode(DEFAULT_LANG)->getUri(),
+            PROPERTY_USER_PASSWORD => 'test' . rand(),
             PROPERTY_USER_ROLES => array(
                 INSTANCE_ROLE_GLOBALMANAGER
             )
         );
+    }
+    
+    public function setUp()
+    {
+        TaoPhpUnitTestRunner::initTest();
+        $this->disableCache();
         
-        $testUserData[PROPERTY_USER_PASSWORD] = 'test' . rand();
-        
-        $data = $testUserData;
-        $data[PROPERTY_USER_PASSWORD] = \core_kernel_users_Service::getPasswordHash()->encrypt($data[PROPERTY_USER_PASSWORD]);
+        // creates a user using remote script from joel
+        $userdata = $this->getUserData();
+        $password = $userdata[PROPERTY_USER_PASSWORD]; 
+        $userdata[PROPERTY_USER_PASSWORD] = \core_kernel_users_Service::getPasswordHash()->encrypt($userdata[PROPERTY_USER_PASSWORD]);
         $tmclass = new \core_kernel_classes_Class(CLASS_TAO_USER);
-        $user = $tmclass->createInstanceWithProperties($data);
+        $user = $tmclass->createInstanceWithProperties($userdata);
         \common_Logger::i('Created user ' . $user->getUri());
         
-        // prepare a lookup table of languages and values
-        $usage = new \core_kernel_classes_Resource(INSTANCE_LANGUAGE_USAGE_GUI);
-        $propValue = new \core_kernel_classes_Property(RDF_VALUE);
-        $langService = \tao_models_classes_LanguageService::singleton();
-        
-        $lookup = array();
-        foreach ($langService->getAvailableLanguagesByUsage($usage) as $lang) {
-            $lookup[$lang->getUri()] = (string) $lang->getUniquePropertyValue($propValue);
-        }
-        
-        $data = array(
-            'rootUrl' => ROOT_URL,
-            'userUri' => $user->getUri(),
-            'userData' => $testUserData,
-            'lang' => $lookup
-        );
-        
-        $this->login = $data['userData'][PROPERTY_USER_LOGIN];
-        $this->password = $data['userData'][PROPERTY_USER_PASSWORD];
-        $this->userUri = $data['userUri'];
+        $this->login = $userdata[PROPERTY_USER_LOGIN];
+        $this->password = $password;
+        $this->userUri = $user->getUri();
     }
 
     public function tearDown()
@@ -79,13 +64,26 @@ abstract class RestTestRunner extends TaoPhpUnitTestRunner
     /**
      * shall be used beyond high level http connections unit tests (default parameters)
      *
-     * @param
-     *            returnType CURLINFO_HTTP_CODE, etc... (default returns rhe http response data
-     *
+     * @param string $url url to call
+     * @param string $method
+     * @param string $returnType CURLINFO_HTTP_CODE, etc... (default returns rhe http response data
+     * @param array $curlOptions (numeric arrays get interpreted as headers)
      * @return mixed
      */
-    protected function curl($url, $method = CURLOPT_HTTPGET, $returnType = "data", $curlopt_httpheaders = array())
+    protected function curl($url, $method = CURLOPT_HTTPGET, $returnType = "data", $curlOptions = array())
     {
+        $options = $this->getDefaultCurlOptions();
+        foreach ($curlOptions as $key => $value) {
+            if (is_numeric($key)) {
+                if (!isset($options[CURLOPT_HTTPHEADER])) {
+                    $options[CURLOPT_HTTPHEADER] = array();
+                }
+                $options[CURLOPT_HTTPHEADER][] = $value;
+            } else {
+                $options[$key] = $value;
+            }
+        }
+        
         $process = curl_init($url);
         if ($method != "DELETE") {
             curl_setopt($process, $method, 1);
@@ -93,24 +91,31 @@ abstract class RestTestRunner extends TaoPhpUnitTestRunner
             curl_setopt($process, CURLOPT_CUSTOMREQUEST, "DELETE");
         }
         
-        curl_setopt($process, CURLOPT_USERPWD, $this->login . ":" . $this->password);
-        curl_setopt($process, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($process, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt_array($process, $options);
         
-        $headers = array_merge(array(
-            "Accept: application/json"
-        ), $curlopt_httpheaders);
-        curl_setopt($process, CURLOPT_HTTPHEADER, $headers);
-        if ($method == CURLOPT_POST) {
-            curl_setopt($process, CURLOPT_POSTFIELDS, "");
-        }
-        // curl_setopt($process,CURLOPT_HTTPHEADER,$curlopt_httpheaders);
         $data = curl_exec($process);
         if ($returnType != "data") {
             $data = curl_getinfo($process, $returnType);
         }
         curl_close($process);
         return $data;
+    }
+
+    /**
+     * Default curl options used for e very call
+     *
+     * @return array
+     */
+    protected function getDefaultCurlOptions()
+    {
+        return array(
+            CURLOPT_USERPWD => $this->login . ":" . $this->password,
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_SSL_VERIFYPEER => false,
+            CURLOPT_HTTPHEADER => array(
+                "Accept: application/json"
+            )
+        );
     }
 
 }
