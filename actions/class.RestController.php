f89cea33e896cc30001481014cd0494cbb8a1aad
christophe garcia <christophe.garcia@taotesting.com>
2016-06-22 15:34:05 +0200

    mineType Accept refactoring

--- actions/class.RestController.php
+++ actions/class.RestController.php
@@ -39,13 +39,18 @@ abstract class tao_actions_RestController extends \tao_actions_CommonModule
     {
         if ($this->hasHeader("Accept")) {
             try {
-                $this->responseEncoding = (tao_helpers_Http::acceptHeader($this->acceptedMimeTypes, $this->getHeader("Accept")));
+                $this->responseEncoding = (tao_helpers_Http::acceptHeader($this->getAcceptableMimeTypes(), $this->getHeader("Accept")));
             } catch (common_exception_ClientException $e) {
                 $this->returnFailure($e);
             }
         }
 
         header('Content-Type: '.$this->responseEncoding);
+    }
+    
+    protected function getAcceptableMimeTypes()
+    {
+        return $this->acceptedMimeTypes;
     }
 
     /**
