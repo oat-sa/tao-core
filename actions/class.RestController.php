bb3942bca0013e941bc828a6d525508145bc38b4
Mikhail Kamarouski <Komarouski@1pt.com>
2016-06-23 16:47:26 -0500

    Sensitive information no more exposed to end-user

--- actions/class.RestController.php
+++ actions/class.RestController.php
@@ -69,7 +69,7 @@ abstract class tao_actions_RestController extends \tao_actions_CommonModule
         if ($withMessage) {
             $data['success']	=  false;
             $data['errorCode']	=  $exception->getCode();
-            $data['errorMsg']	=  ($exception instanceof common_exception_UserReadableException) ? $exception->getUserMessage() : $exception->getMessage();
+            $data['errorMsg']	=  $this->getErrorMessage($exception);
             $data['version']	= TAO_VERSION;
         }
 
@@ -120,5 +120,19 @@ abstract class tao_actions_RestController extends \tao_actions_CommonModule
             default:
                 return json_encode($data);
         }
+    }
+
+    /**
+     * Generate safe message preventing exposing sensitive date in non develop mode
+     * @param Exception $exception
+     * @return string
+     */
+    private function getErrorMessage(Exception $exception)
+    {
+        $defaultMessage =  __('Unexpected error. Please contact administrator');
+        if (DEBUG_MODE) {
+            $defaultMessage = $exception->getMessage();
+        }
+        return ($exception instanceof common_exception_UserReadableException) ? $exception->getUserMessage() :  $defaultMessage;
     }
 }
