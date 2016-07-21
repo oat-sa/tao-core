ca7d7e66c8c9ffcda578ae87dac46881699a1035
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2016-07-21 12:06:44 +0200

    add eslint rules and grunt plugin

---
+++ views/build/grunt/lint.js
@@ -0,0 +1,43 @@
+/**
+ * grunt eslint:file --file=/path/to/file/to/lint
+ * grunt eslint:extension --extension=taoQtiTest
+ */
+module.exports = function(grunt) {
+    'use strict';
+
+    var root    = grunt.option('root');
+    var currentExtension = grunt.option('currentExtension');
+    var reportOutput = grunt.option('reports') || 'reports';
+    var extensionRoot = root + '/' + currentExtension + '/';
+    var extensionSrc = [
+        extensionRoot + '/views/js/**/*.js',
+        '!' + extensionRoot + 'views/js/**/*.min.js',
+        '!' + extensionRoot + 'views/js/**/*.src.js',
+        '!' + extensionRoot + 'views/js/test/**/*.js',
+        '!' + extensionRoot + 'views/js/lib/**/*.js',
+        '!' + extensionRoot + 'views/js/portableSharedLibraries/**/*.js',
+        '!' + extensionRoot + 'views/js/**/jquery.*.js'
+    ];
+
+    grunt.config.merge({
+        eslint : {
+            options : {
+                configFile: '.eslintrc.json',
+            },
+            file : {
+                src : grunt.option('file')
+            },
+            extension : {
+                src : extensionSrc
+            },
+            extensionreport : {
+                options : {
+                    force : true,
+                    format: 'checkstyle',
+                    outputFile:  reportOutput + '/' + currentExtension + '-checkstyle.xml'
+                },
+                src : extensionSrc
+            }
+        }
+    });
+};
