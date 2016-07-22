7ed3b1d0c46dd4e799a9e6b9a836f1d4e67d935b
christophe garcia <christophe.garcia@taotesting.com>
2016-07-22 11:05:51 +0200

    dump version

--- manifest.php
+++ manifest.php
@@ -29,7 +29,7 @@ return array(
     'label' => 'Tao base',
     'description' => 'TAO meta-extension',
     'license' => 'GPL-2.0',
-    'version' => '5.9.2',
+    'version' => '5.9.3',
     'author' => 'Open Assessment Technologies, CRP Henri Tudor',
     'requires' => array(
         'generis' => '>=2.25.1',
