220e96b642b21cf5dac8816a31dc6b4f625db0f5
Joel Bout <joel@taotesting.com>
2016-08-22 17:25:01 +0200

    Implment lock system as service

--- manifest.php
+++ manifest.php
@@ -30,7 +30,7 @@ return array(
     'label' => 'Tao base',
     'description' => 'TAO meta-extension',
     'license' => 'GPL-2.0',
-    'version' => '7.4.6',
+    'version' => '7.4.7',
     'author' => 'Open Assessment Technologies, CRP Henri Tudor',
     'requires' => array(
         'generis' => '>=2.30.0',
