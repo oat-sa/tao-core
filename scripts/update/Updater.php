7092d73aef45b8710e6c75f993669a7ae0110bd3
cmoyon <camille@taotesting.com>
2016-08-22 11:38:20 +0200

    Bimp to version 7.4.7

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -563,7 +563,7 @@ class Updater extends \common_ext_ExtensionUpdater {
             $this->setVersion('6.1.0');
         }
 
-        $this->skip('6.1.0', '7.4.6');
+        $this->skip('6.1.0', '7.4.7');
     }
 
     private function migrateFsAccess() {
