3ba0e618b5c52a423e84d65703f113738de1eb22
antoinerobin <antoine.robin@vesperiagroup.com>
2016-08-24 17:51:13 +0200

    don't iterate on all properties
    (cherry picked from commit 253024858827e31989da82f783dc5e65fa2e6759)

--- views/js/form/post-render-props.js
+++ views/js/form/post-render-props.js
@@ -218,25 +218,23 @@ define([
             _upgradeButtons($container, 'radio');
             _upgradeButtons($container, 'checkbox');
             _toggleModeBtn('disabled');
-            _showErrors($properties);
+            _showErrors($container);
         }
 
-        function _showErrors($properties){
-            $properties.each(function(){
-                var $currentProperty = $(this);
-                var $editContainer = $currentProperty.children('div:first');
-
-                if($currentProperty.attr('id').indexOf('property') === 0){
-                    //hide index and show properties if there is an error
-                    if($editContainer.children().children('.error').length !== 0){
-                        _hideIndexes($editContainer);
-                        _showProperties($editContainer);
-                        $editContainer.slideToggle( function(){
-                            $editContainer.parent().toggleClass('property-edit-container-open');
-                        });
-                    }
+        function _showErrors($container){
+            var $editContainer;
+            var $error = $container.find('.error');
+            if($error.length){
+                $editContainer = $error.closest('.property-edit-container');
+                if($editContainer.length){
+                    _hideIndexes($editContainer);
+                    _showProperties($editContainer);
+                    $editContainer.slideToggle( function(){
+                        $editContainer.parent().toggleClass('property-edit-container-open');
+                    });
                 }
-            })
+            }
+
         }
 
         function _hideProperties($container){
