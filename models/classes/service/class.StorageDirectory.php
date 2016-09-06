1351fa23ba001a37ec802c47e93b9e79ad006383
Joel Bout <joel@taotesting.com>
2016-09-06 09:34:22 +0200

    Return correct path

--- models/classes/service/class.StorageDirectory.php
+++ models/classes/service/class.StorageDirectory.php
@@ -41,6 +41,7 @@ class tao_models_classes_service_StorageDirectory extends Directory
 
     public function __construct($id, $filesystemId, $path, Websource $provider = null)
     {
+        common_Logger::i($path);
         parent::__construct($filesystemId, $path);
         $this->id = $id;
         $this->accessProvider = $provider;
@@ -115,7 +116,7 @@ class tao_models_classes_service_StorageDirectory extends Directory
         if (!$adapter instanceof Local) {
             throw new common_exception_InconsistentData(__CLASS__.' can only handle local files');
         }
-        return $adapter->getPathPrefix();
+        return $adapter->getPathPrefix().$this->getPrefix();
     }
 
     /**
