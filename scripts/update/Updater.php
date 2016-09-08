562ece0146aba435b0632d02c5e649b45ffee196
Sam Sipasseuth <sam@taotesting.com>
2016-09-08 11:33:52 +0200

    bump version

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -563,7 +563,7 @@ class Updater extends \common_ext_ExtensionUpdater {
             $this->setVersion('6.1.0');
         }
 
-        $this->skip('6.1.0', '7.6.1');
+        $this->skip('6.1.0', '7.7.0');
     }
 
     private function migrateFsAccess() {
