a1506c99ba4aaaf2ec02b40b661c8736c1e6ff86
christophe garcia <christophe.garcia@taotesting.com>
2016-09-16 10:04:15 +0200

    add use

--- helpers/form/class.Validator.php
+++ helpers/form/class.Validator.php
@@ -1,4 +1,6 @@
 <?php
+
+use oat\oatbox\validator\ValidatorInterface;
 /**  
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
