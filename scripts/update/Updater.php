b74ac915f2bdf45dc052909d83295f96c53006db
cmoyon <camille@taotesting.com>
2016-09-30 17:14:44 +0200

    Bump to version 7.11.6

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -563,7 +563,7 @@ class Updater extends \common_ext_ExtensionUpdater {
             $this->setVersion('6.1.0');
         }
 
-        $this->skip('6.1.0', '7.11.5');
+        $this->skip('6.1.0', '7.11.6');
     }
 
     private function migrateFsAccess() {
