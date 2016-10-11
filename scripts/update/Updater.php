7f06e0ea913d84e1369c851eb16b634f52bd8d48
Alexander Zagovorichev <zagovorichev@gmail.com>
2016-10-11 17:31:28 +0300

    Wrote validation exceptions in the log as "Warning"

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -563,7 +563,7 @@ class Updater extends \common_ext_ExtensionUpdater {
             $this->setVersion('6.1.0');
         }
 
-        $this->skip('6.1.0', '7.15.1');
+        $this->skip('6.1.0', '7.15.2');
     }
 
     private function migrateFsAccess() {
