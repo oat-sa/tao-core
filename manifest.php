e1555551369ed49b810b603296cf024fd0f5c9eb
Christophe Noël <christophe@taotesting.com>
2016-10-27 11:02:01 +0200

    bump version

--- manifest.php
+++ manifest.php
@@ -31,7 +31,7 @@ return array(
     'label' => 'Tao base',
     'description' => 'TAO meta-extension',
     'license' => 'GPL-2.0',
-    'version' => '7.19.0',
+    'version' => '7.19.1',
     'author' => 'Open Assessment Technologies, CRP Henri Tudor',
     'requires' => array(
         'generis' => '>=3.6.0',
