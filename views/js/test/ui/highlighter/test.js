94be426aaa9b8f690c9c42241622fbababf968c5
Christophe Noël <touffi51@gmail.com>
2016-11-09 19:08:27 +0100

    better test suite, but functionality fully broken :)

--- views/js/test/ui/highlighter/test.js
+++ views/js/test/ui/highlighter/test.js
@@ -26,6 +26,8 @@ define([
 ], function($, _, highlighterFactory, selectorMock) {
     'use strict';
 
+    var validSelectionsData;
+
     QUnit.module('highlighterFactory');
 
     QUnit.test('module', function(assert) {
@@ -34,65 +36,130 @@ define([
 
     QUnit.module('highlighter');
 
-    QUnit.test('fully highlights a text node', function(assert) {
-        var input = 'I should end up fully highlighted';
-        var output = '<span class="highlighted">I should end up fully highlighted</span>';
+    function logTest(input) {
+        console.log('=================');
+        console.log(input);
+    }
 
-        // setup test
-        var highlighter = highlighterFactory({
-            selector: selectorMock,
-            className: 'highlighted'
+    function getRangeHtml(range) {
+        return $('<div>').append(range.extractContents()).html();
+    }
+
+    validSelectionsData = [
+        {
+            title:      'fully highlights a plain text node',
+            input:      'I should end up fully highlighted',
+            selection:  'I should end up fully highlighted',
+            output:     '<span class="highlighted">I should end up fully highlighted</span>',
+            buildRange: function(range, fixtureContainer) {
+                range.selectNodeContents(fixtureContainer);
+            }
+        },
+
+        {
+            title:      'partially highlights a plain text node',
+            input:      'I should end up partially highlighted',
+            selection:  'partially',
+            output:     'I should end up <span class="highlighted">partially</span> highlighted',
+            buildRange: function(range, fixtureContainer) {
+                range.setStart(
+                    fixtureContainer.firstChild,
+                    fixtureContainer.firstChild.textContent.indexOf('partially')
+                );
+                range.setEnd(
+                    fixtureContainer.firstChild,
+                    fixtureContainer.firstChild.textContent.indexOf('partially') + 'partially'.length
+                );
+            }
+        },
+
+        {
+            title:      'highlights the text content of a single dom element',
+            input:      '<div>I should end up fully highlighted</div>',
+            selection:  '<div>I should end up fully highlighted</div>',
+            output:     '<div><span class="highlighted">I should end up fully highlighted</span></div>',
+            buildRange: function(range, fixtureContainer) {
+                range.selectNode(fixtureContainer.firstChild);
+            }
+        },
+
+        {
+            title:      'highlights the text content of multiple dom elements',
+            input:      '<ul id="list">' +
+                            '<li>leave me alone</li>' +
+                            '<li>highlight me!</li>' +
+                            '<li>highlight me too!</li>' +
+                            '<li>I am too shy to be highlighted</li>' +
+                        '</ul>',
+            selection:      '<li>highlight me!</li>' +
+                            '<li>highlight me too!</li>',
+            output:     '<ul id="list">' +
+                            '<li>leave me alone</li>' +
+                            '<li><span class="highlighted">highlight me!</span></li>' +
+                            '<li><span class="highlighted">highlight me too!</span></li>' +
+                            '<li>I am too shy to be highlighted</li>' +
+                        '</ul>',
+            buildRange: function(range) {
+                var list = document.getElementById('list');
+                range.setStart(list, 1);
+                range.setEnd(list, 3);
+            }
+        },
+
+        {
+            title:      'highlights a fully selected text with a nested node',
+            input:      'We, meaning <strong>me and my children</strong> should end up fully highlighted',
+            selection:  'We, meaning <strong>me and my children</strong> should end up fully highlighted',
+            output:     '<span class="highlighted">We, meaning </span>' +
+                        '<strong><span class="highlighted">me and my children</span></strong>' +
+                        '<span class="highlighted"> should end up fully highlighted</span>',
+            buildRange: function(range, fixtureContainer) {
+                range.selectNodeContents(fixtureContainer.firstChild);
+            }
+        },
+
+        {
+            title:      'highlights a partially selected text with a nested node',
+            input:      'We, meaning <strong>me and my children</strong> should end up partially highlighted',
+            selection:      'meaning <strong>me and my children</strong> should end up',
+            output:     'We, <span class="highlighted">meaning </span>' +
+                        '<strong><span class="highlighted">me and my children</span></strong>' +
+                        '<span class="highlighted"> should end up</span> partially highlighted',
+            buildRange: function(range, fixtureContainer) {
+                range.selectNodeContents(fixtureContainer.firstChild);
+            }
+        }
+    ];
+
+    QUnit
+        .cases(validSelectionsData)
+        .test('Valid selections', function(data, assert) {
+            // setup test
+            var highlighter = highlighterFactory({
+                selector: selectorMock,
+                className: 'highlighted'
+            });
+            var range = document.createRange();
+
+            var fixtureContainer = document.getElementById('qunit-fixture');
+
+            logTest(data.input);
+
+            QUnit.expect(2);
+
+            fixtureContainer.innerHTML = data.input;
+
+            // create and verify range, to make sure buildRange() is implemented correctly
+            data.buildRange(range, fixtureContainer);
+            assert.equal(getRangeHtml(range), data.selection, 'selection is correct');
+
+            selectorMock.removeAllRanges();
+            selectorMock.addRange(range);
+
+            // highlight
+            highlighter.highlightRanges();
+            assert.equal(fixtureContainer.innerHTML, data.output);
         });
-        var range = document.createRange();
-
-        var fixtureContainer = document.getElementById('qunit-fixture');
-
-        fixtureContainer.innerHTML = input;
-
-        // create selection
-        range.setStart(fixtureContainer.firstChild, 0);
-        range.setEnd(fixtureContainer.firstChild, fixtureContainer.firstChild.length);
-        selectorMock.addRange(range);
-
-        // highlight
-        highlighter.highlightRanges();
-
-        QUnit.expect(1);
-        assert.equal(fixtureContainer.innerHTML, output);
-    });
-
-    QUnit.test('partially highlights a text node', function(assert) {
-        var input = 'I should end up fully highlighted';
-        var output = '<span class="highlighted">I should end up fully highlighted</span>';
-
-        // setup test
-        var highlighter = highlighterFactory({
-            selector: selectorMock,
-            className: 'highlighted'
-        });
-        var range = document.createRange();
-
-        var fixtureContainer = document.getElementById('qunit-fixture');
-
-        fixtureContainer.innerHTML = input;
-
-        // create selection
-        range.setStart(
-            fixtureContainer.firstChild,
-            fixtureContainer.firstChild.textContent.indexOf('partially')
-        );
-        range.setEnd(
-            fixtureContainer.firstChild,
-            fixtureContainer.firstChild.textContent.indexOf('partially') + 'partially'.length
-        );
-        selectorMock.addRange(range);
-
-        // highlight
-        highlighter.highlightRanges();
-
-        QUnit.expect(1);
-        assert.equal(fixtureContainer.innerHTML, output);
-    });
 
 
 
--- views/js/ui/highlighter.js
+++ views/js/ui/highlighter.js
@@ -24,9 +24,9 @@
  * @author Christophe Noël <christophe@taotesting.com>
  */
 define([
-    // 'jquery'
+    'jquery'
 ], function (
-    // $
+    $
 ) {
     'use strict';
 
@@ -36,17 +36,106 @@ define([
      * @param {String} option.className
      */
     return function(options) {
-        var selector = options.selector;
+        var selector = options.selector; // fixme: pass an array of ranges to highlightRanges instead ? yes for keyboard selection
+
+        var isWrapping = false;
+
+        function toggleWrapping() {
+            isWrapping = !isWrapping;
+        }
+
+        function wrapAllTextNodes(rootNode, wrapStartNode, wrapEndNode) {
+            var childNodes = rootNode.childNodes;
+            var node, i;
+
+            for (i = 0; i < childNodes.length; i++) {
+                node = childNodes[i];
+
+                if (node.isSameNode(wrapStartNode)) {
+                    toggleWrapping();
+                } else if (node.isSameNode(wrapEndNode)) {
+                    toggleWrapping();
+                }
+
+                switch(node.nodeType) {
+                    case window.Node.ELEMENT_NODE: {
+                        wrapAllTextNodes(node); // recursive call
+                        break;
+                    }
+                    case window.Node.TEXT_NODE: {
+                        if (isWrapping) {
+                            wrapTextNode(node);
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+
+        function wrapTextNode(node) {
+            console.log('wrapping ' + node.textContent);
+            $(node).wrap($('<span>', {
+                class: options.className
+            }));
+            // node.textContent="yeah!!!";
+        }
+
+        function getStartNode(range) {
+            if (range.startContainer.firstChild.nodeType === window.Node.ELEMENT_NODE) {
+                return range.startContainer.childNodes[range.startOffset];
+            }
+        }
+
+        function getEndNode(range) {
+            if (range.endContainer.firstChild.nodeType === window.Node.ELEMENT_NODE) {
+                return range.endContainer.childNodes[range.endOffset];
+            }
+        }
+
+        function isLiteralNode(node) {
+            var literalNodeTypes = [
+                window.Node.CDATA_SECTION_NODE,
+                window.Node.COMMENT_NODE,
+                window.Node.TEXT_NODE
+            ];
+            return literalNodeTypes.indexOf(node.nodeType) !== -1;
+        }
 
         return {
             highlightRanges: function highlightRanges() {
                 var ranges = selector.getRanges();
-
-                var highlightContainer = document.createElement('span');
-                highlightContainer.className = options.className;
+                // var rangeContent;
+                // console.log('=============================================');
 
                 ranges.forEach(function(range) {
-                    range.surroundContents(highlightContainer);
+
+                    // deal with the simplest case first : select part of a text within another text
+                    if (isLiteralNode(range.commonAncestorContainer)) {
+                        //
+                    }
+
+                    // var rangeContent = range.extractContents();
+                    console.dir(range.commonAncestorContainer);
+                    console.dir(range.startContainer);
+                    console.dir(range.endContainer);
+                    console.dir(getStartNode(range));
+                    console.dir(getEndNode(range));
+                    wrapAllTextNodes(
+                        range.commonAncestorContainer,
+                        getStartNode(range),
+                        getEndNode(range)
+                    );
+                    // range.insertNode(rangeContent);
+                    // console.dir(rangeContent);
+
+                    // console.dir(rangeContent);
+                    // var highlightContainer = document.createElement('span');
+                    // highlightContainer.className = options.className;
+                    //
+                    // highlightContainer.appendChild(rangeContent);
+                    // range.insertNode(highlightContainer);
+                    // rangeContent = range.extractContents();
+                    // wrapTextNode(rangeContent);
                 });
             }
         };
