c9662b6e7734838b72a06f428bd45a6bcbf8e695
Alexander Zagovorichev <zagovorichev@gmail.com>
2016-11-14 14:24:07 +0300

    Improvement for bulkActionPopup component. Cases when no items for action and improvement for denied list

--- manifest.php
+++ manifest.php
@@ -31,7 +31,7 @@ return array(
     'label' => 'Tao base',
     'description' => 'TAO meta-extension',
     'license' => 'GPL-2.0',
-    'version' => '7.22.5',
+    'version' => '7.23.0',
     'author' => 'Open Assessment Technologies, CRP Henri Tudor',
     'requires' => array(
         'generis' => '>=3.6.0',
--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -572,7 +572,7 @@ class Updater extends \common_ext_ExtensionUpdater {
             $this->setVersion('7.17.0');
         }
 
-        $this->skip('7.17.0', '7.22.5');
+        $this->skip('7.17.0', '7.23.0');
     }
 
     private function migrateFsAccess() {
