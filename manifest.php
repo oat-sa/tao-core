92168af9ddb1f9fa47ab8d9057dd26b90390b704
jsconan <jsconan@gmail.com>
2016-12-01 14:44:54 +0100

    Bump version

--- manifest.php
+++ manifest.php
@@ -31,7 +31,7 @@ return array(
     'label' => 'Tao base',
     'description' => 'TAO meta-extension',
     'license' => 'GPL-2.0',
-    'version' => '7.32.0',
+    'version' => '7.33.0',
     'author' => 'Open Assessment Technologies, CRP Henri Tudor',
     'requires' => array(
         'generis' => '>=3.6.0',
