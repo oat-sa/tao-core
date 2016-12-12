4518e9aafaa8baf744542148cf34b54dbd22e10a
Mikhail Kamarouski <Komarouski@1pt.com>
2016-12-12 15:58:23 +0300

    A bit more @PHPDOC

--- helpers/uploadReferencerTrait.php
+++ helpers/uploadReferencerTrait.php
@@ -1,4 +1,22 @@
 <?php
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2016 (original work) Open Assessment Technologies SA;
+ *
+ */
 
 namespace oat\tao\helpers;
 
@@ -6,10 +24,18 @@ use oat\generis\model\fileReference\UrlFileSerializer;
 use oat\oatbox\filesystem\File;
 use oat\oatbox\service\ServiceManager;
 
+/**
+ * Class uploadReferencerTrait
+ * @package oat\tao\helpers
+ */
 trait uploadReferencerTrait
 {
     private $uriSerializer;
 
+    /**
+     *
+     * @return UrlFileSerializer
+     */
     protected function getSerializer()
     {
         if (!$this->uriSerializer) {
@@ -19,6 +45,12 @@ trait uploadReferencerTrait
         return $this->uriSerializer;
     }
 
+    /**
+     * Detects
+     * @param $file
+     * @return File
+     * @throws \common_Exception
+     */
     protected function universalizeUpload($file)
     {
         if (filter_var($file, FILTER_VALIDATE_URL)) {
@@ -32,6 +64,8 @@ trait uploadReferencerTrait
     }
 
     /**
+     * Either create local copy or use original location for tile
+     * Returns absolute path to the file, to be compatible with legacy methods
      * @param $serial
      * @return string
      */
