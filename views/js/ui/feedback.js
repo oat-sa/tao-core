c6aebe83b8ad60c0b4985a27dfd6697226c9fe93
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2016-12-14 17:25:48 +0100

    document the feedback component

--- views/js/ui/feedback.js
+++ views/js/ui/feedback.js
@@ -18,6 +18,19 @@
  */
 
 /**
+ * Displays feedbacks "toasts" messages.
+ *
+ * @example
+ *  feedback().info('This is correct');
+ *
+ *  feedback().warning('You are about to remove %d %s', [5, 'users']);
+ *
+ *  feedback($anotherContainer, {
+ *    timeout : -1
+ *    encodeHtml: false
+ *  })
+ *  .error('<p>Error : </p><ul><li>Reason 1</li>...</ul>');
+ *
  * @author Bertrand Chevrier <bertrand@taotesting.com>
  */
 define([
@@ -31,52 +44,53 @@ define([
 ], function($, _, format, component, wrapLongWords, encode, tpl){
     'use strict';
 
+    //The default container of the feedbacks
     var defaultContainerSelector = '#feedback-box';
 
     //keep a reference to ALL alive feedback
     var currents = [];
 
-    //feedback levels are divided into 2 categories
-    var categories = {
+    //available levels
+    var levels = [
+        'info',
+        'success',
+        'warning',
+        'danger',
+        'error'
+    ];
 
-        //volatiles messages disappear after a certain amount of time.
-        //If 2 messages of the same category appears, only the last one is displayed
-        'volatile'      : ['info', 'success'],
-
-        //persistent feedback stay until their are closed.
-        //Other persistent feedback are merged to keep all the info.
-        //To prevent UI pollution, they may be collapsed  in a notification area
-        'persistent'    : ['warning', 'danger', 'error']
-    };
-
-    //extract the available levels from the categories
-    var levels = _(categories).values().flatten().value();
-
-
-    //the default options
     var defaultOptions = {
         timeout: {
-            info: 2000,
+            info:    2000,
             success: 2000,
             warning: 4000,
-            danger: 4000,
-            error: 8000
+            danger:  4000,
+            error:   8000
         },
         // Note: value depends on font, font-weight and such.
         // 40 is pretty good in the current setup but will
         // never be exact with a non-proportional font.
         wrapLongWordsAfter: 40,
+
+        //by default HTML content is encoded
         encodeHtml : true,
+
+        //change the display (absolute or in the flow)
         popup: true
     };
 
     /**
-     * Enables you to create a new feedback.
-     * example fb().error("content");
+     * Creates a feedback object.
+     *
      * @exports ui/feedback
      * @param {jQUeryElement} [$container] - only to specify another container
-     * @returns {Object} the feedback object
-     * @throws {Error} if the container isn't found
+     * @param {Object} [config] - change the config
+     * @param {Object|Number} [config.timeout] - either one for every level or per level timeout in ms
+     * @param {Number} [config.wrapLongWordsAfter] - add a space in the middle of very long word to enable wrap lines
+     * @param {Boolean} [config.encodeHtml] - weither the message is html encoded
+     * @param {Boolean} [config.popup] - displays the message as a popup or in the flow
+     * @returns {feedback} the feedback object
+     * @throws {TypeError} without a container
      */
     var feedbackFactory = function feedbackFactory( $container, config ){
         var feedback;
@@ -88,8 +102,19 @@ define([
             throw new TypeError('The feedback needs to belong to an existing container');
         }
 
+        /**
+         * @typedef {Object} feedback - the feedback component
+         */
         feedback =  component({
 
+            /**
+             * Creates a message, not displayed.
+             * @param {String} [level = 'info'] - the message level
+             * @param {String} msg - the message to display
+             * @param {Array} [params] - parameters for the message format (%s,%d,%j)
+             * @param {Object} [options] - specify the config
+             * @returns {feedback} chains
+             */
             message : function message(level, msg, params, options){
                 var displayedMessage = msg;
 
@@ -105,7 +130,6 @@ define([
 
                 this.config  = _.defaults(options || {}, this.config);
                 this.config.level = level;
-                this.config.category = _.findKey(categories, [level]);
 
                 // encode plain text string to html
                 if(this.config.encodeHtml){
@@ -127,26 +151,66 @@ define([
                 return this;
             },
 
+            /**
+             * Opens an info message
+             * @param {String} msg - the message to display
+             * @param {Array} [params] - parameters for the message format (%s,%d,%j)
+             * @param {Object} [options] - specify the config
+             * @returns {feedback} chains
+             */
             info : function info(msg, params, options){
                 return this.message('info', msg, params, options).open();
             },
 
+            /**
+             * Opens an success message
+             * @param {String} msg - the message to display
+             * @param {Array} [params] - parameters for the message format (%s,%d,%j)
+             * @param {Object} [options] - specify the config
+             * @returns {feedback} chains
+             */
             success : function success(msg, params, options){
                 return this.message('success', msg, params, options).open();
             },
 
+            /**
+             * Opens an warning message
+             * @param {String} msg - the message to display
+             * @param {Array} [params] - parameters for the message format (%s,%d,%j)
+             * @param {Object} [options] - specify the config
+             * @returns {feedback} chains
+             */
             warning : function warning(msg, params, options){
                 return this.message('warning', msg, params, options).open();
             },
 
+            /**
+             * Opens an danger message
+             * @param {String} msg - the message to display
+             * @param {Array} [params] - parameters for the message format (%s,%d,%j)
+             * @param {Object} [options] - specify the config
+             * @returns {feedback} chains
+             */
             danger : function danger(msg, params, options){
                 return this.message('danger', msg, params, options).open();
             },
 
+            /**
+             * Opens an error message
+             * @param {String} msg - the message to display
+             * @param {Array} [params] - parameters for the message format (%s,%d,%j)
+             * @param {Object} [options] - specify the config
+             * @returns {feedback} chains
+             */
             error : function error(msg, params, options){
                 return this.message('error', msg, params, options).open();
             },
 
+            /**
+             * Opens the message (and close previous one)
+             * ! Method kept for backward compat with previous version !
+             * @returns {feedback} chains
+             */
             open : function open(){
 
                 //close others
@@ -155,16 +219,25 @@ define([
                     .invoke('close');
 
                 //and display
-                this.display();
-                return this;
+                return this.display();
             },
 
+            /**
+             * Closes the message
+             * ! Method kept for backward compat with previous version !
+             * @returns {feedback} chains
+             */
             close : function close(){
                 if(this.is('rendered')){
                     this.destroy();
                 }
             },
 
+            /**
+             * Displays the message (does the render)
+             * ! Method kept for backward compat with previous version !
+             * @returns {feedback} chains
+             */
             display : function display(){
 
                 if(this.is('rendered')){
@@ -215,7 +288,6 @@ define([
             })
             .init(config);
     };
-
 
     return feedbackFactory;
 });
