9faf57d6b658957998182ca0d9f7f802ad4c58f2
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2016-12-23 16:42:49 +0100

    version bump bump

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -637,7 +637,7 @@ class Updater extends \common_ext_ExtensionUpdater {
             $this->setVersion('7.35.0');
         }
 
-        $this->skip('7.35.0', '7.45.5');
+        $this->skip('7.35.0', '7.45.6');
     }
 
     private function migrateFsAccess() {
