23455175776564eccaafc7d2afa97f1d16038961
Dieter Raber <dieter@taotesting.com>
2017-01-23 13:57:27 +0100

    version bump

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -647,7 +647,7 @@ class Updater extends \common_ext_ExtensionUpdater {
             $this->setVersion('7.47.0');
         }
 
-        $this->skip('7.47.0', '7.47.1');
+        $this->skip('7.47.0', '7.47.2');
 
     }
 
