5959c8bf3b1ee8f1001ab5c22372a014983ff770
Joel Bout <joel@taotesting.com>
2017-02-06 10:35:11 +0100

    Allow hyphen in db names

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -677,7 +677,7 @@ class Updater extends \common_ext_ExtensionUpdater {
             $this->setVersion('7.62.0');
         }
 
-        $this->skip('7.62.0', '7.63.0');
+        $this->skip('7.62.0', '7.63.1');
     }
 
     private function migrateFsAccess() {
