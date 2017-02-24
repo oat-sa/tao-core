d2c6f175f007be45bb5f0c945709273a99bcaf4f
Christophe Noël <christophe@taotesting.com>
2017-02-24 14:13:56 +0100

    bump version

--- manifest.php
+++ manifest.php
@@ -33,7 +33,7 @@ return array(
     'label' => 'Tao base',
     'description' => 'TAO meta-extension',
     'license' => 'GPL-2.0',
-    'version' => '7.69.7',
+    'version' => '7.70.0',
     'author' => 'Open Assessment Technologies, CRP Henri Tudor',
     'requires' => array(
         'generis' => '>=3.17.0',
--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -690,7 +690,7 @@ class Updater extends \common_ext_ExtensionUpdater {
             AclProxy::applyRule(new AccessRule('grant', 'http://www.tao.lu/Ontologies/TAO.rdf#BaseUserRole', ['ext'=>'tao','mod' => 'Notification']));
             $this->setVersion('7.69.0');
         }
-      
+
         $this->skip('7.69.0', '7.69.6');
 
         if($this->isVersion('7.69.6')) {
@@ -711,6 +711,8 @@ class Updater extends \common_ext_ExtensionUpdater {
 
             $this->setVersion('7.69.7');
         }
+
+        $this->skip('7.69.7', '7.70.0');
     }
 
     private function migrateFsAccess() {
