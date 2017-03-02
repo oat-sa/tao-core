338b528051b819d7601f327b05ca095fc7608cf5
Christophe Noël <christophe@taotesting.com>
2017-03-02 14:45:07 +0100

    added draggable

---
+++ views/js/test/ui/component/draggable/test.js
@@ -0,0 +1,61 @@
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2017 (original work) Open Assessment Technologies SA;
+ */
+/**
+ */
+
+define([
+    'jquery',
+    'ui/component',
+    'ui/component/placeable',
+    'ui/component/draggable'
+], function ($, componentFactory, makePlaceable, makeDraggable) {
+    'use strict';
+
+    QUnit.module('API');
+
+    QUnit.test('module', function (assert) {
+        QUnit.expect(1);
+
+        assert.ok(typeof makeDraggable === 'function', 'The module expose a function');
+    });
+
+    QUnit.module('Visual test');
+
+    QUnit.asyncTest('.resetPosition() - with default position', function (assert) {
+        var component = componentFactory(),
+            $container = $('#outside');
+
+        QUnit.expect(1);
+
+        makeDraggable(component);
+
+        component
+            .on('render', function(){
+                assert.ok(true);
+                QUnit.start();
+            })
+            .init({
+                x: 100,
+                y: 100
+            })
+            .render($container)
+            .setSize(200, 300);
+    });
+
+
+});
---
+++ views/js/ui/component/draggable.js
@@ -0,0 +1,86 @@
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2016 (original work) Open Assessment Technologies SA;
+ */
+
+/**
+ *
+ *
+ * @author Bertrand Chevrier <bertrand@taotesting.com>
+ * @author Christophe Noël <christophe@taotesting.com>
+ */
+define([
+    'lodash',
+    'interact',
+    'ui/component/placeable'
+], function (_, interact, makePlaceable) {
+    'use strict';
+
+    return function makeDraggable(component) {
+
+        return component
+            .off('.makeDraggable')
+            .on('init.makeDraggable', function() {
+                if (! makePlaceable.isPlaceable(component)) {
+                    makePlaceable(component);
+                }
+                this.setState('draggable');
+            })
+            .on('render.makeDraggable', function() {
+                var self        = this,
+                    $element    = this.getElement(),
+                    element     = $element[0],
+                    $container  = this.getContainer(),
+                    container   = $container[0],
+                    rootNode    = document.querySelector('html');
+
+                interact(element)
+                    .draggable({
+                        autoScroll: true,
+                        restrict: {
+                            restriction: container,
+                            elementRect: {left: 0, right: 1, top: 0, bottom: 1}
+                        },
+                        onmove: function onMove(event) {
+                            self.moveBy(event.dx, event.dy);
+                        }
+                    })
+                    .on('dragstart', function () {
+                        self.setState('moving', true);
+                        self.trigger('dragstart');
+                    })
+                    .on('dragend', function () {
+                        self.setState('moving', false);
+                        self.trigger('dragend');
+                    });
+
+
+                //fix cursor issue with interact <= 1.2.6
+                //cursor remains in moving/sizing by only clicking
+                $element
+                    .off('.makeDraggable')
+                    .on('click.makeDraggable', function(){
+                        _.delay(function(){
+                            if(!self.is('sizing') && !self.is('moving') && rootNode){
+                                rootNode.style.cursor = 'default';
+                            }
+                        }, 25);
+                    });
+
+            });
+    };
+
+});
