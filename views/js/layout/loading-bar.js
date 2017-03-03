8d6b650dd85dca19bae02d8a01c97d9bf94d4250
Aleh Hutnikau <hutnikau@1pt.com>
2017-03-03 11:22:27 +0300

    Add unit test

--- views/js/layout/loading-bar.js
+++ views/js/layout/loading-bar.js
@@ -86,7 +86,7 @@ define(['jquery'],
         return {
             /**
              * Show loading bar
-             * @param bool covered - whether overlay HTML element should be added (disable GUI). Default - true
+             * @param {Boolean} [covered = true] - - whether overlay HTML element should be added (disable GUI).
              */
             start: function (covered) {
                 if (typeof covered === 'undefined') {
