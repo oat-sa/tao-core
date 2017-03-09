e8d9ca7f8446b4d6395f474301585204ef297ec6
antoinerobin <antoine.robin.2014@utt.fr>
2017-03-09 15:33:53 +0100

    allowallow to write the anonymous interface lang

--- install/class.Installator.php
+++ install/class.Installator.php
@@ -255,17 +255,22 @@ class tao_install_Installator{
 
 			$session_name = (isset($installData['session_name']))?$installData['session_name']:self::generateSessionName();
 			$generisConfigWriter->createConfig();
-			$generisConfigWriter->writeConstants(array(
-				'LOCAL_NAMESPACE'			=> $installData['module_namespace'],
-				'GENERIS_INSTANCE_NAME'		=> $installData['instance_name'],
-				'GENERIS_SESSION_NAME'		=> $session_name,
-				'ROOT_PATH'					=> $this->options['root_path'],
+			$constants = array(
+                'LOCAL_NAMESPACE'			=> $installData['module_namespace'],
+                'GENERIS_INSTANCE_NAME'		=> $installData['instance_name'],
+                'GENERIS_SESSION_NAME'		=> $session_name,
+                'ROOT_PATH'					=> $this->options['root_path'],
                 'FILES_PATH'                => $installData['file_path'],
-				'ROOT_URL'					=> $installData['module_url'],
-				'DEFAULT_LANG'				=> $installData['module_lang'],
-				'DEBUG_MODE'				=> ($installData['module_mode'] == 'debug') ? true : false,
-			    'TIME_ZONE'                  => $installData['timezone']
-			));
+                'ROOT_URL'					=> $installData['module_url'],
+                'DEFAULT_LANG'				=> $installData['module_lang'],
+                'DEBUG_MODE'				=> ($installData['module_mode'] == 'debug') ? true : false,
+                'TIME_ZONE'                  => $installData['timezone']
+            );
+
+            $constants['DEFAULT_ANONYMOUS_INTERFACE_LANG'] = (isset($installData['anonymous_lang'])) ? $installData['anonymous_lang'] : $installData['module_lang'];
+
+
+			$generisConfigWriter->writeConstants($constants);
 
 			/*
 			 * 5b - Prepare the file/cache folder (FILES_PATH) not yet defined)
