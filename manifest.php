6723bb144a4073d56d118be1073c641ce0842079
antoinerobin <antoine.robin.2014@utt.fr>
2017-03-27 11:08:19 +0200

    update dependency

--- manifest.php
+++ manifest.php
@@ -37,7 +37,7 @@ return array(
     'version' => '7.90.0',
     'author' => 'Open Assessment Technologies, CRP Henri Tudor',
     'requires' => array(
-        'generis' => '>=3.17.0',
+        'generis' => '>=3.21.0',
     ),
     'models' => array(
         'http://www.tao.lu/Ontologies/TAO.rdf',
