b94cb626c59e3d9b7f9ada03ffebead1c01878eb
antoinerobin <antoine.robin.2014@utt.fr>
2017-03-31 14:25:45 +0200

    update version number

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -772,6 +772,8 @@ class Updater extends \common_ext_ExtensionUpdater {
             }
             $this->setVersion('8.2.0');
         }
+
+        $this->skip('8.2.0', '8.2.1');
     }
 
     private function migrateFsAccess() {
