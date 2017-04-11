d34adc3d33201526c7d048138d9f97bfeda32109
Dieter Raber <dieter@taotesting.com>
2017-04-11 09:13:30 +0200

    fixed forum link
    On a vanilla TAO installation
    
    - checkout this branch
    - change `/tao/includes/constants.php#49` temporarily to *alpha*
    - browse the site, paying attention to the bottom/right corner

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -773,7 +773,7 @@ class Updater extends \common_ext_ExtensionUpdater {
             $this->setVersion('8.2.0');
         }
 
-        $this->skip('8.2.0', '8.3.0');
+        $this->skip('8.2.0', '8.3.1');
     }
 
     private function migrateFsAccess() {
