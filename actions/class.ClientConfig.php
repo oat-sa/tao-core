88536dd053ab460ae81ce9f1e65b75b7cdf92433
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2017-04-20 17:43:28 +0200

    validate and encode client config's params

--- actions/class.ClientConfig.php
+++ actions/class.ClientConfig.php
@@ -16,12 +16,14 @@
 *
 * Copyright (c) 2013 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
 */
-use oat\tao\model\ClientLibRegistry;
+
+use oat\oatbox\service\ServiceManager;
 use oat\tao\model\ClientLibConfigRegistry;
+use oat\tao\model\ClientLibRegistry;
 use oat\tao\model\ThemeRegistry;
 use oat\tao\model\asset\AssetService;
-use \oat\oatbox\service\ServiceManager;
 use oat\tao\model\clientConfig\ClientConfigService;
+use oat\tao\model\routing\Resolver;
 
 /**
  * Generates client side configuration.
@@ -45,49 +47,119 @@ class tao_actions_ClientConfig extends tao_actions_CommonModule {
 
         $libConfigs = ClientLibConfigRegistry::getRegistry()->getMap();
         $this->setData('libConfigs', $libConfigs);
-        
+
         $extendedConfig = $this->getServiceManager()->get(ClientConfigService::SERVICE_ID)->getExtendedConfig();
         foreach ($extendedConfig as $key => $value) {
             $this->setData($key, json_encode($value));
         }
 
-        $extensionManager = common_ext_ExtensionsManager::singleton();
-        $langCode = tao_helpers_I18n::getLangCode();
-
+        //use the resolver in order to validate the route
+        $resolver = $this->getResolver();
 
         //loads the URLs context
         /** @var AssetService $assetService */
         $assetService = ServiceManager::getServiceManager()->get(AssetService::SERVICE_ID);
         $tao_base_www = $assetService->getJsBaseWww('tao');
 
-        $extensionId = ($this->hasRequestParameter('extension')) ? $this->getRequestParameter('extension') : \Context::getInstance()->getExtensionName();
-        $extension = $extensionManager->getExtensionById($extensionId);
-        $base_www = $assetService->getJsBaseWww($extensionId);
-        $base_url = $extension->getConstant('BASE_URL');
+        $base_www = $assetService->getJsBaseWww($resolver->getExtensionId());
+        $base_url = $this->getExtension($resolver->getExtensionId())->getConstant('BASE_URL');
 
-
-        //set contextual data
-        $this->setData('locale', $langCode);
-
+        $langCode = tao_helpers_I18n::getLangCode();
         if(strpos($langCode, '-') > 0){
             $lang = strtolower(substr($langCode, 0, strpos($langCode, '-')));
         } else {
             $lang = strtolower($langCode);
         }
-        $this->setData('lang',              $lang);
-        $this->setData('version',           TAO_VERSION);
-        $this->setData('buster',            $assetService->getCacheBuster());
-        $this->setData('extension',         $this->getRequestParameter('extension'));
-        $this->setData('module',            $this->getRequestParameter('module'));
-        $this->setData('action',            $this->getRequestParameter('action'));
-        $this->setData('base_www',          $base_www);
-        $this->setData('tao_base_www',      $tao_base_www);
-        $this->setData('base_url',          $base_url);
-        $this->setData('shownExtension',    $this->getRequestParameter('shownExtension'));
-        $this->setData('shownStructure',    $this->getRequestParameter('shownStructure'));
-        $this->setData('client_timeout',    $this->getClientTimeout());
+
+        $this->setData('locale', $langCode);
+        $this->setData('client_timeout', $this->getClientTimeout());
+        $this->setData('tao_base_www', $tao_base_www);
+
+        $this->setData('context', json_encode([
+            'root_url'       => ROOT_URL,
+            'base_url'       => $base_url,
+            'taobase_www'    => $tao_base_www,
+            'base_www'       => $base_www,
+            'base_lang'      => $lang,
+            'locale'         => $langCode,
+            'timeout'        => $this->getClientTimeout(),
+            'extension'      => $resolver->getExtensionId(),
+            'module'         => $resolver->getControllerShortName(),
+            'action'         => $resolver->getMethodName(),
+            'shownExtension' => $this->getShownExtension(),
+            'shownStructure' => $this->getShownStructure(),
+        ]));
 
         $this->setView('client_config.tpl');
+    }
+
+    /**
+     * Get an extension by it's id
+     * @param string $extensionId the extension name/id
+     * @return common_ext_Exntesion the extension
+     * @throws Exception if the parameter contains an unknown extension
+     */
+    private function getExtension($extensionId)
+    {
+        try {
+            $extensionManager = common_ext_ExtensionsManager::singleton();
+            return $extensionManager->getExtensionById($extensionId);
+
+        } catch(common_ext_ExtensionException $cee){
+            throw new Exception(__('Wrong parameter shownExtension'), $cee);
+        }
+    }
+
+    /**
+     * Get and validate the extension name of the parameter 'shownExtension'
+     * @return string the validated extension name
+     * @throws Exception if the parameter contains an unknown extension
+     */
+    private function getShownExtension()
+    {
+        if($this->hasRequestParameter('shownExtension')){
+            $extension = $this->getExtension($this->getRequestParameter('shownExtension'));
+            return $extension->getName();
+        }
+        return null;
+    }
+
+    /**
+     * Get and validate the 'shownStructure' parameter
+     * @return string the structure id if found in the list
+     */
+    private function getShownStructure()
+    {
+        if($this->hasRequestParameter('shownStructure')){
+            $structure = $this->getRequestParameter('shownStructure');
+            $perspectives = \oat\tao\model\menu\MenuService::getAllPerspectives();
+            foreach($perspectives as $perspective){
+                if($perspective->getId() == $structure){
+                    return $perspective->getId();
+                }
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Get a resolved route from the GET parameters extension/module/action
+     * @return Resolver
+     * @throws Exception in case a parameter is missing or if the route can't be resolved
+     */
+    private function getResolver()
+    {
+        $url = [
+            $this->hasRequestParameter('extension') ? $this->getRequestParameter('extension') : \Context::getInstance()->getExtensionName(),
+            $this->getRequestParameter('module'),
+            $this->getRequestParameter('action')
+        ];
+        try {
+            $route = new Resolver(new common_http_Request('/' . implode('/', $url)));
+        } catch (ResolverException $re){
+            throw new Exception(__('Wrong or missing parameter extension, module or action'), $re);
+        }
+        return $route;
     }
 }
 ?>
