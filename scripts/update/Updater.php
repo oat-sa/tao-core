07e366a9ef0a1e1e480e5395cd6f7c4d0538a676
Alexander Zagovorichev <zagovorichev@gmail.com>
2017-04-25 10:24:16 +0300

    Removed constant BASE_WWW

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -769,7 +769,7 @@ class Updater extends \common_ext_ExtensionUpdater {
             $this->setVersion('8.2.0');
         }
 
-        $this->skip('8.2.0', '8.5.2');
+        $this->skip('8.2.0', '9.0.0');
     }
 
     private function migrateFsAccess() {
