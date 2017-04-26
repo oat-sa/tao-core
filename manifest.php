00a01df8c56768c9c0dae0e7f31ff3aa6c55ef7f
Joel Bout <joel@taotesting.com>
2017-04-26 17:32:53 +0200

    version bump

--- manifest.php
+++ manifest.php
@@ -35,7 +35,7 @@ return array(
     'label' => 'Tao base',
     'description' => 'TAO meta-extension',
     'license' => 'GPL-2.0',
-    'version' => '9.0.2',
+    'version' => '9.0.3',
     'author' => 'Open Assessment Technologies, CRP Henri Tudor',
     'requires' => array(
         'generis' => '>=3.17.0',
