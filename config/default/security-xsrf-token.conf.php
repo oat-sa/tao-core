6b3307bde6b6d4999039243f1cbc503262cab68f
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2017-04-27 11:51:09 +0200

    fix store class serialization

--- config/default/security-xsrf-token.conf.php
+++ config/default/security-xsrf-token.conf.php
@@ -1,12 +1,6 @@
 <?php
-/**
- * Default config header
- *
- * To replace this add a file /home/bertrand/dev/projects/package-tao-dev/tao/config/header/security-xsrf-token.conf.php
- */
-
 return new oat\tao\model\security\xsrf\TokenService(array(
-    'store' => unserialize('O:45:"oat\\tao\\model\\security\\xsrf\\TokenStoreSession":0:{}'),
+    'store' => new oat\tao\model\security\xsrf\TokenStoreSession(),
     'poolSize' => 10,
     'timeLimit' => 0
 ));
