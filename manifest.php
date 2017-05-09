a6c20c2b247396b6c75daa43ffec7e34f1e98f3a
antoinerobin <antoine.robin.2014@utt.fr>
2017-05-09 09:45:57 +0200

    update version number

--- manifest.php
+++ manifest.php
@@ -35,7 +35,7 @@ return array(
     'label' => 'Tao base',
     'description' => 'TAO meta-extension',
     'license' => 'GPL-2.0',
-    'version' => '9.3.2',
+    'version' => '9.4.0',
     'author' => 'Open Assessment Technologies, CRP Henri Tudor',
     'requires' => array(
         'generis' => '>=3.27.0',
