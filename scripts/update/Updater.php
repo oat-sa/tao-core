d01a1df872a08feb582ebd39f73e58cf1ee507e4
jsconan <jsconan@gmail.com>
2017-05-10 10:50:01 +0200

    Bump version

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -785,7 +785,7 @@ class Updater extends \common_ext_ExtensionUpdater {
             $this->setVersion('9.2.0');
         }
 
-        $this->skip('9.2.0', '9.4.0');
+        $this->skip('9.2.0', '9.5.0');
 
     }
 
