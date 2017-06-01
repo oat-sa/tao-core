7f3b4d8f6600fdcfa9ba9b5598f4e6c88b96a4a7
Vilmos "Chilly" Kovacs <vilmos@taotesting.com>
2017-06-01 11:57:42 +0200

    TAO-3871 change version

--- manifest.php
+++ manifest.php
@@ -35,10 +35,10 @@ return array(
     'label' => 'Tao base',
     'description' => 'TAO meta-extension',
     'license' => 'GPL-2.0',
-    'version' => '10.14.4',
+    'version' => '10.15.0',
     'author' => 'Open Assessment Technologies, CRP Henri Tudor',
     'requires' => array(
-        'generis' => '>=3.31.0',
+        'generis' => '>=3.33.0',
     ),
     'models' => array(
         'http://www.tao.lu/Ontologies/TAO.rdf',
--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -816,7 +816,7 @@ class Updater extends \common_ext_ExtensionUpdater {
             $this->setVersion('10.13.0');
         }
         
-        $this->skip('10.13.0', '10.14.4');
+        $this->skip('10.13.0', '10.15.0');
     }
 
     private function migrateFsAccess() {
