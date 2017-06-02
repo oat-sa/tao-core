11537cafb03c9eae387e2cbe13eeffaa74322a91
Aleh Hutnikau <hutnikau@1pt.com>
2017-06-02 17:08:48 +0300

    Transform filter value

--- manifest.php
+++ manifest.php
@@ -35,7 +35,7 @@ return array(
     'label' => 'Tao base',
     'description' => 'TAO meta-extension',
     'license' => 'GPL-2.0',
-    'version' => '10.14.4',
+    'version' => '10.16.0',
     'author' => 'Open Assessment Technologies, CRP Henri Tudor',
     'requires' => array(
         'generis' => '>=3.31.0',
