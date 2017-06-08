56264939beccd51381bdd609e92a4e478adf0a79
jsconan <jsconan@gmail.com>
2017-06-08 10:27:02 +0200

    Bump version

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -825,7 +825,7 @@ class Updater extends \common_ext_ExtensionUpdater {
             $this->setVersion('10.16.0');
         }
 
-        $this->skip('10.16.0', '10.17.1');
+        $this->skip('10.16.0', '10.18.0');
     }
 
     private function migrateFsAccess() {
