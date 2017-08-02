ffc9c664a1d33236da35bee1c4c7e3339f92f2fe
eric <eacraft13@gmail.com>
2017-08-02 17:34:00 -0500

    Bump minor version (again)
    tao-3868

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -914,7 +914,7 @@ class Updater extends \common_ext_ExtensionUpdater {
             $this->setVersion('12.2.2');
         }
 
-        $this->skip('12.2.2', '12.5.0');
+        $this->skip('12.2.2', '12.6.0');
     }
 
     private function migrateFsAccess() {
