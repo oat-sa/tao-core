2f0e2b328724a1239f9c40ff79b1f3ed914bd0bf
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2017-08-04 16:25:07 +0200

    rebump

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -914,7 +914,7 @@ class Updater extends \common_ext_ExtensionUpdater {
             $this->setVersion('12.2.2');
         }
 
-        $this->skip('12.2.2', '12.7.0');
+        $this->skip('12.2.2', '12.8.0');
     }
 
     private function migrateFsAccess() {
