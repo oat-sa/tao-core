2e523b136adc18f9a2d93778b949decfba84e9ca
siwane <camille@taotesting.com>
2017-08-18 12:41:33 +0000

    Remove not implemented exception

--- models/classes/api/ApiClientConnector.php
+++ models/classes/api/ApiClientConnector.php
@@ -20,6 +20,7 @@
 
 namespace oat\tao\model\api;
 
+use GuzzleHttp\Promise\PromiseInterface;
 use oat\oatbox\Configurable;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
@@ -73,6 +74,55 @@ class ApiClientConnector extends Configurable implements ClientInterface
     }
 
     /**
+     * Asynchronously send an HTTP request.
+     *
+     * @param RequestInterface $request Request to send
+     * @param array            $options Request options to apply to the given
+     *                                  request and to the transfer.
+     *
+     * @return PromiseInterface
+     */
+    public function sendAsync(RequestInterface $request, array $options = [])
+    {
+        return $this->getClient()->sendAsync($request, $options);
+    }
+
+    /**
+     * Create and send an asynchronous HTTP request.
+     *
+     * Use an absolute path to override the base path of the client, or a
+     * relative path to append to the base path of the client. The URL can
+     * contain the query string as well. Use an array to provide a URL
+     * template and additional variables to use in the URL template expansion.
+     *
+     * @param string              $method  HTTP method
+     * @param string|UriInterface $uri     URI object or string.
+     * @param array               $options Request options to apply.
+     *
+     * @return PromiseInterface
+     */
+    public function requestAsync($method, $uri, array $options = [])
+    {
+        return $this->getClient()->requestAsync($method, $uri, $options);
+    }
+
+    /**
+     * Get a client configuration option.
+     *
+     * These options include default request options of the client, a "handler"
+     * (if utilized by the concrete client), and a "base_uri" if utilized by
+     * the concrete client.
+     *
+     * @param string|null $option The config option to retrieve.
+     *
+     * @return mixed
+     */
+    public function getConfig($option = null)
+    {
+        return $this->getClient()->getConfig($option);
+    }
+
+    /**
      * Return the Http Client e.q. Guzzle client
      *
      * @return Client
@@ -90,21 +140,6 @@ class ApiClientConnector extends Configurable implements ClientInterface
     protected function getClientOptions()
     {
         return $this->getOptions();
-    }
-
-    public function sendAsync(RequestInterface $request, array $options = [])
-    {
-        throw new \common_exception_NotImplemented(__METHOD__ . ' is not implemented.');
-    }
-
-    public function requestAsync($method, $uri, array $options = [])
-    {
-        throw new \common_exception_NotImplemented(__METHOD__ . ' is not implemented.');
-    }
-
-    public function getConfig($option = null)
-    {
-        throw new \common_exception_NotImplemented(__METHOD__ . ' is not implemented.');
     }
 
 }
