58d6602a63d1b7be2031b9d202d3230c08755bcc
eric <eacraft13@gmail.com>
2017-08-27 18:04:46 +0200

    Bump minor version (again)
    tao-4482

--- manifest.php
+++ manifest.php
@@ -36,7 +36,7 @@ return array(
     'label' => 'Tao base',
     'description' => 'TAO meta-extension',
     'license' => 'GPL-2.0',
-    'version' => '12.13.1',
+    'version' => '12.14.0',
     'author' => 'Open Assessment Technologies, CRP Henri Tudor',
     'requires' => array(
         'generis' => '>=4.0.0',
