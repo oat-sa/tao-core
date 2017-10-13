e96cf5b085d3dc786f0dba5a6b9a0cdc64e2084a
eric <eacraft13@gmail.com>
2017-10-12 23:14:00 -0500

    Refactor into bundles
    Note: as opposed to optimized modules with a single point of entry (via
    a module name)
    
    tao-4814

--- views/build/grunt/bundle.js
+++ views/build/grunt/bundle.js
@@ -17,65 +17,75 @@ module.exports = function (grunt) {
     /**
      * Compile into a bundle
      */
-    requirejs.taobundle = {
+    requirejs.taoloaderloginbundle = {
         options: {
-            dir: out,
-            modules: [{
-                name: 'controller/login',
-                include: ['lib/require', 'loader/bootstrap'],
-                exclude: ['json!i18ntr/messages.json']
-            }, {
-                name: 'controller/backoffice',
-                include: ['lib/require', 'loader/bootstrap'].concat(libs),
-                exclude: ['json!i18ntr/messages.json', 'mathJax', 'ckeditor']
-            }, {
-                name: 'controller/app',
-                include: ['lib/require', 'loader/bootstrap'].concat(libs),
-                exclude: ['json!i18ntr/messages.json']
-            }, {
-                name: 'controller/routes',
-                include: ext.getExtensionsControllers(['tao']),
-                exclude: ['mathJax', 'controller/login', 'controller/backoffice'].concat(libs)
-            }]
+            exclude: ['json!i18ntr/messages.json'],
+            include: ['controller/login', 'lib/require', 'loader/bootstrap'],
+            out: out + '/tao/loader/login.min.js'
+        }
+    };
+
+    requirejs.taoloaderbackofficebundle = {
+        options: {
+            exclude: ['json!i18ntr/messages.json', 'mathJax', 'ckeditor'],
+            include: ['controller/backoffice', 'lib/require', 'loader/bootstrap'].concat(libs),
+            out: out + '/tao/loader/backoffice.min.js'
+        }
+    };
+
+    requirejs.taoloaderappbundle = {
+        options: {
+            exclude: ['json!i18ntr/messages.json'],
+            include: ['controller/app', 'lib/require', 'loader/bootstrap'].concat(libs),
+            out: out + '/tao/loader/app.min.js'
+        }
+    };
+
+    requirejs.taocontrollersbundle = {
+        options: {
+            exclude: ['mathJax', 'controller/login', 'controller/backoffice'].concat(libs),
+            include: ['controller/routes'].concat(ext.getExtensionsControllers(['tao'])),
+            out: out + '/tao/controllers.min.js'
         }
     };
 
      /**
       * Copy to /dist
       */
-    copy.taobundle = {
+    copy.taoloaderloginbundle = {
         files: [
-            { src: [out + '/controller/login.js'],            dest: '../js/loader/login.min.js' },
-            { src: [out + '/controller/login.js.map'],        dest: '../js/loader/login.min.js.map' },
-            { src: [out + '/controller/backoffice.js'],       dest: '../js/loader/backoffice.min.js' },
-            { src: [out + '/controller/backoffice.js.map'],   dest: '../js/loader/backoffice.min.js.map' },
-            { src: [out + '/controller/app.js'],              dest: '../js/loader/app.min.js' },
-            { src: [out + '/controller/app.js.map'],          dest: '../js/loader/app.min.js.map' },
-            { src: [out + '/controller/routes.js'],           dest: '../js/controllers.min.js' },
-            { src: [out + '/controller/routes.js.map'],       dest: '../js/controllers.min.js.map' }
+            { src: [out + '/tao/loader/login.min.js'],     dest: root + '/tao/views/dist/loader/login.min.js' },
+            { src: [out + '/tao/loader/login.min.js.map'], dest: root + '/tao/views/dist/loader/login.min.js.map' },
         ],
-        options : {
-            process: function (content, srcpath) {
-                //because we change the bundle names during copy
-                if(/login\.js$/.test(srcpath)){
-                    return content.replace('login.js.map', 'login.min.js.map');
-                }
-                if(/backoffice\.js$/.test(srcpath)){
-                    return content.replace('backoffice.js.map', 'backoffice.min.js.map');
-                }
-                if(/routes\.js$/.test(srcpath)){
-                    return content.replace('routes.js.map', 'controllers.min.js.map');
-                }
-
-                return content;
-            }
-        }
+    };
+    copy.taoloaderbackofficebundle = {
+        files: [
+            { src: [out + '/tao/loader/backoffice.min.js'],     dest: root + '/tao/views/dist/loader/backoffice.min.js' },
+            { src: [out + '/tao/loader/backoffice.min.js.map'], dest: root + '/tao/views/dist/loader/backoffice.min.js.map' },
+        ],
+    };
+    copy.taoloaderappbundle = {
+        files: [
+            { src: [out + '/tao/loader/app.min.js'],     dest: root + '/tao/views/dist/loader/app.min.js' },
+            { src: [out + '/tao/loader/app.min.js.map'], dest: root + '/tao/views/dist/loader/app.min.js.map' },
+        ],
+    };
+    copy.taocontrollersbundle = {
+        files: [
+            { src: [out + '/tao/controllers.min.js'],     dest: root + '/tao/views/dist/controllers.min.js' },
+            { src: [out + '/tao/controllers.min.js.map'], dest: root + '/tao/views/dist/controllers.min.js.map' }
+        ],
     };
 
     grunt.config('clean', clean);
     grunt.config('requirejs', requirejs);
     grunt.config('copy', copy);
 
+    grunt.registerTask('taoloaderloginbundle', [ 'clean:bundle', 'requirejs:taoloaderloginbundle', 'copy:taoloaderloginbundle' ]);
+    grunt.registerTask('taoloaderbackofficebundle', [ 'clean:bundle', 'requirejs:taoloaderbackofficebundle', 'copy:taoloaderbackofficebundle' ]);
+    grunt.registerTask('taoloaderappbundle', [ 'clean:bundle', 'requirejs:taoloaderappbundle', 'copy:taoloaderappbundle' ]);
+    grunt.registerTask('taocontrollersbundle', [ 'clean:bundle', 'requirejs:taocontrollersbundle', 'copy:taocontrollersbundle' ]);
+
     // bundle task
-    grunt.registerTask('taobundle', ['clean:bundle', 'requirejs:taobundle', 'copy:taobundle']);
+    grunt.registerTask('taobundle', [ 'taoloaderloginbundle', 'taoloaderbackofficebundle', 'taoloaderappbundle', 'taocontrollersbundle' ]);
 };
