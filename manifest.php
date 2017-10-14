78694f5d2bacca9317b6e024fd055301691cb4a6
eric <eacraft13@gmail.com>
2017-10-14 17:36:00 -0500

    Bump major version
    tao-4814

--- manifest.php
+++ manifest.php
@@ -36,7 +36,7 @@ return array(
     'label' => 'Tao base',
     'description' => 'TAO meta-extension',
     'license' => 'GPL-2.0',
-    'version' => '13.5.0',
+    'version' => '14.0.0',
     'author' => 'Open Assessment Technologies, CRP Henri Tudor',
     'requires' => array(
         'generis' => '>=4.0.0',
@@ -96,7 +96,7 @@ return array(
             InstallNotificationTable::class,
             SetupMaintenanceService::class,
             AddArchiveService::class,
-            dirname(__FILE__).'/scripts/install/SetRequireJSBundlesConfig.php',
+            \oat\scripts\install\SetRequireJSBundlesConfig::class,
         )
     ),
     'update' => 'oat\\tao\\scripts\\update\\Updater',
