aac0073cecbc075efa3d540c2a78178c166895b7
Aleh Hutnikau <hutnikau@1pt.com>
2017-10-31 11:55:15 +0300

    add logger trait

--- manifest.php
+++ manifest.php
@@ -36,7 +36,7 @@ return array(
     'label' => 'Tao base',
     'description' => 'TAO meta-extension',
     'license' => 'GPL-2.0',
-    'version' => '13.8.0',
+    'version' => '13.8.1',
     'author' => 'Open Assessment Technologies, CRP Henri Tudor',
     'requires' => array(
         'generis' => '>=4.0.0',
--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -935,7 +935,7 @@ class Updater extends \common_ext_ExtensionUpdater {
             $this->setVersion('13.2.0');
         }
       
-        $this->skip('13.2.0', '13.8.0');
+        $this->skip('13.2.0', '13.8.1');
     }
 
     private function migrateFsAccess() {
