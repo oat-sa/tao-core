3e6790f6c98725cf14a79802fdaba3325afcffef
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2017-11-02 10:14:00 +0100

    multiple options for actions

--- views/js/controller/main.js
+++ views/js/controller/main.js
@@ -1,6 +1,231 @@
 /**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2014-2017 Open Assessment Technologies SA;
+ */
+
+/**
  * @author Bertrand Chevrier <bertrand@taotesting.com>
  */
+define([
+    'module',
+    'jquery',
+    'lodash',
+    'i18n',
+    'context',
+    'helpers',
+    'uiForm',
+    'core/logger',
+    'layout/section',
+    'layout/actions',
+    'layout/tree',
+    'layout/version-warning',
+    'layout/loading-bar',
+    'layout/nav',
+    'layout/search',
+    'ui/resource/selector',
+    'provider/resources'
+], function(module, $, _, __, context, helpers, uiForm, loggerFactory, sections, actions, treeFactory, versionWarning, loadingBar, nav, search, resourceSelectorFactory, resourceProviderFactory) {
+    'use strict';
+
+    var logger = loggerFactory('controller/main');
+
+    /**
+     * This controller initialize all the layout components used by the backend : sections, actions, tree, loader, etc.
+     * @exports tao/controller/main
+     */
+    return {
+        start: function start() {
+
+            var $doc = $(document);
+
+            versionWarning.init();
+
+            //just before an ajax request
+            $doc.ajaxSend(function() {
+                loadingBar.start();
+            });
+
+            //when an ajax request complete
+            $doc.ajaxComplete(function() {
+                loadingBar.stop();
+            });
+
+            //navigation bindings
+            nav.init();
+
+            //search component
+            search.init();
+
+            //initialize sections
+            sections.on('activate', function(section) {
+                window.scrollTo(0, 0);
+
+                // quick work around issue in IE11
+                // IE randomly thinks there is no id and throws an error
+                // I know it's not logical but with this 'fix' everything works fine
+                if (!section || !section.id) {
+                    return;
+                }
+
+                context.section = section.id;
+
+                //initialize actions
+                actions.init(section.panel);
+
+                switch (section.type) {
+                    case 'tree':
+                        section.panel.addClass('content-panel');
+                        //sectionHeight.init(section.panel);
+
+                        //set up the tree
+                        $('.taotree', section.panel).each(function() {
+                            var $treeElt = $(this);
+                            var $actionBar = $('.tree-action-bar-box', section.panel);
+
+                            var rootNode = $treeElt.data('rootnode');
+                            var treeUrl = context.root_url;
+                            var treeActions = {};
+                            var serverParameters = {
+                                extension: context.shownExtension,
+                                perspective: context.shownStructure,
+                                section: context.section,
+                            };
+                            var resourceProvider = resourceProviderFactory();
+
+                            $.each($treeElt.data('actions'), function(key, val) {
+                                if (actions.getBy(val)) {
+                                    treeActions[key] = val;
+                                }
+                            });
+
+                            //TODO use the treeUrl within the resource provider
+                            if (/\/$/.test(treeUrl)) {
+                                treeUrl += $treeElt.data('url').replace(/^\//, '');
+                            } else {
+                                treeUrl += $treeElt.data('url');
+                            }
+
+                            actions.exec(treeActions.init, {
+                                uri: rootNode
+                            });
+
+                            resourceProvider.getClasses(rootNode, serverParameters)
+                                .then(function(classes) {
+                                    resourceSelectorFactory($treeElt, {
+                                        icon : $treeElt.data('icon'),
+                                        selectionMode: 'both',
+                                        selectClass : true,
+                                        showSelection : false,
+                                        classUri: rootNode,
+                                        classes: classes
+                                    })
+                                    .on('render', function() {
+                                        $actionBar.addClass('active');
+                                    })
+                                    .on('query', function(params) {
+                                        var self = this;
+
+                                        //ask the server the resources from the component query
+                                        resourceProvider.getResources(_.defaults(params, serverParameters))
+                                            .then(function(items) {
+                                                self.update(items, params);
+                                            })
+                                            .catch(function(err) {
+                                                logger.error(err);
+                                            });
+                                    })
+                                    .on('change', function(selection) {
+                                        var length = _.size(selection);
+                                        var permissions  = {
+                                            "item-authoring": true,
+                                            "item-class-new": true,
+                                            "item-delete": true,
+                                            "item-duplicate": true,
+                                            "item-export": true,
+                                            "item-import": true,
+                                            "item-new": true,
+                                            "item-preview": true,
+                                            "item-properties": true,
+                                            "item-translate": true
+                                        };
+
+                                        if(length === 1){
+                                            _.forEach(selection, function(resource, uri) {
+                                                if(resource.classUri){
+                                                    actions.updateContext({
+                                                        uri: uri,
+                                                        id: uri,
+                                                        classUri: uri,
+
+                                                        permissions:  permissions
+                                                    });
+
+                                                    actions.exec(treeActions.selectClass, {
+                                                        uri: uri,
+                                                        id: uri,
+                                                        classUri: uri
+                                                    });
+
+                                                } else {
+                                                    actions.updateContext({
+                                                        uri: uri,
+                                                        id: uri,
+                                                        classUri: rootNode,
+
+                                                        permissions: permissions
+                                                    });
+
+                                                    actions.exec(treeActions.selectInstance, {
+                                                        uri: uri,
+                                                        id: uri,
+                                                        classUri: rootNode
+                                                    });
+                                                }
+                                            });
+                                        } else if (length > 1){
+                                            //multiple mode
+                                        }
+                                    });
+                                })
+                                .catch(function(err) {
+                                    logger.error(err);
+                                });
+                        });
+
+                        $('.navi-container', section.panel).show();
+                        break;
+                    case 'content':
+
+                        //or load the content block
+                        this.loadContentBlock();
+
+                        break;
+                }
+            })
+            .init();
+
+
+            //initialize legacy components
+            helpers.init();
+            uiForm.init();
+        }
+    };
+});
+
+/*
 define([
     'module',
     'jquery',
@@ -20,10 +245,6 @@ define([
 function (module, $, __, context, helpers, uiForm, section, actions, treeFactory, versionWarning, sectionHeight, loadingBar, nav, search) {
     'use strict';
 
-    /**
-     * This controller initialize all the layout components used by the backend : sections, actions, tree, loader, etc.
-     * @exports tao/controller/main
-     */
     return {
         start : function(){
 
@@ -47,7 +268,7 @@ function (module, $, __, context, helpers, uiForm, section, actions, treeFactory
             //search component
             search.init();
 
-            //initialize sections 
+            //initialize sections
             section.on('activate', function(section){
 
                 window.scrollTo(0,0);
@@ -60,8 +281,8 @@ function (module, $, __, context, helpers, uiForm, section, actions, treeFactory
                 }
 
                 context.section = section.id;
-               
-                //initialize actions 
+
+                //initialize actions
                 actions.init(section.panel);
 
                 switch(section.type){
@@ -82,7 +303,7 @@ function (module, $, __, context, helpers, uiForm, section, actions, treeFactory
                                 treeActions[key] = val;
                             }
                         });
-                        
+
                         if(/\/$/.test(treeUrl)){
                             treeUrl += $treeElt.data('url').replace(/^\//, '');
                         } else {
@@ -105,11 +326,11 @@ function (module, $, __, context, helpers, uiForm, section, actions, treeFactory
 
                     $('.navi-container', section.panel).show();
                     break;
-                case 'content' : 
+                case 'content' :
 
                     //or load the content block
                     this.loadContentBlock();
-                    
+
                     break;
                 }
             })
@@ -121,5 +342,5 @@ function (module, $, __, context, helpers, uiForm, section, actions, treeFactory
             uiForm.init();
         }
     };
-});
+});*/
 
--- views/js/layout/actions/binder.js
+++ views/js/layout/actions/binder.js
@@ -1,15 +1,32 @@
 /**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2014-2017 Open Assessment Technologies SA;
+ */
+/**
  * @author Bertrand Chevrier <bertrand@taotesting.com>
  */
 define(['lodash'], function(_){
     'use strict';
 
     /**
-     * Helps you to bind actions' behavior. 
-     * 
+     * Helps you to bind actions' behavior.
+     *
      * To bind a behavior to an action, you need to register a callback under the same name than 'binding' value in the structure.xml
-     * 
-     * 
+     *
+     *
      * @exports layout/actions/binder
      */
     var actionBinder =  {
@@ -22,14 +39,14 @@ define(['lodash'], function(_){
         /**
          * Register a new binding
          *
-         * @example 
+         * @example
          *  binder.register('subClass', function subClass(context){
-         *      //do something with context.uri to create a sub class. 
+         *      //do something with context.uri to create a sub class.
          *  });
-         * 
-         * @param {String} name - the binding name 
-         * @param {ActionBinding} 
-         *  
+         *
+         * @param {String} name - the binding name
+         * @param {ActionBinding}
+         *
          */
         register : function(name, binding){
 
@@ -45,18 +62,18 @@ define(['lodash'], function(_){
          * Execute the binding of an action if one has been registerd
          * @param {Object} action - the action to execute the binding of
          * @param {String} action.binding - the action must contain a binding property that match a registerd binding
-         * @param {ActionContext} context - the context in which to execute the binding 
+         * @param {ActionContext} context - the context in which to execute the binding
          */
         exec : function(action, context){
             var name;
             if(action && action.binding){
-            
+
                 name = action.binding;
                 if(_.isFunction(this._bindings[name])){
 
                     this._bindings[name].call(action, context);
 
-                }   
+                }
             }
         }
     };
