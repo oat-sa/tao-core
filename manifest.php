20fb0f49ea69a7f9292b76e728332b29f1bc5022
Ionut Padurariu <ionut@taotesting.com>
2018-01-05 15:24:30 +0100

    update generis version

--- manifest.php
+++ manifest.php
@@ -39,7 +39,7 @@ return array(
     'version' => '13.4.0',
     'author' => 'Open Assessment Technologies, CRP Henri Tudor',
     'requires' => array(
-        'generis' => '>=4.0.0',
+        'generis' => '>=5.9.0',
     ),
     'models' => array(
         'http://www.tao.lu/Ontologies/TAO.rdf',
