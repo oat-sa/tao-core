bdeee398fc889d1b7c01a17d47ad474c4019293b
Gyula Szucs <gyula@taotesting.com>
2018-01-08 10:52:45 +0100

    TAO-4578 Bump version

--- manifest.php
+++ manifest.php
@@ -40,7 +40,7 @@ return array(
     'label' => 'Tao base',
     'description' => 'TAO meta-extension',
     'license' => 'GPL-2.0',
-    'version' => '14.22.0',
+    'version' => '14.23.0',
     'author' => 'Open Assessment Technologies, CRP Henri Tudor',
     'requires' => array(
         'generis' => '>=6.1.0'
