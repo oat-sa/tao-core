9bfb597fa2e0676fd4218987aa0f6db6116944a1
Ilya Yarkavets <ilya.yarkavets@1pt.com>
2018-01-30 13:15:24 +0300

    Version bump

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -1059,7 +1059,7 @@ class Updater extends \common_ext_ExtensionUpdater {
             $this->setVersion('15.5.0');
         }
 
-        $this->skip('15.5.0', '15.11.0');
+        $this->skip('15.5.0', '15.11.1');
     }
 
     private function migrateFsAccess() {
