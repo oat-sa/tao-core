508d7377c0c7ff4e765f49e7459f839777dff6ef
Sam Sipasseuth <sam@taotesting.com>
2018-02-06 18:26:24 +0100

    directly remove the dom instead of using ui/deleter

--- views/js/ui/uploader.js
+++ views/js/ui/uploader.js
@@ -11,10 +11,9 @@ define([
     'util/bytes',
     'tpl!ui/uploader/uploader',
     'tpl!ui/uploader/fileEntry',
-    'ui/deleter',
     'ui/filesender',
     'ui/progressbar'
-], function($, _, __, async, Pluginifier, context, bytes, uploaderTpl, fileEntryTpl, deleter){
+], function($, _, __, async, Pluginifier, context, bytes, uploaderTpl, fileEntryTpl){
     'use strict';
 
     var ns = 'uploader';
@@ -292,6 +291,22 @@ define([
                     .removeClass('placeholder');
             };
 
+            options.$dropZone.off('click.'+ns).on('click.'+ns, '[data-role=delete]', function(e){
+                var $fileEntry = $(this).parent();
+                var name = $fileEntry.data('file-name');
+                e.preventDefault();
+                e.stopPropagation();
+                if(name){
+                    options.files =  _.reject(options.files, {name : name});
+                    if(options.files.length === 0){
+                        self._reset($elt);
+                    } else {
+                        updateFileName();
+                    }
+                    $fileEntry.remove();
+                }
+            });
+
             if(files.length <= 0 && !append){
 
                 //empty file list, so we reset the plugin
@@ -328,23 +343,6 @@ define([
                                 .html('<ul>' + listContent + '</ul>');
                         }
 
-                        options.$dropZone
-                            .off('delete.delter', 'li')
-                            .on('delete.deleter', 'li', function(e){
-
-                                var name = $(e.target).data('file-name');
-
-                                options.$dropZone
-                                   .off('deleted.deleter')
-                                   .one('deleted.deleter', function(){
-                                        options.files =  _.reject(options.files, {name : name});
-                                        if(options.files.length === 0){
-                                            self._reset($elt);
-                                        } else {
-                                            updateFileName();
-                                        }
-                                    });
-                            });
                     } else {
                         //legacy mode, no dnd support
                         options.files = options.files.slice(0, 1);
@@ -352,9 +350,6 @@ define([
                             .text(files[0].name)
                             .removeClass('placeholder');
                     }
-
-                    //Initialize the deleter on the new file entry
-                    deleter($elt);
 
                     /**
                      * Files has been selected
