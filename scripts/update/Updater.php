501fa5e6c10d3124d634d96dbb47cede50774eec
Ivan Klimchuk <klimchuk@1pt.com>
2018-02-20 15:01:41 +0300

    Fixed version in updater script

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -1087,7 +1087,7 @@ class Updater extends \common_ext_ExtensionUpdater {
         
         $this->skip('17.0.0', '17.2.1');
 
-        if ($this->isVersion('17.1.1')) {
+        if ($this->isVersion('17.2.1')) {
             OntologyUpdater::syncModels();
             $this->setVersion('18.0.0');
         }
