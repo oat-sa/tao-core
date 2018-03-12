00d59ca07d8c6e78b91285084dcf638de8b0caf1
Ivan Klimchuk <ivan.klimchuk@1pt.com>
2018-03-12 18:27:18 +0300

    Fixed comments in default config

--- config/default/userlocks.conf.php
+++ config/default/userlocks.conf.php
@@ -2,12 +2,12 @@
 
 return new oat\tao\model\user\UserLocksService([
     
-    /** Use hard lock for failed logon. Be default soft lock will be used */
+    /** Use hard lock for failed logon. By default soft lock will be used */
     'use_hard_lockout' => false,
 
-    /** Amount of failed login attempts before lockout */
+    /** Amount of failed login attempts before locking */
     'lockout_failed_attempts' => 6,
 
-    /** Duration of soft lock out */
+    /** Duration of soft lock */
     'soft_lockout_period' => 'PT30M'
 ]);
