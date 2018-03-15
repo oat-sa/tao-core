a2610509080c48c62a1b6071030c6b5efaf2a420
Ivan Klimchuk <ivan.klimchuk@1pt.com>
2018-03-15 18:32:03 +0300

    Updated version name

--- manifest.php
+++ manifest.php
@@ -164,9 +164,9 @@ return array(
     ),
     'constants' => array(
         #TAO version number
-        'TAO_VERSION' => '3.3.0-sprint72',
+        'TAO_VERSION' => '3.3.0-sprint72-½',
         #TAO version label
-        'TAO_VERSION_NAME' => '3.3.0-sprint72',
+        'TAO_VERSION_NAME' => '3.3.0-sprint72-½',
         #the name to display
         'PRODUCT_NAME' => 'TAO',
         #TAO release status, use to add specific footer to TAO, available alpha, beta, demo, stable
