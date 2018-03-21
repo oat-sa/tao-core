c935ae5e55d7f7450d6da675b2a1d6fbba2fdd4b
Ivan Klimchuk <ivan.klimchuk@1pt.com>
2018-03-21 15:58:52 +0300

    Bump version

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -674,6 +674,6 @@ class Updater extends \common_ext_ExtensionUpdater {
             $this->setVersion('17.11.0');
         }
 
-        $this->skip('17.11.0', '17.12.2');
+        $this->skip('17.11.0', '17.12.3');
     }
 }
