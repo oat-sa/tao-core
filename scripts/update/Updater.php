cc6dbe04390a554fad445e8e7650c3e6900b4d0d
siwane <camille@taotesting.com>
2018-04-17 13:51:48 +0200

    Bump to version 18.0.5

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -743,7 +743,7 @@ class Updater extends \common_ext_ExtensionUpdater {
             $this->setVersion('17.17.0');
         }
 
-        $this->skip('17.17.0', '18.0.4');
+        $this->skip('17.17.0', '18.0.5');
     }
 
 }
