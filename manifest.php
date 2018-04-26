92ac77daefe20c54b1427aed8453269e2c67a689
siwane <camille@taotesting.com>
2018-04-26 11:24:17 +0200

    Bump version

--- manifest.php
+++ manifest.php
@@ -42,7 +42,7 @@ return array(
     'label' => 'TAO Base',
     'description' => 'TAO meta-extension',
     'license' => 'GPL-2.0',
-    'version' => '18.3.1',
+    'version' => '18.3.2',
     'author' => 'Open Assessment Technologies, CRP Henri Tudor',
     'requires' => array(
         'generis' => '>=7.2.0',
