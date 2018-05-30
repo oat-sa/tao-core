93f95719b901b8625af71200110a69b824955bfb
Aleh Hutnikau <hutnikau@1pt.com>
2018-05-30 10:59:14 +0300

    bump version

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -1085,7 +1085,7 @@ class Updater extends \common_ext_ExtensionUpdater {
             $this->setVersion('17.0.0');
         }
         
-        $this->skip('17.0.0', '17.4.0');
+        $this->skip('17.0.0', '17.4.0.1');
     }
 
     private function migrateFsAccess() {
