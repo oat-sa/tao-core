4d41196b85938e09b28524c94a5d8bb498cb3b55
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2018-06-15 14:47:37 +0200

    version update

--- manifest.php
+++ manifest.php
@@ -171,9 +171,9 @@ return array(
     ),
     'constants' => array(
         #TAO version number
-        'TAO_VERSION' => '3.3.0-sprint78',
+        'TAO_VERSION' => '3.3.0-sprint79',
         #TAO version label
-        'TAO_VERSION_NAME' => '3.3.0-sprint78',
+        'TAO_VERSION_NAME' => '3.3.0-sprint79',
         #the name to display
         'PRODUCT_NAME' => 'TAO',
         #TAO release status, use to add specific footer to TAO, available alpha, beta, demo, stable
