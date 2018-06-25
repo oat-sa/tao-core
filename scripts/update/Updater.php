27c88aa4dfab3104e23199b161a7379cbc70a16b
Christophe Noel <christophe@taotesting.com>
2018-06-25 13:06:50 +0000

    bump version

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -822,7 +822,7 @@ class Updater extends \common_ext_ExtensionUpdater {
             $this->setVersion('19.10.0');
         }
 
-        $this->skip('19.10.0', '19.11.1');
+        $this->skip('19.10.0', '19.11.2');
 
     }
 
