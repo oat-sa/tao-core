0feb6c9bcdeb317b9e56232a07c3d86cb94173b9
jsconan <jsconan@gmail.com>
2018-07-13 14:35:00 +0200

    bundle assets

--- manifest.php
+++ manifest.php
@@ -171,9 +171,9 @@ return array(
     ),
     'constants' => array(
         #TAO version number
-        'TAO_VERSION' => '3.3.0-sprint80',
+        'TAO_VERSION' => '3.3.0-sprint81',
         #TAO version label
-        'TAO_VERSION_NAME' => '3.3.0-sprint80',
+        'TAO_VERSION_NAME' => '3.3.0-sprint81',
         #the name to display
         'PRODUCT_NAME' => 'TAO',
         #TAO release status, use to add specific footer to TAO, available alpha, beta, demo, stable
