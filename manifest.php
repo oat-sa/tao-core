c7610a5eae46d4011fdaa186cfe9c1fb0be498e9
Oleksander Zagovorychev <zagovorichev@gmail.com>
2018-07-23 13:00:33 +0300

    Fixed php fatal error

--- manifest.php
+++ manifest.php
@@ -43,7 +43,7 @@ return array(
     'label' => 'TAO Base',
     'description' => 'TAO meta-extension',
     'license' => 'GPL-2.0',
-    'version' => '19.3.1',
+    'version' => '19.3.2',
     'author' => 'Open Assessment Technologies, CRP Henri Tudor',
     'requires' => array(
         'generis' => '>=7.2.0',
