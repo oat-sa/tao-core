18620fcecb031e326df23c42b2eee970f3c1f4a8
Ionut Padurariu <ionut@taotesting.com>
2018-08-02 18:13:36 +0200

    add unit test

---
+++ test/model/Tree/GenerisTreeFactoryBuilderServiceTest.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2018 (original work) Open Assessment Technologies SA;
+ *
+ */
+
+namespace oat\test\model\Tree;
+
+use oat\tao\model\GenerisTreeFactory;
+use oat\tao\model\Tree\GenerisTreeFactoryBuilderService;
+
+class GenerisTreeFactoryBuilderServiceTest extends \PHPUnit_Framework_TestCase
+{
+    public function testBuild()
+    {
+        $service = new GenerisTreeFactoryBuilderService();
+        $treeFactory = $service->build(true, [], 10, 0);
+
+        $this->assertInstanceOf(GenerisTreeFactory::class, $treeFactory);
+    }
+
+    public function testShowNoLabelIsOverwritten()
+    {
+        $service = new GenerisTreeFactoryBuilderService();
+        $this->assertFalse($service->isShowNoLabel());
+
+        $service->setShowLabel(true);
+        $this->assertTrue($service->isShowNoLabel());
+    }
+}
