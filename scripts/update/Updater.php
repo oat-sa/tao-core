590622e4b8c8dabba204d805dc04bb6b31bd5555
Oleksandr Khitko <oleksandr@taotesting.com>
2018-10-16 10:45:06 +0200

    Remove code duplication

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -169,6 +169,7 @@ class Updater extends \common_ext_ExtensionUpdater {
         }
 
         // Hotfix to register ApplicationService for instances with old tao-core version
+        // ApplicationService was introduced in tao-core version 20.1.0
         if ($this->isBetween('6.0.1', '20.0.4')) {
             $options = [];
             if(defined('ROOT_PATH') && is_readable(ROOT_PATH.'build')){
@@ -853,19 +854,6 @@ class Updater extends \common_ext_ExtensionUpdater {
             $this->setVersion('19.20.0');
         }
 
-        $this->skip('19.20.0', '20.0.4');
-        if ($this->isVersion('20.0.4')) {
-            $options = [];
-            if(defined('ROOT_PATH') && is_readable(ROOT_PATH.'build')){
-                $content = file_get_contents(ROOT_PATH.'build');
-                $options[ApplicationService::OPTION_BUILD_NUMBER] = $content;
-            }
-
-            $applicationService = new ApplicationService($options);
-            $this->getServiceManager()->register(ApplicationService::SERVICE_ID, $applicationService);
-            $this->setVersion('20.1.0');
-        }
-
-        $this->skip('20.1.0', '20.2.3');
+        $this->skip('19.20.0', '20.2.3');
     }
 }
