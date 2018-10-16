9dbac18305721bad0624f3ee5c66257817b6c9f3
Aleksej Tikhanovich <aleksej@taotesting.com>
2018-10-16 12:05:58 +0300

    Fixed Version

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -854,6 +854,6 @@ class Updater extends \common_ext_ExtensionUpdater {
             $this->setVersion('19.20.0');
         }
 
-        $this->skip('19.20.0', '21.0.0');
+        $this->skip('19.20.0', '20.3.0');
     }
 }
