34c42b48720ae1fce12ba303b7ce2be0a7a9d732
Aleh Hutnikau <hutnikau@1pt.com>
2018-10-31 10:33:18 +0300

    update sprint constant

--- manifest.php
+++ manifest.php
@@ -170,9 +170,9 @@ return array(
     ),
     'constants' => array(
         #TAO version number
-        'TAO_VERSION' => '3.3.0-sprint82',
+        'TAO_VERSION' => '3.3.0-sprint82.6',
         #TAO version label
-        'TAO_VERSION_NAME' => '3.3.0-sprint82',
+        'TAO_VERSION_NAME' => '3.3.0-sprint82.6',
         #the name to display
         'PRODUCT_NAME' => 'TAO',
         #TAO release status, use to add specific footer to TAO, available alpha, beta, demo, stable
