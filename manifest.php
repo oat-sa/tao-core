3309ee17ecb8f28b7fe76415059dff9aea07e120
Ionut Padurariu <ionut@taotesting.com>
2018-11-02 15:09:23 +0100

    Sprint Increase

--- manifest.php
+++ manifest.php
@@ -172,9 +172,9 @@ return array(
     ),
     'constants' => array(
         #TAO version number
-        'TAO_VERSION' => '3.3.0-sprint88',
+        'TAO_VERSION' => '3.3.0-sprint89',
         #TAO version label
-        'TAO_VERSION_NAME' => '3.3.0-sprint88',
+        'TAO_VERSION_NAME' => '3.3.0-sprint89',
         #the name to display
         'PRODUCT_NAME' => 'TAO',
         #TAO release status, use to add specific footer to TAO, available alpha, beta, demo, stable
