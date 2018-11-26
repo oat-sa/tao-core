0e08ab50962df206ff9088c6d7b1cb63b93d11bf
Pavel Hendelman <pavel.handleman@gmail.com>
2018-11-26 13:20:48 +0300

    TAO-7150 copyright information

--- views/js/test/ui/tooltip/test.js
+++ views/js/test/ui/tooltip/test.js
@@ -13,7 +13,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
- * Copyright (c) 2015 (original work) Open Assessment Technologies SA ;
+ * Copyright (c) 2015-2018 (original work) Open Assessment Technologies SA ;
  */
 /**
  * @author Jean-Sébastien Conan <jean-sebastien.conan@vesperiagroup.com>
