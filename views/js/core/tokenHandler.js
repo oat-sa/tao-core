e59fa7ec53608e93c477aa4f86a97f811215ce26
Martin Nicholson <martin@taotesting.com>
2019-01-14 14:59:56 +0100

    Extra methods for token queue & cookies

--- views/js/core/tokenHandler.js
+++ views/js/core/tokenHandler.js
@@ -18,58 +18,113 @@
 /**
  * @author Jean-Sébastien Conan <jean-sebastien.conan@vesperiagroup.com>
  */
-define(function () {
+define([
+    'jquery',
+    'lodash'
+],
+function ($, _) {
     'use strict';
 
+    var defaults = {
+        maxPoolSize: 6, // TR should set this to 1 to force sequential AJAX requests
+        tokenTimeLimit: 1000 * 60 * 15
+    };
+
     /**
-     * Stores the security token
-     * @param {String} initialToken
+     * Stores the security token queue
+     * @param {Object} [config]
+     * @param {String} [config.initialToken]
+     * @param {String} [config.maxPoolSize]
+     * @param {String} [config.tokenTimeLimit]
      * @returns {tokenHandler}
      */
-    function tokenHandlerFactory(initialToken) {
-        //var tokenTimeLimit = 10000;
-        //var token = initialToken || null;
+    function tokenHandlerFactory(config) { // initialToken) {
+
         // Initialise queue, empty queue will produce a null token
         var tokenQueue = [];
-        if (initialToken) {
+
+        config = _.defaults({}, config, defaults);
+
+        if (config.initialToken) {
             tokenQueue.push({
-                value: initialToken,
+                value: config.initialToken,
                 receivedAt: Date.now()
             });
         }
 
-        // function isExpired(token) {
-        //     return Date.now() - token.receivedAt < tokenTimeLimit;
-        // }
+        // Hardcode cookie tokens
+        $.cookie('tao_tokens', 'a;b;c;d;e');
 
-        // time out tokens here?
+        if (tokenQueue.length === 0) {
+            setQueue(readCookieTokens('tao_tokens'));
+            console.info('Q:', tokenQueue);
+        }
+
+        /**
+         * Reads token strings from a cookie
+         * @param {String} name - name of the cookie
+         * @returns {Array}
+         */
+        function readCookieTokens(name) {
+            var tokenList = $.cookie(name);
+            $.cookie(name, '');
+            if (tokenList) {
+                return tokenList.split(';');
+            }
+            return [];
+        }
+
+        /**
+         * Sets the whole queue of tokens in one go
+         * @param {Array} tokens - the token strings
+         */
+        function setQueue(tokens) {
+            tokenQueue = _.map(tokens, function(token) {
+                return {
+                    value: token,
+                    receivedAt: Date.now()
+                };
+            });
+        }
+
+        /**
+         * Checks if a token in the pool is expired
+         * @param {String} token
+         * @returns {Boolean}
+         */
+        function isExpired(token) {
+            return Date.now() - token.receivedAt > config.tokenTimeLimit;
+        }
 
         return {
             /**
-             * Gets the current security token.
-             * Once the token is got, it is erased from the memory and a new token must be provided.
+             * Gets the next security token from the token queue
+             * Once the token is got, it is erased from the memory (one use only)
              * @returns {String}
              */
             getToken: function getToken() {
-                //var currentToken = token;
-                //token = null;
                 var currentToken = tokenQueue.length ? tokenQueue.shift().value : null ;
                 console.log('tokenHandler.getToken (shift)', currentToken);
                 return currentToken;
             },
 
             /**
-             * Sets the current security token
+             * Adds a new security token to the token queue
+             * Deletes old tokens to keep queue within maximum pool size
              * @param {String} newToken
              * @returns {Object} - this
              */
             setToken: function setToken(newToken) {
-                //token = newToken;
+                // check against max pool size, if queue is full we should bump oldest token
+                while (tokenQueue.length >= config.maxPoolSize) {
+                    tokenQueue.shift();
+                }
                 tokenQueue.push({
                     value: newToken,
                     receivedAt: Date.now()
                 });
                 console.log('tokenHandler.setToken (push)', newToken);
+                console.info('Q:', tokenQueue);
                 return this;
             }
         };
