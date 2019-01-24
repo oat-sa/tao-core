3a5d087c6a8163811d66553073739ade605d4015
ricproenca-tao <ricardo@taotesting.com>
2019-01-24 11:20:24 +0100

    version bump to 22.14.7

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -909,6 +909,6 @@ class Updater extends \common_ext_ExtensionUpdater {
             $this->setVersion('22.13.1');
         }
 
-        $this->skip('22.13.1', '22.14.6');
+        $this->skip('22.13.1', '22.14.7');
     }
 }
