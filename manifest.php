189a434a29dfcae497041db21e0affa1d983b28a
siarhei.baradzin <siarhei.baradzin@1pt.com>
2019-02-01 15:18:14 +0300

    bump version

--- manifest.php
+++ manifest.php
@@ -45,7 +45,7 @@ return array(
     'label' => 'TAO Base',
     'description' => 'TAO meta-extension',
     'license' => 'GPL-2.0',
-    'version' => '24.4.1',
+    'version' => '25.0.0',
     'author' => 'Open Assessment Technologies, CRP Henri Tudor',
     'requires' => array(
         'generis' => '>=8.2.0',
