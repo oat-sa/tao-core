36814604b17c4f755acacbac4df10fcc65eac0d5
jsconan <jsconan@gmail.com>
2019-02-01 15:21:00 +0100

    Calculator: use a dedicated list of available labels

--- views/js/ui/maths/calculator/core/terms.js
+++ views/js/ui/maths/calculator/core/terms.js
@@ -13,84 +13,87 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
- * Copyright (c) 2018 Open Assessment Technologies SA ;
+ * Copyright (c) 2018-2019 Open Assessment Technologies SA ;
  */
 /**
  * @author Jean-Sébastien Conan <jean-sebastien@taotesting.com>
  */
-define(['i18n'], function (__) {
+define([
+    'i18n',
+    'ui/maths/calculator/core/labels'
+], function (__, labels) {
     'use strict';
 
     return {
         // Digits definition
         NUM0: {
-            label: '0',
+            label: labels.NUM0,
             value: '0',
             type: 'digit',
             description: __('Digit 0')
         },
         NUM1: {
-            label: '1',
+            label: labels.NUM1,
             value: '1',
             type: 'digit',
             description: __('Digit 1')
         },
         NUM2: {
-            label: '2',
+            label: labels.NUM2,
             value: '2',
             type: 'digit',
             description: __('Digit 2')
         },
         NUM3: {
-            label: '3',
+            label: labels.NUM3,
             value: '3',
             type: 'digit',
             description: __('Digit 3')
         },
         NUM4: {
-            label: '4',
+            label: labels.NUM4,
             value: '4',
             type: 'digit',
             description: __('Digit 4')
         },
         NUM5: {
-            label: '5',
+            label: labels.NUM5,
             value: '5',
             type: 'digit',
             description: __('Digit 5')
         },
         NUM6: {
-            label: '6',
+            label: labels.NUM6,
             value: '6',
             type: 'digit',
             description: __('Digit 6')
         },
         NUM7: {
-            label: '7',
+            label: labels.NUM7,
             value: '7',
             type: 'digit',
             description: __('Digit 7')
         },
         NUM8: {
-            label: '8',
+            label: labels.NUM8,
             value: '8',
             type: 'digit',
             description: __('Digit 8')
         },
         NUM9: {
-            label: '9',
+            label: labels.NUM9,
             value: '9',
             type: 'digit',
             description: __('Digit 9')
         },
         DOT: {
-            label: '.',
+            label: labels.DOT,
             value: '.',
             type: 'digit',
             description: __('Dot')
         },
-        POW10: {
-            label: 'e',
+        EXP10: {
+            label: labels.EXP10,
             value: 'e',
             type: 'digit',
             description: __('Power of 10')
@@ -98,13 +101,13 @@ define(['i18n'], function (__) {
 
         // Aggregators
         LPAR: {
-            label: '(',
+            label: labels.LPAR,
             value: '(',
             type: 'aggregator',
             description: __('Left parenthesis')
         },
         RPAR: {
-            label: ')',
+            label: labels.RPAR,
             value: ')',
             type: 'aggregator',
             description: __('Right parenthesis')
@@ -112,55 +115,55 @@ define(['i18n'], function (__) {
 
         // Operators
         SUB: {
-            label: '-',
+            label: labels.SUB,
             value: '-',
             type: 'operator',
             description: __('Binary operator -')
         },
         NEG: {
-            label: '\u207B',
+            label: labels.NEG,
             value: '\u207B',
             type: 'operator',
             description: __('Unary operator -')
         },
         ADD: {
-            label: '+',
+            label: labels.ADD,
             value: '+',
             type: 'operator',
             description: __('Binary operator +')
         },
         MUL: {
-            label: '\u00D7',
+            label: labels.MUL,
             value: '*',
             type: 'operator',
             description: __('Binary operator *')
         },
         DIV: {
-            label: '\u00F7',
+            label: labels.DIV,
             value: '/',
             type: 'operator',
             description: __('Binary operator /')
         },
         MOD: {
-            label: 'mod',
+            label: labels.MOD,
             value: '%',
             type: 'operator',
             description: __('Binary operator modulo')
         },
         POW: {
-            label: '^',
+            label: labels.POW,
             value: '^',
             type: 'operator',
             description: __('Power of')
         },
         FAC: {
-            label: '!',
+            label: labels.FAC,
             value: '!',
             type: 'operator',
             description: __('Factorial')
         },
         ASSIGN: {
-            label: '=',
+            label: labels.ASSIGN,
             value: '=',
             type: 'operator',
             description: __('Assign')
@@ -168,7 +171,7 @@ define(['i18n'], function (__) {
 
         // Variables
         ANS: {
-            label: 'Ans',
+            label: labels.ANS,
             value: 'ans',
             type: 'variable',
             description: __('Last result')
@@ -176,13 +179,13 @@ define(['i18n'], function (__) {
 
         // Constants
         PI: {
-            label: '\u03C0',
+            label: labels.PI,
             value: 'PI',
             type: 'constant',
             description: __('Value of PI')
         },
         E: {
-            label: 'e',
+            label: labels.E,
             value: 'E',
             type: 'constant',
             description: __('Value of E')
@@ -190,19 +193,19 @@ define(['i18n'], function (__) {
 
         // Errors
         NAN: {
-            label: 'Error',
+            label: labels.NAN,
             value: 'NaN',
             type: 'error',
             description: __('Error in value')
         },
         INFINITY: {
-            label: 'Infinity',
+            label: labels.INFINITY,
             value: 'Infinity',
             type: 'error',
             description: __('Error in result')
         },
         ERROR: {
-            label: 'Syntax error',
+            label: labels.ERROR,
             value: '#',
             type: 'error',
             description: __('Error in syntax')
@@ -210,157 +213,157 @@ define(['i18n'], function (__) {
 
         // Functions
         EXP: {
-            label: 'exp',
+            label: labels.EXP,
             value: 'exp',
             type: 'function',
             description: __('Exponent')
         },
         SQRT: {
-            label: '\u221A',
+            label: labels.SQRT,
             value: 'sqrt',
             type: 'function',
             description: __('Square root')
         },
         CBRT: {
-            label: '\u221B',
+            label: labels.CBRT,
             value: 'cbrt',
             type: 'function',
             description: __('Cube root')
         },
         NTHRT: {
-            label: '\u221A',
+            label: labels.SQRT,
             value: 'nthrt',
             type: 'function',
             description: __('Nth root')
         },
         FLOOR: {
-            label: 'floor',
+            label: labels.FLOOR,
             value: 'floor',
             type: 'function',
             description: __('Round to lower whole number')
         },
         CEIL: {
-            label: 'ceil',
+            label: labels.CEIL,
             value: 'ceil',
             type: 'function',
             description: __('Round to upper whole number')
         },
         ROUND: {
-            label: 'round',
+            label: labels.ROUND,
             value: 'round',
             type: 'function',
             description: __('Round to closest whole number')
         },
         TRUNC: {
-            label: 'trunc',
+            label: labels.TRUNC,
             value: 'trunc',
             type: 'function',
             description: __('Whole number part')
         },
         SIN: {
-            label: 'sin',
+            label: labels.SIN,
             value: 'sin',
             type: 'function',
             description: __('Sine')
         },
         COS: {
-            label: 'cos',
+            label: labels.COS,
             value: 'cos',
             type: 'function',
             description: __('Cosine')
         },
         TAN: {
-            label: 'tan',
+            label: labels.TAN,
             value: 'tan',
             type: 'function',
             description: __('Tangent')
         },
         ASIN: {
-            label: 'sin<sup>\u207B1</sup>',
+            label: labels.ASIN,
             value: 'asin',
             type: 'function',
             description: __('Arc sine')
         },
         ACOS: {
-            label: 'cos<sup>\u207B1</sup>',
+            label: labels.ACOS,
             value: 'acos',
             type: 'function',
             description: __('Arc cosine')
         },
         ATAN: {
-            label: 'tan<sup>\u207B1</sup>',
+            label: labels.ATAN,
             value: 'atan',
             type: 'function',
             description: __('Arc tangent')
         },
         SINH: {
-            label: 'sinh',
+            label: labels.SINH,
             value: 'sinh',
             type: 'function',
             description: __('Hyperbolic sine')
         },
         COSH: {
-            label: 'cosh',
+            label: labels.COSH,
             value: 'cosh',
             type: 'function',
             description: __('Hyperbolic cosine')
         },
         TANH: {
-            label: 'tanh',
+            label: labels.TANH,
             value: 'tanh',
             type: 'function',
             description: __('Hyperbolic tangent')
         },
         ASINH: {
-            label: 'sinh<sup>\u207B1</sup>',
+            label: labels.ASINH,
             value: 'asinh',
             type: 'function',
             description: __('Hyperbolic arc sine')
         },
         ACOSH: {
-            label: 'cosh<sup>\u207B1</sup>',
+            label: labels.ACOSH,
             value: 'acosh',
             type: 'function',
             description: __('Hyperbolic arc cosine')
         },
         ATANH: {
-            label: 'tanh<sup>\u207B1</sup>',
+            label: labels.ATANH,
             value: 'atanh',
             type: 'function',
             description: __('Hyperbolic arc tangent')
         },
         LN: {
-            label: 'ln',
+            label: labels.LN,
             value: 'ln',
             type: 'function',
             description: __('Natural logarithm')
         },
         LOG: {
-            label: 'ln',
+            label: labels.LN,
             value: 'log',
             type: 'function',
             description: __('Natural logarithm')
         },
         LG: {
-            label: 'log',
+            label: labels.LOG,
             value: 'lg',
             type: 'function',
             description: __('Base-10 logarithm')
         },
         LOG10: {
-            label: 'log',
+            label: labels.LOG,
             value: 'log10',
             type: 'function',
             description: __('Base-10 logarithm')
         },
         ABS: {
-            label: 'abs',
+            label: labels.ABS,
             value: 'abs',
             type: 'function',
             description: __('Absolute value')
         },
         RAND: {
-            label: 'random',
+            label: labels.RAND,
             value: 'random',
             type: 'function',
             description: __('Random value')
