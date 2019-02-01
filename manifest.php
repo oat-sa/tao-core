4ed53b5c2cc342af082ef00f74951054347c7c65
handleman <pavel.handleman@gmail.com>
2019-02-01 14:16:46 +0300

    version bump

--- manifest.php
+++ manifest.php
@@ -45,7 +45,7 @@ return array(
     'label' => 'TAO Base',
     'description' => 'TAO meta-extension',
     'license' => 'GPL-2.0',
-    'version' => '24.3.0',
+    'version' => '24.3.1',
     'author' => 'Open Assessment Technologies, CRP Henri Tudor',
     'requires' => array(
         'generis' => '>=8.2.0',
