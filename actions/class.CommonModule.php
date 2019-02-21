dd696e0eef628f0cc4fda3ab3d287529e97f9cba
siwane <camille@taotesting.com>
2019-02-21 16:39:48 +0100

    Add unit est for renderer test

--- actions/class.CommonModule.php
+++ actions/class.CommonModule.php
@@ -17,7 +17,7 @@
  * Copyright (c) 2002-2008 (original work) Public Research Centre Henri Tudor & University of Luxembourg (under the project TAO & TAO2);
  *               2008-2010 (update and modification) Deutsche Institut für Internationale Pädagogische Forschung (under the project TAO-TRANSFER);
  *               2009-2012 (update and modification) Public Research Centre Henri Tudor (under the project TAO-SUSTAIN & TAO-DEV);
- *
+ *               2013-2019 (update and modification) Open Assessment Technologies SA;
  */
 
 use oat\oatbox\http\LegacyController;
--- models/classes/mvc/RendererTrait.php
+++ models/classes/mvc/RendererTrait.php
@@ -1,9 +1,21 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: siwane
- * Date: 05/02/19
- * Time: 22:09
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2019 (original work) Open Assessment Technologies SA;
+ *
  */
 
 namespace oat\tao\model\mvc;
@@ -13,10 +25,15 @@ use Renderer;
 trait RendererTrait
 {
     /**
-     * @var Renderer
+     * @var Renderer The renderer engine to display view
      */
     protected $renderer;
 
+    /**
+     * Get the renderer engine
+     *
+     * @return Renderer
+     */
     public function getRenderer()
     {
         if (!isset($this->renderer)) {
@@ -25,18 +42,37 @@ trait RendererTrait
         return $this->renderer;
     }
 
+    /**
+     * Helper to set renderer view
+     *
+     * @param $identifier
+     * @return $this
+     */
     public function setView($identifier)
     {
         $this->getRenderer()->setTemplate($identifier);
         return $this;
     }
 
+
+    /**
+     * Helper to set renderer data
+     *
+     * @param $key
+     * @param $value
+     * @return $this
+     */
     public function setData($key, $value)
     {
         $this->getRenderer()->setData($key, $value);
         return $this;
     }
 
+    /**
+     * Check if the renderer has a view
+     *
+     * @return bool
+     */
     public function hasView()
     {
         return isset($this->renderer) && $this->renderer->hasTemplate();
