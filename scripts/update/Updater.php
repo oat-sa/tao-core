4009c41e4c6f51f6b90c04a6f33069f7962e1c56
Aliaksandr Katovich <sunsaybasilis@gmail.com>
2019-03-27 13:53:27 +0300

    version bump

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -1003,6 +1003,6 @@ class Updater extends \common_ext_ExtensionUpdater {
             $this->setVersion('30.1.1');
         }
 
-        $this->skip('30.1.1', '30.1.2');
+        $this->skip('30.1.1', '30.1.3');
     }
 }
