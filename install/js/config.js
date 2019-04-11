b4708a0aaf16da2b057ab846b9541583ea3592df
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2019-04-11 10:37:33 +0200

    move jqueryui to the install

--- install/js/config.js
+++ install/js/config.js
@@ -4,7 +4,7 @@ require.config({
 
     paths : {
         'jquery'            : '../../views/js/lib/jquery-1.9.1',
-        'jqueryui'          : '../../views/js/lib/jquery-ui-1.9.2.custom.min',
+        'jqueryui'          : 'jquery-ui-1.9.2.custom.min',
         'spin'              : 'spin.min',
         'async'             : '../../views/js/lib/async'
     }
