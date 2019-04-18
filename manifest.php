1680ff4f64dafd6f40f3314173109ea89d9f6727
siwane <camille@taotesting.com>
2019-04-18 14:26:55 +0200

    Merge develop

--- manifest.php
+++ manifest.php
@@ -53,7 +53,7 @@ return array(
     'label' => 'TAO Base',
     'description' => 'TAO meta-extension',
     'license' => 'GPL-2.0',
-    'version' => '33.0.3',
+    'version' => '33.1.1',
     'author' => 'Open Assessment Technologies, CRP Henri Tudor',
     'requires' => array(
         'generis' => '>=9.0.0',
