db7f895d372cbefcb175289ce20564e58b4e22bd
Pavel Handleman <pavel.handleman@gmail.com>
2019-04-30 18:01:42 +0300

    TAO-8045 unit test select tree methods

--- views/js/test/generis.tree.select/test.js
+++ views/js/test/generis.tree.select/test.js
@@ -30,7 +30,6 @@ define(['jquery', 'lodash', 'lib/jquery.mockjax/jquery.mockjax','uri', 'generis.
 
     QUnit.test('generis.tree.select initialization usual and paginated', function(assert) {
         var done = assert.async();
-        var getDataUrl = '/taoTestCenter/TestCenterManager/getData';
         var rootNode = 'http://www.tao.lu/Ontologies/TAOSubject.rdf#Subject';
         var selected = [];
         var testTakers1000 = {"data":"Test-taker","type":"class","attributes":{"id":"http_2_www_0_tao_0_lu_1_Ontologies_1_TAOSubject_0_rdf_3_Subject","class":"node-class","data-uri":"http:\/\/www.tao.lu\/Ontologies\/TAOSubject.rdf#Subject","data-classUri":null,"data-signature":"4837dfe9a9ac93af9b404605843ac4bc31a16e4f898075a132a718c1c04d5406"},"state":"open","children":[{"data":"Generation yeGf","type":"class","attributes":{"id":"http_2_localhost_4_88_1__3_i1556529018304924","class":"node-class","data-uri":"http:\/\/localhost:88\/#i1556529018304924","data-classUri":"http:\/\/www.tao.lu\/Ontologies\/TAOSubject.rdf#Subject","data-signature":"994b71c8921c9a75beef2d9395e1f7fd757a1fe202fa30ffc2cefcd36a5e98fe"},"state":"open","children":[{"data":"Test-Taker  0","type":"instance","attributes":{"id":"http_2_localhost_4_88_1__3_i1556529019327625","class":"node-instance","data-uri":"http:\/\/localhost:88\/#i1556529019327625","data-classUri":"http:\/\/localhost:88\/#i1556529018304924","data-signature":"dff3eff0857ea118c317c01f76f736bc3930f18b1afca81bafdeb3d2bcb6f534"}},{"data":"Test-Taker  1","type":"instance","attributes":{"id":"http_2_localhost_4_88_1__3_i1556529019723426","class":"node-instance","data-uri":"http:\/\/localhost:88\/#i1556529019723426","data-classUri":"http:\/\/localhost:88\/#i1556529018304924","data-signature":"8d813c7e5d1ad20dc31fd5d32f34b4a3c9a9b8ab6cf3c48cbb7c148b33949eed"}},{"data":"Test-Taker  10","type":"instance","attributes":{"id":"http_2_localhost_4_88_1__3_i1556529021538635","class":"node-instance","data-uri":"http:\/\/localhost:88\/#i1556529021538635","data-classUri":"http:\/\/localhost:88\/#i1556529018304924","data-signature":"7ccb3b451f2f435ce35008bb2112d40af8572cb199ab99d7ced9ad9951ac5b63"}},{"data":"Test-Taker  100","type":"instance","attributes":{"id":"http_2_localhost_4_88_1__3_i15565290355669125","class":"node-instance","data-uri":"http:\/\/localhost:88\/#i15565290355669125","data-classUri":"http:\/\/localhost:88\/#i1556529018304924","data-signature":"cf9735098a9ef2a33170d2deac6c5156e404fd6d26f7f4d3b4723759eb606b65"}},{"data":"Test-Taker  101","type":"instance","attributes":{"id":"http_2_localhost_4_88_1__3_i1556529035778126","class":"node-instance","data-uri":"http:\/\/localhost:88\/#i1556529035778126","data-classUri":"http:\/\/localhost:88\/#i1556529018304924","data-signature":"012ec45aa31d943f59a9b819d63dc3b4ea216e11ed487af6820f2fdf3a3a10ca"}},{"data":"Test-Taker  102","type":"instance","attributes":{"id":"http_2_localhost_4_88_1__3_i15565290356882127","class":"node-instance","data-uri":"http:\/\/localhost:88\/#i15565290356882127","data-classUri":"http:\/\/localhost:88\/#i1556529018304924","data-signature":"f6a7319336205ae4b1e75131e1115d691834aaddf7c6f6550b12dac6ecd00576"}},{"data":"Test-Taker  103","type":"instance","attributes":{"id":"http_2_localhost_4_88_1__3_i15565290362076128","class":"node-instance","data-uri":"http:\/\/localhost:88\/#i15565290362076128","data-classUri":"http:\/\/localhost:88\/#i1556529018304924","data-signature":"9b4960b4df95020c34e3a92b6e182dbf8895fc2c91fbdb5ef20351293febd716"}},{"data":"Test-Taker  104","type":"instance","attributes":{"id":"http_2_localhost_4_88_1__3_i15565290369733129","class":"node-instance","data-uri":"http:\/\/localhost:88\/#i15565290369733129","data-classUri":"http:\/\/localhost:88\/#i1556529018304924","data-signature":"130022af77f553fd0cc125159c0a353242a13da7841e554f47fd8b55bb9f3231"}},{"data":"Test-Taker  105","type":"instance","attributes":{"id":"http_2_localhost_4_88_1__3_i15565290362440130","class":"node-instance","data-uri":"http:\/\/localhost:88\/#i15565290362440130","data-classUri":"http:\/\/localhost:88\/#i1556529018304924","data-signature":"5af77f61d488d3f7482a73c764df232f7965301156c670a894d0e89adb9b2d0a"}},{"data":"Test-Taker  106","type":"instance","attributes":{"id":"http_2_localhost_4_88_1__3_i15565290363000131","class":"node-instance","data-uri":"http:\/\/localhost:88\/#i15565290363000131","data-classUri":"http:\/\/localhost:88\/#i1556529018304924","data-signature":"a06020054544ed2fcfe17be9bc07efe85ad51ab0ddfb8a06b46cdf7143d58ec2"}}],"count":1000}],"count":0};
@@ -88,7 +87,7 @@ define(['jquery', 'lodash', 'lib/jquery.mockjax/jquery.mockjax','uri', 'generis.
             paginate : 10,
             checkResourcePermissions: options.isDacEnabled
         });
-
+        assert.expect(29);
         assert.ok(tree10,  'tree was initialized and prepared');
         assert.ok(tree1000,  ' paginated tree was initialized and prepared');
 
@@ -107,6 +106,16 @@ define(['jquery', 'lodash', 'lib/jquery.mockjax/jquery.mockjax','uri', 'generis.
         assert.ok(tree10.treeOptions.callback.onopen,  'onopen callback is defined');
         assert.ok(tree10.treeOptions.callback.onselect,  'onselect callback is defined');
 
+        assert.ok(tree10.__proto__.check,  'check method is defined');
+        assert.ok(tree10.__proto__.checkPermissionsRecursively,  'checkPermissionsRecursively method is defined');
+        assert.ok(tree10.__proto__.convertDataWithPermissions,  'convertDataWithPermissions method is defined');
+        assert.ok(tree10.__proto__.getChecked,  'getChecked method is defined');
+        assert.ok(tree10.__proto__.init,  'init method is defined');
+        assert.ok(tree10.__proto__.paginateInstances,  'paginateInstances method is defined');
+        assert.ok(tree10.__proto__.removeHiddenNodes,  'removeHiddenNodes method is defined');
+        assert.ok(tree10.__proto__.saveData,  'saveData method is defined');
+        assert.ok(tree10.__proto__.trace,  'trace method is defined');
+
 
         _.delay(function () {
             assert.equal($(treeContainerSelector + ' '+ paginateAllSelector).length, 0, 'usual tree doesent have "pagination all" button');
@@ -121,6 +130,8 @@ define(['jquery', 'lodash', 'lib/jquery.mockjax/jquery.mockjax','uri', 'generis.
         },10);
 
     });
+
+
 
 
 });
