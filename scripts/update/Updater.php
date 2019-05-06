2cede0134ba9c44d3c0c69efc5945c2bbb202630
Martin Nicholson <martin@taotesting.com>
2019-05-06 11:44:27 +0200

    Version bump

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -1035,6 +1035,6 @@ class Updater extends \common_ext_ExtensionUpdater {
             $this->setVersion('34.0.0');
         }
 
-        $this->skip('34.0.0', '34.0.2');
+        $this->skip('34.0.0', '34.0.3');
     }
 }
