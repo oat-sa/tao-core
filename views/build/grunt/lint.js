df9b80f3ba5b4f7534b4b812ea8d1008b6c4060d
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2019-05-08 14:46:14 +0200

    open eslint task to multiple report format

--- views/build/grunt/lint.js
+++ views/build/grunt/lint.js
@@ -2,24 +2,26 @@
  * grunt eslint:file --file=/path/to/file/to/lint
  * grunt eslint:extension --extension=taoQtiTest
  */
-module.exports = function(grunt) {
-    'use strict';
 
-    var root    = grunt.option('root');
-    var currentExtension = grunt.option('currentExtension');
-    var reportOutput = grunt.option('reports') || 'reports';
-    var extensionRoot = root + '/' + currentExtension + '/';
+module.exports = function(grunt) {
+
+    const root             = grunt.option('root');
+    const currentExtension = grunt.option('currentExtension') || 'tao';
+    const reportOutput     = grunt.option('reports') || 'reports';
+    const reportFormat     = grunt.option('format') || 'checkstyle';
+    const extensionRoot    = `${root}/${currentExtension}`;
+
     var extensionSrc = [
         extensionRoot + '/views/js/**/*.js',
-        '!' + extensionRoot + 'views/js/**/*.min.js',
-        '!' + extensionRoot + 'views/js/**/*.src.js',
-        '!' + extensionRoot + 'views/js/test/**/*.js',
-        '!' + extensionRoot + 'views/js/lib/**/*.js',
-        '!' + extensionRoot + 'views/js/legacyPortableSharedLib/**/*.js',
-        '!' + extensionRoot + 'views/js/portableLib/**/*.js',
-        '!' + extensionRoot + 'views/js/pciCreator/dev/**/*.js',
-        '!' + extensionRoot + 'views/js/picCreator/dev/**/*.js',
-        '!' + extensionRoot + 'views/js/**/jquery.*.js'
+        `!${extensionRoot}/views/js/**/*.min.js`,
+        `!${extensionRoot}/views/js/**/*.src.js`,
+        `!${extensionRoot}/views/js/test/**/*.js`,
+        `!${extensionRoot}/views/js/lib/**/*.js`,
+        `!${extensionRoot}/views/js/legacyPortableSharedLib/**/*.js`,
+        `!${extensionRoot}/views/js/portableLib/**/*.js`,
+        `!${extensionRoot}/views/js/pciCreator/dev/**/*.js`,
+        `!${extensionRoot}/views/js/picCreator/dev/**/*.js`,
+        `!${extensionRoot}/views/js/**/jquery.*.js`
     ];
 
     grunt.config.merge({
@@ -35,8 +37,8 @@ module.exports = function(grunt) {
             },
             extensionreport : {
                 options : {
-                    format: 'checkstyle',
-                    outputFile:  reportOutput + '/' + currentExtension + '-checkstyle.xml'
+                    format: reportFormat,
+                    outputFile:  `${reportOutput}/${reportFormat.toUpperCase()}-${currentExtension}.xml`
                 },
                 src : extensionSrc
             }
