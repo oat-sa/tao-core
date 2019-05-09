258b4441ae38841a48f93cc2c6a05f20174db1ea
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2019-05-09 11:17:57 +0200

    update formatter extension in lint

--- views/build/grunt/lint.js
+++ views/build/grunt/lint.js
@@ -1,15 +1,43 @@
+/*
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2014-2019 (original work) Open Assessment Technlogies SA
+ *
+ */
+
 /**
  * grunt eslint:file --file=/path/to/file/to/lint
  * grunt eslint:extension --extension=taoQtiTest
  */
-
 module.exports = function(grunt) {
+    'use strict';
+
+    const formatterExtensions = {
+        stylish : 'txt',
+        checkstyle : 'xml',
+        json : 'json',
+        html : 'html'
+    };
 
     const root             = grunt.option('root');
     const currentExtension = grunt.option('currentExtension') || 'tao';
-    const reportOutput     = grunt.option('reports') || 'reports';
-    const reportFormat     = grunt.option('format') || 'checkstyle';
     const extensionRoot    = `${root}/${currentExtension}`;
+    const reportOutput     = grunt.option('reports') || 'reports';
+    const format           = grunt.option('format') || 'checkstyle';
+    const outputFile       = `${reportOutput}/${format.toUpperCase()}-${currentExtension}.${formatterExtensions[format]}`;
+
 
     var extensionSrc = [
         extensionRoot + '/views/js/**/*.js',
@@ -37,8 +65,8 @@ module.exports = function(grunt) {
             },
             extensionreport : {
                 options : {
-                    format: reportFormat,
-                    outputFile:  `${reportOutput}/${reportFormat.toUpperCase()}-${currentExtension}.xml`
+                    format,
+                    outputFile
                 },
                 src : extensionSrc
             }
