c6800f9d6096ebb5fa3c9d328fd1e6f853c3063e
Pavel Handleman <pavel.handleman@gmail.com>
2019-05-10 14:04:38 +0300

    version bump

--- manifest.php
+++ manifest.php
@@ -53,7 +53,7 @@ return array(
     'label' => 'TAO Base',
     'description' => 'TAO meta-extension',
     'license' => 'GPL-2.0',
-    'version' => '34.3.0',
+    'version' => '34.2.1',
     'author' => 'Open Assessment Technologies, CRP Henri Tudor',
     'requires' => array(
         'generis' => '>=10.1.0',
--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -1034,6 +1034,6 @@ class Updater extends \common_ext_ExtensionUpdater {
             $tokenStore->removeTokens();
             $this->setVersion('34.0.0');
         }
-        $this->skip('34.0.0', '34.3.0');
+        $this->skip('34.0.0', '34.2.1');
     }
 }
