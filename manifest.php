78ded6e4b7f3682bda8d430e7b297918ae27065f
Martin Nicholson <martin@taotesting.com>
2019-05-21 13:45:30 +0200

    Version bump to 34.2.1.2

--- manifest.php
+++ manifest.php
@@ -53,7 +53,7 @@ return array(
     'label' => 'TAO Base',
     'description' => 'TAO meta-extension',
     'license' => 'GPL-2.0',
-    'version' => '34.2.1.1',
+    'version' => '34.2.1.2',
     'author' => 'Open Assessment Technologies, CRP Henri Tudor',
     'requires' => array(
         'generis' => '>=10.1.0',
