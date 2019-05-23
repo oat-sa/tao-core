5638de17575679e839ef0b4080bd3351392c1a98
jsconan <jsconan@gmail.com>
2019-05-23 13:40:45 +0200

    Add ui/form/validator

---
+++ views/js/test/ui/form/validator/validator/test.js
@@ -0,0 +1,480 @@
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2019 (original work) Open Assessment Technologies SA ;
+ */
+
+/**
+ * @author Jean-Sébastien Conan <jean-sebastien@taotesting.com>
+ */
+define([
+    'lodash',
+    'core/promise',
+    'ui/form/validator/validator'
+], function (
+    _,
+    Promise,
+    validatorFactory
+) {
+    'use strict';
+
+    QUnit.module('Factory');
+
+    QUnit.test('module', function (assert) {
+        assert.expect(3);
+
+        assert.equal(typeof validatorFactory, 'function', 'The module exposes a function');
+        assert.equal(typeof validatorFactory(), 'object', 'The factory produces an object');
+        assert.notStrictEqual(validatorFactory(), validatorFactory(), 'The factory provides a different object on each call');
+    });
+
+    QUnit.cases.init([
+        {title: 'validate'},
+        {title: 'addValidation'},
+        {title: 'getValidation'},
+        {title: 'getValidations'},
+        {title: 'removeValidation'},
+        {title: 'removeValidations'}
+    ]).test('module API ', function (data, assert) {
+        var instance = validatorFactory();
+        assert.expect(1);
+        assert.equal(typeof instance[data.title], 'function', 'The instance exposes a "' + data.title + '" function');
+    });
+
+    QUnit.module('API');
+
+    QUnit.test('manage rules', function (assert) {
+        var fooRule = {
+            id: 'foo',
+            predicate: /^\w+$/
+        };
+        var barRule = {
+            id: 'bar',
+            predicate: function(value) {
+                return value === 'foo';
+            }
+        };
+        var instance = validatorFactory({
+            validations: [fooRule, barRule]
+        });
+
+        assert.expect(21);
+
+        assert.deepEqual(instance.getValidations(), [fooRule, barRule], 'The list of rules is filled');
+        assert.equal(instance.getValidation('foo'), fooRule, 'There is a rule identified by foo');
+        assert.equal(instance.getValidation('bar'), barRule, 'There is a rule identified by bar');
+
+        instance.removeValidations();
+        assert.deepEqual(instance.getValidations(), [], 'The list of rules is now empty');
+        assert.equal(instance.getValidation('foo'), null, 'There is no rule identified by foo anymore');
+        assert.equal(instance.getValidation('bar'), null, 'There is no rule identified by bar anymore');
+
+        assert.throws(function() {
+            instance.addValidation();
+        }, 'Should raise an error as the rule is missing');
+
+        assert.throws(function() {
+            instance.addValidation({});
+        }, 'Should raise an error as the rule is empty');
+
+        assert.throws(function() {
+            instance.addValidation({id: ''});
+        }, 'Should raise an error as the id is empty');
+
+        assert.throws(function() {
+            instance.addValidation({id: 'required'});
+        }, 'Should raise an error as the predicate is missing');
+
+        assert.throws(function() {
+            instance.addValidation({id: 'required', predicate: false});
+        }, 'Should raise an error as the predicate is invalid');
+
+        instance = validatorFactory();
+        assert.deepEqual(instance.getValidations(), [], 'The list of rules is empty');
+        assert.equal(instance.getValidation('foo'), null, 'There is no rule identified by foo');
+        assert.equal(instance.getValidation('bar'), null, 'There is no rule identified by bar');
+
+        instance.addValidation(fooRule);
+        assert.equal(instance.getValidation('foo'), fooRule, 'There is a rule identified by foo');
+
+        instance.addValidation(barRule);
+        assert.equal(instance.getValidation('bar'), barRule, 'There is a rule identified by bar');
+
+        assert.deepEqual(instance.getValidations(), [fooRule, barRule], 'The list of rules is set');
+
+        instance.removeValidation('foo');
+        assert.equal(instance.getValidation('foo'), null, 'There is no rule identified by foo anymore');
+
+        assert.deepEqual(instance.getValidations(), [barRule], 'The list of rules only contains the rule identified by bar');
+
+        instance.removeValidation('bar');
+        assert.equal(instance.getValidation('bar'), null, 'There is no rule identified by bar anymore');
+
+        assert.deepEqual(instance.getValidations(), [], 'The list of rules is empty');
+    });
+
+    QUnit.test('validate no rules', function (assert) {
+        var ready = assert.async();
+        var instance = validatorFactory();
+
+        assert.expect(3);
+
+        Promise
+            .all([
+                new Promise(function(resolve) {
+                    instance.validate('')
+                        .then(function() {
+                            assert.ok(true, 'The value is valid');
+                        })
+                        .catch(function(err) {
+                            assert.ok(false, 'The value should be valid');
+                            assert.pushResult({
+                                result: false,
+                                message: err
+                            });
+                        })
+                        .then(function() {
+                            resolve();
+                        });
+                }),
+                new Promise(function(resolve) {
+                    instance.validate('foo')
+                        .then(function() {
+                            assert.ok(true, 'The value is valid');
+                        })
+                        .catch(function(err) {
+                            assert.ok(false, 'The value should be valid');
+                            assert.pushResult({
+                                result: false,
+                                message: err
+                            });
+                        })
+                        .then(function() {
+                            resolve();
+                        });
+                }),
+                new Promise(function(resolve) {
+                    instance.validate('10')
+                        .then(function() {
+                            assert.ok(true, 'The value is valid');
+                        })
+                        .catch(function(err) {
+                            assert.ok(false, 'The value should be valid');
+                            assert.pushResult({
+                                result: false,
+                                message: err
+                            });
+                        })
+                        .then(function() {
+                            resolve();
+                        });
+                })
+            ])
+            .catch(function(err) {
+                assert.ok(false, 'The operation should not fail!');
+                assert.pushResult({
+                    result: false,
+                    message: err
+                });
+            })
+            .then(function() {
+                ready();
+            });
+    });
+
+    QUnit.test('validate rules', function (assert) {
+        var ready = assert.async();
+        var defaultMessage = 'Oops!';
+        var instance = validatorFactory({
+            defaultMessage: defaultMessage,
+            validations: [{
+                id: 'numeric',
+                message: 'This field must be numerical',
+                predicate: /^\d+$/,
+                precedence: 2
+            }, {
+                id: 'required',
+                message: 'This field is required',
+                predicate: function (value) {
+                    return value.length > 0;
+                },
+                precedence: 1
+            }, {
+                id: 'domain',
+                message: 'This value must not be greater than 10',
+                predicate: function (value) {
+                    value = parseInt(value, 10) || 0;
+                    return Promise.resolve(value <= 10);
+                },
+                precedence: 3
+            }]
+        });
+
+        assert.expect(7);
+
+        Promise
+            .all([
+                new Promise(function(resolve) {
+                    instance.validate('')
+                        .then(function() {
+                            assert.ok(false, 'The value should not be valid');
+                        })
+                        .catch(function(messages) {
+                            assert.ok(true, 'The value is not valid');
+                            assert.deepEqual(messages, ['This field is required', 'This field must be numerical'], 'The expected messages are returned');
+                        })
+                        .then(function() {
+                            resolve();
+                        });
+                }),
+                new Promise(function(resolve) {
+                    instance.validate('foo')
+                        .then(function() {
+                            assert.ok(false, 'The value should not be valid');
+                        })
+                        .catch(function(messages) {
+                            assert.ok(true, 'The value is not valid');
+                            assert.deepEqual(messages, ['This field must be numerical'], 'The expected messages are returned');
+                        })
+                        .then(function() {
+                            resolve();
+                        });
+                }),
+                new Promise(function(resolve) {
+                    instance.validate('100')
+                        .then(function() {
+                            assert.ok(false, 'The value should not be valid');
+                        })
+                        .catch(function(messages) {
+                            assert.ok(true, 'The value is not valid');
+                            assert.deepEqual(messages, ['This value must not be greater than 10'], 'The expected messages are returned');
+                        })
+                        .then(function() {
+                            resolve();
+                        });
+                }),
+                new Promise(function(resolve) {
+                    instance.validate('10')
+                        .then(function() {
+                            assert.ok(true, 'The value is valid');
+                        })
+                        .catch(function(err) {
+                            assert.ok(false, 'The value should be valid');
+                            assert.pushResult({
+                                result: false,
+                                message: err
+                            });
+                        })
+                        .then(function() {
+                            resolve();
+                        });
+                })
+            ])
+            .catch(function(err) {
+                assert.ok(false, 'The operation should not fail!');
+                assert.pushResult({
+                    result: false,
+                    message: err
+                });
+            })
+            .then(function() {
+                ready();
+            });
+    });
+
+    QUnit.test('validate precise rule', function (assert) {
+        var ready = assert.async();
+        var defaultMessage = 'Oops!';
+        var instance = validatorFactory({
+            defaultMessage: defaultMessage,
+            validations: [{
+                id: 'precise',
+                message: 'This field must be yes',
+                predicate: 'yes'
+            }]
+        });
+
+        assert.expect(7);
+
+        Promise
+            .all([
+                new Promise(function(resolve) {
+                    instance.validate('')
+                        .then(function() {
+                            assert.ok(false, 'The value should not be valid');
+                        })
+                        .catch(function(messages) {
+                            assert.ok(true, 'The value is not valid');
+                            assert.deepEqual(messages, ['This field must be yes'], 'The expected messages are returned');
+                        })
+                        .then(function() {
+                            resolve();
+                        });
+                }),
+                new Promise(function(resolve) {
+                    instance.validate('foo')
+                        .then(function() {
+                            assert.ok(false, 'The value should not be valid');
+                        })
+                        .catch(function(messages) {
+                            assert.ok(true, 'The value is not valid');
+                            assert.deepEqual(messages, ['This field must be yes'], 'The expected messages are returned');
+                        })
+                        .then(function() {
+                            resolve();
+                        });
+                }),
+                new Promise(function(resolve) {
+                    instance.validate('100')
+                        .then(function() {
+                            assert.ok(false, 'The value should not be valid');
+                        })
+                        .catch(function(messages) {
+                            assert.ok(true, 'The value is not valid');
+                            assert.deepEqual(messages, ['This field must be yes'], 'The expected messages are returned');
+                        })
+                        .then(function() {
+                            resolve();
+                        });
+                }),
+                new Promise(function(resolve) {
+                    instance.validate('yes')
+                        .then(function() {
+                            assert.ok(true, 'The value is valid');
+                        })
+                        .catch(function(err) {
+                            assert.ok(false, 'The value should be valid');
+                            assert.pushResult({
+                                result: false,
+                                message: err
+                            });
+                        })
+                        .then(function() {
+                            resolve();
+                        });
+                })
+            ])
+            .catch(function(err) {
+                assert.ok(false, 'The operation should not fail!');
+                assert.pushResult({
+                    result: false,
+                    message: err
+                });
+            })
+            .then(function() {
+                ready();
+            });
+    });
+
+    QUnit.test('validate list', function (assert) {
+        var ready = assert.async();
+        var defaultMessage = 'Oops!';
+        var instance = validatorFactory({
+            defaultMessage: defaultMessage,
+            validations: [{
+                id: 'precise',
+                message: 'This field must be yes or no',
+                predicate: ['yes', 'no']
+            }]
+        });
+
+        assert.expect(8);
+
+        Promise
+            .all([
+                new Promise(function(resolve) {
+                    instance.validate('')
+                        .then(function() {
+                            assert.ok(false, 'The value should not be valid');
+                        })
+                        .catch(function(messages) {
+                            assert.ok(true, 'The value is not valid');
+                            assert.deepEqual(messages, ['This field must be yes or no'], 'The expected messages are returned');
+                        })
+                        .then(function() {
+                            resolve();
+                        });
+                }),
+                new Promise(function(resolve) {
+                    instance.validate('foo')
+                        .then(function() {
+                            assert.ok(false, 'The value should not be valid');
+                        })
+                        .catch(function(messages) {
+                            assert.ok(true, 'The value is not valid');
+                            assert.deepEqual(messages, ['This field must be yes or no'], 'The expected messages are returned');
+                        })
+                        .then(function() {
+                            resolve();
+                        });
+                }),
+                new Promise(function(resolve) {
+                    instance.validate('100')
+                        .then(function() {
+                            assert.ok(false, 'The value should not be valid');
+                        })
+                        .catch(function(messages) {
+                            assert.ok(true, 'The value is not valid');
+                            assert.deepEqual(messages, ['This field must be yes or no'], 'The expected messages are returned');
+                        })
+                        .then(function() {
+                            resolve();
+                        });
+                }),
+                new Promise(function(resolve) {
+                    instance.validate('yes')
+                        .then(function() {
+                            assert.ok(true, 'The value is valid');
+                        })
+                        .catch(function(err) {
+                            assert.ok(false, 'The value should be valid');
+                            assert.pushResult({
+                                result: false,
+                                message: err
+                            });
+                        })
+                        .then(function() {
+                            resolve();
+                        });
+                }),
+                new Promise(function(resolve) {
+                    instance.validate('no')
+                        .then(function() {
+                            assert.ok(true, 'The value is valid');
+                        })
+                        .catch(function(err) {
+                            assert.ok(false, 'The value should be valid');
+                            assert.pushResult({
+                                result: false,
+                                message: err
+                            });
+                        })
+                        .then(function() {
+                            resolve();
+                        });
+                })
+            ])
+            .catch(function(err) {
+                assert.ok(false, 'The operation should not fail!');
+                assert.pushResult({
+                    result: false,
+                    message: err
+                });
+            })
+            .then(function() {
+                ready();
+            });
+    });
+});
---
+++ views/js/ui/form/validator/validator.js
@@ -0,0 +1,207 @@
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2019 Open Assessment Technologies SA ;
+ */
+/**
+ * Simple validator engine. Apply a collection of validation rules on a value.
+ * @author Jean-Sébastien Conan <jean-sebastien@taotesting.com>
+ */
+define([
+    'lodash',
+    'i18n',
+    'core/collections',
+    'core/promise'
+], function (
+    _,
+    __,
+    collections,
+    Promise
+) {
+    'use strict';
+
+    /**
+     * @typedef {Object} validationRule Defines a validation rule to apply on a value.
+     * @property {String} id - The validation identifier
+     * @property {RegExp|Function|String|String[]} predicate - The validation rule to apply on the value.
+     *                                                         It could be either a RegExp or a Function.
+     *                                                         The function must return a boolean value,
+     *                                                         it can wrap it in a Promise too.
+     * @property {String} [message] - The message returned in case of failed validation
+     * @property {Number} [precedence] - The precedence order for sorting
+     */
+
+    /**
+     * @typedef {Object} validatorConfig Defines the config entries available to setup a form widget validator
+     * @property {validationRule[]} validations - The list of validations to apply
+     * @property {String} defaultMessage - The default message returned when a validation fails and no message is set
+     */
+
+    /**
+     * Defaults config for the validator
+     * @type {validatorConfig}
+     */
+    var defaults = {
+        defaultMessage: __('Invalid input')
+    };
+
+    /**
+     * Validates a value
+     * @param {String} value
+     * @param {validationRule} validation
+     * @returns {Boolean|Promise}
+     */
+    function validateValue(value, validation) {
+        if (validation.predicate instanceof RegExp) {
+            return validation.predicate.test(value);
+        } else if (_.isFunction(validation.predicate)) {
+            return validation.predicate(value);
+        } else if (_.isArray(validation.predicate)) {
+            return _.indexOf(validation.predicate, value) > -1;
+        }
+        return validation.predicate === value;
+    }
+
+    /**
+     * Compares validation rules
+     * @param {validationRule} a
+     * @param {validationRule} b
+     * @returns {Number}
+     */
+    function compareRule(a, b) {
+        return ((a && a.precedence) || 0) - ((b && b.precedence) || 0);
+    }
+
+    /**
+     * Creates a simple form widget's validator.
+     * It manages and applies a collection of validation rules on a value.
+     *
+     * @param {validatorConfig} config
+     * @param {validationRule[]} [config.validations] - The list of validation rules to apply
+     * @param {String} [config.defaultMessage] - The default message returned when a validation fails and no message is set
+     * @returns {validator}
+     */
+    return function validatorFactory(config) {
+        var validations = new collections.Map();
+
+        /**
+         * @typedef {Object} validator
+         */
+        var validator = {
+            /**
+             * Runs all validation rules on a value
+             * @param {String} value
+             * @returns {Promise} Will provide the list of error messages if the validation failed.
+             */
+            validate: function validate(value) {
+                var rules = this.getValidations();
+                rules.sort(compareRule);
+
+                return Promise
+                    .all(rules.map(function (validation) {
+                        return Promise.resolve(validateValue(value, validation));
+                    }))
+                    .then(function (results) {
+                        var errors = _.reduce(results, function (list, result, index) {
+                            if (!result) {
+                                list.push(rules[index].message || config.defaultMessage);
+                            }
+                            return list;
+                        }, []);
+
+                        if (errors.length) {
+                            return Promise.reject(errors);
+                        }
+                    });
+            },
+
+            /**
+             * Adds a validation rule
+             * @param {validationRule} validation
+             * @returns {validator}
+             * @throws {TypeError} if the validation object is not valid
+             */
+            addValidation: function addValidation(validation) {
+                if (!_.isPlainObject(validation)) {
+                    throw new TypeError('The validation must be an object');
+                }
+                if (!_.isString(validation.id) || !validation.id) {
+                    throw new TypeError('The validation must contain an identifier');
+                }
+                if (!_.isFunction(validation.predicate) &&
+                    !_.isRegExp(validation.predicate) &&
+                    !_.isString(validation.predicate) &&
+                    !_.isArray(validation.predicate)) {
+                    throw new TypeError('The validation must provide a predicate');
+                }
+
+                validations.set(validation.id, validation);
+
+                return this;
+            },
+
+            /**
+             * Gets a validation rule by its identifier
+             * @param {String} id
+             * @returns {validationRule|null}
+             */
+            getValidation: function getValidation(id) {
+                if (validations.has(id)) {
+                    return validations.get(id);
+                }
+                return null;
+            },
+
+            /**
+             * Gets the list of validation rules.
+             * @returns {validationRule[]}
+             */
+            getValidations: function getValidations() {
+                var list = [];
+                validations.forEach(function (validation) {
+                    list.push(validation);
+                });
+                return list;
+            },
+
+            /**
+             * Removes a validation rule
+             * @param {String} id
+             * @returns {validator}
+             */
+            removeValidation: function removeValidation(id) {
+                if (validations.has(id)) {
+                    validations.delete(id);
+                }
+                return this;
+            },
+
+            /**
+             * Removes all validation rules
+             * @returns {validator}
+             */
+            removeValidations: function removeValidations() {
+                validations.clear();
+
+                return this;
+            }
+        };
+
+        config = _.defaults(_.clone(config) || {}, defaults);
+        _.forEach(config.validations, validator.addValidation);
+
+        return validator;
+    };
+});
