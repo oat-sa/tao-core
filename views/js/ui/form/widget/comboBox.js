a93f8d34ffae4a4fb02adaa3d45a301696d764d8
jsconan <jsconan@gmail.com>
2019-05-23 19:10:21 +0200

    Add  ui/form/widget/comboBox

---
+++ views/js/ui/form/widget/comboBox.js
@@ -0,0 +1,74 @@
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2019 Open Assessment Technologies SA ;
+ */
+/**
+ * Defines a comboBox widget
+ * @author Jean-Sébastien Conan <jean-sebastien@taotesting.com>
+ */
+define([
+    'jquery',
+    'lodash',
+    'i18n',
+    'tpl!ui/form/widget/tpl/comboBox'
+], function ($, _, __, comboBoxTpl) {
+    'use strict';
+
+    /**
+     * Makes sure a value is an array
+     * @param {*} value
+     * @returns {Array}
+     */
+    function forceArray(value) {
+        if (value && !_.isArray(value)) {
+            value = [value];
+        } else {
+            value = value || [];
+        }
+        return value;
+    }
+
+    /**
+     * Defines the provider for a comboBox widget.
+     *
+     * @example
+     * widgetFactory.registerProvider('comboBox', comboBoxProvider);
+     */
+    return {
+        /**
+         * Initialize the widget.
+         * @param {widgetConfig} config
+         */
+        init: function init(config) {
+            // Simply set the type.
+            // The default template is already a text input.
+            config.widgetType = 'combo-box';
+
+            // the value must be an array
+            config.range = forceArray(config.range);
+
+            // initial value
+            this.on('render', function() {
+                this.getWidgetElement().val(this.getConfig().value);
+            });
+        },
+
+        /**
+         * Expose the template to the factory and it will apply it
+         */
+        template: comboBoxTpl
+    };
+});
---
+++ views/js/ui/form/widget/tpl/comboBox.tpl
@@ -0,0 +1,13 @@
+<div class="form-widget {{widgetType}}">
+    <div class="widget-label">
+        {{> ui-form-widget-label }}
+    </div>
+    <div class="widget-field">
+        <select name="{{uri}}">
+            <option value="">{{__ 'Choose a value...'}}</option>
+            {{#each range}}
+            <option value="{{this.uri}}">{{this.label}}</option>
+            {{/each}}
+        </select>
+    </div>
+</div>
