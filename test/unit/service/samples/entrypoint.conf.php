0d8dafd52b2ed4b90244a9c6e4d946d3144ccc6f
Aleh Hutnikau <hutnikau@1pt.com>
2019-05-31 18:31:31 +0300

    mock entrypoints

--- test/unit/service/samples/entrypoint.conf.php
+++ test/unit/service/samples/entrypoint.conf.php
@@ -4,60 +4,12 @@
  */
 use oat\tao\model\entryPoint\Entrypoint;
 
-class EntrypointMock1 implements Entrypoint
-{
-
-    public function getId() {
-        return 'EntrypointMock1';
-    }
-
-    public function getTitle() {
-        return __("EntrypointMock1 titile");
-    }
-
-    public function getLabel() {
-        return __('EntrypointMock1 label');
-    }
-
-    public function getDescription() {
-        return __('EntrypointMock1 description');
-    }
-
-    public function getUrl() {
-        return _url('index', 'EntrypointMock1', 'tao');
-    }
-}
-
-class EntrypointMock2 implements Entrypoint
-{
-
-    public function getId() {
-        return 'EntrypointMock2';
-    }
-
-    public function getTitle() {
-        return __("EntrypointMock2 titile");
-    }
-
-    public function getLabel() {
-        return __('EntrypointMock2 label');
-    }
-
-    public function getDescription() {
-        return __('EntrypointMock2 description');
-    }
-
-    public function getUrl() {
-        return _url('index', 'EntrypointMock2', 'tao');
-    }
-}
-
 return new oat\tao\model\entryPoint\EntryPointService(array(
     'existing' => array(
         'passwordreset' => new oat\tao\model\entryPoint\PasswordReset(),
-        'deliveryServer' => new EntrypointMock1(),
-        'guestaccess' => new oat\taoDeliveryRdf\model\guest\GuestAccess(),
-        'proctoringDelivery' => new EntrypointMock2(),
+        'deliveryServer' => $this->getMockBuilder(Entrypoint::class)->getMock(),
+        'guestaccess' => $this->getMockBuilder(Entrypoint::class)->getMock(),
+        'proctoringDelivery' => $this->getMockBuilder(Entrypoint::class)->getMock(),
     ),
     'postlogin' => array(
         'deliveryServer',
