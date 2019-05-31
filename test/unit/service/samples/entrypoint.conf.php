e161c9897e3cfc5d632716602481165a1a488bc3
Aleh Hutnikau <hutnikau@1pt.com>
2019-05-31 18:18:53 +0300

    do not use proctoiring classes in unit tests

--- test/unit/service/samples/entrypoint.conf.php
+++ test/unit/service/samples/entrypoint.conf.php
@@ -2,13 +2,62 @@
 /**
  * Default config header created during install
  */
+use oat\tao\model\entryPoint\Entrypoint;
+
+class EntrypointMock1 implements Entrypoint
+{
+
+    public function getId() {
+        return 'EntrypointMock1';
+    }
+
+    public function getTitle() {
+        return __("EntrypointMock1 titile");
+    }
+
+    public function getLabel() {
+        return __('EntrypointMock1 label');
+    }
+
+    public function getDescription() {
+        return __('EntrypointMock1 description');
+    }
+
+    public function getUrl() {
+        return _url('index', 'EntrypointMock1', 'tao');
+    }
+}
+
+class EntrypointMock2 implements Entrypoint
+{
+
+    public function getId() {
+        return 'EntrypointMock2';
+    }
+
+    public function getTitle() {
+        return __("EntrypointMock2 titile");
+    }
+
+    public function getLabel() {
+        return __('EntrypointMock2 label');
+    }
+
+    public function getDescription() {
+        return __('EntrypointMock2 description');
+    }
+
+    public function getUrl() {
+        return _url('index', 'EntrypointMock2', 'tao');
+    }
+}
 
 return new oat\tao\model\entryPoint\EntryPointService(array(
     'existing' => array(
         'passwordreset' => new oat\tao\model\entryPoint\PasswordReset(),
-        'deliveryServer' => new oat\taoProctoring\model\entrypoint\ProctoringDeliveryServer(),
+        'deliveryServer' => new EntrypointMock1(),
         'guestaccess' => new oat\taoDeliveryRdf\model\guest\GuestAccess(),
-        'proctoringDelivery' => new oat\taoProctoring\model\entrypoint\ProctoringDeliveryServer(),
+        'proctoringDelivery' => new EntrypointMock2(),
     ),
     'postlogin' => array(
         'deliveryServer',
