f2ddad4a250dbf57d527ad82eeaadfb974880592
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2019-06-05 10:01:32 +0200

    version bump

--- manifest.php
+++ manifest.php
@@ -53,7 +53,7 @@ return array(
     'label' => 'TAO Base',
     'description' => 'TAO meta-extension',
     'license' => 'GPL-2.0',
-    'version' => '36.1.0',
+    'version' => '36.1.1',
     'author' => 'Open Assessment Technologies, CRP Henri Tudor',
     'requires' => array(
         'generis' => '>=11.3.0',
