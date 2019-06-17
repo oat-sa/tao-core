950f78b18e5b2d85508e39dccdc09573efc651ad
siarhei.baradzin <siarhei@taotesting.com>
2019-06-17 15:43:53 +0300

    bump version

--- manifest.php
+++ manifest.php
@@ -53,10 +53,10 @@ return array(
     'label' => 'TAO Base',
     'description' => 'TAO meta-extension',
     'license' => 'GPL-2.0',
-    'version' => '36.3.1',
+    'version' => '36.4.1',
     'author' => 'Open Assessment Technologies, CRP Henri Tudor',
     'requires' => array(
-        'generis' => '>=11.3.0',
+        'generis' => '>=11.5.0',
     ),
     'models' => array(
         'http://www.tao.lu/Ontologies/TAO.rdf',
