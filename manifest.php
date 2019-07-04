e9400a9dafc68743eb9d92e6a08b7fcd23a10263
siwane <camille@taotesting.com>
2019-07-04 16:41:35 +0200

    Add release candidate version 3.3RC02 for tao version

--- manifest.php
+++ manifest.php
@@ -49,7 +49,7 @@ return array(
     'label' => 'TAO Base',
     'description' => 'TAO meta-extension',
     'license' => 'GPL-2.0',
-    'version' => '27.1.3',
+    'version' => '27.1.4',
     'author' => 'Open Assessment Technologies, CRP Henri Tudor',
     'requires' => array(
         'generis' => '>=8.2.2',
@@ -180,9 +180,9 @@ return array(
     ],
     'constants' => array(
         #TAO version number
-        'TAO_VERSION' => '3.3.0-sprint96',
+        'TAO_VERSION' => '3.3.0-RC02',
         #TAO version label
-        'TAO_VERSION_NAME' => '3.3.0-sprint96',
+        'TAO_VERSION_NAME' => '3.3.0-RC02',
         #the name to display
         'PRODUCT_NAME' => 'TAO',
         #TAO release status, use to add specific footer to TAO, available alpha, beta, demo, stable
--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -925,6 +925,6 @@ class Updater extends \common_ext_ExtensionUpdater {
             $this->setVersion('27.0.0');
         }
 
-        $this->skip('27.0.0', '27.1.3');
+        $this->skip('27.0.0', '27.1.4');
     }
 }
