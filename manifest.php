08a4957f57af4322acddfce7eeaa70e4cdf21b72
Martin Nicholson <martin@taotesting.com>
2019-07-19 10:53:27 +0200

    Updated TAO_VERSION

--- manifest.php
+++ manifest.php
@@ -189,9 +189,9 @@ return array(
     ],
     'constants' => array(
         #TAO version number
-        'TAO_VERSION' => '3.3.0-sprint106',
+        'TAO_VERSION' => '3.3.0-sprint107',
         #TAO version label
-        'TAO_VERSION_NAME' => '3.3.0-sprint106',
+        'TAO_VERSION_NAME' => '3.3.0-sprint107',
         #the name to display
         'PRODUCT_NAME' => 'TAO',
         #TAO release status, use to add specific footer to TAO, available alpha, beta, demo, stable
