1e8f1ab1f6c56d7f85cf348f79affd2da864da6d
Aleh Hutnikau <hutnikau@1pt.com>
2019-08-12 19:29:27 +0300

    bump version

--- manifest.php
+++ manifest.php
@@ -52,7 +52,7 @@ return array(
     'label' => 'TAO Base',
     'description' => 'TAO meta-extension',
     'license' => 'GPL-2.0',
-    'version' => '38.6.0',
+    'version' => '38.7.0',
     'author' => 'Open Assessment Technologies, CRP Henri Tudor',
     'requires' => array(
         'generis' => '>=12.1.0',
