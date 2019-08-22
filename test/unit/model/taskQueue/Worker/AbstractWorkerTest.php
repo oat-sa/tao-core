c80d86d3269b0522cc22c9e0d0cd0265adbeace8
Bartlomiej Marszal <bartlomiej@taotesting.com>
2019-08-22 16:11:39 +0200

    AbstractWorkerTest extended

--- test/unit/model/taskQueue/Worker/AbstractWorkerTest.php
+++ test/unit/model/taskQueue/Worker/AbstractWorkerTest.php
@@ -20,24 +20,83 @@
 
 namespace oat\tao\test\unit\model\taskQueue\Worker;
 
+use common_session_Session;
+use common_user_User;
+use core_kernel_classes_Resource;
+use oat\generis\model\data\Ontology;
+use oat\generis\model\user\UserFactoryServiceInterface;
 use oat\generis\test\TestCase;
-
 use common_report_Report as Report;
+use oat\oatbox\log\LoggerService;
+use oat\oatbox\session\SessionService;
+use oat\oatbox\user\User;
 use oat\tao\model\taskQueue\QueuerInterface;
 use oat\tao\model\taskQueue\Task\TaskInterface;
 use oat\tao\model\taskQueue\TaskLogInterface;
 use oat\tao\model\taskQueue\Worker\AbstractWorker;
+use PHPUnit_Framework_MockObject_MockObject;
+use Zend\ServiceManager\ServiceLocatorInterface;
 
 class AbstractWorkerTest extends TestCase
 {
-    /** @var QueuerInterface */
+    /** @var QueuerInterface | PHPUnit_Framework_MockObject_MockObject */
     private $queue;
 
-    /** @var TaskLogInterface */
+    /** @var TaskLogInterface | PHPUnit_Framework_MockObject_MockObject */
     private $taskLog;
 
     /** @var DummyWorker */
     private $subject;
+
+    /**
+     * @var ServiceLocatorInterface|PHPUnit_Framework_MockObject_MockObject
+     */
+    private $serviceLocatorMock;
+
+    /** @var TaskInterface | PHPUnit_Framework_MockObject_MockObject $task */
+    private $taskMock;
+
+    /**
+     * @var SessionService | PHPUnit_Framework_MockObject_MockObject
+     */
+    private $sessionServiceMock;
+
+    /**
+     * @var common_session_Session | PHPUnit_Framework_MockObject_MockObject
+     */
+    private $commonSession;
+    /**
+     * @var User | PHPUnit_Framework_MockObject_MockObject
+     */
+    private $userMock;
+    /**
+     * @var UserFactoryServiceInterface | PHPUnit_Framework_MockObject_MockObject
+     */
+    private $userFactoryServiceMock;
+
+    /**
+     * @var common_user_User | PHPUnit_Framework_MockObject_MockObject
+     */
+    private $commonUserMock;
+
+    /**
+     * @var Ontology | PHPUnit_Framework_MockObject_MockObject
+     */
+    private $modelMock;
+
+    /**
+     * @var core_kernel_classes_Resource | PHPUnit_Framework_MockObject_MockObject
+     */
+    private $userResourceMock;
+    /**
+     * @var LoggerService | PHPUnit_Framework_MockObject_MockObject
+     */
+    private $loggerServiceMock;
+
+    /**
+     * @var common_report_Report | PHPUnit_Framework_MockObject_MockObject
+     */
+    private $reportMock;
 
     protected function setUp()
     {
@@ -46,33 +105,97 @@ class AbstractWorkerTest extends TestCase
         $this->queue = $this->createMock(QueuerInterface::class);
         $this->taskLog = $this->createMock(TaskLogInterface::class);
 
+        $this->sessionServiceMock = $this->createMock(SessionService::class);
+        $this->userFactoryServiceMock = $this->createMock(UserFactoryServiceInterface::class);
+        $this->loggerServiceMock = $this->createMock(LoggerService::class);
+        $this->serviceLocatorMock = $this->getServiceLocatorMock([
+            SessionService::class => $this->sessionServiceMock,
+            UserFactoryServiceInterface::SERVICE_ID => $this->userFactoryServiceMock,
+            LoggerService::SERVICE_ID => $this->loggerServiceMock
+        ]);
+
+        $this->modelMock = $this->createMock(Ontology::class);
+
+        $this->taskMock = $this->createMock(TaskInterface::class);
+
         $this->subject = new DummyWorker($this->queue, $this->taskLog);
+        $this->subject->setServiceLocator($this->serviceLocatorMock);
+        $this->subject->setModel($this->modelMock);
+    }
+
+    public function testProcessTaskInvokeNotReport()
+    {
+        $this->taskMock = $this->getTaskMockCallback();
+        $this->taskLog->method('getStatus')->willReturn(TaskLogInterface::STATUS_RUNNING);
+        $this->taskLog->method('setStatus')->willReturn(1);
+
+        $this->loggerServiceMock->expects($this->once())->method('warning');
+        $this->taskMock->method('__invoke')->willReturn(true);
+
+
+        $result = $this->subject->processTask($this->taskMock);
+
+        $this->assertSame('completed', $result);
     }
 
     public function testProcessCancelledTask()
     {
-       $task = $this->createMock(TaskInterface::class);
-       $task
-           ->method('getId')
-           ->willReturn('id');
+        /** @var TaskInterface | PHPUnit_Framework_MockObject_MockObject $task */
+        $task = $this->createMock(TaskInterface::class);
+        $task
+            ->method('getId')
+            ->willReturn('id');
 
-       $this->taskLog
-           ->expects($this->once())
-           ->method('getStatus')
-           ->with('id')
-           ->willReturn(TaskLogInterface::STATUS_CANCELLED);
-       $this->taskLog
-           ->expects($this->once())
-           ->method('setReport')
-           ->with(
-               'id',
-               Report::createInfo('Task id has been cancelled, message was not processed.'),
-               TaskLogInterface::STATUS_CANCELLED
-           );
+        $this->taskLog
+            ->expects($this->once())
+            ->method('getStatus')
+            ->with('id')
+            ->willReturn(TaskLogInterface::STATUS_CANCELLED);
+        $this->taskLog
+            ->expects($this->once())
+            ->method('setReport')
+            ->with(
+                'id',
+                Report::createInfo('Task id has been cancelled, message was not processed.'),
+                TaskLogInterface::STATUS_CANCELLED
+            );
 
-       $this->queue->expects($this->once())->method('acknowledge')->with($task);
+        $this->queue->expects($this->once())->method('acknowledge')->with($task);
 
-       $this->assertEquals(TaskLogInterface::STATUS_CANCELLED, $this->subject->processTask($task));
+        $this->assertEquals(TaskLogInterface::STATUS_CANCELLED, $this->subject->processTask($task));
+    }
+
+    public function testProcessTaskStartUserSession()
+    {
+        $this->taskMock->method('getOwner')->willReturn('ownerString');
+        $this->commonSession = $this->createMock(common_session_Session::class);
+        $this->sessionServiceMock->method('getCurrentSession')->willReturn($this->commonSession);
+        $this->userMock = $this->createMock(User::class);
+        $this->commonSession->method('getUser')->willReturn($this->userMock);
+        $this->userMock->method('getIdentifier')->willReturn('userIdString');
+        $this->userResourceMock = $this->createMock(core_kernel_classes_Resource::class);
+
+        $this->modelMock->method('getResource')->willReturn($this->userResourceMock);
+        $this->commonUserMock = $this->createMock(common_user_User::class);
+        $this->userFactoryServiceMock->method('createUser')->willReturn($this->commonUserMock);
+
+        $this->loggerServiceMock->expects($this->exactly(2))->method('info');
+        $this->sessionServiceMock->expects($this->once())->method('setSession');
+
+        $result = $this->subject->processTask($this->taskMock);
+        $this->assertSame('unknown', $result);
+    }
+
+    /**
+     * @return TaskInterface | PHPUnit_Framework_MockObject_MockObject
+     */
+    private function getTaskMockCallback() {
+        $mock = $this
+            ->getMockBuilder(TaskInterface::class)
+            ->disableOriginalConstructor()
+            ->setMethods(['__invoke', 'getStatus'])
+            ->getMockForAbstractClass();
+        return $mock;
     }
 }
 
@@ -82,4 +205,4 @@ class DummyWorker extends AbstractWorker
     {
         return null;
     }
-};
+}
