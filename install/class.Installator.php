5c7161d6f7da1ef71a1907efae2fde2fd87e58a6
Julien Sébire <julien@taotesting.com>
2019-09-02 11:58:21 +0200

    Removed Spanner driver detection for aws.

--- install/class.Installator.php
+++ install/class.Installator.php
@@ -20,9 +20,6 @@
  *               2013-2017 (update and modification) Open Assessment Technologies SA (under the project TAO-PRODUCT);
  */
 
-use OAT\Library\DBALSpanner\SpannerConnection;
-use OAT\Library\DBALSpanner\SpannerDriver;
-use OAT\Library\DBALSpanner\SpannerPlatform;
 use oat\tao\helpers\InstallHelper;
 use oat\oatbox\install\Installer;
 use oat\oatbox\service\ServiceManager;
@@ -184,14 +181,6 @@ class tao_install_Installator {
 				$dbConnectionParams['portability'] = \Doctrine\DBAL\Portability\Connection::PORTABILITY_ALL;
 				$dbConnectionParams['fetch_case'] = PDO::CASE_LOWER;
 			}
-            if($installData['db_driver'] == SpannerDriver::DRIVER_NAME) {
-                $dbConnectionParams = [
-                    'dbname' => $installData['db_name'],
-                    'instance' => $installData['db_host'],
-                    'driverClass' => SpannerDriver::class,
-                    'platform' => new SpannerPlatform(),
-                ];
-            }
 
 			$dbCreator = new tao_install_utils_DbalDbCreator($dbConnectionParams);
 			
