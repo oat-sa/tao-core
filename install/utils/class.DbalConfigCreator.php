cf62dbdc470e0ea7c529b7fb8aa23bd5fe07389a
Julien Sébire <julien@taotesting.com>
2019-10-14 13:42:55 +0200

    Fixed database configurator.

--- install/utils/class.DbalConfigCreator.php
+++ install/utils/class.DbalConfigCreator.php
@@ -28,10 +28,13 @@ class tao_install_utils_DbalConfigCreator {
 
     public function createDbalConfig($installData)
     {
+        // Oracle driver stores db host as db name.
         if($installData['db_driver'] == 'pdo_oci'){
             $installData['db_name'] = $installData['db_host'];
             $installData['db_host'] = '';
         }
+
+        // Default configuration.
         $dbConnectionParams = array(
             'driver' => $installData['db_driver'],
             'host' => $installData['db_host'],
@@ -39,23 +42,31 @@ class tao_install_utils_DbalConfigCreator {
             'user' => $installData['db_user'],
             'password' => $installData['db_pass'],
         );
+        
+        // Split host and port if port is present.
         $hostParts = explode(':', $installData['db_host']);
         if (count($hostParts) == 2) {
             $dbConnectionParams['host'] = $hostParts[0];
             $dbConnectionParams['port'] = $hostParts[1];
         }
-        if($installData['db_driver'] == 'pdo_mysql'){
-            $dbConnectionParams['dbname'] = '';
-        }
+        
+        // Oracle driver uses portability construct
         if($installData['db_driver'] == 'pdo_oci'){
             $dbConnectionParams['wrapperClass'] = 'Doctrine\DBAL\Portability\Connection';
             $dbConnectionParams['portability'] = \Doctrine\DBAL\Portability\Connection::PORTABILITY_ALL;
             $dbConnectionParams['fetch_case'] = PDO::CASE_LOWER;
         }
-        // reset db name for mysql
-        if ($installData['db_driver'] == 'pdo_mysql'){
-            $dbConnectionParams['dbname'] = $installData['db_name'];
+        
+        // Spanner driver is not registere in DBAL, so needs the correct classes for driver and platform.
+        if($installData['db_driver'] == SpannerDriver::DRIVER_NAME) {
+            $dbConnectionParams = [
+                'dbname' => $installData['db_name'],
+                'instance' => $installData['db_host'],
+                'driverClass' => SpannerDriver::class,
+                'platform' => new SpannerPlatform(),
+            ];
         }
+
         return array(
             'driver' => 'dbal',
             'connection' => $dbConnectionParams,
