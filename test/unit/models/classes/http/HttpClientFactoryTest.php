420e0c1ec6e056c97e646cfa411a7cac0258f79d
Julien Sébire <julien@taotesting.com>
2019-11-21 20:41:52 +0100

    Completed HttpClientFactory test.

--- test/unit/models/classes/http/HttpClientFactoryTest.php
+++ test/unit/models/classes/http/HttpClientFactoryTest.php
@@ -17,34 +17,36 @@
  * Copyright (c) 2019 (original work) Open Assessment Technologies SA;
  */
 
-namespace oat\tao\model\http;
+namespace oat\tao\test\unit\model\http;
 
 use GuzzleHttp\Client;
+use GuzzleHttp\HandlerStack;
 use oat\generis\test\TestCase;
 use OAT\Library\CorrelationIds\Registry\CorrelationIdsRegistryInterface;
+use oat\tao\model\http\HttpClientFactory;
 use oat\tao\model\mvc\CorrelationIdsService;
 
 class HttpClientFactoryTest extends TestCase
 {
     public function testCreate()
     {
-        $options = ['key' => 'value'];
+        $key = 'one key';
+        $value = 'one value';
+        $options = [$key => $value];
 
-        $registry = $this->getMock(CorrelationIdsRegistryInterface::class);
-        
-        $correlationIdsService = $this->getMockBuilder(CorrelationIdsService::class)
-            ->disableOriginalConstructor()
-            ->setMethods(['getRegistry'])
-            ->getMock();
-        $correlationIdsService->method('getRegistry')->willReturn($registry);
-        
+        $registry = $this->createMock(CorrelationIdsRegistryInterface::class);
+        $correlationIdsService = $this->createConfiguredMock(CorrelationIdsService::class, ['getRegistry' => $registry]);
         $serviceLocator = $this->getServiceLocatorMock([CorrelationIdsService::class => $correlationIdsService]);
 
         $subject = new HttpClientFactory();
         $subject->setServiceLocator($serviceLocator);
 
         $client = $subject->create($options);
-        
+
         $this->assertInstanceOf(Client::class, $client);
+        /** @var Client $client */
+
+        $this->assertInstanceOf(HandlerStack::class, $client->getConfig('handler'));
+        $this->assertEquals($value, $client->getConfig($key));
     }
 }
