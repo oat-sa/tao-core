c99de32b085012bc38207da847580f8c49d34928
Juan Luis <juanluis.gutierrezdossantos@taotesting.com>
2020-01-27 18:32:20 +0100

    Bump version

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -1228,6 +1228,6 @@ class Updater extends \common_ext_ExtensionUpdater {
             $this->setVersion('38.11.1');
         }
 
-        $this->skip('38.11.1', '38.13.3');
+        $this->skip('38.11.1', '38.13.3.1');
     }
 }
