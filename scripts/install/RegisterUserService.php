b45bf18eb16ede3ea0d719da0faa89894c5f7829
Joel Bout <joel@taotesting.com>
2020-01-29 15:02:25 +0100

    Add Generis UserService

---
+++ scripts/install/RegisterUserService.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2020 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
+ *
+ */
+
+namespace oat\tao\scripts\install;
+
+use oat\oatbox\extension\InstallAction;
+use oat\tao\model\taskQueue\Queue;
+use oat\tao\model\taskQueue\Queue\Broker\InMemoryQueueBroker;
+use oat\tao\model\taskQueue\Queue\TaskSelector\WeightStrategy;
+use oat\tao\model\taskQueue\QueueDispatcher;
+use oat\tao\model\taskQueue\QueueDispatcherInterface;
+use oat\tao\model\taskQueue\TaskLog;
+use oat\tao\model\taskQueue\TaskLogInterface;
+use oat\oatbox\user\UserService;
+use oat\tao\model\user\GenerisUserService;
+
+class RegisterUserServices extends InstallAction
+{
+    public function __invoke($params)
+    {
+        if (!$this->getServiceManager()->has(UserService::SERVICE_ID)) {
+            $this->getServiceManager()->register(UserService::SERVICE_ID, new GenerisUserService());
+        }
+        return \common_report_Report::createSuccess('Task Queue service successfully registered.');
+    }
+}
