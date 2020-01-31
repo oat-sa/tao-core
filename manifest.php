bceee3b0643c7dd8a77d28975d2cc1e795569ed7
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2020-01-31 10:50:29 +0100

    udpate tao version

--- manifest.php
+++ manifest.php
@@ -192,9 +192,9 @@ return [
     ],
     'constants' => [
         #TAO version number
-        'TAO_VERSION' => '3.4.0-sprint120',
+        'TAO_VERSION' => '3.4.0-sprint121',
         #TAO version label
-        'TAO_VERSION_NAME' => '3.4.0-sprint120',
+        'TAO_VERSION_NAME' => '3.4.0-sprint121',
         #the name to display
         'PRODUCT_NAME' => 'TAO',
         #TAO release status, use to add specific footer to TAO, available alpha, beta, demo, stable
