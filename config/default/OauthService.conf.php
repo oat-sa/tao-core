c244128a1ad4eac0857972db616bb89cf21f7f3d
Ivan Klimchuk <ivan.klimchuk@1pt.com>
2020-02-14 02:01:52 +0300

    Set default config with no lockout implementation

--- config/default/OauthService.conf.php
+++ config/default/OauthService.conf.php
@@ -4,6 +4,7 @@
  * Default config header created during install
  */
 
+use oat\tao\model\oauth\lockout\NoLockout;
 use oat\tao\model\oauth\OauthService;
 use oat\tao\model\oauth\DataStore;
 use oat\tao\model\oauth\nonce\NoNonce;
@@ -11,5 +12,6 @@ use oat\tao\model\oauth\nonce\NoNonce;
 return new OauthService([
     'store' => new DataStore([
         'nonce' => new NoNonce()
-    ])
+    ]),
+    'lockout' => new NoLockout()
 ]);
