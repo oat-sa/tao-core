55ca88ff81477723b6a4641d702ef3c657da211d
Bartlomiej Marszal <bartlomiej@taotesting.com>
2020-03-03 12:51:29 +0100

    class.DbalDbCreator refactor

--- install/utils/class.DbalDbCreator.php
+++ install/utils/class.DbalDbCreator.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -22,323 +23,262 @@
  *
  */
 
+use Doctrine\DBAL\Configuration;
 use Doctrine\DBAL\DBALException;
+use Doctrine\DBAL\Driver\Connection;
+use Doctrine\DBAL\DriverManager;
+use Doctrine\DBAL\Schema\AbstractSchemaManager;
+use Doctrine\DBAL\Schema\Schema;
 
-class tao_install_utils_DbalDbCreator {
-
-
-    /**
-     * @var array
-     */
+class tao_install_utils_DbalDbCreator
+{
+    /** @var array */
     private $dbConfiguration;
-    /**
-     * @var Doctrine\DBAL\Connection
-     */
-    private $connection;
-	/**
-	 * @var Doctrine\DBAL\Schema\Schema
-	 */
-	private $schema = null;
-	
 
-	/**
-	 * @author "Lionel Lecaque, <lionel@taotesting.com>"
-	 */
-	private function createMysqlStatementsIndex(){
-	    $index = new \Doctrine\DBAL\Schema\Index('k_po',array('predicate(164)','object(164)'));
-	    $table = new \Doctrine\DBAL\Schema\Table('statements');
-	    $this->getSchemaManager()->createIndex($index,$table);
-	    $index = new \Doctrine\DBAL\Schema\Index('k_sp',array('predicate(164)','subject(164)'));
-	    $this->getSchemaManager()->createIndex($index,$table);
-	}
-	
-	
-	
+    /** @var Connection */
+    private $connection;
+
+    /** @var Schema */
+	private $schema = null;
+
     /**
-     * @author "Lionel Lecaque, <lionel@taotesting.com>"
      * @param array $params
      * @throws tao_install_utils_Exception
      */
-    public function __construct($params){
-   		try{
+    public function __construct(array $params)
+    {
+        try {
             $this->connection = $this->buildDbalConnection($params);
             $this->dbConfiguration = $params;
             $this->buildSchema();
-
-   		}
-   		catch(Exception $e){
-   			$this->connection = null;
+        } catch (Exception $e) {
+            $this->connection = null;
             common_Logger::e($e->getMessage() . $e->getTraceAsString());
-   			throw new tao_install_utils_Exception('Unable to connect to the database ' . $params['dbname'] . ' with the provided credentials: ' . $e->getMessage());
-   		}
-   	}
-
-    /**
-     * @author "Lionel Lecaque, <lionel@taotesting.com>"
-     * @param string $dbName
-     */
-    public function dbExists($dbName){
-        $sm = $this->getSchemaManager();
-		common_Logger::d('Check if database with name \'' .$dbName. '\' exists for driver ' . $this->dbConfiguration['driver']);
-        if($this->dbConfiguration['driver'] == 'pdo_oci'){
-        	common_Logger::d('Oracle special query dbExist');
-        	return in_array(strtoupper($dbName),$sm->listDatabases());
-        }
-        else {
-        	return in_array($dbName,$sm->listDatabases());
+            throw new tao_install_utils_Exception('Unable to connect to the database ' . $params['dbname'] . ' with the provided credentials: ' . $e->getMessage());
         }
     }
+
     /**
-     * @author "Lionel Lecaque, <lionel@taotesting.com>"
-     * @param string $tableName
+     * @throws DBALException
      */
-    public function tableExists($tableName){
+    private function createMysqlStatementsIndex(): void
+    {
+        $index = new \Doctrine\DBAL\Schema\Index('k_po', ['predicate(164)', 'object(164)']);
+        $table = new \Doctrine\DBAL\Schema\Table('statements');
+        $this->getSchemaManager()->createIndex($index, $table);
+        $index = new \Doctrine\DBAL\Schema\Index('k_sp', ['predicate(164)', 'subject(164)']);
+        $this->getSchemaManager()->createIndex($index, $table);
+    }
+
+    public function dbExists(string $dbName): bool
+    {
+        $sm = $this->getSchemaManager();
+        common_Logger::d('Check if database with name \'' . $dbName . '\' exists for driver ' . $this->dbConfiguration['driver']);
+        if ($this->dbConfiguration['driver'] === 'pdo_oci') {
+            common_Logger::d('Oracle special query dbExist');
+            return in_array(strtoupper($dbName), $sm->listDatabases());
+        }
+
+        return in_array($dbName, $sm->listDatabases());
+    }
+
+    public function tableExists(string $tableName){
     	$sm = $this->getSchemaManager();
     	return $sm->tableExists($tableName);
     }
-    
-    
-    /**
-     * @author "Lionel Lecaque, <lionel@taotesting.com>"
-     * @param string $name
-     */
-    public function setDatabase($name){
+
+    public function setDatabase(string $name): void
+    {
         $this->connection->close();
         common_Logger::d('Switch to database ' . $name);
         $this->dbConfiguration['dbname'] = $name;
         $this->connection = $this->buildDbalConnection($this->dbConfiguration);
-        
     }
 
     /**
-     * @param $params
-     * @return \Doctrine\DBAL\Connection
+     * @throws DBALException
      */
-    private function buildDbalConnection($params)
+    private function buildDbalConnection(array $params): Connection
     {
-        $config = new Doctrine\DBAL\Configuration();
-        return  \Doctrine\DBAL\DriverManager::getConnection($params, $config);
+        $config = new Configuration();
+        return  DriverManager::getConnection($params, $config);
     }
 
-    
     /**
-     * @author "Lionel Lecaque, <lionel@taotesting.com>"
+     * @return string[]
      */
-    public function listDatabases(){
-    	$sm = $this->getSchemaManager();
-    	return $sm->listDatabases();
+    public function listDatabases(): array
+    {
+        $sm = $this->getSchemaManager();
+        return $sm->listDatabases();
 
     }
-    
-    /**
-     * @author "Lionel Lecaque, <lionel@taotesting.com>"
-     */
-    private function createModelsSchema(){
-	    $table = $this->schema->createTable('models');
-	    $table->addColumn('modelid', 'string', ['length' => 25, 'notnull' => true]);
-	    $table->addColumn('modeluri', 'string', ['length' => 255]);
-        $table->setPrimaryKey(array('modelid'));
-        $table->addOption('engine' , 'MyISAM');
-    }
-    /**
-     * @author 'Lionel Lecaque, <lionel@taotesting.com>'
-     */
-    private function createStatementsSchena(){
-    	$table = $this->schema->createTable('statements');
-    	$table->addColumn('id', 'string', ['length' => 25, 'notnull' => true]);
 
-    	$table->addColumn('modelid', 'string', ['length' => 25, 'notnull' => true]);
-    	$table->addColumn('subject', 'string', ['length' => 255]);
-    	$table->addColumn('predicate', 'string', ['length' => 255]);
-    	if($this->dbConfiguration['driver'] == 'pdo_oci' ) {
-    		$table->addColumn('object', 'string', ['length' => 4000]);
-    	} else {
-    		$table->addColumn('object', 'text', []);
-    	}
+    private function createModelsSchema(): void
+    {
+        $table = $this->schema->createTable('models');
+        $table->addColumn('modelid', 'string', ['length' => 25, 'notnull' => true]);
+        $table->addColumn('modeluri', 'string', ['length' => 255]);
+        $table->setPrimaryKey(['modelid']);
+        $table->addOption('engine', 'MyISAM');
+    }
+
+    private function createStatementsSchena(): void
+    {
+        $table = $this->schema->createTable('statements');
+        $table->addColumn('id', 'string', ['length' => 25, 'notnull' => true]);
+
+        $table->addColumn('modelid', 'string', ['length' => 25, 'notnull' => true]);
+        $table->addColumn('subject', 'string', ['length' => 255]);
+        $table->addColumn('predicate', 'string', ['length' => 255]);
+        if ($this->dbConfiguration['driver'] === 'pdo_oci') {
+            $table->addColumn('object', 'string', ['length' => 4000]);
+        } else {
+            $table->addColumn('object', 'text', []);
+        }
         $table->addColumn('l_language', 'string', ['length' => 255]);
 
-    	$table->addColumn('author', 'string', ['length' => 255]);
-    	$table->addColumn('epoch', 'string' , ['notnull' => true]);
+        $table->addColumn('author', 'string', ['length' => 255]);
+        $table->addColumn('epoch', 'string', ['notnull' => true]);
 
-    	$table->setPrimaryKey(['id']);
+        $table->setPrimaryKey(['id']);
 
-    	if($this->dbConfiguration['driver'] != 'pdo_mysql'){
-    	   	$table->addIndex(['subject', 'predicate'], 'k_sp');
-    		common_Logger::d('driver is ' . $this->dbConfiguration['driver']);
-    	   	if($this->dbConfiguration['driver'] != 'pdo_sqlsrv' 
-    	   			&& $this->dbConfiguration['driver'] != 'pdo_oci'){
-    	   		$table->addIndex(['predicate', 'object'], 'k_po');
-    	   	}
-    	}
+        if ($this->dbConfiguration['driver'] !== 'pdo_mysql') {
+            $table->addIndex(['subject', 'predicate'], 'k_sp');
+            common_Logger::d('driver is ' . $this->dbConfiguration['driver']);
+            if ($this->dbConfiguration['driver'] !== 'pdo_sqlsrv'
+                && $this->dbConfiguration['driver'] !== 'pdo_oci') {
+                $table->addIndex(['predicate', 'object'], 'k_po');
+            }
+        }
 
-        $table->addOption('engine' , 'MyISAM');
-    }
-    
-
-    /**
-     * 
-     * @author "Lionel Lecaque, <lionel@taotesting.com>"
-     */
-    private function createKeyValueStoreTable(){
-        $table = $this->schema->createTable("kv_store");
-        $table->addColumn('kv_id',"string",array("notnull" => null,"length" => 255));
-        $table->addColumn('kv_value',"text",array("notnull" => null));
-        $table->addColumn('kv_time',"integer",array("notnull" => null,"length" => 30));
-        $table->setPrimaryKey(array("kv_id"));
-        $table->addOption('engine' , 'MyISAM');
+        $table->addOption('engine', 'MyISAM');
     }
 
-    
-    /**
-     * @author "Lionel Lecaque, <lionel@taotesting.com>"
-     */
-    private function createSequenceUriProvider(){
-    	$table = $this->schema->createTable("sequence_uri_provider");
-    	$table->addColumn("uri_sequence", "integer",array("notnull" => true,"autoincrement" => true));
-    	$table->addOption('engine' , 'MyISAM');
-    	$table->setPrimaryKey(array("uri_sequence"));
-    	
-    	//$this->schema->createSequence('sequence_uri_provider_uri_sequence_seq');
+    private function createKeyValueStoreTable(): void
+    {
+        $table = $this->schema->createTable('kv_store');
+        $table->addColumn('kv_id', 'string', ['notnull' => null, 'length' => 255]);
+        $table->addColumn('kv_value', 'text', ['notnull' => null]);
+        $table->addColumn('kv_time', 'integer', ['notnull' => null, 'length' => 30]);
+        $table->setPrimaryKey(['kv_id']);
+        $table->addOption('engine', 'MyISAM');
     }
-    
-    /**
-     * @author "Lionel Lecaque, <lionel@taotesting.com>"
-     * @return Doctrine\DBAL\Schema\Schema
-     */
-    private  function buildSchema(){
-    	if($this->schema == null){
 
-    		$this->schema = new \Doctrine\DBAL\Schema\Schema() ;
-			$this->createModelsSchema();
-			$this->createStatementsSchena();
-// 			$this->createResourceToTable();
-// 			$this->createResourceHasClass();
-// 			$this->createClassToTable();
-// 			$this->createClassAdditionalProp();
-			$this->createSequenceUriProvider();
-			$this->createKeyValueStoreTable();
-    	}
-    	return $this->schema;
-    	
-    	
+    private function createSequenceUriProvider(): void
+    {
+        $table = $this->schema->createTable('sequence_uri_provider');
+        $table->addColumn('uri_sequence', 'integer', ['notnull' => true, 'autoincrement' => true]);
+        $table->addOption('engine', 'MyISAM');
+        $table->setPrimaryKey(['uri_sequence']);
     }
-    
+
+    private function buildSchema(): Schema
+    {
+        if ($this->schema === null) {
+            $this->schema = new Schema();
+            $this->createModelsSchema();
+            $this->createStatementsSchena();
+            $this->createSequenceUriProvider();
+            $this->createKeyValueStoreTable();
+        }
+        return $this->schema;
+    }
+
     /**
-     * @author "Lionel Lecaque, <lionel@taotesting.com>"
-     * @param unknown $file
+     * @throws tao_install_utils_SQLParsingException
      */
-    public function loadProc($file){
-        
-        $procedureCreator = new tao_install_utils_ProceduresCreator($this->dbConfiguration['driver'],$this->connection);
+    public function loadProc($file): void
+    {
+        $procedureCreator = new tao_install_utils_ProceduresCreator($this->dbConfiguration['driver'], $this->connection);
         $procedureCreator->load($file);
-
     }
-    
 
-    public function addModel($modelId,$namespace){
+    public function addModel($modelId, $namespace)
+    {
         common_Logger::d('add modelid :' . $k . ' with NS :' . $v);
-        $this->connection->insert("models" , array('modelid' => $k , 'modeluri' => $v ));
+        $this->connection->insert("models", ['modelid' => $k, 'modeluri' => $v]);
     }
-    
-    /**
-     * @author "Lionel Lecaque, <lionel@taotesting.com>"
-     */
-    public function addModels(){
-        foreach ($this->modelArray as $k => $v){
+
+    public function addModels(): void
+    {
+        foreach ($this->modelArray as $k => $v) {
             $this->addModel();
         }
-    } 
-    
-    
-    /**
-     * @author "Lionel Lecaque, <lionel@taotesting.com>"
-     */
-    public function removeGenerisUser(){
-       $this->connection->executeUpdate("DELETE FROM statements WHERE subject = ?" , array('http://www.tao.lu/Ontologies/TAO.rdf#installator'));    
     }
-    
-    /**
-     * @author "Lionel Lecaque, <lionel@taotesting.com>"
-     */
-    public function initTaoDataBase(){
-    	$platform = $this->connection->getDatabasePlatform();
-    	$queries = $this->schema->toSql($platform);
-    	foreach ($queries as $query){
-    	   	$this->connection->executeUpdate($query);
-    	}
-    	if($this->dbConfiguration['driver'] == 'pdo_mysql'){
-    	    $this->createMysqlStatementsIndex();
-    	}
+
+    public function removeGenerisUser(): void
+    {
+        $this->connection->executeUpdate("DELETE FROM statements WHERE subject = ?", ['http://www.tao.lu/Ontologies/TAO.rdf#installator']);
     }
-    
+
     /**
-     * @author "Lionel Lecaque, <lionel@taotesting.com>"
+     * @throws DBALException
      */
-    public function destroyTaoDatabase(){
-    	$platform = $this->connection->getDatabasePlatform();
-    	$queries = $this->schema->toDropSql($platform);
-    	foreach ($queries as $query){
-    		$this->connection->executeUpdate($query);
-    	}
-    	//drop sequence
-    	$sm = $this->getSchemaManager();
-    	$sequences = $sm->listSequences();
-    	foreach($sequences as $name){
-    		$sm->dropSequence($name);
-    	}
-    	
-    	
+    public function initTaoDataBase(): void
+    {
+        $platform = $this->connection->getDatabasePlatform();
+        $queries = $this->schema->toSql($platform);
+        foreach ($queries as $query) {
+            $this->connection->executeUpdate($query);
+        }
+        if ($this->dbConfiguration['driver'] === 'pdo_mysql') {
+            $this->createMysqlStatementsIndex();
+        }
     }
-    
-    /**
-     * @author "Lionel Lecaque, <lionel@taotesting.com>"
-     */
-    public function listTables(){
-    	$sm = $this->getSchemaManager();
-    	return $sm->listTableNames();
-    
-    }
-    
-    /**
-     * @author "Lionel Lecaque, <lionel@taotesting.com>"
-     * @param unknown $database
-     */
-    public function dropDatabase($database){
+
+    public function destroyTaoDatabase(): void
+    {
+        $platform = $this->connection->getDatabasePlatform();
+        $queries = $this->schema->toDropSql($platform);
+        foreach ($queries as $query) {
+            $this->connection->executeUpdate($query);
+        }
+        //drop sequence
         $sm = $this->getSchemaManager();
-        return $sm->dropDatabase($database);
+        foreach ($sequences as $name) {
+            $sm->dropSequence($name);
+        }
+    }
+
+    public function listTables(): void
+    {
+        $sm = $this->getSchemaManager();
+        return $sm->listTableNames();
 
     }
 
-    /**
-     * 
-     * @author Lionel Lecaque, lionel@taotesting.com
-     * @param string $database
-     */
-    public function createDatabase($database){
+    public function dropDatabase($database): void
+    {
+        $sm = $this->getSchemaManager();
+        $sm->dropDatabase($database);
+    }
+
+    public function createDatabase(string $database): void
+    {
         $sm = $this->getSchemaManager();
         $escapedName = $sm->getDatabasePlatform()->quoteIdentifier($database);
-        return $sm->createDatabase($escapedName);
+        $sm->createDatabase($escapedName);
     }
-    
+
     /**
-     * 
-     * @author Lionel Lecaque, lionel@taotesting.com
+     * @throws common_exception_Error
      */
-    public function cleanDb(){
+    public function cleanDb(): void
+    {
         $sm = $this->getSchemaManager();
-        $platform = $this->connection->getDatabasePlatform();
         $tables = $sm->listTableNames();
-        
+
         while (!empty($tables)) {
             $oldCount = count($tables);
-            foreach(array_keys($tables) as $id){
+            foreach (array_keys($tables) as $id) {
                 $name = $tables[$id];
                 try {
                     $sm->dropTable($name);
-                    common_Logger::d('Droped table: '  . $name);
+                    common_Logger::d('Droped table: ' . $name);
                     unset($tables[$id]);
                 } catch (DBALException $e) {
-                    common_Logger::w('Failed to drop: '  . $name);
+                    common_Logger::w('Failed to drop: ' . $name);
                 }
             }
             if (count($tables) == $oldCount) {
@@ -347,14 +287,8 @@ class tao_install_utils_DbalDbCreator {
         }
     }
 
-    /**
-     * @return Doctrine\DBAL\Schema\AbstractSchemaManager
-     */
-    private function getSchemaManager()
+    private function getSchemaManager(): AbstractSchemaManager
     {
         return $this->connection->getSchemaManager();
-
     }
-
-
 }
