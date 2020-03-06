58430194a1c07f2d8e2a9ddbe4c26f6c0418afa3
Sergei Mikhailov <sergei.mikhailov@taotesting.com>
2020-03-06 14:52:34 +0100

    UDI-76 fix(test): initialize required global variables in `FormContainerTest`

--- test/unit/helpers/form/FormContainerTest.php
+++ test/unit/helpers/form/FormContainerTest.php
@@ -68,6 +68,14 @@ class FormContainerTest extends GenerisTestCase
     }
 
     /**
+     * @before
+     */
+    public function initGlobalVariables(): void
+    {
+        $_SERVER['REQUEST_URI'] = '';
+    }
+
+    /**
      * @dataProvider dataProvider
      *
      * @param string $expected
