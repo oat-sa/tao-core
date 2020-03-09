fa07ba7264dd89c9ea03d748ea0711a21ab7bbb1
Sergei Mikhailov <sergei.mikhailov@taotesting.com>
2020-03-09 09:26:25 +0100

    TBD-7 chore: set strict types to `ActionProtector`

--- models/classes/security/ActionProtector.php
+++ models/classes/security/ActionProtector.php
@@ -1,4 +1,4 @@
-<?php
+<?php declare(strict_types=1);
 
 /**
  * This program is free software; you can redistribute it and/or
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
- * Copyright (c) 2015 (original work) Open Assessment Technologies SA;
+ * Copyright (c) 2015 – 2020 (original work) Open Assessment Technologies SA;
  *
  */
 
@@ -32,15 +32,12 @@ use oat\tao\model\settings\CspHeaderSettingsInterface;
  */
 class ActionProtector extends ConfigurableService
 {
-
-    const SERVICE_ID = 'tao/actionProtection';
+    public const SERVICE_ID = 'tao/actionProtection';
 
     /**
      * Set the header that defines which sources are allowed to embed the pages.
-     *
-     * @return void
      */
-    public function setFrameAncestorsHeader()
+    public function setFrameAncestorsHeader(): void
     {
         /** @var SettingsStorage $settingsStorage */
         $settingsStorage = $this->getServiceLocator()->get(SettingsStorage::SERVICE_ID);
@@ -51,7 +48,7 @@ class ActionProtector extends ConfigurableService
         }
 
         // Wrap directives in quotes
-        if (in_array($whitelistedSources, ['self', 'none'])) {
+        if (in_array($whitelistedSources, ['self', 'none'], true)) {
             $whitelistedSources = ["'" . $whitelistedSources . "'"];
         }
 
@@ -59,13 +56,9 @@ class ActionProtector extends ConfigurableService
             $whitelistedSources = json_decode($settingsStorage->get(CspHeaderSettingsInterface::CSP_HEADER_LIST), true);
         }
 
-        if (!is_array($whitelistedSources)) {
-            $whitelistedSources = [$whitelistedSources];
-        }
-
         header(sprintf(
             'Content-Security-Policy: frame-ancestors %s',
-            implode(' ', $whitelistedSources)
+            implode(' ', (array)$whitelistedSources)
         ));
     }
 }
