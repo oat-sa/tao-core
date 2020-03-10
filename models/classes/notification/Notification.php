bdf442d15abfade635cbd4571848027277b22510
Bartłomiej Marszał <bartlomiej@taotesting.com>
2020-03-10 15:14:41 +0100

    Update models/classes/notification/Notification.php
    fix copyright date

--- models/classes/notification/Notification.php
+++ models/classes/notification/Notification.php
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
- * Copyright (c) 2017 (original work) Open Assessment Technologies SA;
+ * Copyright (c) 2020 (original work) Open Assessment Technologies SA;
  *
  */
 
