1175372ffe2d74ac17ffde9d5afa8eaa8d5dd5df
Sergii <sergii@taotesting.com>
2020-03-10 15:54:13 +0100

    copyright added

--- models/classes/resources/SecureResourceService.php
+++ models/classes/resources/SecureResourceService.php
@@ -2,6 +2,25 @@
 
 declare(strict_types=1);
 
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2013-2020 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
+ *
+ */
+
 namespace oat\tao\model\resources;
 
 use common_exception_Error;
@@ -22,7 +41,7 @@ class SecureResourceService extends ConfigurableService
      * @return core_kernel_classes_Resource[]
      * @throws common_exception_Error
      */
-    public function getAllChildren(core_kernel_classes_Class $resource): array
+    public function getAllChildren(core_kernel_classes_Class $resource)
     {
         $children = $resource->getInstances(true);
         $permissionService = $this->getPermissionProvider();
