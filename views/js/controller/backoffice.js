ec9b6f4e86a4d2abe2be0797529c6a58ed902458
Juan Luis Gutierrez Dos Santos <juanluis.gutierrezdossantos@taotesting.com>
2020-03-23 11:09:27 +0100

    Dont display error on cancelled

--- views/js/controller/backoffice.js
+++ views/js/controller/backoffice.js
@@ -64,6 +64,9 @@ define([
             //intercept errors
             //TODO this should belongs to the Router
             $doc.ajaxError(function (event, request, settings) {
+                // Request was manually aborted, isn't a error
+                if (request.statusText === 'abort') return;
+
                 var ajaxResponse;
                 var errorMessage = __('Unknown Error');
 
