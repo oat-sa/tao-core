b24c6ee1fa43d24bfc98f713739ca63c1361b1c5
Sergei Mikhailov <sergei.mikhailov@taotesting.com>
2020-04-02 12:12:00 +0200

    NFER-66 chore: extract a lockout persistence layer location from a storage implementation into `LockoutStorageAbstract` NFER-66 chore: inject a key-value persistence ID into `KvLockoutStorage` during the service installation

--- models/classes/oauth/lockout/storage/LockoutStorageAbstract.php
+++ models/classes/oauth/lockout/storage/LockoutStorageAbstract.php
@@ -15,22 +15,35 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
- * Copyright (c) 2020 (original work) (update and modification) Open Assessment Technologies SA (under the project TAO-PRODUCT)
+ * Copyright (c) 2020 (original work) Open Assessment Technologies SA;
  */
 
 declare(strict_types=1);
 
 namespace oat\tao\model\oauth\lockout\storage;
 
-class KvLockoutStorage extends LockoutStorageAbstract
+use common_persistence_Persistence as Persistence;
+use oat\generis\persistence\PersistenceManager;
+use oat\oatbox\service\ConfigurableService;
+
+abstract class LockoutStorageAbstract extends ConfigurableService implements LockoutStorageInterface
 {
-    public function store(string $ip, int $ttl = 0)
+    /** @var Persistence */
+    private $persistence;
+
+    public function getPersistenceId(): string
     {
-        return true;
+        return $this->getOption(self::OPTION_PERSISTENCE);
     }
 
-    public function getFailedAttempts(string $ip, int $timeout): int
+    protected function getPersistence(): Persistence
     {
-        return 0;
+        if ($this->persistence === null) {
+            $this->persistence = $this->getServiceLocator()
+                ->get(PersistenceManager::SERVICE_ID)
+                ->getPersistenceById($this->getPersistenceId());
+        }
+
+        return $this->persistence;
     }
 }
--- scripts/install/SetUpOAuthLockoutService.php
+++ scripts/install/SetUpOAuthLockoutService.php
@@ -29,7 +29,6 @@ use oat\tao\model\oauth\lockout\IPLockout;
 use oat\tao\model\oauth\lockout\LockoutInterface;
 use oat\tao\model\oauth\lockout\NoLockout;
 use oat\tao\model\oauth\lockout\storage\KvLockoutStorage;
-use oat\tao\model\oauth\lockout\storage\LockoutStorageInterface;
 use oat\tao\model\oauth\lockout\storage\RdsLockoutStorage;
 use oat\tao\model\oauth\OauthService;
 
@@ -154,7 +153,7 @@ class SetUpOAuthLockoutService extends ScriptAction
         $options[IPLockout::OPTION_IP_FACTORY] = new IPFactory();
         switch ($this->getOption(self::OPT_STORAGE)) {
             case self::STORAGE_KV:
-                $options[IPLockout::OPTION_LOCKOUT_STORAGE] = new KvLockoutStorage();
+                $options[IPLockout::OPTION_LOCKOUT_STORAGE] = new KvLockoutStorage([RdsLockoutStorage::OPTION_PERSISTENCE => 'default_kv']);
                 break;
             case self::STORAGE_RDS:
                 $options[IPLockout::OPTION_LOCKOUT_STORAGE] = new RdsLockoutStorage([RdsLockoutStorage::OPTION_PERSISTENCE => 'default']);
