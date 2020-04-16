3f7a02715d8a2f5422d6c01b9bea027269c9f942
Aleh Hutnikau <aleh.hutnikau@1pt.com>
2020-04-16 16:41:27 +0300

    bump version

--- scripts/tools/migrations/AbstractMigration.php
+++ scripts/tools/migrations/AbstractMigration.php
@@ -4,8 +4,18 @@
 namespace oat\tao\scripts\tools\migrations;
 
 use Doctrine\Migrations\AbstractMigration as DoctrineAbstractMigration;
+use oat\oatbox\service\ServiceManager;
 
 abstract class AbstractMigration extends DoctrineAbstractMigration
 {
-
+    /**
+     * Temporary helper until the service manager
+     * gets properly injected migration scripts
+     *
+     * @return ServiceManager
+     */
+    public function getServiceManager(): ServiceManager
+    {
+        return ServiceManager::getServiceManager();
+    }
 }
