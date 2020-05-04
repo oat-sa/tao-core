5beea0699bf8675261db02c11bc72d6dbd8ee21b
Mikhail Kamarouski <Mikhail.Kamarouski@1pt.com>
2020-05-04 14:13:46 +0300

    Namespaces simplified; copyrights updated

--- test/unit/model/taskQueue/TaskLog/Decorator/RedirectUrlEntityDecoratorTest.php
+++ test/unit/model/taskQueue/TaskLog/Decorator/RedirectUrlEntityDecoratorTest.php
@@ -15,12 +15,14 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
- * Copyright (c) 2017 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
+ * Copyright (c) 2017-2020 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
  *
  */
 
 namespace oat\tao\test\unit\model\taskQueue\TaskLog\Decorator;
 
+use DateTime;
+use DateTimeZone;
 use oat\oatbox\user\AnonymousUser;
 use oat\tao\model\taskQueue\TaskLog;
 use oat\tao\model\taskQueue\TaskLog\CategorizedStatus;
@@ -111,8 +113,8 @@ class RedirectUrlEntityDecoratorTest extends TestCase
 
     protected function getFixtureEntity($status = TaskLogInterface::STATUS_COMPLETED)
     {
-        $this->createdAt = new \DateTime('2017-11-16 14:11:42', new \DateTimeZone('UTC'));
-        $this->updatedAt = new \DateTime('2017-11-16 17:12:30', new \DateTimeZone('UTC'));
+        $this->createdAt = new DateTime('2017-11-16 14:11:42', new DateTimeZone('UTC'));
+        $this->updatedAt = new DateTime('2017-11-16 17:12:30', new DateTimeZone('UTC'));
 
         return TaskLogEntity::createFromArray([
             'id' => 'rdf#i1508337970199318643',
@@ -131,7 +133,7 @@ class RedirectUrlEntityDecoratorTest extends TestCase
                 'children' => []
             ],
             'master_status' => true
-        ], \DateTime::RFC3339);
+        ], DateTime::RFC3339);
     }
 
     protected function getFixtureEntityData($status = TaskLogInterface::STATUS_COMPLETED)
--- test/unit/model/taskQueue/TaskLog/Entity/TaskLogEntityTest.php
+++ test/unit/model/taskQueue/TaskLog/Entity/TaskLogEntityTest.php
@@ -15,13 +15,15 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
- * Copyright (c) 2017 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
+ * Copyright (c) 2017-2020 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
  *
  */
 
 namespace oat\tao\test\unit\model\taskQueue\TaskLog\Entity;
 
 use common_report_Report as Report;
+use DateTime;
+use DateTimeZone;
 use oat\tao\model\taskQueue\TaskLog\CategorizedStatus;
 use oat\tao\model\taskQueue\TaskLog\Entity\TaskLogEntity;
 use oat\tao\model\taskQueue\TaskLogInterface;
@@ -31,8 +33,8 @@ class TaskLogEntityTest extends TestCase
 {
     public function testEntityCreated()
     {
-        $createdAt = new \DateTime('2017-11-16 14:11:42', new \DateTimeZone('UTC'));
-        $updatedAt = new \DateTime('2017-11-16 17:12:30', new \DateTimeZone('UTC'));
+        $createdAt = new DateTime('2017-11-16 14:11:42', new DateTimeZone('UTC'));
+        $updatedAt = new DateTime('2017-11-16 17:12:30', new DateTimeZone('UTC'));
 
         $entity = TaskLogEntity::createFromArray([
             'id' => 'rdf#i1508337970199318643',
@@ -51,7 +53,7 @@ class TaskLogEntityTest extends TestCase
                 'children' => []
             ],
             'master_status' => true
-        ], \DateTime::RFC3339);
+        ], DateTime::RFC3339);
 
         $this->assertInstanceOf(TaskLogEntity::class, $entity);
         $this->assertInstanceOf(CategorizedStatus::class, $entity->getStatus());
@@ -91,7 +93,7 @@ class TaskLogEntityTest extends TestCase
             'created_at' => '2017-02-01 12:00:01',
             'updated_at' => '2017-02-01 14:00:01',
             'report' => [],
-        ], \DateTime::RFC3339);
+        ], DateTime::RFC3339);
 
         $this->assertNull($entity->getReport());
     }
