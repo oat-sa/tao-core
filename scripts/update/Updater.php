e13fa1fa96616e30d455f52c8ba55df155210635
Gabriel Felipe Soares <gabriel.felipe.soares@taotesting.com>
2020-05-04 14:31:15 +0200

    Skip version

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -1343,10 +1343,6 @@ class Updater extends \common_ext_ExtensionUpdater
             $this->setVersion('42.0.4');
         }
 
-        $this->skip('42.0.4', '42.0.5');
-
-        if ($this->isVersion('42.0.5')) {
-            $this->setVersion('42.1.0');
-        }
+        $this->skip('42.0.4', '42.1.0');
     }
 }
