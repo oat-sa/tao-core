802242650f4cc003f94ba01ff095809dc4f89e3c
uncleempty <ilya@taotesting.com>
2020-06-05 11:14:40 +0300

    Added service option to make indexdocument builder factory configurable; Changed implementation of indexdocument creator methods

--- models/classes/search/index/IndexService.php
+++ models/classes/search/index/IndexService.php
@@ -23,25 +23,18 @@ declare(strict_types=1);
 
 namespace oat\tao\model\search\index;
 
-use ArrayIterator;
-use core_kernel_classes_Class;
-use core_kernel_classes_Property;
-use core_kernel_classes_Resource;
-use Iterator;
-use oat\generis\model\OntologyAwareTrait;
+use Elasticsearch\Common\Exceptions\Missing404Exception;
 use oat\generis\model\OntologyRdfs;
-use oat\generis\model\WidgetRdf;
 use oat\oatbox\extension\script\MissingOptionException;
 use oat\oatbox\service\ConfigurableService;
-use oat\tao\elasticsearch\SlugCreator;
-use oat\tao\model\menu\MenuService;
-use oat\tao\model\resources\ResourceIterator;
-use oat\tao\model\search\Search;
 use oat\tao\model\search\SearchTokenGenerator;
 use oat\tao\model\TaoOntology;
-use oat\tao\model\WidgetDefinitions;
-use tao_helpers_form_GenerisFormFactory;
-use tao_helpers_Slug;
+use oat\tao\model\search\Search;
+use oat\tao\model\menu\MenuService;
+use oat\generis\model\OntologyAwareTrait;
+use oat\generis\model\kernel\persistence\smoothsql\search\ComplexSearchService;
+use oat\search\helper\SupportedOperatorHelper;
+use oat\tao\model\resources\ResourceIterator;
 
 /**
  * Class IndexService
@@ -52,17 +45,10 @@ class IndexService extends ConfigurableService
     use OntologyAwareTrait;
 
     const SERVICE_ID = 'tao/IndexService';
-    const ALLOWED_DYNAMIC_TYPES = [
-        WidgetDefinitions::PROPERTY_TEXTBOX,
-        WidgetDefinitions::PROPERTY_TEXTAREA,
-        WidgetDefinitions::PROPERTY_HTMLAREA,
-        WidgetDefinitions::PROPERTY_CHECKBOX,
-        WidgetDefinitions::PROPERTY_COMBOBOX,
-        WidgetDefinitions::PROPERTY_RADIOBOX,
-    ];
+    const INDEX_MAP_PROPERTY_DEFAULT = 'default';
+    const INDEX_MAP_PROPERTY_FUZZY = 'fuzzy';
 
-    /** @var array */
-    private $map;
+    const OPTION_DOCUMENT_BUILDER_FACTORY = 'documentBuilderFactory';
 
     /**
      * Run a full reindexing
@@ -75,10 +61,20 @@ class IndexService extends ConfigurableService
         $indexIterator = new IndexIterator($iterator);
         $indexIterator->setServiceLocator($this->getServiceLocator());
         $searchService = $this->getServiceLocator()->get(Search::SERVICE_ID);
-        $this->logInfo('starting indexation');
         $result = $searchService->index($indexIterator);
         $this->logDebug($result . ' resources have been indexed by ' . static::class);
         return $result;
+    }
+
+    /**
+     * Returns a factory to get the IndexDocument Builder
+     *
+     * return mixed
+     */
+    public function getDocumentBuilderFactory()
+    {
+        $factory = $this->getOption(self::OPTION_DOCUMENT_BUILDER_FACTORY);
+        return $factory;
     }
 
     /**
@@ -87,26 +83,15 @@ class IndexService extends ConfigurableService
      * @throws \common_Exception
      * @throws \common_exception_InconsistentData
      */
-    public function createDocumentFromResource(\core_kernel_classes_Resource $resource)
+    public function createDocumentFromResource(\core_kernel_classes_Resource $resource): ?IndexDocument
     {
-        $tokenGenerator = new SearchTokenGenerator();
+        //TODO: check if it is valid in all cases
+        $resourceTypes = $resource->getTypes();
+        $resourceType = current(array_keys($resourceTypes));
 
-        $body = [];
-        $indexesProperties = [];
-        foreach ($tokenGenerator->generateTokens($resource) as $data) {
-            /** @var OntologyIndex $index */
-            list($index, $strings) = $data;
-            $body[$index->getIdentifier()] = $strings;
-            $indexesProperties[$index->getIdentifier()] = $this->getIndexProperties($index);
-        }
-        $body['type'] = $this->getTypesForResource($resource);
+        $document = $this->getDocumentBuilderFactory()->getDocumentBuilderByResourceType($resourceType)->createDocumentFromResource($resource);
 
-        return new IndexDocument(
-            $resource->getUri(),
-            $body,
-            $indexesProperties,
-            $this->getDynamicProperties($body['type'], $resource)
-        );
+        return $document;
     }
 
     /**
@@ -115,73 +100,18 @@ class IndexService extends ConfigurableService
      * @throws MissingOptionException
      * @throws \common_Exception
      */
-    public function createDocumentFromArray($array = [])
+    public function createDocumentFromArray($array = []): ?IndexDocument
     {
-        if (!isset($array['id'])) {
-            throw new MissingOptionException('Missed id property for the index document');
-        }
-
         if (!isset($array['body'])) {
-            throw new MissingOptionException('Missed body property for the index document');
+            throw new \common_exception_MissingParameter('body');
         }
-        $body = $array['body'];
-        $indexProperties = [];
-        if (isset($array['indexProperties'])) {
-            $indexProperties = $array['indexProperties'];
+        if (!isset($array['body']['type'])) {
+            throw new \common_exception_MissingParameter('body[type]');
         }
-        $document = new IndexDocument(
-            $array['id'],
-            $body,
-            $indexProperties
-        );
+
+        $document = $this->getDocumentBuilderFactory()->getDocumentBuilderByResourceType($array['body']['type'])->createDocumentFromArray($array);
+
         return $document;
-    }
-
-    /**
-     * @param OntologyIndex $index
-     * @return mixed
-     * @throws \common_Exception
-     */
-    public function getIndexProperties(OntologyIndex $index)
-    {
-        if (!isset($this->map[$index->getIdentifier()])) {
-            $indexProperty = new IndexProperty(
-                $index->getIdentifier(),
-                $index->isFuzzyMatching(),
-                $index->isDefaultSearchable()
-            );
-            $this->map[$index->getIdentifier()] = $indexProperty;
-        }
-        return $this->map[$index->getIdentifier()];
-    }
-
-    /**
-     * @param $resource
-     * @return array
-     */
-    public function getTypesForResource($resource)
-    {
-        $toDo = [];
-        foreach ($resource->getTypes() as $class) {
-            $toDo[] = $class->getUri();
-        }
-
-        $done = [OntologyRdfs::RDFS_RESOURCE, TaoOntology::CLASS_URI_OBJECT];
-        $toDo = array_diff($toDo, $done);
-
-        $classes = [];
-        while (!empty($toDo)) {
-            $class = new core_kernel_classes_Class(array_pop($toDo));
-            $classes[] = $class->getUri();
-            foreach ($class->getParentClasses() as $parent) {
-                if (!in_array($parent->getUri(), $done)) {
-                    $toDo[] = $parent->getUri();
-                }
-            }
-            $done[] = $class->getUri();
-        }
-
-        return $classes;
     }
 
     /**
@@ -208,68 +138,5 @@ class IndexService extends ConfigurableService
             }
         }
         return array_values($classes);
-    }
-
-    private function getDynamicProperties(array $classes, core_kernel_classes_Resource $resource): Iterator
-    {
-        $customProperties = [];
-
-        foreach ($classes as $class) {
-            $properties = tao_helpers_form_GenerisFormFactory::getClassProperties(
-                new core_kernel_classes_Class($class)
-            );
-
-            foreach ($properties as $property) {
-                /** @var core_kernel_classes_Resource $propertyType |null */
-                $propertyType = $property->getOnePropertyValue(
-                    new core_kernel_classes_Property(
-                        WidgetRdf::PROPERTY_WIDGET
-                    )
-                );
-                if (null === $propertyType) {
-                    continue;
-                }
-
-                $propertyTypeUri = $propertyType->getUri();
-
-                if (!in_array($propertyTypeUri, self::ALLOWED_DYNAMIC_TYPES)) {
-                    continue;
-                }
-
-                $propertyTypeArray = explode('#', $propertyTypeUri, 2);
-                $propertyTypeId = end($propertyTypeArray);
-                $customPropertyLabel = $property->getLabel();
-
-                if (false === $propertyTypeId) {
-                    continue;
-                }
-
-                $fieldName = $propertyTypeId . '_' . tao_helpers_Slug::create($customPropertyLabel);
-                $propertyValue = $resource->getOnePropertyValue($property);
-
-                if (null === $propertyValue) {
-                    continue;
-                }
-
-                $customProperties[$fieldName] = (string)$propertyValue;
-
-                if ($propertyTypeUri === WidgetDefinitions::PROPERTY_COMBOBOX
-                    || $propertyTypeUri === WidgetDefinitions::PROPERTY_RADIOBOX) {
-                    $customProperties[$fieldName] = (string)$propertyValue->getLabel();
-                }
-
-                if ($propertyTypeUri === WidgetDefinitions::PROPERTY_CHECKBOX) {
-                    $customPropertiesValues = $resource->getPropertyValues($property);
-                    $customProperties[$fieldName] = array_map(
-                        function (string $propertyValue): string {
-                            return (new core_kernel_classes_Property($propertyValue))->getLabel();
-                        },
-                        $customPropertiesValues
-                    );
-                }
-            }
-        }
-
-        return new ArrayIterator($customProperties);
     }
 }
