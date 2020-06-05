9c4c6cbbe0e41a641352420f08800a6e4bd3a2f0
uncleempty <ilya@taotesting.com>
2020-06-05 11:15:34 +0300

    Added taskQueue task to create/update documents in index

---
+++ models/classes/search/tasks/UpdateResourceInIndex.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2018 (original work) Open Assessment Technologies SA;
+ */
+
+declare(strict_types=1);
+
+namespace oat\tao\model\search\tasks;
+
+use oat\generis\model\OntologyAwareTrait;
+use oat\oatbox\action\Action;
+use oat\tao\model\search\index\IndexService;
+use oat\tao\model\search\Search;
+use oat\tao\model\taskQueue\Task\TaskAwareInterface;
+use oat\tao\model\taskQueue\Task\TaskAwareTrait;
+use Zend\ServiceManager\ServiceLocatorAwareInterface;
+use Zend\ServiceManager\ServiceLocatorAwareTrait;
+use \common_report_Report as Report;
+
+/**
+ * Class UpdateResourceInIndex
+ *
+ * @author Ilya Yarkavets <ilya@taotesting.com>
+ * @package oat\tao\model\search\tasks
+ */
+class UpdateResourceInIndex implements Action, ServiceLocatorAwareInterface, TaskAwareInterface
+{
+    use ServiceLocatorAwareTrait;
+    use OntologyAwareTrait;
+    use TaskAwareTrait;
+
+    public function __invoke($params): Report
+    {
+        if (empty($params) || empty($params[0])) {
+            throw new \common_exception_MissingParameter();
+        }
+
+        $createdResource = $this->getResource($params[0]);
+
+        $resourceType = current(array_keys($createdResource->getTypes()));
+
+        /** @var IndexService $indexService */
+        $indexService = $this->getServiceLocator()->get(IndexService::SERVICE_ID);
+
+        $factory = $indexService->getDocumentBuilderFactory();
+
+        $documentBuilder = $factory->getDocumentBuilderByResourceType($resourceType);
+
+        $indexDocument = $documentBuilder->createDocumentFromResource($createdResource);
+
+        /** @var Search $searchService */
+        $searchService = $this->getServiceLocator()->get(Search::SERVICE_ID);
+
+        $numberOfIndexed = $searchService->index([$indexDocument]);
+
+        if ($numberOfIndexed === 1) {
+            $type = Report::TYPE_SUCCESS;
+            $message = "Document in index was succesfully updated.";
+        } else {
+            $type = Report::TYPE_WARNING;
+            $message = "There is a difference between number of documents to be indexed/reindexed and number of indexed/reindexed documents.";
+        }
+
+        return new Report($type, $message);
+    }
+}
