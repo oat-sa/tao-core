4f821cddce9e590d5e20e2d6c7695278c73b4a09
Oleksandr Khitko <oleksandr@taotesting.com>
2020-06-11 15:43:57 +0200

    Fixed version bump

--- manifest.php
+++ manifest.php
@@ -57,7 +57,7 @@ return [
     'label' => 'TAO Base',
     'description' => 'TAO meta-extension',
     'license' => 'GPL-2.0',
-    'version' => '42.13.2',
+    'version' => '42.13.0',
     'author' => 'Open Assessment Technologies, CRP Henri Tudor',
     'requires' => [
         'generis' => '>=12.21.0',
--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -1361,6 +1361,6 @@ class Updater extends \common_ext_ExtensionUpdater
             $this->setVersion('42.11.0');
         }
 
-        $this->skip('42.11.0', '42.13.2');
+        $this->skip('42.11.0', '42.13.0');
     }
 }
