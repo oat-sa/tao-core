0dc789d18c2c606af1e69a42de9c423d8822095c
uncleempty <ilya@taotesting.com>
2020-06-19 11:50:34 +0300

    Bumped platform version in manifest

--- manifest.php
+++ manifest.php
@@ -200,9 +200,9 @@ return [
     ],
     'constants' => [
         #TAO version number
-        'TAO_VERSION' => '3.4.0-sprint130',
+        'TAO_VERSION' => '3.4.0-sprint131',
         #TAO version label
-        'TAO_VERSION_NAME' => '3.4.0-sprint130',
+        'TAO_VERSION_NAME' => '3.4.0-sprint131',
         #the name to display
         'PRODUCT_NAME' => 'TAO',
         #TAO release status, use to add specific footer to TAO, available alpha, beta, demo, stable
