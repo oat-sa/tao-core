3c96a42b3b27f95e3be8b5f1f8966a51810859ed
Sergei Mikhailov <sergei.mikhailov@taotesting.com>
2020-07-16 10:01:14 +0200

    UNO-341 fix(db): separate unique indexes between `list_items.uri` and `list_items.list_uri`

--- migrations/Version202007151446482234_tao.php
+++ migrations/Version202007151446482234_tao.php
@@ -31,12 +31,9 @@ final class Version202007151446482234_tao extends AbstractMigration
 
         $listItemsTable->setPrimaryKey([RdsValueCollectionRepository::FIELD_ITEM_ID]);
 
-        //TODO: add and check indexes
         $listItemsTable->addIndex([RdsValueCollectionRepository::FIELD_ITEM_LABEL]);
-        $listItemsTable->addIndex([RdsValueCollectionRepository::FIELD_ITEM_URI]);
-        $listItemsTable->addUniqueIndex(
-            [RdsValueCollectionRepository::FIELD_ITEM_LIST_URI, RdsValueCollectionRepository::FIELD_ITEM_URI]
-        );
+        $listItemsTable->addUniqueIndex([RdsValueCollectionRepository::FIELD_ITEM_LIST_URI]);
+        $listItemsTable->addUniqueIndex([RdsValueCollectionRepository::FIELD_ITEM_URI]);
 
         OntologyUpdater::syncModels();
     }
