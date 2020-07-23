1d27534a7a766f0195423174204fa8c966018e20
Sergii <sergii@taotesting.com>
2020-07-23 10:22:44 +0200

    chore: version bump

--- manifest.php
+++ manifest.php
@@ -62,7 +62,7 @@ return [
     'label' => 'TAO Base',
     'description' => 'TAO meta-extension',
     'license' => 'GPL-2.0',
-    'version' => '44.13.1',
+    'version' => '44.13.2',
     'author' => 'Open Assessment Technologies, CRP Henri Tudor',
     'requires' => [
         'generis' => '>=12.31.0',
