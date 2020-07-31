c7ed6e98fbb5e95c896f3b5dc646dd801a859ff0
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2020-07-31 11:49:35 +0200

    chore: update version in manifest

--- manifest.php
+++ manifest.php
@@ -206,9 +206,9 @@ return [
     ],
     'constants' => [
         #TAO version number
-        'TAO_VERSION' => '3.4.0-sprint133',
+        'TAO_VERSION' => '3.4.0-sprint134',
         #TAO version label
-        'TAO_VERSION_NAME' => '3.4.0-sprint133',
+        'TAO_VERSION_NAME' => '3.4.0-sprint134',
         #the name to display
         'PRODUCT_NAME' => 'TAO',
         #TAO release status, use to add specific footer to TAO, available alpha, beta, demo, stable
