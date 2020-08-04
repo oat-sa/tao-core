992849f484072ac82fcd062622c16f928b0d32a6
Gabriel Felipe Soares <gabriel.felipe.soares@taotesting.com>
2020-08-04 17:52:50 +0200

    added missing tests

---
+++ test/unit/models/classes/security/Business/Domain/Key/KeyChainQueryTest.php
@@ -0,0 +1,40 @@
+<?php declare(strict_types=1);
+
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2020 (original work) Open Assessment Technologies SA;
+ */
+
+namespace oat\tao\test\unit\model\security\Business\Domain\Key;
+
+use oat\generis\test\TestCase;
+use oat\tao\model\security\Business\Domain\Key\KeyChainQuery;
+
+class KeyChainQueryTest extends TestCase
+{
+    /** @var KeyChainQuery */
+    private $subject;
+
+    public function setUp(): void
+    {
+        $this->subject = new KeyChainQuery('123456');
+    }
+
+    public function testGetters(): void
+    {
+        $this->assertSame('123456', $this->subject->getIdentifier());
+    }
+}
