96162f9a297afe6bd670a60251733f1072571b03
Andrew Shapiro <shpr.andrew@gmail.com>
2020-09-01 01:50:16 +0300

    Feature/TAO-10503/Allow customization of the login template

--- actions/class.Main.php
+++ actions/class.Main.php
@@ -248,7 +248,7 @@ class tao_actions_Main extends tao_actions_CommonModule
 
         $this->setData('show_gdpr', !empty($config['show_gdpr']) && $config['show_gdpr']);
 
-        $this->setData('content-template', ['blocks/login.tpl', 'tao']);
+        $this->setData('content-template', $config['template']);
 
         $this->setView('layout.tpl', 'tao');
     }
