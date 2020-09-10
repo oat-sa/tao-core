3ffdebd2409217544ec21ba86b69d003cee503fc
gitromba <mihaly.hajdu@taotesting.com>
2020-09-10 11:26:57 +0200

    add test files

---
+++ models/classes/accessControl/TestClass.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace oat\tao\model\accessControl;
+
+class TestClass
+{
+    private $a;
+
+    public function __construct(int $a)
+    {
+        $this->a = $a;
+    }
+
+    public function add(int $b)
+    {
+        return $this->a = $this->a + $b;
+    }
+
+    public function toString()
+    {
+        return (string)$this->a;
+    }
+}
