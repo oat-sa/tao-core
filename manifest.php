08457d0919e3f73633bf4a9cc57b1e27011f4d63
jsconan <jsconan@gmail.com>
2020-09-11 10:24:47 +0200

    chore: update sprint version

--- manifest.php
+++ manifest.php
@@ -207,9 +207,9 @@ return [
     ],
     'constants' => [
         #TAO version number
-        'TAO_VERSION' => '3.4.0-sprint136',
+        'TAO_VERSION' => '3.4.0-sprint137',
         #TAO version label
-        'TAO_VERSION_NAME' => '3.4.0-sprint136',
+        'TAO_VERSION_NAME' => '3.4.0-sprint137',
         #the name to display
         'PRODUCT_NAME' => 'TAO',
         #TAO release status, use to add specific footer to TAO, available alpha, beta, demo, stable
