52f0e3419fcbd3e56b928e87c4da174fee06e222
andrii.ponamarov <andrii.ponamarov@taotesting.com>
2020-10-16 15:41:32 +0200

    Add description

--- models/classes/websource/BaseWebsource.php
+++ models/classes/websource/BaseWebsource.php
@@ -120,6 +120,7 @@ abstract class BaseWebsource extends Configurable implements Websource
     /**
      * Get a file's mime-type.
      * @param string $filePath The path to the file.
+     * @param string $acceptHeader The accept header value.
      * @return string|false The file mime-type or false on failure.
      * @throws \common_exception_Error
      * @throws \common_exception_NotFound
