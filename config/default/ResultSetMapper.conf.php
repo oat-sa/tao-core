46a469c5032bbdf1ad2b4c540f26da09e4cfed4d
bartlomiej <bartlomiej@taotesting.com>
2020-12-03 14:16:51 +0100

    Update default config file for ResultSetMapper

--- config/default/ResultSetMapper.conf.php
+++ config/default/ResultSetMapper.conf.php
@@ -4,43 +4,42 @@ use oat\tao\model\search\ResultSetMapper;
 
 return new ResultSetMapper(
     [
-        ResultSetMapper::OPTION_STRUCTURE_MAP =>
-            [
-                'default' => [
-                    'label' => [
-                        'id' => 'label',
-                        'label' => __('Label'),
-                        'sortable' => false
-                    ]
-                ],
-                'results' => [
-                    'label' => [
-                        'id' => 'label',
-                        'label' => __('Label'),
-                        'sortable' => false
-                    ],
-                    'test_taker_name' => [
-                        'id' => 'test_taker_name',
-                        'label' => __('Test Taker Name'),
-                        'sortable' => false
-                    ],
-                    'test_taker' => [
-                        'id' => 'test_taker',
-                        'label' => __('Test Taker ID'),
-                        'sortable' => false
-                    ],
-                    'delivery' => [
-                        'id' => 'delivery',
-                        'label' => __('Delivery Uri'),
-                        'sortable' => false,
-                        'visible' => false
-                    ],
-                    'delivery_execution_start_time' => [
-                        'id' => 'delivery_execution_start_time',
-                        'label' => __('Start Time'),
-                        'sortable' => false,
-                    ],
+        ResultSetMapper::OPTION_STRUCTURE_MAP => [
+            'default' => [
+                'label' => [
+                    'id' => 'label',
+                    'label' => __('Label'),
+                    'sortable' => false
                 ]
+            ],
+            'results' => [
+                'label' => [
+                    'id' => 'label',
+                    'label' => __('Label'),
+                    'sortable' => false
+                ],
+                'test_taker_name' => [
+                    'id' => 'test_taker_name',
+                    'label' => __('Test Taker Name'),
+                    'sortable' => false
+                ],
+                'test_taker' => [
+                    'id' => 'test_taker',
+                    'label' => __('Test Taker ID'),
+                    'sortable' => false
+                ],
+                'delivery' => [
+                    'id' => 'delivery',
+                    'label' => __('Delivery Uri'),
+                    'sortable' => false,
+                    'visible' => false
+                ],
+                'delivery_execution_start_time' => [
+                    'id' => 'delivery_execution_start_time',
+                    'label' => __('Start Time'),
+                    'sortable' => false,
+                ],
             ]
+        ]
     ]
 );
