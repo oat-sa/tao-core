c8bbd1bce3e8b74956c97d4a31a7ee21ae150b63
bartlomiej <bartlomiej@taotesting.com>
2020-12-04 16:48:59 +0100

    migration update

--- migrations/Version202011301939462234_tao.php
+++ migrations/Version202011301939462234_tao.php
@@ -23,33 +23,32 @@ final class Version202011301939462234_tao extends AbstractMigration
     {
         $this->getServiceManager()->register(ResultSetMapper::SERVICE_ID, new ResultSetMapper(
             [
-                ResultSetMapper::OPTION_STRUCTURE_MAP =>
-                    [
-                        'default' => [
-                            'label' => [
-                                'id' => 'label',
-                                'label' => __('Label'),
-                                'sortable' => false
-                            ]
-                        ],
-                        'results' => [
-                            'label' => [
-                                'id' => 'label',
-                                'label' => __('Label'),
-                                'sortable' => false
-                            ],
-                            'test_taker_name' => [
-                                'id' => 'test_taker_name',
-                                'label' => __('Test Taker'),
-                                'sortable' => false
-                            ],
-                            'test_taker' => [
-                                'id' => 'test_taker',
-                                'label' => __('Test Taker'),
-                                'sortable' => false
-                            ],
+                ResultSetMapper::OPTION_STRUCTURE_MAP => [
+                    'default' => [
+                        'label' => [
+                            'id' => 'label',
+                            'label' => __('Label'),
+                            'sortable' => false
                         ]
+                    ],
+                    'results' => [
+                        'label' => [
+                            'id' => 'label',
+                            'label' => __('Label'),
+                            'sortable' => false
+                        ],
+                        'test_taker_name' => [
+                            'id' => 'test_taker_name',
+                            'label' => __('Test Taker'),
+                            'sortable' => false
+                        ],
+                        'test_taker' => [
+                            'id' => 'test_taker',
+                            'label' => __('Test Taker'),
+                            'sortable' => false
+                        ],
                     ]
+                ]
             ]
         ));
     }
