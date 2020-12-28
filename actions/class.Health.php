247a668756213b5f9356d95edf4224d4a42ed6f3
Aleh Hutnikau <hutnikau@1pt.com>
2020-12-28 19:14:23 +0300

    feat: improve tao health check action
    Tao health check not just checks that Tao is installed but also that all the post install scripts were executed and installation was fnished and Tao is ready to be used.

--- actions/class.Health.php
+++ actions/class.Health.php
@@ -1,7 +1,12 @@
 <?php
 
+use oat\tao\model\service\ApplicationService;
+use oat\tao\model\http\HttpJsonResponseTrait;
+
 class tao_actions_Health extends tao_actions_CommonModule
 {
+    use HttpJsonResponseTrait;
+
     /**
      * Simple endpoint for health checking the TAO instance.
      *
@@ -10,6 +15,19 @@ class tao_actions_Health extends tao_actions_CommonModule
      */
     public function index()
     {
-        return;
+        try {
+            /** @var ApplicationService $applicationService */
+            $applicationService = $this->getServiceLocator()->get(ApplicationService::SERVICE_ID);
+            if ($applicationService->isInstallationFinished()) {
+                $this->setSuccessJsonResponse([
+                    'success' => $applicationService->isInstallationFinished()
+                ]);
+            } else {
+                throw new common_exception_Error('Installation is not finished');
+            }
+        } catch (Throwable $exception) {
+            $this->logError(sprintf('Error getting media relation: %s, ', $exception->getMessage()));
+            $this->setErrorJsonResponse($exception->getMessage(), $exception->getCode());
+        }
     }
 }
