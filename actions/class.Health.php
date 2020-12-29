dbd356c429333337d86eb273febb3357e743845c
Aleh Hutnikau <hutnikau@1pt.com>
2020-12-29 15:05:30 +0300

    change exception message

--- actions/class.Health.php
+++ actions/class.Health.php
@@ -26,7 +26,7 @@ class tao_actions_Health extends tao_actions_CommonModule
                 throw new common_exception_Error('Installation is not finished');
             }
         } catch (Throwable $exception) {
-            $this->logError(sprintf('Error getting media relation: %s, ', $exception->getMessage()));
+            $this->logError(sprintf('Error during health check: %s, ', $exception->getMessage()));
             $this->setErrorJsonResponse($exception->getMessage(), $exception->getCode());
         }
     }
