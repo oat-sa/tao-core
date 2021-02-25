23cc1c6f58bf834b778d54c644b5f05b27081b30
404passionFruit <404passionFruit@gmail.com>
2021-02-25 13:20:49 +0300

    build: script to lint changed js files in tao

---
+++ views/build/lintChanged.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+CHANGED=$(git diff HEAD --name-only -- ../js/)
+LINTFILES=""
+
+for file in $CHANGED
+do
+  LINTFILES+=" --file=../../$file"
+done
+
+grunt eslint:file --quiet $LINTFILES
