9cf77e27a91a844e8bc10c078d5431c76427c69e
Mikhail Kamarouski <mikhail.kamarouski@1pt.com>
2021-03-31 15:16:25 +0300

    chore: cleanup dead code

--- scripts/install/RegisterClassMetadataServices.php
+++ scripts/install/RegisterClassMetadataServices.php
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
- * Copyright (c) 2020 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
+ * Copyright (c) 2020-2021 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
  *
  */
 
@@ -28,8 +28,6 @@ use oat\tao\model\accessControl\func\AccessRule;
 use oat\tao\model\accessControl\func\AclProxy;
 use oat\tao\model\Lists\Business\Service\ClassMetadataSearcherProxy;
 use oat\tao\model\Lists\Business\Service\ClassMetadataService;
-use oat\tao\model\Lists\Business\Service\GetClassMetadataValuesService;
-use oat\tao\model\Lists\Business\Service\ValueCollectionService;
 use oat\tao\model\Lists\Presentation\Web\RequestHandler\ClassMetadataSearchRequestHandler;
 use oat\tao\model\Lists\Presentation\Web\RequestValidator\ClassMetadataSearchRequestValidator;
 use oat\tao\model\user\TaoRoles;
