b8f3de5f4273a302ca05d2949e8cb11399649b4b
Roman Linkov <roman.voknil@gmail.com>
2021-04-01 13:16:35 +0300

    fix: refactoring of command adding atomic roles

--- scripts/install/AddAtomicRoles.php
+++ scripts/install/AddAtomicRoles.php
@@ -1,40 +1,55 @@
 <?php
 
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2020 (original work) Open Assessment Technologies SA;
+ *
+ * @author Sergei Mikhailov <sergei.mikhailov@taotesting.com>
+ */
+
 declare(strict_types=1);
 
-namespace oat\tao\migrations;
+namespace oat\tao\scripts\install;
 
-use Doctrine\DBAL\Schema\Schema;
+use oat\oatbox\extension\InstallAction;
+use oat\oatbox\reporting\Report;
 use oat\tao\model\accessControl\func\AccessRule;
 use oat\tao\model\accessControl\func\AclProxy;
 use oat\tao\model\user\TaoRoles;
-use oat\tao\scripts\tools\migrations\AbstractMigration;
 use oat\tao\scripts\update\OntologyUpdater;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
-final class Version202103301905282234_tao extends AbstractMigration
+class AddAtomicRoles extends InstallAction
 {
-    public function getDescription(): string
-    {
-        return 'Create atomic roles for items and assign permissions to them';
-    }
 
-    public function up(Schema $schema): void
+    public function __invoke($params = [])
     {
         OntologyUpdater::syncModels();
 
+        $updatedRoles = [];
         foreach ($this->getRulesForRole() as $role => $rules) {
             foreach ($rules as $rule) {
                 AclProxy::applyRule($this->createAclRulesForRole($role, $rule));
             }
+            preg_match("/.+#(\w+)Role$/", $role, $roleDefinition);
+            $updatedRoles[] = $roleDefinition[1] ?? $role;
         }
-    }
 
-    public function down(Schema $schema): void
-    {
-        $this->throwIrreversibleMigrationException();
+        return Report::createSuccess(
+            sprintf('Atomic roles "%s" successfully updated with rules', implode(', ',$updatedRoles))
+        );
     }
 
     private function getRulesForRole(): array
