cad4bd905735b95c255ec850cd424ee22366de24
Gabriel Felipe Soares <gabriel.felipe.soares@taotesting.com>
2021-06-02 09:51:29 +0200

    chore: improve migrations

--- migrations/Version202106010945122237_tao.php
+++ migrations/Version202106010945122237_tao.php
@@ -5,8 +5,9 @@ declare(strict_types=1);
 namespace oat\tao\migrations;
 
 use Doctrine\DBAL\Schema\Schema;
+use oat\oatbox\service\ConfigurableService;
 use oat\tao\model\search\SearchProxy;
-use oat\tao\model\search\strategy\GenerisSearch;
+use oat\tao\model\search\Search;
 use oat\tao\scripts\tools\migrations\AbstractMigration;
 
 final class Version202106010945122237_tao extends AbstractMigration
@@ -18,18 +19,27 @@ final class Version202106010945122237_tao extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        $this->getServiceManager()->register(
-            SearchProxy::SERVICE_ID,
-            new SearchProxy(
-                [
-                    SearchProxy::OPTION_DEFAULT_SEARCH_CLASS => GenerisSearch::class
-                ]
-            )
+        /** @var Search $legacySearch */
+        $legacySearch = $this->getServiceManager()->get(Search::SERVICE_ID);
+
+        $searchProxy = new SearchProxy(
+            [
+                SearchProxy::OPTION_DEFAULT_SEARCH_CLASS => $legacySearch
+            ]
         );
+
+        $this->getServiceManager()->register(SearchProxy::SERVICE_ID, $searchProxy);
     }
 
     public function down(Schema $schema): void
     {
+        /** @var SearchProxy $searchProxy */
+        $searchProxy = $this->getServiceManager()->get(SearchProxy::SERVICE_ID);
+
+        /** @var ConfigurableService|Search $legacySearch */
+        $legacySearch = $searchProxy->getOption(SearchProxy::OPTION_DEFAULT_SEARCH_CLASS);
+
         $this->getServiceManager()->unregister(SearchProxy::SERVICE_ID);
+        $this->getServiceManager()->register(Search::SERVICE_ID, $legacySearch);
     }
 }
