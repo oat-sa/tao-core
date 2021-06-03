edcebaa9bd998ddf3c7ad88142ffcab1a52f75a5
Gabriel Felipe Soares <gabrielfs7@gmail.com>
2021-06-03 17:06:32 +0200

    feature: making search proxy central point for search

--- models/classes/search/SearchProxy.php
+++ models/classes/search/SearchProxy.php
@@ -23,25 +23,55 @@ declare(strict_types=1);
 namespace oat\tao\model\search;
 
 use Exception;
+use InvalidArgumentException;
 use oat\generis\model\GenerisRdf;
 use oat\generis\model\OntologyAwareTrait;
 use oat\oatbox\service\ConfigurableService;
 use oat\tao\model\AdvancedSearch\AdvancedSearchChecker;
 use oat\tao\model\TaoOntology;
-use oat\tao\model\search\strategy\GenerisSearch;
 use Psr\Http\Message\ServerRequestInterface;
 
-class SearchProxy extends ConfigurableService
+class SearchProxy extends ConfigurableService implements Search
 {
     use OntologyAwareTrait;
+
+    public const OPTION_ADVANCED_SEARCH_CLASS = 'advanced_search_class';
+    public const OPTION_DEFAULT_SEARCH_CLASS = 'default_search_class';
 
     private const GENERIS_SEARCH_WHITELIST = [
         GenerisRdf::CLASS_ROLE,
         TaoOntology::CLASS_URI_TAO_USER,
     ];
 
-    /** @var GenerisSearch */
-    private $generisSearch;
+    public function getAdvancedSearch(): ?SearchInterface
+    {
+        return $this->getService(self::OPTION_ADVANCED_SEARCH_CLASS);
+    }
+
+    public function getDefaultSearch(): SearchInterface
+    {
+        $defaultSearch = $this->getService(self::OPTION_DEFAULT_SEARCH_CLASS);
+
+        if ($defaultSearch) {
+            return $defaultSearch;
+        }
+
+        throw new InvalidArgumentException(sprintf('Option %s is required', self::OPTION_DEFAULT_SEARCH_CLASS));
+    }
+
+    public function withAdvancedSearch(SearchInterface $search): self
+    {
+        $this->setOption(self::OPTION_ADVANCED_SEARCH_CLASS, $search);
+
+        return $this;
+    }
+
+    public function withDefaultSearch(SearchInterface $search): self
+    {
+        $this->setOption(self::OPTION_DEFAULT_SEARCH_CLASS, $search);
+
+        return $this;
+    }
 
     /**
      * @throws Exception
@@ -49,9 +79,6 @@ class SearchProxy extends ConfigurableService
     public function searchByQuery(SearchQuery $query): array
     {
         $results = $this->executeSearch($query);
-        if (!$results instanceof ResultSet) {
-            throw new Exception('Result has to be instance of ResultSet');
-        }
 
         return $this->getResultSetResponseNormalizer()
             ->normalize($query, $results, '');
@@ -66,32 +93,77 @@ class SearchProxy extends ConfigurableService
         $queryParams = $request->getQueryParams();
         $results = $this->executeSearch($query);
 
-        if (!$results instanceof ResultSet) {
-            throw new Exception('Result has to be instance of ResultSet');
-        }
-
         return $this->getResultSetResponseNormalizer()
             ->normalize($query, $results, $queryParams['params']['structure']);
     }
 
-    public function withGenerisSearch(GenerisSearch $search): self
+    /**
+     * @inheritDoc
+     */
+    public function query($queryString, $type, $start = 0, $count = 10, $order = 'id', $dir = 'DESC')
     {
-        $this->generisSearch = $search;
+        return $this->getIndexSearch()->query($queryString, $type, $start, $count, $order, $dir);
+    }
 
-        return $this;
+    /**
+     * @inheritDoc
+     */
+    public function flush()
+    {
+        return $this->getIndexSearch()->flush();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function index($documents)
+    {
+        return $this->getIndexSearch()->index($documents);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function remove($resourceId)
+    {
+        return $this->getIndexSearch()->remove($resourceId);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function supportCustomIndex()
+    {
+        return $this->getIndexSearch()->supportCustomIndex();
     }
 
     private function executeSearch(SearchQuery $query): ResultSet
     {
-        if ($this->isForcingGenerisSearch($query)) {
-            return $this->searchWithGeneris($query);
+        if ($query->isEmptySearch()) {
+            return new ResultSet([], 0);
         }
 
-        if ($this->getElasticSearchChecker()->isEnabled()) {
-            return $this->getElasticSearchBridge()->search($query);
+        if ($this->isForcingDefaultSearch($query) || !$this->getAdvancedSearchChecker()->isEnabled()) {
+            $result = $this->getIdentifierSearcher()->search($query);
+
+            if ($result->getTotalCount() > 0) {
+                return $result;
+            }
+
+            return $this->getDefaultSearch()->query(
+                $query->getTerm(),
+                $query->getParentClass(),
+                $query->getStartRow(),
+                $query->getRows()
+            );
         }
 
-        return $this->getGenerisSearchBridge()->search($query);
+        return $this->getAdvancedSearch()->query(
+            $this->getAdvancedSearchQueryString($query),
+            $query->getRootClass(),
+            $query->getStartRow(),
+            $query->getRows()
+        );
     }
 
     private function getResultSetResponseNormalizer(): ResultSetResponseNormalizer
@@ -99,19 +171,14 @@ class SearchProxy extends ConfigurableService
         return $this->getServiceLocator()->get(ResultSetResponseNormalizer::class);
     }
 
-    private function getElasticSearchChecker(): AdvancedSearchChecker
+    private function getAdvancedSearchChecker(): AdvancedSearchChecker
     {
         return $this->getServiceLocator()->get(AdvancedSearchChecker::class);
     }
 
-    private function getElasticSearchBridge(): ElasticSearchBridge
+    private function getIdentifierSearcher(): IdentifierSearcher
     {
-        return $this->getServiceLocator()->get(ElasticSearchBridge::class);
-    }
-
-    private function getGenerisSearchBridge(): GenerisSearchBridge
-    {
-        return $this->getServiceLocator()->get(GenerisSearchBridge::class);
+        return $this->getServiceLocator()->get(IdentifierSearcher::class);
     }
 
     private function getQueryFactory(): SearchQueryFactory
@@ -119,31 +186,34 @@ class SearchProxy extends ConfigurableService
         return $this->getServiceLocator()->get(SearchQueryFactory::class);
     }
 
-    private function isForcingGenerisSearch(SearchQuery $query): bool
+    private function isForcingDefaultSearch(SearchQuery $query): bool
     {
         return in_array($query->getParentClass(), self::GENERIS_SEARCH_WHITELIST, true);
     }
 
-    private function searchWithGeneris(SearchQuery $query): ResultSet
+    private function getIndexSearch(): SearchInterface
     {
-        return $this->getGenerisSearch()->query(
-            $query->getTerm(),
-            $query->getParentClass(),
-            $query->getStartRow(),
-            $query->getRows()
-        );
+        return $this->getAdvancedSearchChecker()->isEnabled()
+            ? $this->getAdvancedSearch()
+            : $this->getDefaultSearch();
     }
 
-    private function getGenerisSearch(): GenerisSearch
+    private function getService(string $option): ?SearchInterface
     {
-        if (!$this->generisSearch) {
-            /**
-             * @TODO We need to implement better search driver management: https://oat-sa.atlassian.net/browse/ADF-251
-             */
-            $this->generisSearch = new GenerisSearch();
-            $this->propagate($this->generisSearch);
+        if (!$this->hasOption($option)) {
+            return null;
         }
 
-        return $this->generisSearch;
+        /** @var SearchInterface $search */
+        $search = $this->getOption($option);
+
+        $this->propagate($search);
+
+        return $search;
+    }
+
+    private function getAdvancedSearchQueryString(SearchQuery $query): string
+    {
+        return $query->getTerm() . sprintf(' AND parent_classes: "%s"', $query->getParentClass());
     }
 }
---
+++ test/unit/models/classes/search/IdentifierSearchTest.php
@@ -0,0 +1,125 @@
+<?php
+
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2021 (original work) Open Assessment Technologies SA;
+ */
+
+declare(strict_types=1);
+
+namespace oat\tao\test\unit\model\search;
+
+use core_kernel_classes_Class;
+use core_kernel_classes_Resource;
+use oat\generis\model\data\Ontology;
+use oat\generis\test\TestCase;
+use oat\tao\model\search\IdentifierSearcher;
+use oat\tao\model\search\ResultSet;
+use oat\tao\model\search\SearchQuery;
+
+class IdentifierSearchTest extends TestCase
+{
+    private const LOCAL_NAMESPACE = 'http://something';
+
+    /** @var IdentifierSearcher */
+    private $subject;
+
+    /** @var Ontology */
+    private $ontology;
+
+    public function setUp(): void
+    {
+        $this->ontology = $this->createMock(Ontology::class);
+
+        $this->subject = new IdentifierSearcher();
+        $this->subject->withLocalNamespace(self::LOCAL_NAMESPACE);
+        $this->subject->setServiceLocator(
+            $this->getServiceLocatorMock(
+                [
+                    Ontology::SERVICE_ID => $this->ontology,
+                ]
+            )
+        );
+    }
+
+    public function testSearchByNoUri(): void
+    {
+        $query = new SearchQuery(
+            'user input',
+            'rootClass',
+            'parentClass',
+            1,
+            10,
+            1
+        );
+
+        $this->assertEquals(new ResultSet([], 0), $this->subject->search($query));
+    }
+
+    public function testSearchByUri(): void
+    {
+        $uri = self::LOCAL_NAMESPACE;
+        $label = 'My Resource label';
+
+        $query = new SearchQuery(
+            $uri,
+            'rootClass',
+            'parentClass',
+            1,
+            10,
+            1
+        );
+
+        $resultSetMock = new ResultSet(
+            [
+                [
+                    'id' => $uri,
+                    'label' => $label,
+                ],
+            ],
+            1
+        );
+
+        $resource = $this->createMock(core_kernel_classes_Resource::class);
+        $class = $this->createMock(core_kernel_classes_Class::class);
+
+        $this->ontology
+            ->method('getResource')
+            ->willReturn($resource);
+
+        $this->ontology
+            ->method('getClass')
+            ->willReturn($class);
+
+        $resource
+            ->method('exists')
+            ->willReturn(true);
+
+        $resource
+            ->method('isInstanceOf')
+            ->willReturn(true);
+
+        $resource
+            ->method('getUri')
+            ->willReturn($uri);
+
+        $resource
+            ->method('getLabel')
+            ->willReturn($label);
+
+        $this->assertEquals($resultSetMock, $this->subject->search($query));
+    }
+}
