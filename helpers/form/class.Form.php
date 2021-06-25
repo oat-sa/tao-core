63fccefdbb7cc1bf3ee0a717a26b88b494a65ac3
Chinnu Francis <chinnu@focaloid.com>
2021-06-25 13:44:55 +0530

    fix: add a method to check asyncfile upload

--- helpers/form/class.Form.php
+++ helpers/form/class.Form.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -610,6 +611,20 @@ abstract class tao_helpers_form_Form
 
         foreach ($this->elements as $element) {
             if ($element instanceof tao_helpers_form_elements_File) {
+                $returnValue = true;
+                break;
+            }
+        }
+
+        return $returnValue;
+    }
+    
+    public function hasAsyncFileUpload(): bool
+    {
+        $returnValue = false;
+
+        foreach ($this->elements as $element) {
+            if ($element instanceof tao_helpers_form_elements_xhtml_AsyncFile) {
                 $returnValue = true;
                 break;
             }
