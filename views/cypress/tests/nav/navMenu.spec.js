61e7245a0c0407c13f7a202f4057c0d69270cd47
alshoja <alshoja@focaloid.com>
2021-06-30 22:34:07 +0530

    fix: Nav & Item spec updated

---
+++ views/cypress/tests/nav/navMenu.spec.js
@@ -0,0 +1,52 @@
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2021 (original work) Open Assessment Technologies SA ;
+ */
+
+describe('Main Menu', () => {
+    beforeEach(() => {
+        cy.loginAsAdmin();
+        cy.fixture('urls').as('urls');
+    });
+
+    it('should contain atleast 5 menu elements', function () {
+        cy.visit(this.urls.items);
+        cy.get('ul.main-menu').find('li').its('length').should('be.gte', 5);
+        cy.location('pathname').should('eq', this.urls.index);
+    });
+
+    it('should contain these menu items', function () {
+        cy.visit(this.urls.items);
+        cy.get('ul.main-menu')
+            .children()
+            .should($lis => {
+                expect($lis.eq(0), 'first menu').to.contain('Items');
+                expect($lis.eq(1), 'second menu').to.contain('Tests');
+                expect($lis.eq(2), 'third menu').to.contain('Test-takers');
+                expect($lis.eq(3), 'fourth menu').to.contain('Groups');
+                expect($lis.eq(4), 'fifth menu').to.contain('Deliveries');
+                expect($lis.eq(5), 'sixth menu').to.contain('Results');
+            });
+
+        cy.location('pathname').should('eq', this.urls.index);
+    });
+
+    it('should contain atleast one active item', function () {
+        cy.visit(this.urls.items);
+        cy.get('ul.main-menu').children().should('have.class', 'active');
+        cy.location('pathname').should('eq', this.urls.index);
+    });
+});
