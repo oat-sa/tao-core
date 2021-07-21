164240945601ed5270f3d5332758314aeae8f4fb
Cristian Lagos <17564918+clagosarias@users.noreply.github.com>
2021-07-21 17:30:21 +0200

    feat: updated urls utils with manage users, media and tests

--- views/cypress/utils/urls.js
+++ views/cypress/utils/urls.js
@@ -1,4 +1,8 @@
 export default {
     index: '/tao/Main/index',
-    login: '/tao/Main/login'
+    login: '/tao/Main/login',
+    addUser: '/tao/Main/index?structure=users&ext=tao&section=add_user',
+    manageUsers: '/tao/Main/index?structure=users&ext=tao&section=list_users',
+    mediaManager: '/tao/Main/index?structure=taoMediaManager&ext=taoMediaManager&section=media_manager',
+    testsManager: '/tao/Main/index?structure=tests&ext=taoTests&section=manage_tests'
 };
