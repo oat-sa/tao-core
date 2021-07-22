c8c45fbc75ba26467decbd58aa451cb167d99a45
Cristian Lagos <17564918+clagosarias@users.noreply.github.com>
2021-07-22 13:07:21 +0200

    feat: updated test-taker-role test to use common functions

--- views/cypress/tests/test-taker-role.spec.js
+++ views/cypress/tests/test-taker-role.spec.js
@@ -15,55 +15,39 @@
  *
  * Copyright (c) 2021 (original work) Open Assessment Technologies SA ;
  */
-import urls from '../utils/urls';
-import users from '../utils/users';
-import selectors from '../utils/selectors';
+ import urls from '../utils/urls';
+ import users from '../utils/users';
+ import userRoles from '../utils/userRoles';
+ import selectors from '../utils/selectors';
 
-describe('Test Taker Role', () => {
-    const userLogin = users.user_test_taker.login;
-    const userPassword = users.user_test_taker.password;
+ describe('Test Taker Role', () => {
+     const userLogin = users.user_test_taker.login;
+     const userPassword = users.user_test_taker.password;
 
-    before(() => {
-        cy.loginAsAdmin();
-    });
+     before(() => {
+         cy.loginAsAdmin();
+         cy.intercept('GET', '**/add*').as('add');
+         cy.visit(urls.addUser);
+         cy.wait('@add', {
+             requestTimeout: 10000
+         });
+         cy.addUser(selectors.addUserForm, users.user_test_taker, userRoles.testTaker);
+         cy.intercept('GET', '**/logout*').as('logout');
+         cy.logoutAttempt();
+         cy.wait('@logout', {
+             requestTimeout: 10000
+         });
+     });
 
-    describe('Create test taker user', () => {
-        it('Admin can manage test takers', function() {
-            cy.intercept('GET', '/taoTestTaker/TestTaker/*').as('testTakerTree');
-            cy.visit(urls.testTakersManager);
-            cy.wait('@testTakerTree', {
-                requestTimeout: 10000
-            });
-        });
+     describe('Login', () => {
+         it('Logged in successfully', function() {
+             cy.loginAttempt(userLogin, userPassword);
+             cy.contains('.settings-menu li', users.user_test_taker.login);
+             cy.get('#logout');
+         });
+     });
 
-        it('Admin can create test taker', function() {
-            cy.intercept('POST', '/taoTestTaker/TestTaker/addInstance').as('addTestTaker');
-            cy.get('#testtaker-new').click();
-            cy.wait('@addTestTaker', {
-                requestTimeout: 10000
-            }).its('response.body.success').should('eq', true);
-            cy.intercept('POST', '/tao/GenerisTree/getData').as('testTakerData');
-            cy.wait('@testTakerData', {
-                requestTimeout: 10000
-            });
-            cy.addUser(selectors.addTestTakerForm, users.user_test_taker);
-        });
-    });
-
-    describe('Login as test taker user', () => {
-        it('Login Successfully', function() {
-            cy.intercept('GET', '**/logout*').as('logout');
-            cy.logoutAttempt();
-            cy.wait('@logout', {
-                requestTimeout: 10000
-            });
-            cy.loginAttempt(userLogin, userPassword);
-            cy.contains('.settings-menu li', users.user_test_taker.login);
-            cy.get('#logout');
-        });
-    });
-
-    describe('Only has access to deliveries', () => {
+     describe('Only has access to deliveries', () => {
         it("Is in delivery scope", function() {
             cy.get('body').should('have.class', 'delivery-scope')
         });
@@ -77,10 +61,10 @@ describe('Test Taker Role', () => {
         });
     });
 
-    after(() => {
-        cy.logoutAttempt();
-        cy.loginAsAdmin();
-        cy.visit(urls.manageUsers);
-        cy.deleteUser(users.user_test_taker);
-    });
-});
+     after(() => {
+         cy.logoutAttempt();
+         cy.loginAsAdmin();
+         cy.visit(urls.manageUsers);
+         cy.deleteUser(users.user_test_taker);
+     });
+ });
