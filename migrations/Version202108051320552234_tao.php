c23425a954f398d4c8e66cbbda352a4638f897b9
Hanna Dzmitryieva <bziondik@gmail.com>
2021-08-05 17:00:32 +0300

    fix: add reports for migration

--- migrations/Version202108051320552234_tao.php
+++ migrations/Version202108051320552234_tao.php
@@ -8,7 +8,7 @@ use Doctrine\DBAL\Schema\Schema;
 use oat\tao\scripts\tools\migrations\AbstractMigration;
 use oat\tao\scripts\install\SetImageAligmentConfig;
 use oat\tao\model\ClientLibConfigRegistry;
-
+use common_report_Report as Report;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
@@ -22,11 +22,14 @@ final class Version202108051320552234_tao extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        $this->propagate(new SetImageAligmentConfig())([false]);
+        $this->addReport(
+            $this->propagate(new SetImageAligmentConfig())([false])
+        );
     }
 
     public function down(Schema $schema): void
     {
         ClientLibConfigRegistry::getRegistry()->remove('ui/image/ImgStateActive');
+        $this->addReport(new Report(Report::TYPE_SUCCESS, 'Remove configurations for ui/image/ImgStateActive'));
     }
 }
