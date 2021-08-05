ea511b890ddb33bb80264ea3fa597573d8bc945e
Juan Luis Gutierrez Dos Santos <juanluis.gutierrezdossantos@taotesting.com>
2021-08-05 14:42:37 +0200

    chore: Working version

--- views/cypress/support/resourceTree.js
+++ views/cypress/support/resourceTree.js
@@ -36,8 +36,10 @@ Cypress.Commands.add('addClass', formSelector => {
 
 Cypress.Commands.add('addClassToRoot', (rootSelector, formSelector, name) => {
     cy.log('COMMAND: addClassToRoot', name)
+        .intercept('GET', `**/taoItems/Items/getOntologyData**`).as('treeRender')
         .intercept('POST', `**/taoItems/Items/editClassLabel`).as('editClassLabel')
         .wait('@editClassLabel', { requestTimeout: 10000 })
+        .wait('@treeRender', { requestTimeout: 10000 })
         .get(`${rootSelector} a`)
         .first()
         .click()
@@ -50,7 +52,9 @@ Cypress.Commands.add('moveClass', (formSelector, moveSelector, moveConfirmSelect
         .get(`li[title="${name}"] a`)
         .first()
         .click()
-        .intercept('GET', '**tao/RestResource/getAll**').as('classToMove')
+        .intercept('GET', '**/tao/RestResource/getAll**').as('classToMove')
+        .intercept('POST', `**/taoItems/Items/editClassLabel`).as('editClassLabel')
+        .wait('@editClassLabel', { requestTimeout: 10000 })
         .get(moveSelector)
         .click()
         .wait('@classToMove', { requestTimeout: 10000 })
@@ -63,36 +67,48 @@ Cypress.Commands.add('moveClass', (formSelector, moveSelector, moveConfirmSelect
         .get(`li[title="${name}"] a`).should('not.exist');
 });
 
-Cypress.Commands.add('moveClassFromRoot', (rootSelector, formSelector, moveSelector, moveConfirmSelector, name, nameWhereMove) => {
+Cypress.Commands.add('moveClassFromRoot', (rootSelector, formSelector, moveSelector, moveConfirmSelector, deleteSelector, confirmSelector, name, nameWhereMove) => {
     cy.log('COMMAND: moveClassFromRoot', name)
         .addClassToRoot(rootSelector, formSelector, name)
         .addClassToRoot(rootSelector, formSelector, nameWhereMove)
         .intercept('GET', `**/taoItems/Items/getOntologyData**`).as('treeRender')
         .intercept('POST', `**/taoItems/Items/editClassLabel`).as('editClassLabel')
-        .get(`${rootSelector} a`)
-        .first()
         .wait('@treeRender', { requestTimeout: 10000 })
         .wait('@editClassLabel', { requestTimeout: 10000 })
+        .get(`${rootSelector} a`)
+        .first()
         .click()
         .get(`${rootSelector} li[title="${name}"] a`)
         .moveClass(formSelector, moveSelector, moveConfirmSelector, name, nameWhereMove)
-        .deleteClassFromRoot(
+        .deleteClass(
             rootSelector,
             formSelector,
-            moveSelector,
-            moveConfirmSelector,
-            nameWhereMove
+            deleteSelector,
+            confirmSelector,
+            nameWhereMove,
+            true
         );
 });
 
-Cypress.Commands.add('deleteClass', (formSelector, deleteSelector, confirmSelector, name) => {
+Cypress.Commands.add('deleteClass', (rootSelector, formSelector, deleteSelector, confirmSelector, name, isMove = false) => {
     cy.log('COMMAND: deleteClass', name)
+        .get(`${rootSelector} a`)
         .contains('a', name).click()
         .get(formSelector)
         .should('exist')
-        .get(deleteSelector)
+
+    if(!isMove) {
+        cy.intercept('GET', `**/tao/ResourceRelations/**`).as('resourceRelations')
+    }
+
+    cy.get(deleteSelector)
         .click()
-        .get('.modal-body label[for=confirm]')
+
+    if(!isMove) {
+        cy.wait('@resourceRelations', { requestTimeout: 10000 })
+    }
+
+    cy.get('.modal-body label[for=confirm]')
         .click()
         .get(confirmSelector)
         .click();
@@ -100,11 +116,13 @@ Cypress.Commands.add('deleteClass', (formSelector, deleteSelector, confirmSelect
 
 Cypress.Commands.add('deleteClassFromRoot', (rootSelector, formSelector, deleteSelector, confirmSelector, name) => {
     cy.log('COMMAND: deleteClassFromRoot', name)
+        .intercept('GET', `**/taoItems/Items/getOntologyData**`).as('treeRender')
+        .wait('@treeRender', { requestTimeout: 20000 })
         .get(`${rootSelector} a`)
         .first()
         .click()
         .get(`li[title="${name}"] a`)
-        .deleteClass(formSelector, deleteSelector, confirmSelector, name)
+        .deleteClass(rootSelector, formSelector, deleteSelector, confirmSelector, name)
 });
 
 Cypress.Commands.add('deleteEmptyClassFromRoot', (rootSelector, formSelector, deleteSelector, confirmSelector, name) => {
@@ -128,15 +146,18 @@ Cypress.Commands.add('selectNode', (rootSelector, formSelector, name) => {
     cy.get(formSelector).should('exist');
 });
 
-Cypress.Commands.add('deleteNode', (deleteSelector, name) => {
-    cy.log('COMMAND: deleteNode', name);
-
-    cy.contains('a', name).click();
-
-    cy.get(deleteSelector).click();
-    cy.get('[data-control="ok"]').click();
-
-    cy.contains('a', name).should('not.exist');
+Cypress.Commands.add('deleteNode', (rootSelector, deleteSelector, name) => {
+    cy.log('COMMAND: deleteNode', name)
+        .intercept('GET', `**/taoItems/Items/getOntologyData**`).as('treeRender')
+        .intercept('POST', `**/taoItems/Items/editItem`).as('editItem')
+        .wait('@treeRender', { requestTimeout: 10000 })
+        .wait('@editItem', { requestTimeout: 10000 })
+        .get(`${rootSelector} a`)
+        .contains('a', name).click()
+        .get(deleteSelector).click()
+        .get('[data-control="ok"]').click()
+        .get(`${rootSelector} a`)
+        .contains('a', name).should('not.exist');
 });
 
 Cypress.Commands.add('renameSelected', (formSelector, newName) => {
@@ -147,8 +168,6 @@ Cypress.Commands.add('renameSelected', (formSelector, newName) => {
         .type(newName)
         .get('button[id="Save"]')
         .click()
-        .intercept('POST', `**/taoItems/Items/editClassLabel`).as('editClassLabel')
-        .wait('@editClassLabel', { requestTimeout: 10000 })
         .get(formSelector).should('exist')
-        .get(`${formSelector} input[class="global"][type="text"]`).should('have.value', newName);
+        .get(`${ formSelector } input[name*=label]`).should('have.value', newName);
 });
