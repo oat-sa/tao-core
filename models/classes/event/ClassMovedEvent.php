247571b9011c454f83abfddbc60708b7d97031ec
Mikhail Kamarouski <mikhail.kamarouski@1pt.com>
2021-08-12 11:59:24 +0300

    chore: new event related to the class movement

---
+++ models/classes/event/ClassMovedEvent.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2021 (original work) Open Assessment Technologies SA
+ *
+ */
+
+namespace oat\tao\model\event;
+
+use core_kernel_classes_Resource;
+use oat\oatbox\event\Event;
+
+class ClassMovedEvent implements Event
+{
+    protected $class;
+
+    public function __construct(core_kernel_classes_Resource $class)
+    {
+        $this->class = $class;
+    }
+
+    public function getName()
+    {
+        return get_class($this);
+    }
+
+    public function getClass(): core_kernel_classes_Resource
+    {
+        return $this->class;
+    }
+}
