51828e254f926409b787936f87cd1dd3dbffeff6
bartlomiej <bartlomiej@taotesting.com>
2021-08-18 16:08:21 +0200

    Tao platform version bump 2021.09

--- manifest.php
+++ manifest.php
@@ -233,9 +233,9 @@ return [
     ],
     'constants' => [
         #TAO version number
-        'TAO_VERSION' => '2021.08.1 LTS',
+        'TAO_VERSION' => '2021.09',
         #TAO version label
-        'TAO_VERSION_NAME' => '2021.08.1 LTS',
+        'TAO_VERSION_NAME' => '2021.09',
         #the name to display
         'PRODUCT_NAME' => 'TAO',
         #TAO release status, use to add specific footer to TAO, available alpha, beta, demo, stable
