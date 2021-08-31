a9815685b17936724fbd58df934d9258639f4e53
Ionut Mihai Sandu <ionut-mihai@taotesting.com>
2021-08-31 14:24:17 +0200

    feat: add namespace helper and modify the validation on edit label

---
+++ test/unit/helpers/NamespaceHelperTest.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2017 (original work) Open Assessment Technologies SA
+ *
+ */
+
+namespace oat\tao\test\unit\helpers;
+
+use oat\generis\test\Test;
+use oat\tao\helpers\NamespaceHelper;
+
+/**
+ * Test the class tao_helpers_Display
+ *
+ * @author Bertrand Chevrier <bertrand@taotesting.com>
+ */
+class NamespaceHelperTest extends TestCase
+{
+    /** @var NamespaceHelper */
+    private $subject;
+
+    public function getNameSpacesTest()
+    {
+        $this->subject = new NamespaceHelper();
+        $expected = [];
+        $this->assertEqual($expected, $this->subject->getNameSpaces());
+    }
+
+    public function addNameSpacesTest()
+    {
+        $this->subject = new NamespaceHelper();
+        $this->subject->addNameSpaces(['testNameSpace']);
+        $expected = ['testNameSpace'];
+
+        $this->assertEqual($expected, $this->subject->getNameSpaces());
+        $this->subject->addNameSpaces(['testNameSpace2']);
+        $expected = ['testNameSpace', 'testNameSpace2'];
+        $this->assertEqual($expected, $this->subject->getNameSpaces());
+    }
+}
