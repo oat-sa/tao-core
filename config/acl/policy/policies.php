1d680e0a7409437fac28b86b2b8c2ed0d95d780a
Gabriel Felipe Soares <gabriel.felipe.soares@taotesting.com>
2021-09-03 08:52:35 +0200

    chore: added policy file

---
+++ config/acl/policy/policies.php
@@ -0,0 +1,31 @@
+<?php
+
+use oat\tao\model\accessControl\ActionAccessControl;
+use oat\tao\model\accessControl\func\AccessRule;
+use oat\tao\model\user\TaoRoles;
+
+return [
+    //Policy 1
+    [
+        'name' => 'MainAccessPolicy',
+        'version' => 1,
+        'permissions' => [
+            tao_actions_Languages::class . '::changeLanguage' => [
+                TaoRoles::ANONYMOUS => ActionAccessControl::DENY,
+                TaoRoles::BACK_OFFICE => ActionAccessControl::READ,
+            ]
+        ],
+        'routing' => [
+            [
+                AccessRule::GRANT,
+                TaoRoles::ANONYMOUS,
+                [
+                    'ext' => 'tao',
+                    'mod' => 'Main',
+                    'act' => 'entry'
+                ]
+            ]
+        ]
+    ],
+    //Policy N... []
+];
