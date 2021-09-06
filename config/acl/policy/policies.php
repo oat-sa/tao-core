0726a465e0c9925e47138d68cb675abb0b46de01
Gabriel Felipe Soares <gabriel.felipe.soares@taotesting.com>
2021-09-06 17:28:59 +0200

    chore: apply policies with checksum

--- config/acl/policy/policies.php
+++ config/acl/policy/policies.php
@@ -6,23 +6,47 @@ use oat\tao\model\user\TaoRoles;
 return [
     //Policy 1
     [
-        'name' => 'MainAccessPolicy',
-        'version' => 1,
-        'actionPermissions' => [
-            tao_actions_PocController::class => [
-                'someActionOrFeature' => [
-                    TaoRoles::ANONYMOUS => ActionAccessControl::DENY,
-                    TaoRoles::BACK_OFFICE => ActionAccessControl::READ,
+        'id' => 'mainAccessPolicy',
+        'permissions' => [
+            'action' => [
+                tao_actions_PocController::class => [
+                    'someActionOrFeature' => [
+                        TaoRoles::BACK_OFFICE => ActionAccessControl::READ,
+                        TaoRoles::ANONYMOUS => ActionAccessControl::DENY,
+                    ],
                 ],
             ],
-        ],
-        'routePermissions' => [
-            TaoRoles::BACK_OFFICE => [
-                [
-                    'ext' => 'tao',
-                    'mod' => 'PocController',
-                    'act' => 'index',
+            'route' => [
+                TaoRoles::BACK_OFFICE => [
+                    [
+                        'ext' => 'tao',
+                        'mod' => 'PocController',
+                        'act' => 'index',
+                    ],
+                ]
+            ]
+        ]
+    ],
+    //Policy 2
+    [
+        'id' => 'otherAccessPolicy',
+        'permissions' => [
+            'action' => [
+                tao_actions_PocController::class => [
+                    'someActionOrFeature' => [
+                        TaoRoles::BACK_OFFICE => ActionAccessControl::DENY,
+                        TaoRoles::ANONYMOUS => ActionAccessControl::GRANT,
+                    ],
                 ],
+            ],
+            'route' => [
+                TaoRoles::GLOBAL_MANAGER => [
+                    [
+                        'ext' => 'tao',
+                        'mod' => 'PocController',
+                        'act' => 'index',
+                    ],
+                ]
             ]
         ]
     ],
--- models/classes/accessControl/PolicyApplier.php
+++ models/classes/accessControl/PolicyApplier.php
@@ -22,59 +22,121 @@ declare(strict_types=1);
 
 namespace oat\tao\model\accessControl;
 
+use Exception;
 use oat\oatbox\reporting\Report;
 use oat\oatbox\service\ConfigurableService;
 use oat\tao\scripts\tools\accessControl\SetRolesAccess;
 
 class PolicyApplier extends ConfigurableService
 {
+    public const SERVICE_ID = 'tao/PolicyApplier';
+    public const OPTIONS_POLICIES = 'policies';
+
     public function applyPolicies(array $policyPaths): Report
     {
+        /**
+         * 1) Single place to store all permissions
+         * 2) No need to apply on update/install separately
+         *    checksum
+         *    version
+         *    ....
+         * 3) Needs to be fast
+         *
+         * Formats? php, json,
+         */
+
         $report = Report::createSuccess('Applying polices...');
+
+        $configPolicies = $this->getPolicies(); //@TODO Probably we should store it on other place than filesystem (DB?)
+        $appliedPolicies = [];
+
+        $newPolicies = $this->getNewPolicies($policyPaths);
+
+        /**
+         * Get policies that were removed
+         */
+        $configPoliciesIds = array_keys($configPolicies);
+        $newPoliciesIds = array_keys($newPolicies);
+        $removedPolicies = array_diff($configPoliciesIds, $newPoliciesIds);
+
+        foreach ($removedPolicies as $removedPolicyId) {
+            /**
+             * FIXME @TODO Remove delete permissions
+             */
+            $configPolicies[$removedPolicyId];
+        }
+
+        foreach ($newPolicies as $policy) {
+            $policyId = $policy['id'];
+            $checksum = crc32(json_encode($policy));
+
+            if ($this->isPolicyUpdated($configPolicies, $policyId, $checksum)) {
+                /**
+                 * FIXME @TODO Apply the difference only
+                 */
+                $this->applyPermissions(
+                    $policy['permissions']['action'],
+                    $policy['permissions']['route']
+                );
+            }
+
+            $appliedPolicies[$policyId] = [
+                'checksum' => $checksum,
+                'currentState' => $policy,
+            ];
+
+            $report->add(
+                Report::createSuccess(
+                    'Added policies: ' . var_export($policy['id'], true)
+                )
+            );
+        }
+
+        $this->setOption(self::OPTIONS_POLICIES, $appliedPolicies);
+
+        $this->getServiceManager()->register(self::SERVICE_ID, $this);
+
+        return $report;
+    }
+
+    private function getNewPolicies(array $policyPaths): array
+    {
+        $policies = [];
 
         foreach ($policyPaths as $policyPath) {
             foreach (glob($policyPath . '/*.php') as $policy) {
                 $policyFile = realpath($policy);
 
-                $report->add(
-                    Report::createInfo(
-                        sprintf(
-                            'Processing policy file %s',
-                            $policyFile
-                        )
-                    )
-                );
-
                 if (!is_readable($policyFile)) {
-                    $report->add(
-                        Report::createError(
-                            sprintf(
-                                'Policy file %s not found',
-                                $policyFile
-                            )
+                    throw new Exception(
+                        sprintf(
+                            'Policy file "%s" is not readable',
+                            $policyFile
                         )
                     );
                 }
 
-                /** @var array $policies */
-                $policies = require $policyFile;
+                /** @var array $policiesContent */
+                $policiesContent = require $policyFile;
 
-                foreach ($policies as $policy) {
-                    $this->applyPermissions(
-                        $policy['actionPermissions'],
-                        $policy['routePermissions']
-                    );
-
-                    $report->add(
-                        Report::createSuccess(
-                            'Added policies: ' . var_export($policy['name'], true)
-                        )
-                    );
+                foreach ($policiesContent as $policy) {
+                    $policies[$policy['id']] = $policy;
                 }
             }
         }
 
-        return $report;
+        return $policies;
+    }
+
+    private function isPolicyUpdated(array $existingPolicies, string $policyId, int $checksum): bool
+    {
+        $existingPolicy = $existingPolicies[$policyId] ?? null;
+
+        if ($existingPolicy) {
+            return $checksum === $existingPolicy['checksum'];
+        }
+
+        return true;
     }
 
     private function applyPermissions(array $permissions, array $rules, $isRevoke = false): void
@@ -87,14 +149,17 @@ class PolicyApplier extends ConfigurableService
             ],
         ];
 
-        //var_export($options); exit();
-
         if ($isRevoke) {
             array_unshift($options, '--' . SetRolesAccess::OPTION_REVOKE);
         }
 
         $setRolesAccess = $this->getSetRolesAccess();
         $setRolesAccess($options);
+    }
+
+    private function getPolicies(): array
+    {
+        return $this->getOption(self::OPTIONS_POLICIES, []);
     }
 
     private function getSetRolesAccess(): SetRolesAccess
