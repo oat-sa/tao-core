2c6aa5937d3ef0326ab204151be66079c3b46f98
Gabriel Felipe Soares <gabrielfs7@gmail.com>
2021-09-23 13:08:09 +0200

    feat: [GREP] - Usage - Filter available values on depended properties (#3115)
    * feat: new parameter for search
    
    * feat: adding method to locate related items
    
    * feat: filter based on property values
    
    * chore: rename method
    
    * chore: validate parameter
    
    * feat: load available parameters by parent
    
    * feat: add intial ajax call to get available secondary properties options
    
    * feat: add new param in Abstract search element request data
    
    * feat: separate filtering values from single/multiple parent property
    
    * feat: refactor ES6
    
    * chore: add unit tests
    
    * chore: add unit tests
    
    * feat: use injection to handle container dependency
    
    * feat: remove unnecessary comment
    
    * chore: add unit test
    
    Co-authored-by: Gabriel Felipe Soares <gabriel.felipe.soares@taotesting.com>
    Co-authored-by: Cristian Lagos <17564918+clagosarias@users.noreply.github.com>

--- models/classes/Lists/DataAccess/Repository/DependencyRepository.php
+++ models/classes/Lists/DataAccess/Repository/DependencyRepository.php
@@ -23,8 +23,10 @@ declare(strict_types=1);
 namespace oat\tao\model\Lists\DataAccess\Repository;
 
 use common_persistence_SqlPersistence;
+use Doctrine\DBAL\Connection;
 use Doctrine\DBAL\Query\QueryBuilder;
 use Doctrine\DBAL\FetchMode;
+use InvalidArgumentException;
 use oat\generis\persistence\PersistenceManager;
 use oat\oatbox\service\ConfigurableService;
 use oat\tao\model\Lists\Business\Contract\DependencyRepositoryInterface;
@@ -33,6 +35,31 @@ use oat\tao\model\Lists\Business\Domain\DependencyCollection;
 
 class DependencyRepository extends ConfigurableService implements DependencyRepositoryInterface
 {
+    public function findChildListIds(array $options): array
+    {
+        if (empty($options['parentListUris']) || empty($options['parentListValues'])) {
+            throw new InvalidArgumentException('Parameters (parentListUris, parentListValues) are required');
+        }
+
+        $query = $this->getQueryBuilder();
+        $expressionBuilder = $query->expr();
+
+        return $query->select('list_item_id')
+            ->from(RdsValueCollectionRepository::TABLE_LIST_ITEMS_DEPENDENCIES, 'dependencies')
+            ->innerJoin(
+                'dependencies',
+                RdsValueCollectionRepository::TABLE_LIST_ITEMS,
+                'items',
+                $expressionBuilder->eq('dependencies.value', 'items.uri')
+            )
+            ->andWhere($expressionBuilder->in('items.list_uri', ':parent_list_uri'))
+            ->andWhere($expressionBuilder->in('dependencies.value', ':parent_list_value'))
+            ->setParameter('parent_list_uri', $options['parentListUris'], Connection::PARAM_STR_ARRAY)
+            ->setParameter('parent_list_value', $options['parentListValues'], Connection::PARAM_STR_ARRAY)
+            ->execute()
+            ->fetchAll(FetchMode::COLUMN);
+    }
+
     public function findAll(array $options): DependencyCollection
     {
         $remoteListUri = $options['listUri'];
