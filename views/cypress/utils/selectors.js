cc8462cd1c263d2d782bb6c4886ff342fa988206
Cristian Lagos <17564918+clagosarias@users.noreply.github.com>
2021-10-08 09:30:48 +0200

    feat: update selectors

--- views/cypress/utils/selectors.js
+++ views/cypress/utils/selectors.js
@@ -1,9 +1,18 @@
 export default {
     addUserForm: 'form[action="/tao/Users/add"]',
 
+    classOptions: '[action="/taoItems/Items/editItemClass"]',
+
     deleteConfirm: '[data-control="ok"]',
+
+    editClass: '#item-class-schema',
+    editClassUrl: 'taoItems/Items/editItemClass',
+
+    itemClassForm: 'form[action="/taoItems/Items/editClassLabel"]',
 
     labelSelector: '[data-testid=Label]',
 
     manageUserTable: '#user-list',
+
+    root: '[data-uri="http://www.tao.lu/Ontologies/TAOItem.rdf#Item"]',
 };
