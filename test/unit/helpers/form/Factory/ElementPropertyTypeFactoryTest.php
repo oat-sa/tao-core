06f3d6780b199c607fbdeb493c7b35c5b3cdcda0
Gabriel Felipe Soares <gabriel.felipe.soares@taotesting.com>
2021-10-20 08:42:46 +0200

    chore: unit tests

--- test/unit/helpers/form/Factory/ElementPropertyTypeFactoryTest.php
+++ test/unit/helpers/form/Factory/ElementPropertyTypeFactoryTest.php
@@ -22,11 +22,16 @@ declare(strict_types=1);
 
 namespace oat\tao\helpers\test\unit\helpers\form\Factory;
 
+use core_kernel_classes_Property;
 use oat\generis\test\TestCase;
+use oat\tao\helpers\form\Factory\ElementFactoryContext;
 use oat\tao\helpers\form\Factory\ElementPropertyTypeFactory;
+use oat\tao\model\Context\ContextInterface;
 use oat\tao\model\featureFlag\FeatureFlagCheckerInterface;
 use oat\tao\model\Lists\Business\Specification\SecondaryPropertySpecification;
 use oat\tao\model\Specification\PropertySpecificationInterface;
+use PHPUnit\Framework\MockObject\MockObject;
+use tao_helpers_form_elements_xhtml_Combobox;
 
 class ElementPropertyTypeFactoryTest extends TestCase
 {
@@ -42,21 +47,92 @@ class ElementPropertyTypeFactoryTest extends TestCase
     /** @var FeatureFlagCheckerInterface */
     private $featureFlagChecker;
 
+    /** @var MockObject|tao_helpers_form_elements_xhtml_Combobox */
+    private $element;
+
+    /** @var array */
+    private $propertyMap;
+
     public function setUp(): void
     {
         $this->primaryPropertySpecification = $this->createMock(PropertySpecificationInterface::class);
         $this->secondaryPropertySpecification = $this->createMock(SecondaryPropertySpecification::class);
         $this->featureFlagChecker = $this->createMock(FeatureFlagCheckerInterface::class);
+        $this->element = $this->getMockBuilder(tao_helpers_form_elements_xhtml_Combobox::class)
+            ->setMethodsExcept(
+                [
+                    'setEmptyOption',
+                    'setOptions',
+                    'getOptions',
+                    'setValue',
+                    'getValue',
+                    'addAttribute',
+                    'getAttributes',
+                ]
+            )->getMock();
+        $this->propertyMap = [
+            'longlist' => [
+                'title' => 'Combobox Title',
+                'widget' => tao_helpers_form_elements_xhtml_Combobox::WIDGET_ID,
+            ],
+        ];
 
         $this->sut = new ElementPropertyTypeFactory(
             $this->primaryPropertySpecification,
             $this->secondaryPropertySpecification,
             $this->featureFlagChecker
         );
+        $this->sut->withElement($this->element);
+        $this->sut->withPropertyMap($this->propertyMap);
     }
 
     public function testCreate(): void
     {
-        $this->markTestIncomplete('TODO');
+        $property = $this->createMock(core_kernel_classes_Property::class);
+        $context = $this->createMock(ContextInterface::class);
+        $context->method('getParameter')
+            ->willReturnCallback(
+                function ($param) use ($property) {
+                    if ($param === ElementFactoryContext::PARAM_INDEX) {
+                        return 1;
+                    }
+
+                    if ($param === ElementFactoryContext::PARAM_PROPERTY) {
+                        return $property;
+                    }
+
+                    if ($param === ElementFactoryContext::PARAM_DATA) {
+                        return [
+                            '1_type' => 'longlist',
+                        ];
+                    }
+                }
+            );
+
+        $this->featureFlagChecker
+            ->method('isEnabled')
+            ->willReturn(true);
+
+        $this->primaryPropertySpecification
+            ->method('isSatisfiedBy')
+            ->with($property)
+            ->willReturn(true);
+
+        $this->secondaryPropertySpecification
+            ->method('isSatisfiedBy')
+            ->willReturn(true);
+
+        $element = $this->sut->create($context);
+
+        $this->assertSame(
+            [
+                'longlist' => 'Combobox Title'
+            ],
+            $element->getOptions()
+        );
+        $this->assertSame(
+            tao_helpers_form_elements_xhtml_Combobox::WIDGET_ID,
+            $element->getAttributes()[ElementPropertyTypeFactory::PROPERTY_TYPE_ATTRIBUTE] ?? null
+        );
     }
 }
