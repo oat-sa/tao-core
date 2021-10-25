142387c4e2a7dc9c9b02503d8f255bf1d4ded701
Silvia_Lozano <sylvilozano@gmail.com>
2021-10-25 12:13:55 +0200

    fix: correct version mismatch in November release

--- manifest.php
+++ manifest.php
@@ -236,9 +236,9 @@ return [
     ],
     'constants' => [
         #TAO version number
-        'TAO_VERSION' => '2021.10',
+        'TAO_VERSION' => '2021.11',
         #TAO version label
-        'TAO_VERSION_NAME' => '2021.10',
+        'TAO_VERSION_NAME' => '2021.11',
         #the name to display
         'PRODUCT_NAME' => 'TAO',
         #TAO release status, use to add specific footer to TAO, available alpha, beta, demo, stable
