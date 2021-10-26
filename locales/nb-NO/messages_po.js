639dd49a9e0ac80703721e42557451169da9b6c0
Adrian Lopez <adrian.lopezma@gmail.com>
2021-10-26 12:03:01 +0200

    feat: Implement nb-NO translations

--- locales/nb-NO/messages_po.js
+++ locales/nb-NO/messages_po.js
@@ -1,1 +1,1 @@
-[]
+{"Delete":"Slett","Edit Tree":"Rediger treet","Export":"Eksport","Import":"Import","Lists":"Lister","Manage test takers":"Manage test-takers","Properties":"Egenskaper","Record and manage test takers.":"Record and manage test-takers.","Remote Lists":"Eksterne Lister","Settings":"Innstillinger","Test takers library":"Test-takers library","Trees":"Tr\u00e6r","Users":"Brukere","View Tree":"Se Tre"}
