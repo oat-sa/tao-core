b7304605a64693f64d4a1983711c186f50ff3050
Adrian Lopez Martinez <adrian.lopezma@gmail.com>
2021-11-08 08:02:35 +0100

    fix: fix tao version

--- manifest.php
+++ manifest.php
@@ -237,9 +237,9 @@ return [
     ],
     'constants' => [
         #TAO version number
-        'TAO_VERSION' => '2021.10',
+        'TAO_VERSION' => '2021.12',
         #TAO version label
-        'TAO_VERSION_NAME' => '2021.10',
+        'TAO_VERSION_NAME' => '2021.12',
         #the name to display
         'PRODUCT_NAME' => 'TAO',
         #TAO release status, use to add specific footer to TAO, available alpha, beta, demo, stable
