7242b7c8386f8f7c7ececcca732b207190913832
Cristian Lagos <17564918+clagosarias@users.noreply.github.com>
2021-11-19 17:38:41 +0100

    fix: add response.message return

--- views/js/layout/actions/common.js
+++ views/js/layout/actions/common.js
@@ -282,10 +282,14 @@ define([
                         } else {
                             if (response.success && !response.deleted) {
                                 $(actionContext.tree).trigger('refresh.taotree');
-                                reject(response.msg || __('Unable to delete the selected resource because you do not have the required rights to delete part of its content.'));
+                                reject(response.msg
+                                       || response.message
+                                       || __('Unable to delete the selected resource because you do not have the required rights to delete part of its content.'));
                             }
 
-                            reject(response.msg || __("Unable to delete the selected resource"));
+                            reject(response.msg
+                                   || response.message
+                                   || __("Unable to delete the selected resource"));
                         }
                     });
                 }, function cancel(){
