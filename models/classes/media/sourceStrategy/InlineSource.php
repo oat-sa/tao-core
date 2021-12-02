d2f614dbffa95cf402a5bfcd963bac0d73309e8f
Augustas Nedzinskas <augustas@taotesting.com>
2021-12-02 14:11:10 +0100

    fix: use Stream abstraction, add unit test

--- models/classes/media/sourceStrategy/InlineSource.php
+++ models/classes/media/sourceStrategy/InlineSource.php
@@ -26,10 +26,11 @@ use GuzzleHttp\Psr7\Stream;
 use League\MimeTypeDetection\GeneratedExtensionToMimeTypeMap;
 use oat\tao\model\media\MediaBrowser;
 use oat\tao\model\media\mediaSource\DirectorySearchQuery;
+use RuntimeException;
 
 class InlineSource implements MediaBrowser
 {
-    const FILENAME_MASK = 'inline-media-%s';
+    public const FILENAME_PREFIX = 'inline-media-';
 
     public function getDirectories(DirectorySearchQuery $params): array
     {
@@ -48,7 +49,9 @@ class InlineSource implements MediaBrowser
 
     public function download($link)
     {
-        return stream_get_contents($this->getFileStream($link));
+        $stream = $this->getFileStream($link);
+
+        return $stream->getContents();
     }
 
     public function getFileStream($link)
@@ -58,19 +61,40 @@ class InlineSource implements MediaBrowser
 
     public function getBaseName($link)
     {
-        return sprintf(self::FILENAME_MASK, md5($link)) . '.' . $this->detectFileExtension($link);
+        return self::FILENAME_PREFIX . md5($link) . '.' . $this->detectFileExtension($link);
     }
 
     private function detectFileExtension($link): ?string
     {
-        $fp = $this->createResource($link);
-        $meta = stream_get_meta_data($fp);
+        $stream = $this->getFileStream($link);
+        $mimetype = $stream->getMetadata('mediatype');
+        $extension = array_search($mimetype, GeneratedExtensionToMimeTypeMap::MIME_TYPES_FOR_EXTENSIONS);
+        if (!$extension) {
+            throw new RuntimeException(
+                sprintf('Could not determined inline asset file extension from the mime type: %s.', $mimetype)
+            );
+        }
 
-        return array_search($meta['mediatype'], GeneratedExtensionToMimeTypeMap::MIME_TYPES_FOR_EXTENSIONS);
+        return $extension;
     }
 
     private function createResource($link)
     {
-        return fopen($link, 'r');
+        $this->verifyDataUrlScheme($link);
+
+        $resource = @fopen($link, 'r');
+        if (!$resource) {
+            throw new RuntimeException('Invalid Data URL, could not create a resource.');
+        }
+
+        return $resource;
+    }
+
+    private function verifyDataUrlScheme($link): void
+    {
+        $urlScheme = parse_url($link, PHP_URL_SCHEME);
+        if ($urlScheme !== 'data') {
+            throw new RuntimeException('Provided URL should match the Data (RFC 2397) scheme.');
+        }
     }
 }
