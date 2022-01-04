74d4e522e33939bfdf9307776a7b10104b907137
Andre Machado <andre.machado@taotesting.com>
2022-01-04 14:04:58 +0100

    fix: tao community version and version name

--- manifest.php
+++ manifest.php
@@ -245,9 +245,9 @@ return [
     ],
     'constants' => [
         #TAO version number
-        'TAO_VERSION' => '2022.01',
+        'TAO_VERSION' => '2022.02 LTS',
         #TAO version label
-        'TAO_VERSION_NAME' => '2022.01',
+        'TAO_VERSION_NAME' => '2022.02 LTS',
         #the name to display
         'PRODUCT_NAME' => 'TAO',
         #TAO release status, use to add specific footer to TAO, available alpha, beta, demo, stable
