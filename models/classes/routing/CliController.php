18e963545799d21b0ba8e4c8b1c6fa4c69f4acbd
Camille Moyon <camille@taotesting.com>
2022-01-18 13:47:02 +0100

    fix: load translation when action is invoked

--- models/classes/routing/CliController.php
+++ models/classes/routing/CliController.php
@@ -15,54 +15,65 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
- * Copyright (c) 2016-2018 (original work) Open Assessment Technologies SA;
+ * Copyright (c) 2016-2021 (original work) Open Assessment Technologies SA;
  *
  */
 
+declare(strict_types=1);
+
 namespace oat\tao\model\routing;
 
+use common_exception_Error;
+use Exception;
+use oat\oatbox\action\Action;
 use oat\oatbox\action\ActionService;
 use oat\oatbox\action\ResolutionException;
-use common_report_Report as Report;
+use oat\oatbox\reporting\Report;
+use oat\oatbox\log\LoggerService;
+use oat\oatbox\reporting\ReportInterface;
+use oat\oatbox\user\UserLanguageServiceInterface;
 use oat\tao\model\cliArgument\ArgumentService;
 use oat\oatbox\service\ServiceManagerAwareTrait;
 use oat\oatbox\service\ServiceManagerAwareInterface;
+use tao_helpers_I18n;
 
-/**
- * Class CliController
- * @author Aleh Hutnikau, <hutnikau@1pt.com>
- * @package oat\tao\model\routing
- */
 class CliController implements ServiceManagerAwareInterface
 {
     use ServiceManagerAwareTrait;
 
     /**
-     * @param string $actionIdentifier fully qualified action class name
-     * @param array $params Params to be passed to action's __invoke method
-     * @return Report
+     * @throws common_exception_Error
      */
     public function runAction($actionIdentifier, array $params = [])
     {
         try {
-            $actionService = $this->getServiceLocator()->get(ActionService::SERVICE_ID);
-            $action = $actionService->resolve($actionIdentifier);
+            $action = $this->getResolver()->resolve($actionIdentifier);
         } catch (ResolutionException $e) {
-            return new Report(Report::TYPE_ERROR, $e->getMessage());
+            return new Report(ReportInterface::TYPE_ERROR, $e->getMessage());
         }
 
         $this->propagate($action);
+
+        $this->loadTranslations($action);
 
         $this->getServiceLocator()->get(ArgumentService::SERVICE_ID)->load($action, $params);
 
         try {
             $report = call_user_func($action, $params);
             if (empty($report)) {
-                $shortName = (new \ReflectionClass($action))->getName();
-                $report = new \common_report_Report(\common_report_Report::TYPE_INFO, "Action '${shortName}' ended gracefully with no report returned.");
+                $report = new Report(
+                    ReportInterface::TYPE_INFO,
+                    sprintf(
+                        'Action \'%s\' ended gracefully with no report returned.',
+                        $this->getResolver()->getActionName($action)
+                    )
+                );
             }
-        } catch (\Exception $e) {
-            $report = new Report(Report::TYPE_ERROR, __('An exception occured while running "%s"', $actionIdentifier));
+        } catch (Exception $e) {
+            $report = new Report(
+                Report::TYPE_ERROR,
+                __('An exception has occurred while running "%s".', $actionIdentifier)
+            );
 
             $message = $e->getMessage();
             $previous = $e->getPrevious();
@@ -77,5 +88,22 @@ class CliController implements ServiceManagerAwareInterface
         }
 
         return $report;
+    }
+
+    private function loadTranslations(Action $action): void
+    {
+        try {
+            tao_helpers_I18n::init(
+                $this->getResolver()->getActionExtension($action),
+                $this->getServiceLocator()->get(UserLanguageServiceInterface::SERVICE_ID)->getDefaultLanguage()
+            );
+        } catch (ResolutionException $e) {
+            $this->getServiceLocator()->get(LoggerService::SERVICE_ID)->notice('Unable to load translations.');
+        }
+    }
+
+    private function getResolver(): ActionService
+    {
+        return $this->getServiceLocator()->get(ActionService::SERVICE_ID);
     }
 }
