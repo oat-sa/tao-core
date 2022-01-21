efbc997fbcf62209d8d0871e46e3bb77fa1807df
Héctor Arroyo <hector.arroyo@taotesting.com>
2022-01-21 14:50:03 +0100

    chore: Ignore default sections in TAO_ADVANCED_SEARCH_DISABLED_SECTIONS

--- test/unit/AdvancedSearch/AdvancedSearchCheckerTest.php
+++ test/unit/AdvancedSearch/AdvancedSearchCheckerTest.php
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
- * Copyright (c) 2020-2021 (original work) Open Assessment Technologies SA;
+ * Copyright (c) 2020-2022 (original work) Open Assessment Technologies SA;
  */
 
 declare(strict_types=1);
@@ -28,14 +28,18 @@ use oat\tao\model\search\SearchProxy;
 use PHPUnit\Framework\MockObject\MockObject;
 use oat\tao\model\featureFlag\FeatureFlagChecker;
 use oat\tao\model\AdvancedSearch\AdvancedSearchChecker;
+use common_ext_ExtensionsManager;
 
 class AdvancedSearchCheckerTest extends TestCase
 {
+    /** @var common_ext_ExtensionsManager|MockObject */
+    private $extensionsManager;
+
     /** @var FeatureFlagChecker|MockObject */
     private $featureFlagChecker;
 
     /** @var AdvancedSearchChecker */
-    private $advancedSearchChecker;
+    private $sut;
 
     /** @var SearchProxy|MockObject */
     private $search;
@@ -44,10 +48,13 @@ class AdvancedSearchCheckerTest extends TestCase
     {
         $this->featureFlagChecker = $this->createMock(FeatureFlagChecker::class);
         $this->search = $this->createMock(SearchInterface::class);
-        $this->advancedSearchChecker = new AdvancedSearchChecker();
-        $this->advancedSearchChecker->setServiceLocator(
+        $this->extensionsManager = $this->createMock(common_ext_ExtensionsManager::class);
+
+        $this->sut = new AdvancedSearchChecker();
+        $this->sut->setServiceLocator(
             $this->getServiceLocatorMock(
                 [
+                    common_ext_ExtensionsManager::SERVICE_ID => $this->extensionsManager,
                     FeatureFlagChecker::class => $this->featureFlagChecker,
                     SearchProxy::SERVICE_ID => $this->search,
                 ]
@@ -69,7 +76,7 @@ class AdvancedSearchCheckerTest extends TestCase
             ->method('supportCustomIndex')
             ->willReturn($supportsCustomIndex);
 
-        $this->assertEquals($expected, $this->advancedSearchChecker->isEnabled());
+        $this->assertEquals($expected, $this->sut->isEnabled());
     }
 
     public function isEnabledDataProvider(): array
@@ -94,6 +101,86 @@ class AdvancedSearchCheckerTest extends TestCase
                 'advancedSearchDisabled' => true,
                 'supportsCustomIndex' => false,
                 'expected' => false,
+            ],
+        ];
+    }
+
+    /**
+     * @backupGlobals Used to preserve former $_ENV value between tests
+     * @dataProvider getDisabledSectionsDataProvider
+     */
+    public function testGetDisabledSections(
+        array $expected,
+        bool $taoBookletEnabled,
+        array $envVars
+    ): void {
+        $_ENV = array_merge($_ENV ?? [], $envVars);
+
+        $this->extensionsManager
+            ->method('isEnabled')
+            ->with('taoBooklet')
+            ->willReturn($taoBookletEnabled);
+
+        $disabledSections = $this->sut->getDisabledSections();
+        sort($disabledSections);
+
+        $this->assertEquals($expected, $disabledSections);
+    }
+
+    public function getDisabledSectionsDataProvider(): array
+    {
+        return [
+            'Default sections without taoBooklet installed' => [
+                'expected' => ['results'],
+                'taoBookletEnabled' => false,
+                'envVars' => [],
+            ],
+            'Default sections with taoBooklet enabled' => [
+                'expected' => ['results', 'taoBooklet_main'],
+                'taoBookletEnabled' => true,
+                'envVars' => [],
+            ],
+            'Sections configured without taoBooklet enabled' => [
+                'expected' => ['results', 'section3'],
+                'taoBookletEnabled' => false,
+                'envVars' => [
+                    'TAO_ADVANCED_SEARCH_DISABLED_SECTIONS' => 'section3'
+                ],
+            ],
+            'Sections configured with empty values and without taoBooklet enabled' => [
+                'expected' => ['results', 'section3'],
+                'taoBookletEnabled' => false,
+                'envVars' => [
+                    'TAO_ADVANCED_SEARCH_DISABLED_SECTIONS' => ',,section3,,,'
+                ],
+            ],
+            'Sections configured with taoBooklet installed' => [
+                'expected' => ['results', 'section3', 'section4', 'taoBooklet_main'],
+                'taoBookletEnabled' => true,
+                'envVars' => [
+                    'TAO_ADVANCED_SEARCH_DISABLED_SECTIONS' => 'section3, section4'
+                ],
+            ],
+            'Sections configured with empty values and taoBooklet installed' => [
+                'expected' => ['results', 'section3', 'section4', 'taoBooklet_main'],
+                'taoBookletEnabled' => true,
+                'envVars' => [
+                    'TAO_ADVANCED_SEARCH_DISABLED_SECTIONS' => ',,,section3, section4,,,'
+                ],
+            ],
+            'Duplicated sections configured without taoBooklet enabled' => [
+                'expected' => ['results', 'section3'],
+                'taoBookletEnabled' => false,
+                'envVars' => [
+                    'TAO_ADVANCED_SEARCH_DISABLED_SECTIONS' => 'section3,section3'
+                ],
+            ],
+            'Duplicated sections configured with taoBooklet installed' => [
+                'expected' => ['results', 'section3', 'taoBooklet_main'],
+                'taoBookletEnabled' => true,
+                'envVars' => [
+                    'TAO_ADVANCED_SEARCH_DISABLED_SECTIONS' => 'section3,section3'
+                ],
             ],
         ];
     }
