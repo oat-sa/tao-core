3159569cdd6d102a34371936d4f777e662c393f6
Nikita Pimenov <nikita.pimenov@taotesting.com>
2022-01-26 19:49:30 +0300

    test: added LanguageListClassSpecificationTest

---
+++ test/unit/models/classes/Language/Business/Specification/LanguageListClassSpecificationTest.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2022 (original work) Open Assessment Technologies SA;
+ */
+
+declare(strict_types=1);
+
+namespace oat\tao\test\unit\model\Language\Business\Specification;
+
+use core_kernel_classes_Class;
+use oat\generis\test\TestCase;
+use oat\tao\model\Language\Business\Specification\LanguageClassSpecification;
+use tao_models_classes_LanguageService;
+
+
+class LanguageListClassSpecificationTest extends TestCase
+{
+    private const LANGUAGES_CLASS_URI = tao_models_classes_LanguageService::CLASS_URI_LANGUAGES;
+
+    /** @var LanguageClassSpecification */
+    private $sut;
+
+    protected function setUp(): void
+    {
+        $this->sut = new LanguageClassSpecification();
+
+        $this->class = $this->createMock(core_kernel_classes_Class::class);
+    }
+
+    public function testIsSatisfiedByValid(): void
+    {
+        $this->class
+            ->method('getUri')
+            ->willReturn(self::LANGUAGES_CLASS_URI);
+
+        $this->assertTrue($this->sut->isSatisfiedBy($this->class));
+    }
+
+    public function testIsSatisfiedByWithInvalidParentClass(): void
+    {
+        $this->class
+            ->method('getUri')
+            ->willReturn('Other URI');
+
+        $this->assertFalse($this->sut->isSatisfiedBy($this->class));
+    }
+}
