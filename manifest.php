58c53f5f202b3fae4f378ff6ce0e749d80538494
Hanna Dzmitryieva <bziondik@gmail.com>
2022-02-21 13:40:39 +0300

    fix: set version to '2022.02'

--- manifest.php
+++ manifest.php
@@ -247,9 +247,9 @@ return [
     ],
     'constants' => [
         #TAO version number
-        'TAO_VERSION' => '2022.01',
+        'TAO_VERSION' => '2022.02',
         #TAO version label
-        'TAO_VERSION_NAME' => '2022.01',
+        'TAO_VERSION_NAME' => '2022.02',
         #the name to display
         'PRODUCT_NAME' => 'TAO',
         #TAO release status, use to add specific footer to TAO, available alpha, beta, demo, stable
