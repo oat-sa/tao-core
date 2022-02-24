e9c2263a8db7ddc6f0d5672f1e653698003f1c10
Héctor Arroyo <hector.arroyo@taotesting.com>
2022-02-24 11:55:31 +0100

    feat: Add log messages to AgnosticImportHandler

--- models/classes/import/service/AgnosticImportHandler.php
+++ models/classes/import/service/AgnosticImportHandler.php
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
- * Copyright (c) 2021 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
+ * Copyright (c) 2021-2022 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
  */
 
 declare(strict_types=1);
@@ -23,6 +23,8 @@ declare(strict_types=1);
 namespace oat\tao\model\import\service;
 
 use oat\oatbox\filesystem\File;
+use oat\oatbox\log\LoggerAwareTrait;
+use oat\oatbox\log\TaoLoggerAwareInterface;
 use oat\oatbox\reporting\Report;
 use oat\tao\model\import\Processor\ImportFileErrorHandlerInterface;
 use oat\tao\model\import\Processor\ImportFileProcessorInterface;
@@ -33,8 +35,10 @@ use tao_models_classes_import_CsvUploadForm;
 use tao_models_classes_import_ImportHandler;
 use Throwable;
 
-class AgnosticImportHandler implements tao_models_classes_import_ImportHandler, TaskParameterProviderInterface
+class AgnosticImportHandler implements tao_models_classes_import_ImportHandler, TaskParameterProviderInterface, TaoLoggerAwareInterface
 {
+    use LoggerAwareTrait;
+
     public const STATISTICAL_METADATA_SERVICE_ID = self::class . '::STATISTICAL_METADATA';
 
     /** @var UploadService */
@@ -136,6 +140,15 @@ class AgnosticImportHandler implements tao_models_classes_import_ImportHandler, 
     private function processFile(File $file): Report
     {
         if ($this->fileProcessor) {
+            $this->logDebug(
+                sprintf(
+                    "%s: Using %s to process %s",
+                    self::class,
+                    get_class($this->fileProcessor),
+                    $file->getBasename()
+                )
+            );
+
             return $this->fileProcessor->process($file);
         }
 
@@ -155,8 +168,26 @@ class AgnosticImportHandler implements tao_models_classes_import_ImportHandler, 
     private function handleException(Throwable $exception): Report
     {
         if ($this->errorHandler) {
+            $this->logInfo(
+                sprintf(
+                    "%s: Forwarding exception %s to error handler: %s",
+                    self::class,
+                    get_class($exception),
+                    $exception->getMessage()
+                )
+            );
+
             return $this->errorHandler->handle($exception);
         }
+
+        $this->logDebug(
+            sprintf(
+                "%s: Got exception %s: %s",
+                self::class,
+                get_class($exception),
+                $exception->getMessage()
+            )
+        );
 
         return Report::create(
             Report::TYPE_ERROR,
