08ebdc13a96c66045b8507506239a16265c40b38
Yuriy Synyaiev <yuriy.synyaiev@taotesting.com>
2022-03-30 22:15:54 +0200

    feat: add isVisibility feature helper

---
+++ views/js/helpers/features.js
@@ -0,0 +1,62 @@
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2022 Open Assessment Technologies SA;
+ */
+
+define(['module'], function (module) {
+    'use strict';
+    const config = module.config();
+    const featuresVisibilityList = config.visibility || [];
+    const featuresKeys = Object.keys(featuresVisibilityList);
+
+    /**
+     * Build regexp from lookupPath converting '*' to '\S*'
+     * @param {String} lookupPath raw string of path to lookup
+     * @returns {RegExp} regexp to lookup in features list
+     */
+    const buildRegexp = lookupPath => {
+        lookupPath = lookupPath.replaceAll('*', '\\S*');
+
+        try {
+            return RegExp(`^${lookupPath}$`);
+        } catch (e) {
+            console.warn(`Lookup feature ${lookupPath} was not properly checked`);
+
+            return RegExp('');
+        }
+    };
+
+    return {
+        /**
+         * Check is feature configured to be visible
+         * based on client_lib_config_registry.conf.php
+         * @param {String} featurePath path to feature supporting * ex('test/itemSession/*')
+         * @returns {Boolean} true if feature is visible
+         */
+        isVisible: featurePath => {
+            const regexp = buildRegexp(featurePath);
+            let targetKey;
+            let isPathExists = featuresKeys.some(featureKey => {
+                const isMatch = regexp.test(featureKey);
+
+                if (isMatch) targetKey = featureKey;
+                return isMatch;
+            });
+
+            return isPathExists && featuresVisibilityList[targetKey] === 'show';
+        }
+    };
+});
---
+++ views/js/test/helpers/features/test.js
@@ -0,0 +1,49 @@
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2022 (original work) Open Assessment Technologies SA ;
+ */
+
+define(['helpers/features'], function(features) {
+    'use strict';
+
+    QUnit.module('features');
+
+    const testData = [
+        {lookup: 'levelOne/featureVisible', expected: true},
+        {lookup: 'levelOne/featureHidden', expected: false},
+
+        {lookup: 'levelOne/levelTwo/featureVisible', expected: true},
+        {lookup: 'levelOne/levelTwo/featureHidden', expected: false},
+
+        {lookup: 'levelOne/*/featureVisible', expected: true},
+        {lookup: 'levelOne/*/featureHidden', expected: false},
+
+        {lookup: 'levelOne/*', expected: true},
+        {lookup: 'levelOne/levelTwo/LevelTree/*', expected: false},
+        {lookup: 'levelOne/levelTwo/*/featureHidden', expected: false}
+
+    ];
+
+    QUnit
+        .cases.init(testData)
+        .test('Feature is visible check', function(data, assert) {
+            assert.equal(
+                features.isVisible(data.lookup),
+                data.expected,
+                `Data lookup "${data.lookup}"`
+            );
+        });
+});
