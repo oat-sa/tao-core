0aaf36f693585a83908946573808429504ad5261
bartlomiej <bartlomiej@taotesting.com>
2022-09-09 14:50:55 +0200

    chore: tao-core release bump

--- manifest.php
+++ manifest.php
@@ -250,13 +250,13 @@ return [
     ],
     'constants' => [
         #TAO version number
-        'TAO_VERSION' => '2022.09',
+        'TAO_VERSION' => '2022.10',
         #TAO version label
-        'TAO_VERSION_NAME' => '2022.09',
+        'TAO_VERSION_NAME' => '2022.10',
         #the name to display
         'PRODUCT_NAME' => 'TAO',
         #TAO release status, use to add specific footer to TAO, available alpha, beta, demo, stable
-        'TAO_RELEASE_STATUS' => 'beta',
+        'TAO_RELEASE_STATUS' => 'stable',
         #TAO default character encoding (mainly used with multi-byte string functions).
         'TAO_DEFAULT_ENCODING' => 'UTF-8',
         # actions directory
