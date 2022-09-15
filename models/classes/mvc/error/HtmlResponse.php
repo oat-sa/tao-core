e6a8e7f62518a6d6232bec359c182d6811fad061
Héctor Arroyo <hector.arroyo@taotesting.com>
2022-09-15 17:26:55 +0200

    chore: Prevent warnings in PHP 8

--- models/classes/mvc/error/HtmlResponse.php
+++ models/classes/mvc/error/HtmlResponse.php
@@ -29,7 +29,6 @@ use oat\tao\helpers\Template;
  */
 class HtmlResponse extends ResponseAbstract
 {
-
     protected $contentType = 'text/html';
 
     public function send()
@@ -38,10 +37,12 @@ class HtmlResponse extends ResponseAbstract
             $message = $this->exception->getMessage();
             $trace = $this->exception->getTraceAsString();
         }
-        $referer = $_SERVER['HTTP_REFERER'];
+
+        $referer = $_SERVER['HTTP_REFERER'] ?? '';
         $returnUrl = (parse_url($referer, PHP_URL_HOST) === parse_url(ROOT_URL, PHP_URL_HOST))
             ? htmlentities($referer, ENT_QUOTES)
             : false;
+
         require $this->createTemplatePath();
     }
 
