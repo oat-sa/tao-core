6ea6338c830e06147ac26e8b8611d2e38366b01d
rgomeztao <raul.gomez.cabello@taotesting.com>
2023-01-19 15:15:10 +0100

    chore: fix strict selective sentence

--- models/classes/search/SearchProxy.php
+++ models/classes/search/SearchProxy.php
@@ -102,12 +102,14 @@ class SearchProxy extends ConfigurableService implements Search
      * @throws Exception
      */
     public function search(ServerRequestInterface $request): array
-    {
+    {;
         $query = $this->getQueryFactory()->create($request);
         $queryParams = $request->getQueryParams();
         $results = $this->executeSearch($query);
 
-        if (in_array($queryParams['params']['rootNode'], self::SAFE_NODES)) {
+        if (isset($queryParams['params']['rootNode'])
+            && in_array($queryParams['params']['rootNode'], self::SAFE_NODES)
+        ) {
             return $this->getResultSetResponseNormalizer()
                 ->normalizeSafeClass($query, $results, $queryParams['params']['structure']);
         }
