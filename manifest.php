5c7e8db9b5d3aa09c68d149cfd5a700c4483e8c9
Karol Stelmaczonek <karol.stelmaczonek@taotesting.com>
2023-03-14 09:53:43 +0100

    chore: change version to 2023.04

--- manifest.php
+++ manifest.php
@@ -346,9 +346,9 @@ return [
     ],
     'constants' => [
         #TAO version number
-        'TAO_VERSION' => '2023.03',
+        'TAO_VERSION' => '2023.04',
         #TAO version label
-        'TAO_VERSION_NAME' => '2023.03',
+        'TAO_VERSION_NAME' => '2023.04',
         #the name to display
         'PRODUCT_NAME' => 'TAO',
         #TAO release status, use to add specific footer to TAO, available alpha, beta, demo, stable
