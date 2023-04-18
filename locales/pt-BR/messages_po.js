4e8f9f3f5ccd1ceae63e410126719b5c2d480507
Alberto Sanchez Gonzalez <alberto.mu.es@gmail.com>
2023-04-18 16:17:35 +0200

    feat: add compile brazilian translation after create translation file

--- locales/pt-BR/messages_po.js
+++ locales/pt-BR/messages_po.js
@@ -1,1 +1,1 @@
-[]
+{"Manage Access Rights":"Gerenciar direitos de acesso","Manage test takers":"Manage test-takers","Record and manage test takers.":"Record and manage test-takers.","Test takers library":"Test-takers library","Users":"Usu\u00e1rios"}
