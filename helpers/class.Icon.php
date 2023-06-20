32f76eab7f98082409055f57ab4820cb1bb3e890
André Luiz Machado <andre.luiz.kbca@gmail.com>
2023-06-20 09:24:47 +0200

    fix: code style

--- helpers/class.Icon.php
+++ helpers/class.Icon.php
@@ -25,10 +25,10 @@ use oat\tao\model\iconBuilder\IconBuilderTrait;
  *
  * PLEASE, DO NOT EDIT THIS CLASS. IT GENERATED AUTOMATICALLY
  *
- * @access        public
- * @author        Dieter Raber, <dieter@taotesting.com>
- * @date          2023-06-16 10:17:15
- * @package       tao
+ * @access      public
+ * @author      Dieter Raber, <dieter@taotesting.com>
+ * @date        2023-06-16 10:17:15
+ * @package     tao
  */
 class tao_helpers_Icon
 {
