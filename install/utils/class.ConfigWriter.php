8f1dd3a4badc338453a98eb7547a1aa45fe5eb3d
Kirill-Hatalski <kirill.gatalsky@taotesting.com>
2023-07-31 14:31:48 +0300

    codestyle fix

--- install/utils/class.ConfigWriter.php
+++ install/utils/class.ConfigWriter.php
@@ -124,7 +124,8 @@ class tao_install_utils_ConfigWriter
                 } elseif (is_numeric($val)) {
                     $content = preg_replace('/(\'' . $name . '\')(.*?)$/ms', '$1, ' . $val . ');', $content);
                 } elseif ((method_exists($val, '__toPhpCode'))) {
-                    $content = preg_replace('/(\'' . $name . '\')(.*?)$/ms', '$1, ' . $val->__toPhpCode() . ');', $content);
+                    $content = preg_replace('/(\'' . $name . '\')(.*?)$/ms', '$1, '
+                        . $val->__toPhpCode() . ');', $content);
                 }
             }
             file_put_contents($this->file, $content);
