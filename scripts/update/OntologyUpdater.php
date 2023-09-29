135492301a30d47f6f33df69f1b8c89246ffd0b5
Vlad Byndych <vladyslav.byndych@taotesting.com>
2023-09-29 17:10:20 +0200

    feat: ontology import/export for neo4j.

--- scripts/update/OntologyUpdater.php
+++ scripts/update/OntologyUpdater.php
@@ -23,13 +23,11 @@
 namespace oat\tao\scripts\update;
 
 use AppendIterator;
-use oat\generis\model\kernel\persistence\file\FileModel;
-use oat\generis\model\data\ModelManager;
-use helpers_RdfDiff;
-use core_kernel_persistence_smoothsql_SmoothModel;
-use common_persistence_SqlPersistence;
 use common_ext_ExtensionsManager;
-use core_kernel_persistence_smoothsql_SmoothIterator;
+use helpers_RdfDiff;
+use oat\generis\model\data\Model;
+use oat\generis\model\data\ModelManager;
+use oat\generis\model\kernel\persistence\file\FileModel;
 use oat\tao\model\extension\ExtensionModel;
 
 class OntologyUpdater
@@ -37,20 +35,11 @@ class OntologyUpdater
     public static function syncModels()
     {
         $currentModel = ModelManager::getModel();
-        $modelIds = array_diff($currentModel->getReadableModels(), ['1']);
 
-        $persistence = common_persistence_SqlPersistence::getPersistence('default');
+        $existingTriples = self::getCurrentTriples($currentModel);
+        $nominalTriples = self::getNominalTriples();
 
-        $smoothIterator = new core_kernel_persistence_smoothsql_SmoothIterator($persistence, $modelIds);
-
-        $nominalModel = new AppendIterator();
-        foreach (common_ext_ExtensionsManager::singleton()->getInstalledExtensions() as $ext) {
-            $nominalModel->append(new ExtensionModel($ext));
-        }
-        $langModel = \tao_models_classes_LanguageService::singleton()->getLanguageDefinition();
-        $nominalModel->append($langModel);
-
-        $diff = helpers_RdfDiff::create($smoothIterator, $nominalModel);
+        $diff = helpers_RdfDiff::create($existingTriples, $nominalTriples);
         self::logDiff($diff);
 
         $diff->applyTo($currentModel);
@@ -81,5 +70,31 @@ class OntologyUpdater
 
         FileModel::toFile($path . DIRECTORY_SEPARATOR . 'add.rdf', $diff->getTriplesToAdd());
         FileModel::toFile($path . DIRECTORY_SEPARATOR . 'remove.rdf', $diff->getTriplesToRemove());
+    }
+
+    public static function getNominalTriples(): \Traversable
+    {
+        $nominalModel = new AppendIterator();
+        foreach (common_ext_ExtensionsManager::singleton()->getInstalledExtensions() as $ext) {
+            $nominalModel->append(new ExtensionModel($ext));
+        }
+        $langModel = \tao_models_classes_LanguageService::singleton()->getLanguageDefinition();
+        $nominalModel->append($langModel);
+        return $nominalModel;
+    }
+
+    public static function getCurrentTriples(Model $currentModel): \Traversable
+    {
+        return new \CallbackFilterIterator(
+            $currentModel->getRdfInterface()->getIterator(),
+            function (\core_kernel_classes_Triple $item) {
+                $isAutomaticIncludeRole = $item->subject === 'http://www.tao.lu/Ontologies/TAO.rdf#GlobalManagerRole'
+                    && $item->predicate === 'http://www.tao.lu/Ontologies/generis.rdf#includesRole';
+                $isGrantAccess = $item->predicate === 'http://www.tao.lu/Ontologies/taoFuncACL.rdf#GrantAccess';
+
+
+                return !$isGrantAccess && !$isAutomaticIncludeRole;
+            }
+        );
     }
 }
