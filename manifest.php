f94b25cd85e3a1a9059133b3e31121eccb1a637e
Janos Pribelszki <janos.pribelszki@taotesting.com>
2023-10-09 08:57:18 +0200

    chore: change version to 2023.11 LTS

--- manifest.php
+++ manifest.php
@@ -356,9 +356,9 @@ return [
     ],
     'constants' => [
         #TAO version number
-        'TAO_VERSION' => '2023.11',
+        'TAO_VERSION' => '2023.11 LTS',
         #TAO version label
-        'TAO_VERSION_NAME' => '2023.11',
+        'TAO_VERSION_NAME' => '2023.11 LTS',
         #the name to display
         'PRODUCT_NAME' => 'TAO',
         #TAO release status, use to add specific footer to TAO, available alpha, beta, demo, stable
