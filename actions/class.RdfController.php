6e188af6bb7ce0ce3056a6a0166b81eefc35ec85
Maria Cano <maria.cano@taotesting.com>
2024-06-13 10:07:40 +0200

    chore: refactor map label function

--- actions/class.RdfController.php
+++ actions/class.RdfController.php
@@ -55,7 +55,7 @@ use oat\tao\model\resources\Exception\ClassDeletionException;
 use oat\generis\model\resource\Contract\ResourceDeleterInterface;
 use oat\tao\model\metadata\exception\InconsistencyConfigException;
 use oat\tao\model\resources\Exception\PartialClassDeletionException;
-use tao_helpers_Display as DisplayHelper;
+use oat\tao\helpers\MapLabelNameService;
 
 /**
  * The TaoModule is an abstract controller,
@@ -468,8 +468,8 @@ abstract class tao_actions_RdfController extends tao_actions_CommonModule
             $this->setData('reload', true);
         }
 
-        $getLabel = DisplayHelper::mapClassesNames($class->getLabel());
-        $this->setData('formTitle', __('Edit class %s', \tao_helpers_Display::htmlize($getLabel)));
+        $getLabel = MapLabelNameService::mapLabelName($class->getLabel());
+        $this->setData('formTitle', __('Edit class %s', tao_helpers_Display::htmlize($getLabel)));
         $this->setData('myForm', $myForm->render());
         $this->setView('form.tpl', 'tao');
     }
