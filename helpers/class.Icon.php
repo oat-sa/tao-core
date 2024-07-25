31ee077d07680a7c0895bb781f39b699985dba63
viktar-dzmitryieu-tao <viktar.dzmitryieu@taotesting.com>
2024-07-25 15:58:26 +0200

    fix: tests failures

--- helpers/class.Icon.php
+++ helpers/class.Icon.php
@@ -25,10 +25,10 @@ use oat\tao\model\iconBuilder\IconBuilderTrait;
  *
  * PLEASE, DO NOT EDIT THIS CLASS. IT GENERATED AUTOMATICALLY
  *
- * @access		public
- * @author		Dieter Raber, <dieter@taotesting.com>
- * @date		2024-07-25 14:25:33
- * @package		tao
+ * @access	public
+ * @author	Dieter Raber, <dieter@taotesting.com>
+ * @date	2024-07-25 14:25:33
+ * @package	tao
  */
 class tao_helpers_Icon
 {
