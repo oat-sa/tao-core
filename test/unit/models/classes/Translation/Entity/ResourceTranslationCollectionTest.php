05944c8fa8441245090e8423de47f0d42f4bc0e9
Gabriel Felipe Soares <gabrielfs7@gmail.com>
2024-09-06 17:53:00 +0200

    chore: adding unit tests

---
+++ test/unit/models/classes/Translation/Entity/ResourceTranslationCollectionTest.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2024 (original work) Open Assessment Technologies SA;
+ *
+ * @author Gabriel Felipe Soares <gabriel.felipe.soares@taotesting.com>
+ */
+
+declare(strict_types=1);
+
+namespace oat\tao\test\unit\model\Translation\Entity;
+
+use oat\tao\model\Translation\Entity\ResourceTranslationCollection;
+use PHPUnit\Framework\TestCase;
+
+class ResourceTranslationCollectionTest extends TestCase
+{
+    /** @var ResourceTranslationCollection */
+    private $sut;
+
+    public function setUp(): void
+    {
+        $this->sut = new ResourceTranslationCollection(
+            'originResourceUri',
+            'uniqueId',
+        );
+    }
+
+    public function testGetters(): void
+    {
+        $this->assertSame(
+            [
+                'originResourceUri' => 'originResourceUri',
+                'uniqueId' => 'uniqueId',
+                'translations' => [],
+            ],
+            $this->sut->jsonSerialize()
+        );
+    }
+}
