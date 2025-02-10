a875ac47f52d7f0435e7709b03e258eb7e32c3d3
Bartłomiej Marszał <bartlomiejmarszal@gmail.com>
2025-02-10 11:21:07 +0100

    feat: change approach on getting services from container

--- models/classes/resources/relation/service/ResourceRelationServiceProxy.php
+++ models/classes/resources/relation/service/ResourceRelationServiceProxy.php
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
- * Copyright (c) 2020 (original work) Open Assessment Technologies SA;
+ * Copyright (c) 2020-2025 (original work) Open Assessment Technologies SA;
  */
 
 declare(strict_types=1);
@@ -24,7 +24,6 @@ namespace oat\tao\model\resources\relation\service;
 
 use common_Logger;
 use oat\oatbox\service\ConfigurableService;
-use oat\oatbox\service\ServiceNotFoundException;
 use oat\tao\model\resources\relation\FindAllQuery;
 use oat\tao\model\resources\relation\ResourceRelationCollection;
 
@@ -66,15 +65,12 @@ class ResourceRelationServiceProxy extends ConfigurableService implements Resour
                 continue;
             }
 
-            foreach ($services as $serviceId) {
-                // Instead of throwing error when service does not exist, we just skip it
-                if (!$this->serviceExist($serviceId, $type)) {
-                    continue;
-                }
+            $container = $this->getServiceManager()->getContainer();
 
+            foreach ($services as $serviceId) {
                 $relations = array_merge(
                     $relations,
-                    $this->getResourceRelationService($serviceId)
+                    $container->get($serviceId)
                         ->findRelations($query)
                         ->getIterator()
                         ->getArrayCopy()
@@ -83,35 +79,6 @@ class ResourceRelationServiceProxy extends ConfigurableService implements Resour
         }
 
         return new ResourceRelationCollection(...$relations);
-    }
-
-    private function getResourceRelationService(string $serviceId): ResourceRelationServiceInterface
-    {
-        try {
-            return $this->getServiceManager()->get($serviceId);
-        } catch (ServiceNotFoundException $exception) {
-            return $this->getServiceManager()->getContainer()->get($serviceId);
-        }
-    }
-
-    private function serviceExist(string $serviceId, string $type): bool
-    {
-        if (
-            $this->getServiceManager()->has($serviceId)
-            || $this->getServiceManager()->getContainer()->has($serviceId)
-        ) {
-            return true;
-        }
-
-        common_Logger::w(
-            sprintf(
-                'Service %s configured as relation service for %s does not exist',
-                $serviceId,
-                $type
-            )
-        );
-
-        return false;
     }
 
     private function getServices(string $type): array
