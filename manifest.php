b575067d9b58aef5d06d42cad951734a7b2d9122
Aleksej Tikhanovich <aleksej@taotesting.com>
2025-08-01 16:52:21 +0200

    fix: set 2025-09 version (#4280)

--- manifest.php
+++ manifest.php
@@ -367,9 +367,9 @@ return [
     ],
     'constants' => [
         #TAO version number
-        'TAO_VERSION' => '2025.08 LTS',
+        'TAO_VERSION' => '2025.09',
         #TAO version label
-        'TAO_VERSION_NAME' => '2025.08 LTS',
+        'TAO_VERSION_NAME' => '2025.09',
         #the name to display
         'PRODUCT_NAME' => 'TAO',
         #TAO release status, use to add specific footer to TAO, available alpha, beta, demo, stable
