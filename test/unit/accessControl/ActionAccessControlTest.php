5e57f21fd6a078f118270024eea6c62daf5ec521
Andrei Shapiro <shpr.andrew@gmail.com>
2025-12-14 18:15:52 +0300

    chore: trigger composer

--- test/unit/accessControl/ActionAccessControlTest.php
+++ test/unit/accessControl/ActionAccessControlTest.php
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
- * Copyright (c) 2021-2025 (original work) Open Assessment Technologies SA;
+ * Copyright (c) 2021 (original work) Open Assessment Technologies SA;
  */
 
 declare(strict_types=1);
