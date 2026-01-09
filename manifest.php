5bcfb269780e9c1829999cebbcf54b00902fd1ba
Aleksej Tikhanovich <aleksej@taotesting.com>
2026-01-09 12:08:53 +0100

    fix: update version

--- manifest.php
+++ manifest.php
@@ -369,9 +369,9 @@ return [
     ],
     'constants' => [
         #TAO version number
-        'TAO_VERSION' => '2026.01',
+        'TAO_VERSION' => '2026.02 LTS',
         #TAO version label
-        'TAO_VERSION_NAME' => '2026.01',
+        'TAO_VERSION_NAME' => '2026.02 LTS',
         #the name to display
         'PRODUCT_NAME' => 'TAO',
         #TAO release status, use to add specific footer to TAO, available alpha, beta, demo, stable
