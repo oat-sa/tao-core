1cc026d2f8655f8562325fe6bd9dc9a11afd5f7b
Aleksej Tikhanovich <aleksej@taotesting.com>
2026-02-20 11:05:53 +0100

    fix: some PR comments

--- models/classes/mvc/error/HttpStatusCode.php
+++ models/classes/mvc/error/HttpStatusCode.php
@@ -1,5 +1,25 @@
 <?php
 
+/*
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 31 Milk St # 960789 Boston, MA 02196 USA.
+ *
+ * Copyright (c) 2026 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
+ */
+
+declare(strict_types=1);
+
 namespace oat\tao\model\mvc\error;
 
 /**
