e4858fa0f472c9c493a2a48329c4721fa29abc63
Martin Nicholson <martin@taotesting.com>
2026-05-06 17:25:36 +0200

    refactor: tweak the temporary modern syntax locations

--- views/js/controller/backoffice.js
+++ views/js/controller/backoffice.js
@@ -90,7 +90,7 @@ define([
 
                 try {
                     ajaxResponse = $.parseJSON(request.responseText);
-                } catch {
+                } catch (err) {
                     errorMessage = `${request.status}: ${request.responseText}`;
                 }
 
