2dab5716fcaf4a7cdb572a478aa96272ae753ba0
Bartłomiej Marszał <bartlomiejmarszal@gmail.com>
2026-06-08 10:04:54 +0200

    chore: set tao version to 2026.07

--- manifest.php
+++ manifest.php
@@ -371,9 +371,9 @@ return [
     ],
     'constants' => [
         #TAO version number
-        'TAO_VERSION' => '2026.06',
+        'TAO_VERSION' => '2026.07',
         #TAO version label
-        'TAO_VERSION_NAME' => '2026.06',
+        'TAO_VERSION_NAME' => '2026.07',
         #the name to display
         'PRODUCT_NAME' => 'TAO',
         #TAO release status, use to add specific footer to TAO, available alpha, beta, demo, stable
