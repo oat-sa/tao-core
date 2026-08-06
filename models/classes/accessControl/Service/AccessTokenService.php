67e2eff789ad579546787e23db7a8e77709e963d
Sergei Mikhailov <contact@sergeimikhailov.com>
2026-08-06 20:58:29 +0200

    fix: access token cache key privacy

--- models/classes/accessControl/Service/AccessTokenService.php
+++ models/classes/accessControl/Service/AccessTokenService.php
@@ -55,7 +55,7 @@ class AccessTokenService
             throw new RuntimeException('OAuth2 credentials not found.', 404);
         }
         $userId = $this->getUserLogin();
-        $key = hash('sha256', "$authUri/$clientId/$userId/$role");
+        $key = hash('sha256', "{$this->getTenantId()}/$authUri/$clientId/$userId/$role");
         $value = $this->cache->get($key);
         if ($value) {
             return json_decode($value, true);
